0c98af29b5717a6c53278ccf4fed006a490a7dd4
Stefan Behnel <scoder@users.berlios.de>
2007-07-30 19:17:36 +0200

    removed zip_safe flag to let setuptools decide

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
     "Topic :: Software Development :: Compilers",
     "Topic :: Software Development :: Libraries :: Python Modules"
   ],
-  zip_safe = False,
 
   scripts = scripts,
   packages=[
