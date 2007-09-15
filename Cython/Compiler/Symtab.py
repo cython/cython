c47a68ab3694b02c5fc8a6dc0f9ad95fdcac5924
Stefan Behnel <scoder@users.berlios.de>
2007-09-15 12:39:56 +0200

    [has|get|set]attr() optimisation

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -513,7 +513,9 @@ class BuiltinScope(Scope):
     # TODO: perhapse these should all be declared in some universal .pxi file? 
     
     builtin_functions = {
-      "hasattr": ["PyObject_HasAttrString", c_bint_type, (py_object_type, c_char_ptr_type)],
+      "hasattr": ["PyObject_HasAttr", c_bint_type, (py_object_type, py_object_type)],
+      "getattr": ["PyObject_GetAttr", py_object_type, (py_object_type, py_object_type)],
+      "setattr": ["PyObject_SetAttr", c_bint_type, (py_object_type, py_object_type), -1],
       "cmp":     ["PyObject_Compare", c_int_type, (py_object_type, py_object_type), None, True],
       "repr":    ["PyObject_Repr", py_object_type, (py_object_type, ), 0],
 #      "str":     ["PyObject_Str", py_object_type, (py_object_type, ), 0],
