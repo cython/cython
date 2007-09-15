7b8adf65ec9f5e9076aac19fc2a26f829bd98240
Stefan Behnel <scoder@users.berlios.de>
2007-09-15 13:44:57 +0200

    removed getattr() optimisation as it disables the usage of a default value

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -514,7 +514,6 @@ class BuiltinScope(Scope):
     
     builtin_functions = {
       "hasattr": ["PyObject_HasAttr", c_bint_type, (py_object_type, py_object_type)],
-      "getattr": ["PyObject_GetAttr", py_object_type, (py_object_type, py_object_type)],
       "setattr": ["PyObject_SetAttr", c_int_type, (py_object_type, py_object_type, py_object_type), -1],
       "cmp":     ["PyObject_Compare", c_int_type, (py_object_type, py_object_type), None, True],
       "repr":    ["PyObject_Repr", py_object_type, (py_object_type, ), 0],
