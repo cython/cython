a3d670b04719c0158282a67146f1f419a1cfaafd
Stefan Behnel <scoder@users.berlios.de>
2007-10-17 09:55:16 +0200

    removed debug code

--- Cython/Distutils/build_ext.py
+++ Cython/Distutils/build_ext.py
@@ -22,7 +22,6 @@ try:
                compile as cython_compile
     from Cython.Compiler.Errors import PyrexError
 except ImportError, e:
-    print e
     PyrexError = None
 
 from distutils.command import build_ext as _build_ext
