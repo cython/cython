bfdb44e541a39d923e0bb18e3d521b661a017cd3
Stefan Behnel <scoder@users.berlios.de>
2008-01-06 08:51:55 +0100

    test 'unsigned long' against maxint+1 to avoid dependency on real long size

--- tests/run/r_vree_1.pyx
+++ tests/run/r_vree_1.pyx
@@ -3,15 +3,26 @@ __doc__ = """
     0L
     >>> test(1)
     1L
-    >>> 2**36
-    68719476736L
-    >>> test(2**36)
-    0L
-    >>> test(2L**36)
-    0L
+
+    >>> import sys
+    >>> sys.maxint + 1 > sys.maxint
+    True
+    >>> type(sys.maxint * 2 + 1) is long
+    True
+
+    >>> test(sys.maxint + 1)
+    2147483648L
+    >>> test(sys.maxint * 2 + 1)
+    4294967295L
+
+    >>> test(8 ** unsigned_long_size() - 1) > sys.maxint
+    True
 """
 
 def test(k):
     cdef unsigned long m
     m = k
     return m
+
+def unsigned_long_size():
+    return sizeof(unsigned long)
