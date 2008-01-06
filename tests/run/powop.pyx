b6d79eae5755b33fd3de1db4bb5af400af7cb397
Stefan Behnel <scoder@users.berlios.de>
2008-01-06 22:58:49 +0100

    extended test case

--- tests/run/powop.pyx
+++ tests/run/powop.pyx
@@ -2,6 +2,12 @@ __doc__ = """
     >>> f(1.0, 2.95)[0] == f(1.0, 2.95)[1]
     True
 
+    >>> g(4)
+    1024
+
+    >>> h(4)
+    625
+
     >>> constant_py() == 2L ** 10
     True
 
@@ -16,6 +22,12 @@ def f(obj2, obj3):
     flt1 = flt2 ** flt3
     obj1 = obj2 ** obj3
     return flt1, obj1
+
+def g(i):
+    return i ** 5
+
+def h(i):
+    return 5 ** i
 
 def constant_py():
     result = 2L ** 10
