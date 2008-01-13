aa0ec1fcb1bafe98c56c25685a155134c96673a8
Stefan Behnel <scoder@users.berlios.de>
2008-01-13 10:11:07 +0100

    reset to None instead of NULL in tp_clear()

--- Cython/Compiler/ModuleNode.py
+++ Cython/Compiler/ModuleNode.py
@@ -767,7 +767,8 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
             code.putln("}")
         for entry in py_attrs:
             name = "p->%s" % entry.cname
-            code.putln("tmp = %s; %s = 0;" % (name, name))
+            code.putln("tmp = %s;" % name)
+            code.put_init_to_py_none(name, entry.type)
             code.putln("Py_XDECREF(tmp);")
         code.putln(
             "return 0;")
