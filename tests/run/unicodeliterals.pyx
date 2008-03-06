7199b302a54410f06e8c7980c45e84afbb53032c
Stefan Behnel <scoder@users.berlios.de>
2008-03-06 12:11:11 +0100

    more unicode testing, also for null bytes

--- tests/run/unicodeliterals.pyx
+++ tests/run/unicodeliterals.pyx
@@ -17,6 +17,27 @@ __doc__ = r"""
     u'\xf8'
     >>> add
     u'S\xf8k ik\xfc\xd6\xe4abc'
+    >>> null
+    u'\x00'
+""" + """
+    >>> len(sa)
+    3
+    >>> len(ua)
+    3
+    >>> len(b)
+    3
+    >>> len(c)
+    6
+    >>> len(d)
+    3
+    >>> len(e)
+    10
+    >>> len(f)
+    1
+    >>> len(add)
+    12
+    >>> len(null)
+    1
 """ + u"""
     >>> sa == 'abc'
     True
@@ -34,6 +55,8 @@ __doc__ = r"""
     True
     >>> add == u'Søk ik' + u'üÖä' + 'abc'
     True
+    >>> null == u'\\x00' # doctest needs a double slash here
+    True
 """
 
 sa = 'abc'
@@ -46,3 +69,4 @@ e = u'\x03\x67\xf8\uf8d2Søk ik'
 f = u'\xf8'
 
 add = u'Søk ik' + u'üÖä' + 'abc'
+null = u'\x00'
