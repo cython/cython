6f80c215b83ac6b30b7181ea27aac00871ce07ae
Stefan Behnel <scoder@users.berlios.de>
2008-04-22 16:36:08 +0200

    let docstring test check string type (str/unicode)

--- tests/run/r_docstrings.pyx
+++ tests/run/r_docstrings.pyx
@@ -1,41 +1,39 @@
 __doc__ = """
-    >>> print f.__doc__
-    This is a function docstring.
+    >>> f.__doc__
+    'This is a function docstring.'
 
-    >>> print C.__doc__
-    This is a class docstring.
-    >>> print CS.__doc__
-    This is a subclass docstring.
+    >>> C.__doc__
+    'This is a class docstring.'
+    >>> CS.__doc__
+    'This is a subclass docstring.'
     >>> print CSS.__doc__
     None
 
-    >>> print T.__doc__
-    This is an extension type docstring.
-    >>> print TS.__doc__
-    This is an extension subtype docstring.
-    >>> print TSS.__doc__
-    None
+    >>> T.__doc__
+    'This is an extension type docstring.'
+    >>> TS.__doc__
+    'This is an extension subtype docstring.'
+    >>> TSS.__doc__
 
 Compare with standard Python:
 
     >>> def f():
-    ...     "This is a function docstring."
-    >>> print f.__doc__
-    This is a function docstring.
+    ...     'This is a function docstring.'
+    >>> f.__doc__
+    'This is a function docstring.'
 
     >>> class C:
-    ...     "This is a class docstring."
+    ...     'This is a class docstring.'
     >>> class CS(C):
-    ...     "This is a subclass docstring."
+    ...     'This is a subclass docstring.'
     >>> class CSS(CS):
     ...     pass
 
-    >>> print C.__doc__
-    This is a class docstring.
-    >>> print CS.__doc__
-    This is a subclass docstring.
-    >>> print CSS.__doc__
-    None
+    >>> C.__doc__
+    'This is a class docstring.'
+    >>> CS.__doc__
+    'This is a subclass docstring.'
+    >>> CSS.__doc__
 """
 
 def f():
