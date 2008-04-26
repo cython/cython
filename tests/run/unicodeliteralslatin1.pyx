bee17b92316e9614a4e587bb6e4e2fbef1719069
Stefan Behnel <scoder@users.berlios.de>
2008-04-26 08:58:21 +0200

    more tests for source code encodings: test latin-1 and the UTF-8 default
    --HG--
    rename : tests/run/unicodeliterals.pyx => tests/run/unicodeliteralsdefault.pyx

---
+++ tests/run/unicodeliteralslatin1.pyx
@@ -0,0 +1,76 @@
+# -*- coding: latin-1 -*-
+
+__doc__ = r"""
+    >>> sa
+    'abc'
+    >>> ua
+    u'abc'
+    >>> b
+    u'123'
+    >>> c
+    u'S\xf8k ik'
+    >>> d
+    u'\xfc\xd6\xe4'
+    >>> e
+    u'\x03g\xf8\uf8d2S\xf8k ik'
+    >>> f
+    u'\xf8'
+    >>> add
+    u'S\xf8k ik\xfc\xd6\xe4abc'
+    >>> null
+    u'\x00'
+""" + """
+    >>> len(sa)
+    3
+    >>> len(ua)
+    3
+    >>> len(b)
+    3
+    >>> len(c)
+    6
+    >>> len(d)
+    3
+    >>> len(e)
+    10
+    >>> len(f)
+    1
+    >>> len(add)
+    12
+    >>> len(null)
+    1
+""" + u"""
+    >>> sa == 'abc'
+    True
+    >>> ua == u'abc'
+    True
+    >>> b == u'123'
+    True
+    >>> c == u'Søk ik'
+    True
+    >>> d == u'üÖä'
+    True
+    >>> e == u'\x03\x67\xf8\uf8d2Søk ik'     # unescaped by Cython
+    True
+    >>> e == u'\\x03\\x67\\xf8\\uf8d2Søk ik' # unescaped by Python
+    True
+    >>> f == u'\xf8'  # unescaped by Cython
+    True
+    >>> f == u'\\xf8' # unescaped by Python
+    True
+    >>> add == u'Søk ik' + u'üÖä' + 'abc'
+    True
+    >>> null == u'\\x00' # unescaped by Python (required by doctest)
+    True
+"""
+
+sa = 'abc'
+ua = u'abc'
+
+b = u'123'
+c = u'Søk ik'
+d = u'üÖä'
+e = u'\x03\x67\xf8\uf8d2Søk ik'
+f = u'\xf8'
+
+add = u'Søk ik' + u'üÖä' + 'abc'
+null = u'\x00'
