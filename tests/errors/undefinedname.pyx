9207d005d634645bceb7bc8330ec3dac379da717
Stefan Behnel <scoder@users.berlios.de>
2008-04-28 10:34:24 +0200

    simple error test for undefined names

---
+++ tests/errors/undefinedname.pyx
@@ -0,0 +1,9 @@
+i = _this_global_name_does_not_exist_
+
+def test(i):
+    return _this_local_name_does_not_exist_
+
+_ERRORS = """
+1:37:undeclared name not builtin: _this_global_name_does_not_exist_
+4:43:undeclared name not builtin: _this_local_name_does_not_exist_
+"""
