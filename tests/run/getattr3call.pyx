33c55b52ea9392c3320cca1483def47570556b52
Stefan Behnel <scoder@users.berlios.de>
2008-05-01 08:18:39 +0200

    enabled test case
    --HG--
    rename : tests/broken/getattr3call.pyx => tests/run/getattr3call.pyx

---
+++ tests/run/getattr3call.pyx
@@ -0,0 +1,31 @@
+__doc__ = """
+    >>> class test(object): a = 1
+    >>> t = test()
+
+    >>> f(t, 'a')
+    1
+    >>> f(t, 'b')
+    Traceback (most recent call last):
+    AttributeError: 'test' object has no attribute 'b'
+
+    >>> g(t, 'a', 2)
+    1
+    >>> g(t, 'b', 2)
+    2
+"""
+
+BROKEN = """
+    >>> h(t, 'a', 2)
+    1
+    >>> h(t, 'b', 2)
+    2
+"""
+
+def f(a, b):
+    return getattr(a, b)
+
+def g(a, b, c):
+    return getattr3(a, b, c)
+
+#def h(a, b, c):
+#    return getattr(a, b, c)
