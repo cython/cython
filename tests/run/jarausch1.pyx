b2693292d55c69f7b9eaeda3365204a95ea5aaea
Stefan Behnel <scoder@users.berlios.de>
2008-05-15 10:38:33 +0200

    work around Cython problem with parsing ur'abc' literals

--- tests/run/jarausch1.pyx
+++ tests/run/jarausch1.pyx
@@ -1,10 +1,10 @@
 __doc__ = u"""
-   >>> py_x = ur'\\\\'
+   >>> py_x = br'\\\\'
    >>> assert x == py_x
 """
 
 import sys
-if sys.version_info[0] >= 3:
-    __doc__ = __doc__.replace(u" ur'", u" r'")
+if sys.version_info[0] < 3:
+    __doc__ = __doc__.replace(u" br'", u" r'")
 
-x = ur'\\'
+x = r'\\'
