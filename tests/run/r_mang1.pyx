c49834957696949ab1ea2281600720981f2e3a78
Stefan Behnel <scoder@users.berlios.de>
2008-05-15 09:44:11 +0200

    test fix

--- tests/run/r_mang1.pyx
+++ tests/run/r_mang1.pyx
@@ -1,12 +1,16 @@
 __doc__ = u"""
     >>> import re
     >>> t
-    ('2',)
+    (u'2',)
     >>> t == re.search('(\\d+)', '-2.80 98\\n').groups()
     True
 """
 
+import sys
+if sys.version_info[0] >= 3:
+    __doc__ = __doc__.replace(u"(u'", u"('")
+
 # this is a string constant test, not a test for 're'
 
 import re
-t = re.search('(\d+)', '-2.80 98\n').groups()
+t = re.search(u'(\d+)', u'-2.80 98\n').groups()
