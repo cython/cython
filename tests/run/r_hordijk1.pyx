46a988daf2b766756f07467ee106a5611b58fdee
Stefan Behnel <scoder@users.berlios.de>
2008-05-15 11:35:28 +0200

    test fix

--- tests/run/r_hordijk1.pyx
+++ tests/run/r_hordijk1.pyx
@@ -1,21 +1,22 @@
 __doc__ = u"""
   >>> try:
   ...     s = Spam()
-  ... except StandardError, e:
+  ... except KeyError, e:
   ...     print("Exception: %s" % e)
   ... else:
   ...     print("Did not raise the expected exception")
-  Exception: This is not a spanish inquisition
+  Exception: u'This is not a spanish inquisition'
 """
 
 import sys
 if sys.version_info[0] >= 3:
-    __doc__ = __doc__.replace(u"Exception, e", u"Exception as e")
+    __doc__ = __doc__.replace(u"Error, e", u"Error as e")
+    __doc__ = __doc__.replace(u" u'", u" '")
 
 cdef extern from "Python.h":
-    ctypedef class types.ListType [object PyListObject]:
+    ctypedef class __builtin__.list [object PyListObject]:
         pass
 
-cdef class Spam(ListType):
+cdef class Spam(list):
     def __init__(self):
-        raise StandardError("This is not a spanish inquisition")
+        raise KeyError(u"This is not a spanish inquisition")
