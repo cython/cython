3b447f810cbcf934e8ffa1df0152ed72717fe438
Stefan Behnel <scoder@users.berlios.de>
2008-05-15 18:49:13 +0200

    enable __future__ imports

---
+++ Cython/Compiler/Future.py
@@ -0,0 +1,5 @@
+import __future__
+
+unicode_literals = __future__.unicode_literals
+
+del __future__
---
+++ tests/run/future_unicode_literals.pyx
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+import sys
+if sys.version_info[0] >= 3:
+    __doc__ = """
+    >>> u == 'test'
+    True
+    >>> isinstance(u, str)
+    True
+"""
+else:
+    __doc__ = """
+    >>> u == u'test'
+    True
+    >>> isinstance(u, unicode)
+    True
+"""
+
+u = "test"
