21b0593a1ec11dc1afd57b0bcd37c19e4ab6952e
Stefan Behnel <scoder@users.berlios.de>
2008-05-23 22:11:00 +0200

    reverted last change, based on extended test case

--- tests/run/classmethod.pyx
+++ tests/run/classmethod.pyx
@@ -1,8 +1,14 @@
 __doc__ = u"""
+>>> class1.view()
+class1
 >>> class1.plus(1)
 6
+>>> class2.view()
+class2
 >>> class2.plus(1)
 7
+>>> class3.view()
+class3
 >>> class3.plus(1)
 8
 """
@@ -13,11 +19,20 @@ def f_plus(cls, a):
 class class1:
     a = 5
     plus = classmethod(f_plus)
+    def view(cls):
+        print cls.__name__
+    view = classmethod(view)
 
 class class2(object):
     a = 6
     plus = classmethod(f_plus)
+    def view(cls):
+        print cls.__name__
+    view = classmethod(view)
 
 cdef class class3:
     a = 7
     plus = classmethod(f_plus)
+    def view(cls):
+        print cls.__name__
+    view = classmethod(view)
