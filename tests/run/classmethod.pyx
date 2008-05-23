3ad16af0a8fc853092702eb7236495f2cc0e9e5a
Stefan Behnel <scoder@users.berlios.de>
2008-05-23 20:07:44 +0200

    simple test case to emulate class methods as function attributes

---
+++ tests/run/classmethod.pyx
@@ -0,0 +1,20 @@
+__doc__ = u"""
+>>> class1.plus1(1)
+2
+>>> class2.plus1(1)
+2
+>>> class3.plus1(1)
+2
+"""
+
+def f_plus(a):
+    return a + 1
+
+class class1:
+    plus1 = f_plus
+
+class class2(object):
+    plus1 = f_plus
+
+cdef class class3:
+    plus1 = f_plus
