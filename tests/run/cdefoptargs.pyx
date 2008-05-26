d8efb40401e4aa3b4401d820387a1c9211ec2cb1
Stefan Behnel <scoder@users.berlios.de>
2008-05-26 10:22:09 +0200

    test cases for optional arguments in cdef functions

---
+++ tests/run/cdefoptargs.pyx
@@ -0,0 +1,21 @@
+__doc__ = u"""
+    >>> call2()
+    >>> call3()
+    >>> call4()
+"""
+
+# the calls:
+
+def call2():
+    b(1,2)
+
+def call3():
+    b(1,2,3)
+
+def call4():
+    b(1,2,3,4)
+
+# the called function:
+
+cdef b(a, b, c=1, d=2):
+    pass
