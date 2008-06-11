f714831a635e4bd24b65b4be7cd8466b30307923
Stefan Behnel <scoder@users.berlios.de>
2008-06-11 21:56:35 +0200

    new test cases from Pyrex

---
+++ tests/errors/se_conddef.pyx
@@ -0,0 +1,6 @@
+if x:
+	def h():
+		pass
+_ERRORS = u"""
+/Local/Projects/D/Pyrex/Source/Tests/Errors1/se_conddef.pyx:2:1: def statement not allowed here
+"""
---
+++ tests/errors/se_nestdef.pyx
@@ -0,0 +1,6 @@
+def f():
+	def g():
+		pass
+_ERRORS = u"""
+/Local/Projects/D/Pyrex/Source/Tests/Errors1/se_nestdef.pyx:2:1: def statement not allowed here
+"""
