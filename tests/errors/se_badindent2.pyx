35a346d4c4f010c8773b981af600a59503c8f148
Stefan Behnel <scoder@users.berlios.de>
2008-06-11 21:55:24 +0200

    new test cases from Pyrex

---
+++ tests/errors/se_badindent2.pyx
@@ -0,0 +1,7 @@
+def f():
+	a = b
+		c = d
+_ERRORS = u"""
+/Local/Projects/D/Pyrex/Source/Tests/Errors1/se_badindent2.pyx:3:0: Possible inconsistent indentation
+/Local/Projects/D/Pyrex/Source/Tests/Errors1/se_badindent2.pyx:3:0: Expected an identifier or literal
+"""
