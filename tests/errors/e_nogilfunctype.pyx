91db1aeb1e7ed6a56e4ab0504fd576b6bf21a294
Robert Bradshaw <robertwb@math.washington.edu>
2008-06-11 17:21:38 -0700

    Fix e_declarations.pyx, e_nogilfunctype.pyx, e_tempcast.pyx. All tests pass.

--- tests/errors/e_nogilfunctype.pyx
+++ tests/errors/e_nogilfunctype.pyx
@@ -1,8 +1,8 @@
 cdef extern from *:
-	cdef void f() nogil
-	cdef void (*fp)()
+	cdef void f()
+	cdef void (*fp)() nogil
 
 fp = f
 _ERRORS = u"""
-/Local/Projects/D/Pyrex/Source/Tests/Errors3/e_nogilfunctype.pyx:5:6: Cannot assign type 'void (void) nogil' to 'void (*)(void)'
+/Local/Projects/D/Pyrex/Source/Tests/Errors3/e_nogilfunctype.pyx:5:6: Cannot assign type 'void (void)' to 'void (*)(void) nogil'
 """
