050a5310d636637a61098f07e56df27a199f2899
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-07-19 22:45:07 +0200

    Testcase for cdef assignments

---
+++ tests/run/cdefassign.pyx
@@ -0,0 +1,15 @@
+
+__doc__ = """
+   >>> test(1, 2)
+   4 1 2 2 0
+"""
+
+def test(x, int y):
+    if True:
+        before = 0
+    cdef int a = 4, b = x, c = y, *p = &y
+    print a, b, c, p[0], before
+
+# Also test that pruning cdefs doesn't hurt 
+def empty():
+    cdef int i
