eaa6d4775567b5744028d46b21ed7c89fcb7ffe8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-07-30 17:13:02 +0200

    Implemented mode flag and strided mode for buffers

--- tests/errors/e_bufaccess.pyx
+++ tests/errors/e_bufaccess.pyx
@@ -8,6 +8,8 @@ def f():
     cdef object[ndim=-1] buf2
     cdef object[int, 'a'] buf3
     cdef object[int,2,3,4,5,6] buf4
+    cdef object[int, 2, 'foo'] buf5
+    cdef object[int, 2, well] buf6
 
 _ERRORS = u"""
 1:11: Buffer types only allowed as function local variables
@@ -17,5 +19,7 @@ _ERRORS = u"""
 8:15: "dtype" missing
 9:21: "ndim" must be an integer
 10:15: Too many buffer options
+11:24: Only allowed buffer modes are "full" or "strided" (as a compile-time string)
+12:28: Only allowed buffer modes are "full" or "strided" (as a compile-time string)
 """
 
