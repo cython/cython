0817b5c8f56265f0fb94c6634d4c1fed8f43a71a
Robert Bradshaw <robertwb@math.washington.edu>
2008-08-05 00:57:30 -0700

    Re-instate 908 with working type names as argument names.

--- tests/compile/cargdef.pyx
+++ tests/compile/cargdef.pyx
@@ -1,3 +1,10 @@
 def f(obj, int i, float f, char *s1, char s2[]):
     pass
     
+cdef g(obj, int i, float f, char *s1, char s2[]):
+    pass
+    
+cdef do_g(object (*func)(object, int, float, char*, char*)):
+    return func(1, 2, 3.14159, "a", "b")
+    
+do_g(&g)
