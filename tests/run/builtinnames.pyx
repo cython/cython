e7650a2295006d5a3401f665cef2c20589aa1d7a
Stefan Behnel <scoder@users.berlios.de>
2008-08-07 18:52:22 +0200

    Py3 test fixes

--- tests/run/builtinnames.pyx
+++ tests/run/builtinnames.pyx
@@ -2,21 +2,21 @@ __doc__ = u"""
 >>> test_c('abc')
 fileabc
 typeabc
->>> print test_file_py('abc')
+>>> print(test_file_py('abc'))
 abc
->>> print range('abc')
+>>> print(range('abc'))
 rangeabc
 """
 
 
 def test_file_py(file):
     assert isinstance(file, str), \
-        "not a string, found '%s' instead" % file.__class__.__name__
+        u"not a string, found '%s' instead" % file.__class__.__name__
     return file
 
 cdef test_file_c(file):
     assert isinstance(file, str), \
-        "not a string, found '%s' instead" % file.__class__.__name__
+        u"not a string, found '%s' instead" % file.__class__.__name__
     return u'file' + file
 
 
