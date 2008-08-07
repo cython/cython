61dd2d7e95c070fa0ce88fe11b7d8bc247302e9a
Stefan Behnel <scoder@users.berlios.de>
2008-08-07 18:34:58 +0200

    extended test case

--- tests/run/builtinnames.pyx
+++ tests/run/builtinnames.pyx
@@ -1,18 +1,22 @@
 __doc__ = u"""
->>> test_c(u'abc')
+>>> test_c('abc')
 fileabc
 typeabc
->>> print test_file_py(u'abc')
+>>> print test_file_py('abc')
 abc
->>> print range(u'abc')
+>>> print range('abc')
 rangeabc
 """
 
 
 def test_file_py(file):
+    assert isinstance(file, str), \
+        "not a string, found '%s' instead" % file.__class__.__name__
     return file
 
 cdef test_file_c(file):
+    assert isinstance(file, str), \
+        "not a string, found '%s' instead" % file.__class__.__name__
     return u'file' + file
 
 
