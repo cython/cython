b743c503e699b9d8bace2642999c9e62cdd0b043
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-08-15 18:12:37 +0200

    Testcase for C array inplace operators

--- tests/run/inplace.pyx
+++ tests/run/inplace.pyx
@@ -7,6 +7,9 @@ __doc__ = u"""
 
     >>> h(56, 7)
     105.0
+
+    >>> arrays()
+    19
 """
 
 def f(a,b):
@@ -26,3 +29,16 @@ def h(double a, double b):
     a += b
     a *= b
     return a
+
+cimport stdlib
+
+def arrays():
+    cdef char* buf = <char*>stdlib.malloc(10)
+    cdef int i = 2
+    cdef object j = 2
+    buf[2] = 0
+    buf[i] += 2
+    buf[2] *= 10
+    buf[j] -= 1
+    print buf[2]
+    stdlib.free(buf)
