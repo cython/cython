2f02248342c22e4839e111ce5c49d0b57ef5abcf
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-08-15 23:54:28 +0200

    Changed compiler directive comment format
    --HG--
    rename : tests/compile/c_options.pyx => tests/compile/c_directives.pyx
    rename : tests/errors/e_options.pyx => tests/errors/e_directives.pyx

--- tests/compile/c_directives.pyx
+++ tests/compile/c_directives.pyx
@@ -1,28 +1,34 @@
-#cython: boundscheck=False
+#     boundscheck  =  False
+# ignoreme = OK
+
+# This testcase is most useful if you inspect the generated C file
 
 print 3
 
 cimport python_dict as asadf, python_exc, cython as cy
 
+def e(object[int, 2] buf):
+    print buf[3, 2] # no bc
+
 @cy.boundscheck(False)
 def f(object[int, 2] buf):
-    print buf[3, 2]
+    print buf[3, 2] # no bc
 
 @cy.boundscheck(True)
 def g(object[int, 2] buf):
-    # Please leave this comment, 
-#cython: this should have no special meaning
+    # The below line should have no meaning 
+# boundscheck = False
     # even if the above line doesn't follow indentation.
-    print buf[3, 2]
+    print buf[3, 2] # bc
 
 def h(object[int, 2] buf):
-    print buf[3, 2]
+    print buf[3, 2] # no bc
     with cy.boundscheck(True):
-        print buf[3,2]
+        print buf[3,2] # bc
 
 from cython cimport boundscheck as bc
 
 def i(object[int] buf):
     with bc(True):
-        print buf[3]
+        print buf[3] # bs
     
