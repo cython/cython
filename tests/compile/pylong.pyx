54ab9f4c0dd22c1428358704caec75016ce2e465
Stefan Behnel <scoder@users.berlios.de>
2008-08-22 23:20:57 +0200

    Py3k test fix

--- tests/compile/pylong.pyx
+++ tests/compile/pylong.pyx
@@ -10,11 +10,11 @@ cdef extern from "longintrepr.h":
     cdef struct _longobject:
         int ob_refcnt 
         PyTypeObject *ob_type
-        int ob_size
+#        int ob_size            # not in Py3k
         unsigned int *ob_digit
 
 def test(temp = long(0)):
     cdef _longobject *l 
     l = <_longobject *> temp 
-    print sizeof(l.ob_size) 
+    #print sizeof(l.ob_size)    # not in Py3k
     print sizeof(l.ob_digit[0])
