7b3554bdba45c6b3f4b4f4366abba6c368b72adb
Robert Bradshaw <robertwb@math.washington.edu>
2008-09-13 20:29:53 -0700

    Unsigned arithmatic, ticket #54

---
+++ tests/run/unsigned.pyx
@@ -0,0 +1,19 @@
+__doc__ = """
+    >>> test_signed()
+    3 <type 'int'>
+    9 <type 'long'>
+    6 <type 'long'>
+    12 <type 'long'>
+"""
+
+
+cdef int i = 1
+cdef long l = 2
+cdef unsigned int ui = 4
+cdef unsigned long ul = 8
+
+def test_signed():
+    print i + l, type(i+l)
+    print i + ul, type(i+ul)
+    print ui + l, type(ui+l)
+    print ui + ul, type(ui+ul)
