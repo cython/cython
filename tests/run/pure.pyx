ff152fe9f936d686026c7f35958d8b3f4b30e31e
Robert Bradshaw <robertwb@math.washington.edu>
2008-10-08 16:08:15 -0700

    Better NULL parsing, touchup pure test

--- tests/run/pure.pyx
+++ tests/run/pure.pyx
@@ -1,8 +1,41 @@
-import sys, os
-sys.path.insert(0, "..")
+__doc__ = """
+>>> test_sizeof()
+True
+True
+True
+True
+True
+
+>>> test_declare(100)
+(100, 100)
+>>> test_declare(100.5)
+(100, 100)
+>>> test_declare(None)
+Traceback (most recent call last):
+...
+TypeError: an integer is required
+
+>>> test_cast(1.5)
+1
+>>> test_cast(None)
+Traceback (most recent call last):
+...
+TypeError: a float is required
+
+>>> test_address(39)
+39
+
+>>> test_locals(5)
+True
+
+>>> test_struct(389, 1.64493)
+(389, 1.64493)
+
+>>> test_imports()
+True
+"""
 
 import cython
-
 
 def test_sizeof():
     x = cython.declare(cython.bint)
@@ -52,15 +85,15 @@ def test_struct(n, x):
 
 import cython as cy
 from cython import declare, cast, locals, address, typedef, p_void, compiled
-from cython import declare as my_declare, locals as my_locals, p_void as my_void_star, typedef as my_typedef, my_compiled
+from cython import declare as my_declare, locals as my_locals, p_void as my_void_star, typedef as my_typedef, compiled as my_compiled
 
 @my_locals(a=cython.p_void)
 def test_imports():
-    a = NULL
-    b = declare(p_void, NULL)
-    c = my_declare(my_void_star, NULL)
-    d = cy.declare(cy.p_void, NULL)
-    compiled and my_compiled
+    a = cython.NULL
+    b = declare(p_void, cython.NULL)
+    c = my_declare(my_void_star, cython.NULL)
+    d = cy.declare(cy.p_void, cython.NULL)
+    return a == d and compiled and my_compiled
 
 MyStruct3 = typedef(MyStruct[3])
 MyStruct4 = my_typedef(MyStruct[4])
