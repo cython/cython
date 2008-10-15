94b89da9773aafa486d6c0725b1975d97c4de564
Lisandro Dalcin <dalcinl@gmail.com>
2008-10-15 12:10:09 -0300

    pyrex backport, accept ctypedef public api declarations

---
+++ tests/compile/ctypedefpubapi.pyx
@@ -0,0 +1,10 @@
+ctypedef public api class Foo [type PyFoo_Type, object PyFooObject]:
+    pass
+
+cdef api:
+    ctypedef public class Bar [type PyBar_Type, object PyBarObject]:
+        pass
+
+cdef public api:
+    ctypedef class Baz [type PyBaz_Type, object PyBazObject]:
+        pass
