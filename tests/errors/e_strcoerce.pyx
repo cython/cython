006262f0963e853023dfd28fa4183d62648fd8a5
Stefan Behnel <scoder@users.berlios.de>
2008-10-26 17:59:17 +0100

    new error test case for string to int conversion

---
+++ tests/errors/e_strcoerce.pyx
@@ -0,0 +1,8 @@
+cdef int c1 = "t"     # works
+cdef int c2 = "te"    # fails
+cdef int cx = "test"  # fails
+
+_ERRORS = """
+2:14: Only coerce single-character ascii strings can be used as ints.
+3:14: Only coerce single-character ascii strings can be used as ints.
+"""
