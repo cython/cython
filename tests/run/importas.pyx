15d20eaf74f1aa4744dfb9611be6c5c2407d63df
Stefan Behnel <scoder@users.berlios.de>
2008-11-13 08:10:10 +0100

    extended test case

--- tests/run/importas.pyx
+++ tests/run/importas.pyx
@@ -1,7 +1,7 @@
 __doc__ = u"""
 >>> import sys as sous
 >>> import distutils.core as corey
->>> from copy import copy as copey
+>>> from copy import deepcopy as copey
 
 >>> sous is _sous
 True
@@ -16,8 +16,21 @@ True
 True
 >>> _copey is not None
 True
+
+>>> print(_sous.__name__)
+sys
+>>> print(sous.__name__)
+sys
+>>> print(_corey.__name__)
+distutils.core
+>>> print(corey.__name__)
+distutils.core
+>>> print(_copey.__name__)
+deepcopy
+>>> print(copey.__name__)
+deepcopy
 """
 
 import sys as _sous
 import distutils.core as _corey
-from copy import copy as _copey
+from copy import deepcopy as _copey
