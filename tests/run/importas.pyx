bf2509b86c3dba6a7df9215f3367f8c68c6d169f
Stefan Behnel <scoder@users.berlios.de>
2008-11-14 21:53:41 +0100

    fix Python level 'import as' of packages

--- tests/run/importas.pyx
+++ tests/run/importas.pyx
@@ -2,6 +2,7 @@ __doc__ = u"""
 >>> import sys as sous
 >>> import distutils.core as corey
 >>> from copy import deepcopy as copey
+>>> import distutils.command as commie
 
 >>> sous is _sous
 True
@@ -9,12 +10,16 @@ True
 True
 >>> copey is _copey
 True
+>>> _commie is commie
+True
 
 >>> _sous is not None
 True
 >>> _corey is not None
 True
 >>> _copey is not None
+True
+>>> _commie is not None
 True
 
 >>> print(_sous.__name__)
@@ -29,8 +34,13 @@ distutils.core
 deepcopy
 >>> print(copey.__name__)
 deepcopy
+>>> print(_commie.__name__)
+distutils.command
+>>> print(commie.__name__)
+distutils.command
 """
 
 import sys as _sous
 import distutils.core as _corey
 from copy import deepcopy as _copey
+import distutils.command as _commie
