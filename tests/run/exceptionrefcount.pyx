354dc42f43aead7fa7502cce3d83236b6db0a5e9
Stefan Behnel <scoder@users.berlios.de>
2008-11-17 14:35:05 +0100

    fixed ref-count bug in try-except handling

---
+++ tests/run/exceptionrefcount.pyx
@@ -0,0 +1,55 @@
+__doc__ = u"""
+>>> class SampleException(Exception): pass
+>>> import sys
+
+>>> def assert_refcount(rc1, rc2, func):
+...     # test ref-counts, but allow a bit of freedom
+...     assert rc2 <= rc1 + 4, "%s, before: %d, after %d" % (
+...         func.__name__, rc1, rc2)
+
+>>> def run_test(repeat, test_func):
+...     initial_refcount = sys.getrefcount(SampleException)
+...     for i in range(repeat):
+...         try: raise SampleException
+...         except:
+...             refcount1 = sys.getrefcount(SampleException)
+...             test_func()
+...             refcount2 = sys.getrefcount(SampleException)
+...             
+...             assert_refcount(refcount1, refcount2, test_func)
+...             assert_refcount(initial_refcount, refcount2, test_func)
+...         refcount3 = sys.getrefcount(SampleException)
+...         assert_refcount(refcount1, refcount3, test_func)
+...         assert_refcount(initial_refcount, refcount3, test_func)
+
+>>> run_test(50, test_no_exception_else)
+>>> run_test(50, test_no_exception)
+>>> run_test(50, test_exception)
+>>> run_test(50, test_finally)
+"""
+
+def test_no_exception():
+    try:
+        a = 1+1
+    except:
+        pass
+
+def test_no_exception_else():
+    try:
+        a = 1+1
+    except:
+        pass
+    else:
+        b = 1+1
+
+def test_exception():
+    try:
+        raise TypeError
+    except:
+        pass
+
+def test_finally():
+    try:
+        a = 1+1
+    finally:
+        b = 1+1
