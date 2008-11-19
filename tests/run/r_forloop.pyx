a9ef5ff869f73413fa0e09d92a93e5f2ef49df27
Stefan Behnel <scoder@users.berlios.de>
2008-11-19 07:59:15 +0100

    extended for-loop test case

--- tests/run/r_forloop.pyx
+++ tests/run/r_forloop.pyx
@@ -4,10 +4,20 @@ __doc__ = u"""
   Spam!
   Spam!
   Spam!
-  Spam!
 
   >>> go_c()
   Spam!
+  Spam!
+  Spam!
+  Spam!
+
+  >>> go_list()
+  Spam!
+  Spam!
+  Spam!
+  Spam!
+
+  >>> go_tuple()
   Spam!
   Spam!
   Spam!
@@ -15,10 +25,25 @@ __doc__ = u"""
 """
 
 def go_py():
-    for i in range(5):
+    for i in range(4):
         print u"Spam!"
 
 def go_c():
     cdef int i
-    for i in range(5):
+    for i in range(4):
+        print u"Spam!"
+
+def go_list():
+    cdef list l = range(4)
+    for i in l:
+        print u"Spam!"
+
+def go_tuple():
+    cdef tuple t = tuple(range(4))
+    for i in t:
+        print u"Spam!"
+
+def go_dict():
+    cdef dict d = dict(zip(range(4), range(4)))
+    for i in d:
         print u"Spam!"
