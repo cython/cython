96b73b35263ea0562eb0f71ad79b7dd49400778b
Stefan Behnel <scoder@users.berlios.de>
2008-11-25 21:44:25 +0100

    test case from ticket 123

---
+++ tests/run/ticket_123.pyx
@@ -0,0 +1,24 @@
+__doc__ = u"""
+>>> ret = repeat_iter()
+>>> for s in ret:
+...     print s
+a
+a
+b
+b
+c
+c
+"""
+
+def repeat_iter():
+    cdef dict e
+    cdef unicode s
+    ret = []
+    e = {u"A": u"a", u"B": u"b", u"C": u"c"}
+    for s in e.itervalues():
+        ret.append(s)
+    for s in e.itervalues():
+        ret.append(s)
+
+    ret.sort()
+    return ret
