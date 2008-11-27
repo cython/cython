4b4d3f43386b13b357d46438e2dd250c3c92c65b
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-11-27 19:29:12 +0100

    Inline function definitions in pxd files

---
+++ tests/run/inlinepxd.pxd
@@ -0,0 +1,3 @@
+
+cdef inline int my_add(int a, int b):
+    return a + b
---
+++ tests/run/inlinepxd.pyx
@@ -0,0 +1,20 @@
+"""
+>>> f()
+3
+>>> g()
+6
+>>> h()
+6
+"""
+
+cimport inlinepxd_support
+from inlinepxd_support cimport my_add as my_add3
+
+def f():
+    return my_add(1, 2)
+
+def g():
+    return inlinepxd_support.my_add(1, 2, 3)
+
+def h():
+    return my_add3(1, 2, 3)
