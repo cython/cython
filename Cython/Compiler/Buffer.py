fc02c5d369c1556ee150da6108da435059c98f2f
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2008-12-16 10:02:51 +0100

    Fix error in buffer typestring checking

--- Cython/Compiler/Buffer.py
+++ Cython/Compiler/Buffer.py
@@ -500,7 +500,7 @@ def mangle_dtype_name(dtype):
     elif dtype.is_ptr:
         return "ptr"
     else:
-        if dtype.typestring is None:
+        if dtype.is_typedef or dtype.is_struct_or_union:
             prefix = "nn_"
         else:
             prefix = ""
@@ -552,10 +552,8 @@ def create_typestringchecker(protocode, defcode, name, dtype):
         defcode.putln("int ok;")
         defcode.putln("ts = __Pyx_ConsumeWhitespace(ts); if (!ts) return NULL;")
         defcode.putln("if (*ts == '1') ++ts;")
-        if dtype.typestring is not None:
-            assert len(dtype.typestring) == 1
-            # Can use direct comparison
-            defcode.putln("ok = (*ts == '%s');" % dtype.typestring)
+        if dtype.is_pyobject:
+            defcode.putln("ok = (*ts == 'O');")
         else:
             # Cannot trust declared size; but rely on int vs float and
             # signed/unsigned to be correctly declared. Use a switch statement
