f45fa3e031263d8c7bcae1025f1ce6cff9362a0f
Stefan Behnel <scoder@users.berlios.de>
2008-12-30 12:34:58 +0100

    fix doctest

--- tests/run/locals.pyx
+++ tests/run/locals.pyx
@@ -1,9 +1,20 @@
-__doc__ = """
-sage: get_locals(1,2,3)
-{'args': (2, 3), 'kwds': {}, 'x': 1, 'y': 'hi', 'z': 5}
+__doc__ = u"""
+>>> sorted( get_locals(1,2,3, k=5) .items())
+[('args', (2, 3)), ('kwds', {'k': 5}), ('x', 1), ('y', 'hi'), ('z', 5)]
 """
 
+import sys
+IS_PY3 = sys.version_info[0] >= 3
+
 def get_locals(x, *args, **kwds):
-    y = "hi"
     cdef int z = 5
+    if IS_PY3:
+        y = u"hi"
+    else:
+        y = "hi"
     return locals()
+
+def sorted(it):
+    l = list(it)
+    l.sort()
+    return l
