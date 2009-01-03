d1a841b91952880d7823e2b768e4af3a8a1b69a6
Stefan Behnel <scoder@users.berlios.de>
2009-01-03 09:16:27 +0100

    test for '**' on int types

--- tests/errors/e_powop.pyx
+++ tests/errors/e_powop.pyx
@@ -1,9 +1,15 @@
 def f():
 	cdef char *str1
 	cdef float flt1, flt2, flt3
+	cdef int int1 = 1, int2 = 2, int3
 	flt1 = str1 ** flt3 # error
 	flt1 = flt2 ** str1 # error
+	int3 = int1 ** int2 # disabled in Cython
+	int3 = 3 ** 4       # disabled in Cython
+
 _ERRORS = u"""
-/Local/Projects/D/Pyrex/Source/Tests/Errors2/e_powop.pyx:4:13: Invalid operand types for '**' (char *; float)
-/Local/Projects/D/Pyrex/Source/Tests/Errors2/e_powop.pyx:5:13: Invalid operand types for '**' (float; char *)
+5:13: Invalid operand types for '**' (char *; float)
+6:13: Invalid operand types for '**' (float; char *)
+7:13: C has no integer powering, use python ints or floats instead '**' (int; int)
+8:10: C has no integer powering, use python ints or floats instead '**' (long; long)
 """
