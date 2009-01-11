481b21b1580fba02838714e2bab91bb9713792a7
Stefan Behnel <scoder@users.berlios.de>
2009-01-11 08:42:08 +0100

    made new test case an error test case
    --HG--
    rename : tests/compile/nonconst_def.pyx => tests/errors/nonconst_def.pyx

--- tests/compile/nonconst_def.pyx
+++
@@ -1,3 +0,0 @@
-import os
-DEF ospath = os.path
-
---
+++ tests/errors/nonconst_def.pyx
@@ -0,0 +1,7 @@
+import os
+DEF ospath = os.path
+
+_ERRORS = u"""
+2:15: Compile-time name 'os' not defined
+2:15: Error in compile-time expression: AttributeError: 'NoneType' object has no attribute 'path'
+"""
