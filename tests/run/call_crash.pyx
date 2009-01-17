27460d6eba8646ca0570daa179e45f6dca574c10
Robert Bradshaw <robertwb@math.washington.edu>
2009-01-17 01:25:34 -0800

    test for temp allocation bug in call

---
+++ tests/run/call_crash.pyx
@@ -0,0 +1,24 @@
+__doc__ = """
+    >>> A().test(3)
+    9
+"""
+
+cdef class A:
+
+    cdef int (*func_ptr)(int)
+    
+    def __init__(self):
+        self.func_ptr = &func
+
+    cdef int do_it(self, int s):
+        cdef int r = first_call(self).func_ptr(s) # the temp for first_call(self) not properly freed
+        return r
+    
+    def test(self, s):
+        return self.do_it(s)
+
+cdef A first_call(A x):
+    return x
+    
+cdef int func(int s):
+    return s*s
