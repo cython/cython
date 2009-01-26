d7b8131d9a76c22d94bf5eeb90ce7c0f2e506650
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-01-26 18:02:43 +0100

    Added test for #184

---
+++ tests/bugs/unsignedbehaviour_T184.pyx
@@ -0,0 +1,35 @@
+"""
+>>> c_call()
+(-10, 10)
+>>> py_call()
+(-10, 10)
+>>> loop()
+19
+>>> rangelist()
+[-3, -2, -1, 0, 1, 2]
+"""
+
+cdef c_g(int a, int b):
+    return (a, b)
+
+def py_g(a, b):
+    return (a, b)
+
+def c_call():
+    cdef unsigned int i = 10
+    return c_g(-i, i)
+
+def py_call():
+    cdef unsigned int i = 10
+    return py_g(-i, i)
+
+def loop():
+    cdef unsigned int i = 10
+    times = 0
+    for i in range(-i,i):
+        times += 1
+    return times
+    
+def rangelist():
+    cdef unsigned int i = 3
+    return list(range(-i, i))
