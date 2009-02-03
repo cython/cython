fb7d630d0a105e3950c0851081cdd42dcfaaef15
Lisandro Dalcin <dalcinl@gmail.com>
2009-02-03 09:59:49 +0100

    Avoid the need to load refnanny module using RTLD_GLOBAL

--- Cython/Compiler/ModuleNode.py
+++ Cython/Compiler/ModuleNode.py
@@ -1585,7 +1585,12 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
         code.putln("PyObject* %s;" % Naming.retval_cname)
         tempdecl_code = code.insertion_point()
 
-        code.put_setup_refcount_context(header3)
+        code.putln("#ifdef CYTHON_REFNANNY")
+        code.putln("void* __pyx_refchk = NULL;")
+        code.putln("__Pyx_Refnanny = (__Pyx_RefnannyAPIStruct*) PyCObject_Import((char *)\"refnanny\", (char *)\"RefnannyAPI\");")
+        code.putln("if (!__Pyx_Refnanny) Py_FatalError(\"failed to import refnanny module\");")
+        code.putln("__pyx_refchk = __Pyx_Refnanny->NewContext(\"%s\", __LINE__);"% header3)
+        code.putln("#endif")
 
         code.putln("%s = PyTuple_New(0); %s" % (Naming.empty_tuple, code.error_goto_if_null(Naming.empty_tuple, self.pos)));
 
@@ -2323,27 +2328,29 @@ bad:
         
 refcount_utility_code = UtilityCode(proto="""
 #ifdef CYTHON_REFNANNY
-__PYX_EXTERN_C void __Pyx_Refnanny_INCREF(void*, PyObject*, int);
-__PYX_EXTERN_C void __Pyx_Refnanny_GOTREF(void*, PyObject*, int);
-__PYX_EXTERN_C void __Pyx_Refnanny_GIVEREF(void*, PyObject*, int);
-__PYX_EXTERN_C void __Pyx_Refnanny_INCREF(void*, PyObject*, int);
-__PYX_EXTERN_C void __Pyx_Refnanny_DECREF(void*, PyObject*, int);
-__PYX_EXTERN_C void* __Pyx_Refnanny_NewContext(char*, int);
-__PYX_EXTERN_C int __Pyx_Refnanny_FinishContext(void*);
-#define __Pyx_INCREF(r) __Pyx_Refnanny_INCREF(__pyx_refchk, r, __LINE__)
-#define __Pyx_GOTREF(r) __Pyx_Refnanny_GOTREF(__pyx_refchk, r, __LINE__)
-#define __Pyx_GIVEREF(r) __Pyx_Refnanny_GIVEREF(__pyx_refchk, r, __LINE__)
-#define __Pyx_DECREF(r) __Pyx_Refnanny_DECREF(__pyx_refchk, r, __LINE__)
+typedef struct {
+  void (*INCREF)(void*, PyObject*, int);
+  void (*DECREF)(void*, PyObject*, int);
+  void (*GOTREF)(void*, PyObject*, int);
+  void (*GIVEREF)(void*, PyObject*, int);
+  void* (*NewContext)(const char*, int);
+  int (*FinishContext)(void*);
+} __Pyx_RefnannyAPIStruct;
+static __Pyx_RefnannyAPIStruct* __Pyx_Refnanny = NULL;
+#define __Pyx_INCREF(r) __Pyx_Refnanny->INCREF(__pyx_refchk, (r), __LINE__)
+#define __Pyx_DECREF(r) __Pyx_Refnanny->DECREF(__pyx_refchk, (r), __LINE__)
 #define __Pyx_XDECREF(r) if((r) == NULL) ; else __Pyx_DECREF(r)
+#define __Pyx_GOTREF(r) __Pyx_Refnanny->GOTREF(__pyx_refchk, (r), __LINE__)
+#define __Pyx_GIVEREF(r) __Pyx_Refnanny->GIVEREF(__pyx_refchk, (r), __LINE__)
 #define __Pyx_SetupRefcountContext(name) \
-  void* __pyx_refchk = __Pyx_Refnanny_NewContext((char*)name, __LINE__)
-#define __Pyx_FinishRefcountContext() __Pyx_Refnanny_FinishContext(__pyx_refchk)
+  void* __pyx_refchk = __Pyx_Refnanny->NewContext((name), __LINE__)
+#define __Pyx_FinishRefcountContext() __Pyx_Refnanny->FinishContext(__pyx_refchk)
 #else
 #define __Pyx_INCREF(r) Py_INCREF(r)
-#define __Pyx_GOTREF(r)
-#define __Pyx_GIVEREF(r)
 #define __Pyx_DECREF(r) Py_DECREF(r)
 #define __Pyx_XDECREF(r) Py_XDECREF(r)
+#define __Pyx_GOTREF(r)
+#define __Pyx_GIVEREF(r)
 #define __Pyx_SetupRefcountContext(name)
 #define __Pyx_FinishRefcountContext() 0
 #endif /* CYTHON_REFNANNY */
