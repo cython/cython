1b399579f3f827a58346a2d5496a1053d506645e
Robert Bradshaw <robertwb@math.washington.edu>
2009-02-11 13:16:53 -0800

    extend size_t test

--- tests/run/size_t.pyx
+++ tests/run/size_t.pyx
@@ -17,7 +17,35 @@ OverflowError: ...
 Traceback (most recent call last):
     ...
 OverflowError: ...
+
+>>> a = A(1,2)
+>>> a.a == 1
+True
+>>> a.b == 2
+True
+>>> a.foo(5)
+5
+>>> a.foo(-1) #doctest: +ELLIPSIS
+Traceback (most recent call last):
+    ...
+OverflowError: ...
+>>> a.foo(1 << 180) #doctest: +ELLIPSIS
+Traceback (most recent call last):
+    ...
+OverflowError: ...
 """
 
 def test(size_t i):
     return i
+
+cdef class A:
+    cdef public size_t a
+    cdef readonly size_t b
+    
+    def __init__(self, size_t a, object b):
+        self.a = a
+        self.b = b
+
+    cpdef size_t foo(self, size_t x):
+        cdef object o = x
+        return o
