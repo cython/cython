f5099da85f2b51f4202bfabd3931aef4ad19ae6f
Robert Bradshaw <robertwb@math.washington.edu>
2009-02-20 22:05:04 -0800

    Test for cmp nodes

---
+++ tests/run/cmp.pyx
@@ -0,0 +1,35 @@
+__doc__ = """
+    >>> single_py(1, 2)
+    True
+    >>> single_py(2, 1)
+    False
+    >>> cascaded_py(1, 2, 3)
+    True
+    >>> cascaded_py(1, 2, -1)
+    False
+    >>> cascaded_py(10, 2, 3)
+    False
+
+    >>> single_c(1, 2)
+    True
+    >>> single_c(2, 1)
+    False
+    >>> cascaded_c(1, 2, 3)
+    True
+    >>> cascaded_c(1, 2, -1)
+    False
+    >>> cascaded_c(10, 2, 3)
+    False
+"""
+
+def single_py(a, b):
+    return a < b
+
+def cascaded_py(a, b, c):
+    return a < b < c
+
+def single_c(int a, int b):
+    return a < b
+
+def cascaded_c(double a, double b, double c):
+    return a < b < c
