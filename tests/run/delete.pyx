30867450349aac47b97eaa3cfc3b4179c498f1a7
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-02-21 16:58:14 +0100

    new temps bug on indexing deletion

---
+++ tests/run/delete.pyx
@@ -0,0 +1,18 @@
+"""
+>>> a = A()
+>>> a.f()
+[2, 1]
+>>> a.g()
+(False, True)
+"""
+
+class A:
+    def f(self):
+        self.refs = [3,2,1]
+        del self.refs[0]
+        return self.refs
+
+    def g(self):
+        self.a = 3
+        del self.a
+        return (hasattr(self, "a"), hasattr(self, "g"))
