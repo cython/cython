894f48091f333c72794cbaec5ab1976936b17f6d
Stefan Behnel <scoder@users.berlios.de>
2009-03-15 09:02:40 +0100

    make code generation tracing an official debug feature, disable all debug features by default

--- Cython/Compiler/DebugFlags.py
+++ Cython/Compiler/DebugFlags.py
@@ -2,5 +2,9 @@ debug_disposal_code = 0
 debug_temp_alloc = 0
 debug_coercion = 0
 
-debug_temp_code_comments = 1
+# Write comments into the C code that show where temporary variables
+# are allocated and released
+debug_temp_code_comments = 0
 
+# Write a call trace of the code generation phase into the C code
+debug_trace_code_generation = 0
