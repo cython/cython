da61f3343e2f52e5908b4dcfd4769aaf504be9d9
Stefan Behnel <scoder@users.berlios.de>
2009-03-25 19:41:53 +0100

    extended error test cases

---
+++ tests/errors/break_outside_loop.pyx
@@ -0,0 +1,33 @@
+
+break
+
+class A:
+    break
+
+cdef class B:
+    break
+
+def test():
+    break
+
+try: break
+except: pass
+
+try: break
+finally: pass
+
+if True:
+    break
+else:
+    break
+
+
+_ERRORS = u'''
+ 2:0: break statement not inside loop
+ 5:4: break statement not inside loop
+ 8:4: break statement not inside loop
+11:4: break statement not inside loop
+16:5: break statement not inside loop
+20:4: break statement not inside loop
+22:4: break statement not inside loop
+'''
