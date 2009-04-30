7701939a9e4ba8aded54082a99eb20e207bc64cc
Stefan Behnel <scoder@users.berlios.de>
2009-04-30 18:23:32 +0200

    don't loose outer closure scope while we still need it

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -1069,6 +1069,8 @@ class FuncDefNode(StatNode, BlockNode):
                             outer_scope_cname,
                             env.scope_class.type.declaration_code(''),
                             Naming.self_cname))
+            code.put_incref(outer_scope_cname, py_object_type)
+            code.put_giveref(outer_scope_cname)
         # ----- Fetch arguments
         self.generate_argument_parsing_code(env, code)
         # If an argument is assigned to in the body, we must 
