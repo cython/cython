082d52d6241a809354198d4362a22b48e590ad2a
Stefan Behnel <scoder@users.berlios.de>
2009-04-30 18:42:30 +0200

    fix test

--- tests/run/cdef_setitem_T284.pyx
+++ tests/run/cdef_setitem_T284.pyx
@@ -1,13 +1,16 @@
 __doc__ = u'''
 >>> no_cdef()
 >>> with_cdef()
+
 >>> test_list(range(11), -2, None)
 [0, 1, 2, 3, 4, 5, 6, 7, 8, None, 10]
+
 >>> test_list(range(11), "invalid index", None)
 Traceback (most recent call last):
 ...
-TypeError: list indices must be integers
+TypeError: list indices must be integers, not str
 '''
+
 def no_cdef():
     lst = range(11)
     ob = 10L
