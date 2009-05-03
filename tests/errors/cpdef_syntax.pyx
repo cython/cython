3f829d6a28c749fe4ae1be7cd931fde5f5847f3c
Stefan Behnel <scoder@users.berlios.de>
2009-05-03 19:47:48 +0200

    removed various invalid syntax combinations for the c(p)def statement (#280)

---
+++ tests/errors/cpdef_syntax.pyx
@@ -0,0 +1,9 @@
+
+cpdef nogil: pass
+cpdef nogil class test: pass
+
+_ERRORS = u"""
+ 2: 6: cdef blocks cannot be declared cpdef
+ 3: 6: cdef blocks cannot be declared cpdef
+ 3:12: Expected ':'
+"""
