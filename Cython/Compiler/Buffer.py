4072b4dd8e04d9f2b61233e565838b2284003e3a
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-05-12 09:32:03 +0200

    Utility code code stream refactor

--- Cython/Compiler/Buffer.py
+++ Cython/Compiler/Buffer.py
@@ -413,7 +413,11 @@ def put_buffer_lookup_code(entry, index_signeds, index_cnames, directives, pos, 
             params.append(s.cname)
         
     # Make sure the utility code is available
-    code.globalstate.use_code_from(funcgen, name=funcname, nd=nd)
+    if funcname not in code.globalstate.utility_codes:
+        code.globalstate.utility_codes.add(funcname)
+        protocode = code.globalstate['utility_code_proto']
+        defcode = code.globalstate['utility_code_def']
+        funcgen(protocode, defcode, name=funcname, nd=nd)
 
     ptr_type = entry.type.buffer_ptr_type
     ptrcode = "%s(%s, %s.buf, %s)" % (funcname,
@@ -581,82 +585,76 @@ def mangle_dtype_name(dtype):
         return prefix + dtype.declaration_code("").replace(" ", "_")
 
 def get_type_information_cname(code, dtype, maxdepth=None):
-    # Output the __Pyx_TypeInfo type information for the given dtype if needed,
+    # Output the run-time type information (__Pyx_TypeInfo) for given dtype,
     # and return the name of the type info struct.
-    namesuffix = mangle_dtype_name(dtype)
-    name = "__Pyx_TypeInfo_%s" % namesuffix
-    structinfo_name = "__Pyx_StructFields_%s" % namesuffix
-
-    # It's critical that walking the type info doesn't use more stack
-    # depth than dtype.struct_nesting_depth() returns, so use an assertion for this
-    if maxdepth is None: maxdepth = dtype.struct_nesting_depth()
-    code.globalstate.use_code_from(type_information_code, name,
-                                   structinfo_name=structinfo_name,
-                                   dtype=dtype, maxdepth=maxdepth)
-    return name
-
-def type_information_code(proto, impl, name, structinfo_name, dtype, maxdepth):
-    # Output the run-time type information (__Pyx_TypeInfo) for given dtype.
-    # Use through get_type_information_cname
     #
     # Structs with two floats of the same size are encoded as complex numbers.
     # One can seperate between complex numbers declared as struct or with native
     # encoding by inspecting to see if the fields field of the type is
     # filled in.
+    namesuffix = mangle_dtype_name(dtype)
+    name = "__Pyx_TypeInfo_%s" % namesuffix
+    structinfo_name = "__Pyx_StructFields_%s" % namesuffix
 
-    if dtype.is_error: return
-    complex_possible = dtype.is_struct_or_union and dtype.can_be_complex()
+    if dtype.is_error: return "<error>"
 
-    code = proto.globalstate['typeinfo']
-
+    # It's critical that walking the type info doesn't use more stack
+    # depth than dtype.struct_nesting_depth() returns, so use an assertion for this
+    if maxdepth is None: maxdepth = dtype.struct_nesting_depth()
     if maxdepth <= 0:
         assert False
 
-    declcode = dtype.declaration_code("")
-    if dtype.is_simple_buffer_dtype():
-        structinfo_name = "NULL"
-    elif dtype.is_struct:
-        fields = dtype.scope.var_entries
-        # Must pre-call all used types in order not to recurse utility code
-        # writing.
-        assert len(fields) > 0
-        types = [get_type_information_cname(proto, f.type, maxdepth - 1)
-                 for f in fields]
-        code.putln("static __Pyx_StructField %s[] = {" % structinfo_name, safe=True)
-        for f, typeinfo in zip(fields, types):
-            code.putln('  {&%s, "%s", offsetof(%s, %s)},' %
-                       (typeinfo, f.name, dtype.declaration_code(""), f.cname), safe=True)
-        code.putln('  {NULL, NULL, 0}', safe=True)
-        code.putln("};", safe=True)
-    else:
-        assert False
-            
-    rep = str(dtype)
-    if dtype.is_int:
-        if dtype.signed == 0:
-            typegroup = 'U'
+    if name not in code.globalstate.utility_codes:
+        code.globalstate.utility_codes.add(name)
+        typecode = code.globalstate['typeinfo']
+        
+        complex_possible = dtype.is_struct_or_union and dtype.can_be_complex()
+        
+        declcode = dtype.declaration_code("")
+        if dtype.is_simple_buffer_dtype():
+            structinfo_name = "NULL"
+        elif dtype.is_struct:
+            fields = dtype.scope.var_entries
+            # Must pre-call all used types in order not to recurse utility code
+            # writing.
+            assert len(fields) > 0
+            types = [get_type_information_cname(proto, f.type, maxdepth - 1)
+                     for f in fields]
+            typecode.putln("static __Pyx_StructField %s[] = {" % structinfo_name, safe=True)
+            for f, typeinfo in zip(fields, types):
+                typecode.putln('  {&%s, "%s", offsetof(%s, %s)},' %
+                           (typeinfo, f.name, dtype.declaration_code(""), f.cname), safe=True)
+            typecode.putln('  {NULL, NULL, 0}', safe=True)
+            typecode.putln("};", safe=True)
         else:
-            typegroup = 'I'
-    elif complex_possible:
-        typegroup = 'C'
-    elif dtype.is_float:
-        typegroup = 'R'
-    elif dtype.is_struct:
-        typegroup = 'S'
-    elif dtype.is_pyobject:
-        typegroup = 'O'
-    else:
-        print dtype
-        assert False
+            assert False
+            
+        rep = str(dtype)
+        if dtype.is_int:
+            if dtype.signed == 0:
+                typegroup = 'U'
+            else:
+                typegroup = 'I'
+        elif complex_possible:
+            typegroup = 'C'
+        elif dtype.is_float:
+            typegroup = 'R'
+        elif dtype.is_struct:
+            typegroup = 'S'
+        elif dtype.is_pyobject:
+            typegroup = 'O'
+        else:
+            print dtype
+            assert False
 
-    code.putln(('static __Pyx_TypeInfo %s = { "%s", %s, sizeof(%s), \'%s\' };'
-                ) % (name,
-                     rep,
-                     structinfo_name,
-                     declcode,
-                     typegroup,
-                     ), safe=True)
-
+        typecode.putln(('static __Pyx_TypeInfo %s = { "%s", %s, sizeof(%s), \'%s\' };'
+                        ) % (name,
+                             rep,
+                             structinfo_name,
+                             declcode,
+                             typegroup,
+                        ), safe=True)
+    return name
 
 
 # Utility function to set the right exception
