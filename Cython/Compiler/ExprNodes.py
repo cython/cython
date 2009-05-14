1eb5949290c42fdb72532c8f812d03eb378515b5
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-05-14 15:25:49 +0200

    Disallow <double*>obj (#313). Also removes warning for <void*>obj.

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -3937,6 +3937,8 @@ class TypecastNode(NewTempExprNode):
         elif from_py and not to_py:
             if self.type.from_py_function:
                 self.operand = self.operand.coerce_to(self.type, env)
+            elif self.type.is_ptr and not self.type.base_type.is_void:
+                error(self.pos, "Python objects can only be cast to void*")
             else:
                 warning(self.pos, "No conversion from %s to %s, python object pointer used." % (self.type, self.operand.type))
         elif from_py and to_py:
