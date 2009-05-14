8a23adf39ecf5b221b355266ffe42e7c88ac5cbe
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-05-14 18:02:32 +0200

    Testcase fix, update error message

--- tests/errors/pyobjcastdisallow_T313.pyx
+++ tests/errors/pyobjcastdisallow_T313.pyx
@@ -5,5 +5,5 @@ cdef void* allowed = <void*>a
 cdef double* disallowed = <double*>a
 
 _ERRORS = u"""
-5:26: Python objects can only be cast to void*
+5:26: Python objects cannot be casted to pointers of primitive types
 """
