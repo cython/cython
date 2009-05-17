c3fc785a7da5ad6f0901a201bfbfffc74a0142ca
Lisandro Dalcin <dalcinl@gmail.com>
2009-05-16 23:07:14 -0300

    fix some tests for Python 3

--- tests/run/cdef_setitem_T284.pyx
+++ tests/run/cdef_setitem_T284.pyx
@@ -1,22 +1,22 @@
 __doc__ = u'''
 >>> no_cdef()
 >>> with_cdef()
->>> test_list(range(11), -2, None)
+>>> test_list(list(range(11)), -2, None)
 [0, 1, 2, 3, 4, 5, 6, 7, 8, None, 10]
->>> test_list(range(11), "invalid index", None)
+>>> test_list(list(range(11)), "invalid index", None) #doctest: +ELLIPSIS
 Traceback (most recent call last):
 ...
-TypeError: list indices must be integers
+TypeError: list indices must be integers...
 '''
 def no_cdef():
-    lst = range(11)
+    lst = list(range(11))
     ob = 10L
     lst[ob] = -10
     dd = {}
     dd[ob] = -10
 
 def with_cdef():
-    cdef list lst = range(11)
+    cdef list lst = list(range(11))
     ob = 10L
     lst[ob] = -10
     cdef dict dd = {}
--- tests/run/pyobjcast_T313.pyx
+++ tests/run/pyobjcast_T313.pyx
@@ -1,8 +1,11 @@
 # Ensure casting still works to void*
 
 """
->>> f()
-('teststring', 'teststring')
+>>> o = f()
+>>> print(o[0])
+teststring
+>>> print(o[1])
+teststring
 """
 
 cdef extern from *:
@@ -11,7 +14,7 @@ cdef extern from *:
 def f():
     cdef void* p1
     cdef PyObject* p2
-    a = "teststring"
+    a = u"teststring"
     p1 = <void*>a
     p2 = <PyObject*>a
     return (<object>p1, <object>p2)
