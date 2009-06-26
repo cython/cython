e00c481a20e53d00ad72fc504556e50a017f3ad1
Stefan Behnel <scoder@users.berlios.de>
2009-06-26 22:33:20 +0200

    test case for class decorators

---
+++ tests/run/classdecorators_T336.pyx
@@ -0,0 +1,44 @@
+__doc__ = u"""
+>>> print('\\n'.join(calls))
+Py-Honk PyTestClass
+PyTestClass
+Py-Hello PyTestClass
+PyTestClass
+Py-Done PyTestClass
+
+>>> c = PyTestClass()
+Ho, Ho, Ho!
+"""
+
+calls = []
+
+class print_msg(object):
+    def __init__(self, message):
+        self.msg = message
+    def __call__(self, c):
+        calls.append( self.msg + c.__name__ )
+        return c
+
+def print_name(c):
+    calls.append( c.__name__ )
+    return c
+
+@print_msg(u"Py-Done ")
+@print_name
+@print_msg(u"Py-Hello ")
+@print_name
+@print_msg(u"Py-Honk ")
+class PyTestClass(object):
+    def __init__(self):
+        print u"Ho, Ho, Ho!"
+
+# not currently working:
+#
+## @print_msg("Cy-Done ")
+## @print_name
+## @print_msg("Cy-Hello ")
+## @print_name
+## @print_msg("Cy-Honk ")
+## cdef class CyTestClass(object):
+##     def __init__(self):
+##         print u"Ho, Ho, Ho!"
