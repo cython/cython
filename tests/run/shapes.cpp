d8f4e673bfafa0d5660f75f893b2030b01aec2e2
Danilo Freitas <dsurviver@gmail.com>
2009-07-07 01:39:50 -0700

    Test files for cpp classes.

---
+++ tests/run/shapes.cpp
@@ -0,0 +1,15 @@
+#include "shapes.h"
+
+using namespace shapes;
+
+Rectangle::Rectangle(int width, int height)
+{
+    this->width = width;
+    this->height = height;
+}
+
+Square::Square(int side)
+{
+    this->side = side;
+}
+
