e4e5154c0e42ce5add3a9aeff8353a25c888990a
Kurt Smith <kwsmith1@wisc.edu>
2009-07-07 13:36:05 -0500

    cython.array implementation & tests.

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -850,7 +850,7 @@ class ModuleScope(Scope):
 
     def declare_c_class(self, name, pos, defining = 0, implementing = 0,
         module_name = None, base_type = None, objstruct_cname = None,
-        typeobj_cname = None, visibility = 'private', typedef_flag = 0, api = 0,
+        typeobj_cname = None, typeptr_cname = None, visibility = 'private', typedef_flag = 0, api = 0,
         buffer_defaults = None):
         #
         #  Look for previous declaration as a type
@@ -880,7 +880,10 @@ class ModuleScope(Scope):
                 type.module_name = module_name
             else:
                 type.module_name = self.qualified_name
-            type.typeptr_cname = self.mangle(Naming.typeptr_prefix, name)
+            if typeptr_cname:
+                type.typeptr_cname = typeptr_cname
+            else:
+                type.typeptr_cname = self.mangle(Naming.typeptr_prefix, name)
             entry = self.declare_type(name, type, pos, visibility = visibility,
                 defining = 0)
             entry.is_cclass = True
