e26d9aacfc467e3e78b42c7111296524f4c4071a
Robert Bradshaw <robertwb@math.washington.edu>
2009-07-14 00:09:58 -0700

    del item test

--- tests/run/delete.pyx
+++ tests/run/delete.pyx
@@ -4,6 +4,10 @@
 [2, 1]
 >>> a.g()
 (False, True)
+>>> del_item({1: 'a', 2: 'b'}, 1)
+{2: 'b'}
+>>> del_item(range(10), 2)
+[0, 1, 3, 4, 5, 6, 7, 8, 9]
 """
 
 class A:
@@ -16,3 +20,7 @@ class A:
         self.a = 3
         del self.a
         return (hasattr(self, u"a"), hasattr(self, u"g"))
+
+def del_item(L, o):
+    del L[o]
+    return L
