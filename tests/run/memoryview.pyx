1b749b6fc866ca1365516f524a7a0d2b9177823e
Kurt Smith <kwmsmith@gmail.com>
2009-07-26 21:52:13 -0500

    coercion and validation for memoryview wrappers.

--- tests/run/memoryview.pyx
+++ tests/run/memoryview.pyx
@@ -1,38 +1,41 @@
 u'''
 >>> f()
 >>> g()
+>>> call()
 '''
 
-# from cython.view cimport memoryview
+from cython.view cimport memoryview
 from cython cimport array, PyBUF_C_CONTIGUOUS
 
 def f():
-    pass
-    # cdef array arr = array(shape=(10,10), itemsize=sizeof(int), format='i')
-    # cdef memoryview mv = memoryview(arr, PyBUF_C_CONTIGUOUS)
+    cdef array arr = array(shape=(10,10), itemsize=sizeof(int), format='i')
+    cdef memoryview mv = memoryview(arr, PyBUF_C_CONTIGUOUS)
+
 def g():
     # cdef int[::1] mview = array((10,), itemsize=sizeof(int), format='i')
     cdef int[::1] mview = array((10,), itemsize=sizeof(int), format='i')
+    mview = array((10,), itemsize=sizeof(int), format='i')
 
 cdef class Foo:
-    cdef int[:] mview
+    cdef int[::1] mview
 
     def __init__(self):
+        self.mview = array((10,), itemsize=sizeof(int), format='i')
         self.mview = array((10,), itemsize=sizeof(int), format='i')
 
 class pyfoo:
 
     def __init__(self):
         self.mview = array((10,), itemsize=sizeof(long), format='l')
-        # self.mview = arr
 
 cdef cdg():
-    cdef double[:] dmv = array((10,), itemsize=sizeof(double), format='d')
+    cdef double[::1] dmv = array((10,), itemsize=sizeof(double), format='d')
+    dmv = array((10,), itemsize=sizeof(double), format='d')
 
-cdef float[:,:] global_mv = array((10,10), itemsize=sizeof(float), format='f')
+cdef float[:,::1] global_mv = array((10,10), itemsize=sizeof(float), format='f')
+global_mv = array((10,10), itemsize=sizeof(float), format='f')
 
 def call():
     cdg()
     f = Foo()
     pf = pyfoo()
-    
