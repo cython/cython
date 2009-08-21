c820c9aa7832b546aea7f0a1b709907d99983402
Stefan Behnel <scoder@users.berlios.de>
2009-08-21 14:33:25 +0200

    fix test that was broken after merge

--- tests/run/cdef_setitem_T284.pyx
+++ tests/run/cdef_setitem_T284.pyx
@@ -1,19 +1,9 @@
 __doc__ = u'''
 >>> no_cdef()
 >>> with_cdef()
-<<<<<<< local
-
->>> test_list(range(11), -2, None)
-=======
 >>> test_list(list(range(11)), -2, None)
->>>>>>> other
 [0, 1, 2, 3, 4, 5, 6, 7, 8, None, 10]
-<<<<<<< local
-
->>> test_list(range(11), "invalid index", None)
-=======
 >>> test_list(list(range(11)), "invalid index", None) #doctest: +ELLIPSIS
->>>>>>> other
 Traceback (most recent call last):
 ...
 TypeError: list indices must be integers...
