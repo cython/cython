5dedda20fb219be566f84904810813ddebfbe259
Stefan Behnel <scoder@users.berlios.de>
2009-09-11 20:15:16 +0200

    added test for ticket 248

---
+++ tests/compile/cimportfrom_T248.pyx
@@ -0,0 +1,16 @@
+from ewing8 cimport (Foo,
+
+
+                     Blarg)
+
+from declandimpl cimport (Sandwich
+
+
+                          ,
+
+
+
+
+                          Tomato)
+
+cdef extern Foo yummy
