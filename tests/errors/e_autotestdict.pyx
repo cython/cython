cb43e5e35b3250d0223ffb9f634eb728f80721d4
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2009-10-05 15:22:38 +0200

    Blatant ommision

--- tests/errors/e_autotestdict.pyx
+++ tests/errors/e_autotestdict.pyx
@@ -1,9 +1,9 @@
 cimport cython
 
-@cython.doctesthack(False)
+@cython.autotestdict(False)
 def foo():
     pass
 
 _ERRORS = u"""
-4:0: The doctesthack compiler directive is not allowed in function scope
+4:0: The autotestdict compiler directive is not allowed in function scope
 """
