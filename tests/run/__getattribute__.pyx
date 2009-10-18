3cb043e6218071dfc2d3770a1c54c3df55b94986
Stefan Behnel <scoder@users.berlios.de>
2009-10-18 14:52:43 +0200

    test case cleanup

--- tests/run/__getattribute__.pyx
+++ tests/run/__getattribute__.pyx
@@ -1,13 +1,24 @@
 __doc__ = u"""
 __getattribute__ and __getattr__ special methods for a single class.
+"""
 
+cdef class just_getattribute:
+    """
     >>> a = just_getattribute()
     >>> a.bar
     'bar'
     >>> a.invalid
     Traceback (most recent call last):
     AttributeError
+    """
+    def __getattribute__(self,n):
+        if n == 'bar':
+            return n
+        else:
+            raise AttributeError
 
+cdef class just_getattr:
+    """
     >>> a = just_getattr()
     >>> a.foo
     10
@@ -16,25 +27,7 @@ __getattribute__ and __getattr__ special methods for a single class.
     >>> a.invalid
     Traceback (most recent call last):
     AttributeError
-
-    >>> a = both()
-    >>> a.foo
-    10
-    >>> a.bar
-    'bar'
-    >>> a.invalid
-    Traceback (most recent call last):
-    AttributeError
-"""
-
-cdef class just_getattribute:
-    def __getattribute__(self,n):
-        if n == 'bar':
-            return n
-        else:
-            raise AttributeError
-
-cdef class just_getattr:
+    """
     cdef readonly int foo
     def __init__(self):
         self.foo = 10
@@ -45,6 +38,16 @@ cdef class just_getattr:
             raise AttributeError
 
 cdef class both:
+    """
+    >>> a = both()
+    >>> a.foo
+    10
+    >>> a.bar
+    'bar'
+    >>> a.invalid
+    Traceback (most recent call last):
+    AttributeError
+    """
     cdef readonly int foo
     def __init__(self):
         self.foo = 10
