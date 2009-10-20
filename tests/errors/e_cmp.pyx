196cb6dd69df542b7581d90a5dacba257fcf0664
Stefan Behnel <scoder@users.berlios.de>
2009-10-20 20:08:57 +0200

    extended test case

--- tests/errors/e_cmp.pyx
+++ tests/errors/e_cmp.pyx
@@ -2,9 +2,15 @@ cdef void foo():
 	cdef int bool, int1
 	cdef char *ptr2
 	cdef int *ptr3
+	cdef object i = 5
+
+	bool = i == ptr2  # evaluated in Python space
+	bool = ptr3 == i # error
 	bool = int1 == ptr2 # error
 	bool = ptr2 == ptr3 # error
+
 _ERRORS = u"""
-5:13: Invalid types for '==' (int, char *)
-6:13: Invalid types for '==' (char *, int *)
+ 8:13: Invalid types for '==' (int *, Python object)
+ 9:13: Invalid types for '==' (int, char *)
+10:13: Invalid types for '==' (char *, int *)
 """
