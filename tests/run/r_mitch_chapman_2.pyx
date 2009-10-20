e8aa2fe4bcc7a0dff37a4ed1d73c44d64d6b876d
Stefan Behnel <scoder@users.berlios.de>
2009-10-20 20:06:22 +0200

    test simplification

--- tests/run/r_mitch_chapman_2.pyx
+++ tests/run/r_mitch_chapman_2.pyx
@@ -1,15 +1,11 @@
 __doc__ = u"""
     >>> boolExpressionsFail()
-    u'Not 2b'
+    'Not 2b'
 """
-
-import sys
-if sys.version_info[0] >= 3:
-    __doc__ = __doc__.replace(u" u'", u" '")
 
 def boolExpressionsFail():
     dict = {1: 1}
     if not "2b" in dict:
-        return u"Not 2b"
+        return "Not 2b"
     else:
-        return u"2b?"
+        return "2b?"
