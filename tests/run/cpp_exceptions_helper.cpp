971566e50236cfed30b02667586a1d294e8660b1
Robert Bradshaw <robertwb@math.washington.edu>
2009-10-22 03:15:34 -0700

    Cpp exception tests.

---
+++ tests/run/cpp_exceptions_helper.cpp
@@ -0,0 +1,15 @@
+#include <stdexcept>
+
+int raise_int(int fire) {
+    if (fire) {
+        throw 1;
+    }
+    return 0;
+}
+
+int raise_index(int fire) {
+    if (fire) {
+        throw std::out_of_range("c++ error");
+    }
+    return 0;
+}
