4bfb5b39dfd8c9fff74fa4b7cbdcc311b9414c77
Lisandro Dalcin <dalcinl@gmail.com>
2009-10-23 16:41:24 -0200

    additional test for type casts (ticket #417)
    --HG--
    extra : rebase_source : 3512ea980463bae13f30b0d8bd395cabe253e65c

--- tests/run/typetest_T417.pyx
+++ tests/run/typetest_T417.pyx
@@ -53,3 +53,29 @@ def foo3(arg):
     TypeError: Cannot convert typetest_T417.Bar to typetest_T417.Foo
     """
     cdef val = <Foo?>arg
+
+
+cdef int count = 0
+
+cdef object getFoo():
+     global count
+     count += 1
+     return Foo()
+
+def test_getFoo():
+    """
+    >>> test_getFoo()
+    1
+    """
+    cdef int old_count = count
+    cdef Foo x = getFoo()
+    return count - old_count
+
+def test_getFooCast():
+    """
+    >>> test_getFooCast()
+    1
+    """
+    cdef int old_count = count
+    cdef Foo x = <Foo?>getFoo()
+    return count - old_count
