a68c7220150c7ebf477bba3ac7259732ef19766e
Stefan Behnel <scoder@users.berlios.de>
2009-10-25 09:17:08 +0100

    fix intern() test in Py3

--- tests/run/intern_T431.pyx
+++ tests/run/intern_T431.pyx
@@ -1,6 +1,15 @@
 __doc__ = u"""
 >>> s == s_interned
 True
+>>> s == s_interned_dynamic
+True
+>>> s == 'abc' == s_interned == s_interned_dynamic
+True
+"""
+
+import sys
+if sys.version_info[0] < 3:
+    __doc__ += u"""
 >>> intern(s) is s_interned
 True
 >>> intern('abc') is s_interned
