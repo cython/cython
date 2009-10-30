7138b1d2a06586d698c0972639f8fea37a62298a
Stefan Behnel <scoder@users.berlios.de>
2009-10-30 13:51:28 +0100

    Py3 test fix

--- tests/run/ishimoto2.pyx
+++ tests/run/ishimoto2.pyx
@@ -4,11 +4,11 @@ class C:
     >>> C().xxx(5)
     5
     >>> C().xxx()
-    u'a b'
+    'a b'
     >>> C().xxx(42)
     42
-    >>> C().xxx() == 'a b'
-    True
+    >>> C().xxx()
+    'a b'
     """
-    def xxx(self, p=u"a b"):
+    def xxx(self, p="a b"):
         return p
