c5443a6f8d5ac0e649385b96f8c742f13a42eec8
Stefan Behnel <scoder@users.berlios.de>
2009-12-06 14:52:52 +0100

    test fix: work around the fact that list comp variables now appear in locals()

--- tests/run/locals_expressions_T430.pyx
+++ tests/run/locals_expressions_T430.pyx
@@ -6,7 +6,7 @@ __doc__ = u"""
 [('args', (2, 3)), ('kwds', {'k': 5}), ('x', 1), ('y', 'hi'), ('z', 5)]
 
 >>> sorted(get_locals_items_listcomp(1,2,3, k=5))
-[('args', (2, 3)), ('kwds', {'k': 5}), ('x', 1), ('y', 'hi'), ('z', 5)]
+[('args', (2, 3)), ('item', None), ('kwds', {'k': 5}), ('x', 1), ('y', 'hi'), ('z', 5)]
 """
 
 def get_locals(x, *args, **kwds):
@@ -20,6 +20,7 @@ def get_locals_items(x, *args, **kwds):
     return locals().items()
 
 def get_locals_items_listcomp(x, *args, **kwds):
+    # FIXME: 'item' should *not* appear in locals() !
     cdef int z = 5
     y = "hi"
     return [ item for item in locals().items() ]
