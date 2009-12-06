99eac7ae52760e94dd2234c6a4e89dbd8af31420
Stefan Behnel <scoder@users.berlios.de>
2009-12-06 11:22:59 +0100

    support charptr.decode() with non-literal encodings and fix some temp usage issues; test case split
    --HG--
    rename : tests/run/carray_slicing.pyx => tests/run/charptr_decode.pyx

--- tests/run/carray_slicing.pyx
+++ tests/run/carray_slicing.pyx
@@ -13,61 +13,6 @@ def slice_charptr_end():
     """
     return cstring[:1], cstring[:3], cstring[:9]
 
-@cython.test_assert_path_exists("//PythonCapiCallNode")
-@cython.test_fail_if_path_exists("//AttributeNode")
-def slice_charptr_decode():
-    """
-    >>> print(str(slice_charptr_decode()).replace("u'", "'"))
-    ('a', 'abc', 'abcABCqtp')
-    """
-    return (cstring[:1].decode('UTF-8'),
-            cstring[:3].decode('UTF-8'),
-            cstring[:9].decode('UTF-8'))
-
-@cython.test_assert_path_exists("//PythonCapiCallNode")
-@cython.test_fail_if_path_exists("//AttributeNode")
-def slice_charptr_decode_slice2():
-    """
-    >>> print(str(slice_charptr_decode_slice2()).replace("u'", "'"))
-    ('a', 'bc', 'tp')
-    """
-    return (cstring[0:1].decode('UTF-8'),
-            cstring[1:3].decode('UTF-8'),
-            cstring[7:9].decode('UTF-8'))
-
-@cython.test_assert_path_exists("//PythonCapiCallNode")
-@cython.test_fail_if_path_exists("//AttributeNode")
-def slice_charptr_decode_strlen():
-    """
-    >>> print(str(slice_charptr_decode_strlen()).replace("u'", "'"))
-    ('abcABCqtp', 'bcABCqtp', '')
-    """
-    return (cstring.decode('UTF-8'),
-            cstring[1:].decode('UTF-8'),
-            cstring[9:].decode('UTF-8'))
-
-@cython.test_assert_path_exists("//PythonCapiCallNode")
-@cython.test_fail_if_path_exists("//AttributeNode")
-def slice_charptr_decode_unbound():
-    """
-    >>> print(str(slice_charptr_decode_unbound()).replace("u'", "'"))
-    ('a', 'abc', 'abcABCqtp')
-    """
-    return (bytes.decode(cstring[:1], 'UTF-8'),
-            bytes.decode(cstring[:3], 'UTF-8', 'replace'),
-            bytes.decode(cstring[:9], 'UTF-8'))
-
-@cython.test_assert_path_exists("//PythonCapiCallNode")
-@cython.test_fail_if_path_exists("//AttributeNode")
-def slice_charptr_decode_errormode():
-    """
-    >>> print(str(slice_charptr_decode_errormode()).replace("u'", "'"))
-    ('a', 'abc', 'abcABCqtp')
-    """
-    return (cstring[:1].decode('UTF-8', 'strict'),
-            cstring[:3].decode('UTF-8', 'replace'),
-            cstring[:9].decode('UTF-8', 'unicode_escape'))
-
 @cython.test_assert_path_exists("//ForFromStatNode",
                                 "//ForFromStatNode//SliceIndexNode")
 @cython.test_fail_if_path_exists("//ForInStatNode")
@@ -117,7 +62,7 @@ def slice_charptr_for_loop_c():
 @cython.test_fail_if_path_exists("//ForInStatNode")
 def slice_charptr_for_loop_c_dynamic_bounds():
     """
-    >>> slice_charptr_for_loop_c()
+    >>> slice_charptr_for_loop_c_dynamic_bounds()
     ['a', 'b', 'c']
     ['b', 'c', 'A', 'B']
     ['B', 'C', 'q', 't', 'p']
