4cdba9650c6734fd6d7ff7f3a6649361d7493b04
Robert Bradshaw <robertwb@math.washington.edu>
2010-01-14 17:54:55 -0800

    More operator overloading parsing/testing.

--- tests/run/cpp_operators_helper.h
+++ tests/run/cpp_operators_helper.h
@@ -1,6 +1,6 @@
-#define UN_OP(op) const char* operator op () { return "unary"#op; }
-#define POST_UN_OP(op) const char* operator op (int x) { return "post"#op; }
-#define BIN_OP(op) const char* operator op (int x) { return "binary"#op; }
+#define UN_OP(op) const char* operator op () { return "unary "#op; }
+#define POST_UN_OP(op) const char* operator op (int x) { return "post "#op; }
+#define BIN_OP(op) const char* operator op (int x) { return "binary "#op; }
 
 class TestOps {
 
@@ -23,5 +23,12 @@ public:
     BIN_OP(*);
     BIN_OP(/);
     BIN_OP(%);
+
+    BIN_OP(<<);
+    BIN_OP(>>);
+    
+    BIN_OP(|);
+    BIN_OP(&);
+    BIN_OP(^);
     
 };
