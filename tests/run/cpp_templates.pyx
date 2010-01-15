3946047641dc781e0ea5dc58fbcbf77a6745646e
Robert Bradshaw <robertwb@math.washington.edu>
2010-01-15 11:58:11 -0800

    Remove broken test from working one.

--- tests/run/cpp_templates.pyx
+++ tests/run/cpp_templates.pyx
@@ -62,19 +62,3 @@ def test_pair(int i, double x):
         del pair
 
 
-
-def test_wrap_pair(int i, double x):
-    """
-    >>> test_wrap_pair(1, 1.5)
-    (1, 1.5, True, False)
-    >>> test_wrap_pair(2, 2.25)
-    (2, 2.25, True, False)
-    """
-    cdef Pair[int, double] *pair
-    cdef Wrap[Pair[int, double]] *wrap
-    try:
-        pair = new Pair[int, double](i, x)
-        warp = new Wrap[Pair[int, double]](deref(pair))
-        return wrap.get().first(), wrap.get().second(), deref(wrap) == deref(wrap)
-    finally:
-        del pair, wrap
