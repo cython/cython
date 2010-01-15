b5d8ddf564331195ef9649bbd21b0fdfff5a5e79
Robert Bradshaw <robertwb@math.washington.edu>
2010-01-14 17:22:18 -0800

    Running C++ operator tests.

---
+++ tests/run/cpp_operators_helper.h
@@ -0,0 +1,27 @@
+#define UN_OP(op) const char* operator op () { return "unary"#op; }
+#define POST_UN_OP(op) const char* operator op (int x) { return "post"#op; }
+#define BIN_OP(op) const char* operator op (int x) { return "binary"#op; }
+
+class TestOps {
+
+public:
+
+    UN_OP(-);
+    UN_OP(+);
+    UN_OP(*);
+    UN_OP(~);
+    UN_OP(!);
+    UN_OP(&);
+    
+    UN_OP(++);
+    UN_OP(--);
+    POST_UN_OP(++);
+    POST_UN_OP(--);
+    
+    BIN_OP(+);
+    BIN_OP(-);
+    BIN_OP(*);
+    BIN_OP(/);
+    BIN_OP(%);
+    
+};
