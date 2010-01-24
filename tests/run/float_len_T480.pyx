28e6b608528a512c76bc442881991d439a71514e
Stefan Behnel <scoder@users.berlios.de>
2010-01-24 16:29:01 +0100

    fix #480: float() as a type cast for function return values

--- tests/run/float_len_T480.pyx
+++ tests/run/float_len_T480.pyx
@@ -1,9 +1,23 @@
 def f(x):
     return x
 
-def float_len(x):
+def len_f(x):
     """
-    >>> float_len([1,2,3])
+    >>> len_f([1,2,3])
+    3
+    """
+    return len(f(x))
+
+def float_len_f(x):
+    """
+    >>> float_len_f([1,2,3])
     3.0
     """
-    float(len(f(x)))
+    return float(len(f(x)))
+
+def cast_len_f(x):
+    """
+    >>> cast_len_f([1,2,3])
+    3.0
+    """
+    return <double>len(f(x))
