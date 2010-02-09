bf320361c75f31364f2364f073cc2e03882df36b
Stefan Behnel <scoder@users.berlios.de>
2010-02-09 10:06:03 +0100

    new test directory for test modules that wrap C/C++ source files naming convention in test runner to search for related source files in the test directory tests for wrapping C++ code

---
+++ tests/wrappers/cpp_overload_wrapper_lib.cpp
@@ -0,0 +1,55 @@
+
+#include "cppwrap_lib.h"
+
+void voidfunc (void)
+{
+}
+
+double doublefunc (double a, double b, double c)
+{
+    return a + b + c;
+}
+
+
+DoubleKeeper::DoubleKeeper ()
+    : number (1.0) 
+{
+}
+
+DoubleKeeper::DoubleKeeper (double factor)
+    : number (factor)
+{
+}
+
+DoubleKeeper::~DoubleKeeper ()
+{
+}
+
+double DoubleKeeper::get_number () const
+{
+    return number;
+}
+
+void DoubleKeeper::set_number (double f)
+{
+    number = f;
+}
+
+void DoubleKeeper::set_number ()
+{
+    number = 1.0;
+}
+
+double
+DoubleKeeper::transmogrify (double value) const
+{
+    return value*number;
+}
+
+
+double
+transmogrify_from_cpp (DoubleKeeper const *obj, double value)
+{
+    return obj->transmogrify (value);
+}
+
