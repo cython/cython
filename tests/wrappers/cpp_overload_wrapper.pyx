8a387300807691829bb0f9c2be5b49da93ff8ddf
Stefan Behnel <scoder@users.berlios.de>
2010-02-21 14:27:30 +0100

    fix wrapper tests, now that the compiler is fixed

--- tests/wrappers/cpp_overload_wrapper.pyx
+++ tests/wrappers/cpp_overload_wrapper.pyx
@@ -1,7 +1,18 @@
 
-cimport cppwrap_lib
+cimport cpp_overload_wrapper_lib as cppwrap_lib
 
 cdef class DoubleKeeper:
+    """
+    >>> d = DoubleKeeper()
+    >>> d.get_number()
+    1.0
+    >>> d.set_number(5.5)
+    >>> d.get_number()
+    5.5
+    >>> d.set_number(0)
+    >>> d.get_number()
+    0.0
+    """
     cdef cppwrap_lib.DoubleKeeper* keeper
 
     def __cinit__(self, number=None):
@@ -23,14 +34,33 @@ cdef class DoubleKeeper:
         return self.keeper.get_number()
 
     def transmogrify(self, double value):
+        """
+        >>> d = DoubleKeeper(5.5)
+        >>> d.transmogrify(1.0)
+        5.5
+        >>> d.transmogrify(2.0)
+        11.0
+        """
         return self.keeper.transmogrify(value)
 
 
 def voidfunc():
+    """
+    >>> voidfunc()
+    """
     cppwrap_lib.voidfunc()
 
 def doublefunc(double x, double y, double z):
+    """
+    >>> doublefunc(1.0, 2.0, 3.0) == 1.0 + 2.0 + 3.0
+    True
+    """
     return cppwrap_lib.doublefunc(x, y, z)
 
 def transmogrify_from_cpp(DoubleKeeper obj not None, double value):
+    """
+    >>> d = DoubleKeeper(2.0)
+    >>> d.transmogrify(3.0) == 6.0
+    True
+    """
     return cppwrap_lib.transmogrify_from_cpp(obj.keeper, value)
