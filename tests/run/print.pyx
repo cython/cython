ae4e138b1a7adb43c9096e6fbe48ca753df0cbb9
Stefan Behnel <scoder@users.berlios.de>
2010-03-09 09:29:47 +0100

    implemented 'print >> stream'

--- tests/run/print.pyx
+++ tests/run/print.pyx
@@ -14,3 +14,29 @@ def f(a, b):
     print a, b
     print a, b,
     print 42, u"spam"
+
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def s(stream, a, b):
+    """
+    >>> stream = StringIO()
+    >>> s(stream, 1, 'test')
+    >>> print(stream.getvalue())
+    <BLANKLINE>
+    1
+    1 test
+    1 test
+    1 test 42 spam
+    <BLANKLINE>
+    """
+    print >> stream
+    print >> stream, a
+    print >> stream, a,
+    print >> stream, b
+    print >> stream, a, b
+    print >> stream, a, b,
+    print >> stream, 42, u"spam"
