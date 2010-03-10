9d72edd963a81b3e838cf1a05880b302e1959525
Craig Citro <craigcitro@gmail.com>
2010-03-09 16:51:51 -0800

    Fix for autotestdict & property not playing well together.

--- tests/run/extpropertyref.pyx
+++ tests/run/extpropertyref.pyx
@@ -3,12 +3,17 @@ cdef class Spam:
     property eggs:
     
         def __get__(self):
+            """
+            This is the docstring for Spam.eggs.__get__
+            """
             return 42
 
 def tomato():
     """
     >>> tomato()
     42
+    >>> sorted(__test__.keys())
+    [u'Spam.eggs.__get__ (line 5)', u'tomato (line 11)']
     """
     cdef Spam spam
     cdef object lettuce
