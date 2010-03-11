377b71177c09d04079ad1f13098189b352c33105
Stefan Behnel <scoder@users.berlios.de>
2010-03-11 09:59:05 +0100

    py2.3 build fix

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.sysconfig import get_python_lib
 import os, os.path
 import sys
 
-if 'sdist' in sys.argv and sys.platform != "win32":
+if 'sdist' in sys.argv and sys.platform != "win32" and sys.version_info >= (2,4):
     # Record the current revision in .hgrev
-    import subprocess # os.popen is cleaner but depricated
+    import subprocess # os.popen is cleaner but deprecated
     changset = subprocess.Popen("hg log --rev tip | grep changeset", 
                                 shell=True,
                                 stdout=subprocess.PIPE).stdout.read()
