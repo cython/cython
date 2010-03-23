036bfbba4f4c1bcaff9e875b63a27f77006add67
Stefan Behnel <scoder@users.berlios.de>
2010-03-23 20:12:47 +0100

    better test case

--- tests/run/iterdict.pyx
+++ tests/run/iterdict.pyx
@@ -1,6 +1,11 @@
+
+cimport cython
+
 dict_size = 4
 d = dict(zip(range(10,dict_size+10), range(dict_size)))
 
+@cython.test_fail_if_path_exists(
+    "//WhileStatNode")
 def items(dict d):
     """
     >>> items(d)
@@ -12,6 +17,10 @@ def items(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iteritems(dict d):
     """
     >>> iteritems(d)
@@ -23,6 +32,10 @@ def iteritems(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iteritems_int(dict d):
     """
     >>> iteritems_int(d)
@@ -35,6 +48,10 @@ def iteritems_int(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iteritems_tuple(dict d):
     """
     >>> iteritems_tuple(d)
@@ -46,11 +63,19 @@ def iteritems_tuple(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iteritems_listcomp(dict d):
     cdef list l = [(k,v) for k,v in d.iteritems()]
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterkeys(dict d):
     """
     >>> iterkeys(d)
@@ -62,6 +87,10 @@ def iterkeys(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterkeys_int(dict d):
     """
     >>> iterkeys_int(d)
@@ -74,6 +103,10 @@ def iterkeys_int(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterdict(dict d):
     """
     >>> iterdict(d)
@@ -85,6 +118,10 @@ def iterdict(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterdict_int(dict d):
     """
     >>> iterdict_int(d)
@@ -97,6 +134,10 @@ def iterdict_int(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterdict_reassign(dict d):
     """
     >>> iterdict_reassign(d)
@@ -110,11 +151,23 @@ def iterdict_reassign(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def iterdict_listcomp(dict d):
+    """
+    >>> iterdict_listcomp(d)
+    [10, 11, 12, 13]
+    """
     cdef list l = [k for k in d]
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def itervalues(dict d):
     """
     >>> itervalues(d)
@@ -126,6 +179,10 @@ def itervalues(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def itervalues_int(dict d):
     """
     >>> itervalues_int(d)
@@ -138,7 +195,28 @@ def itervalues_int(dict d):
     l.sort()
     return l
 
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
 def itervalues_listcomp(dict d):
+    """
+    >>> itervalues_listcomp(d)
+    [0, 1, 2, 3]
+    """
+    cdef list l = [v for v in d.itervalues()]
+    l.sort()
+    return l
+
+@cython.test_assert_path_exists(
+    "//WhileStatNode",
+    "//WhileStatNode/SimpleCallNode",
+    "//WhileStatNode/SimpleCallNode/NameNode")
+def itervalues_kwargs(**d):
+    """
+    >>> itervalues_kwargs(a=1, b=2, c=3, d=4)
+    [1, 2, 3, 4]
+    """
     cdef list l = [v for v in d.itervalues()]
     l.sort()
     return l
