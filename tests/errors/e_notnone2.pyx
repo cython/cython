f76dcd95415c4e4b580213dc592f2d978c587547
Stefan Behnel <scoder@users.berlios.de>
2010-04-15 11:43:57 +0200

    support 'not None' and 'or None' on any Python argument

--- tests/errors/e_notnone2.pyx
+++ tests/errors/e_notnone2.pyx
@@ -1,6 +1,6 @@
-def eggs(int x not None, y not None):
+def eggs(int x not None, char* y not None):
 	pass
 _ERRORS = u"""
-1:0: Only extension type arguments can have 'not None'
-1:0: Only extension type arguments can have 'not None'
+1:0: Only Python type arguments can have 'not None'
+1:0: Only Python type arguments can have 'not None'
 """
