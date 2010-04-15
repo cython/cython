f674dd6a0788e0038f07d1246c8e8bbb925d0622
Lisandro Dalcin <dalcinl@gmail.com>
2010-04-14 21:15:42 -0300

    do not emit C code for unused special method docstrings

--- tests/compile/specmethdocstring.pyx
+++ tests/compile/specmethdocstring.pyx
@@ -1,8 +1,30 @@
 cdef class C:
+    def __cinit__(self):
+        "This is an unusable docstring."
     def __init__(self):
         "This is an unusable docstring."
+    def __dealloc__(self):
+        "This is an unusable docstring."
+    def __richcmp__(self, other, int op):
+        "This is an unusable docstring."
+    def __nonzero__(self):
+        "This is an unusable docstring."
+        return False
+    def __contains__(self, other):
+        "This is an unusable docstring."
+
     property foo:
         def __get__(self):
             "So is this."
         def __set__(self, x):
             "And here is another one."
+
+    def __add__(self, other):
+        "usable docstring"
+    def __iter__(self):
+        "usable docstring"
+        return False
+    def __next__(self):
+        "usable docstring"
+        return False
+
