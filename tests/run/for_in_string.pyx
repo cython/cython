0108dc3c25ca82af518aab81a3e50f7cbcd36f3d
Stefan Behnel <scoder@users.berlios.de>
2010-04-20 08:25:35 +0200

    extended test case

--- tests/run/for_in_string.pyx
+++ tests/run/for_in_string.pyx
@@ -1,3 +1,4 @@
+cimport cython
 
 bytes_abc = b'abc'
 bytes_ABC = b'ABC'
@@ -20,6 +21,8 @@ def for_in_bytes(bytes s):
     else:
         return 'X'
 
+@cython.test_assert_path_exists("//ForFromStatNode")
+@cython.test_fail_if_path_exists("//ForInStatNode")
 def for_char_in_bytes(bytes s):
     """
     >>> for_char_in_bytes(bytes_abc)
@@ -34,6 +37,25 @@ def for_char_in_bytes(bytes s):
     else:
         return 'X'
 
+@cython.test_assert_path_exists("//ForFromStatNode")
+@cython.test_fail_if_path_exists("//ForInStatNode")
+def for_char_in_enumerate_bytes(bytes s):
+    """
+    >>> for_char_in_enumerate_bytes(bytes_abc)
+    'X'
+    >>> for_char_in_enumerate_bytes(bytes_ABC)
+    2
+    """
+    cdef char c
+    cdef Py_ssize_t i
+    for i, c in enumerate(s):
+        if c == b'C':
+            return i
+    else:
+        return 'X'
+
+@cython.test_assert_path_exists("//ForFromStatNode")
+@cython.test_fail_if_path_exists("//ForInStatNode")
 def for_pyunicode_in_unicode(unicode s):
     """
     >>> for_pyunicode_in_unicode(unicode_abc)
@@ -45,5 +67,22 @@ def for_pyunicode_in_unicode(unicode s):
     for c in s:
         if c == u'C':
             return 'C'
+    else:
+        return 'X'
+
+@cython.test_assert_path_exists("//ForFromStatNode")
+@cython.test_fail_if_path_exists("//ForInStatNode")
+def for_pyunicode_in_enumerate_unicode(unicode s):
+    """
+    >>> for_pyunicode_in_enumerate_unicode(unicode_abc)
+    'X'
+    >>> for_pyunicode_in_enumerate_unicode(unicode_ABC)
+    2
+    """
+    cdef Py_UNICODE c
+    cdef Py_ssize_t i
+    for i, c in enumerate(s):
+        if c == u'C':
+            return i
     else:
         return 'X'
