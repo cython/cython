e11eefc9c8f003d6678be743968a6ffa342273ac
dalcinl <none@none>
2010-05-02 21:43:09 -0300

    Disable test for libc.stdint (missing stdint.h in MSVC)

--- tests/compile/libc_all.pyx
+++ tests/compile/libc_all.pyx
@@ -7,7 +7,7 @@ cimport libc.limits
 cimport libc.locale
 cimport libc.signal
 cimport libc.stddef
-cimport libc.stdint
+#cimport libc.stdint # XXX MSVC
 cimport libc.stdio
 cimport libc.stdlib
 cimport libc.string
@@ -18,7 +18,7 @@ from libc cimport limits
 from libc cimport locale
 from libc cimport signal
 from libc cimport stddef
-from libc cimport stdint
+#from libc cimport stdint # XXX MSVC
 from libc cimport stdio
 from libc cimport stdlib
 from libc cimport string
@@ -29,7 +29,7 @@ from libc.limits cimport *
 from libc.locale cimport *
 from libc.signal cimport *
 from libc.stddef cimport *
-from libc.stdint cimport *
+#from libc.stdint cimport * # XXX MSVC
 from libc.stdio  cimport *
 from libc.stdlib cimport *
 from libc.string cimport *
