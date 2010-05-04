555a5640cf45addaaec2cab7b0e03bf9edd5daee
Stefan Behnel <scoder@users.berlios.de>
2010-05-04 09:52:05 +0200

    new test for ticket #354

---
+++ tests/run/bint_property_T354.pyx
@@ -0,0 +1,15 @@
+
+cdef class Test:
+    """
+    >>> t = Test(True)
+    >>> t.some_ro_bool
+    True
+    >>> t.some_public_bool
+    True
+    """
+    cdef public bint some_public_bool
+    cdef readonly bint some_ro_bool
+
+    def __init__(self, bint boolval):
+        self.some_ro_bool = boolval
+        self.some_public_bool = boolval
