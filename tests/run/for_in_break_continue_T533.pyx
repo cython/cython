a64268fb614f393df2c8ac8683f5ddfe47fe0e3a
Stefan Behnel <scoder@users.berlios.de>
2010-05-09 13:14:09 +0200

    fix ticket #533: 'continue' in the 'else' block of a for-in-loop leaks a reference

--- tests/run/for_in_break_continue_T533.pyx
+++ tests/run/for_in_break_continue_T533.pyx
@@ -2,6 +2,9 @@
 def for_in():
     """
     >>> for_in()
+    CONTINUE -1
+    CONTINUE 4
+    BREAK 6
     6
     """
     i = -1
@@ -10,6 +13,28 @@ def for_in():
             if i > 5:
                 break
         else:
+            print "CONTINUE", i
             continue
+        print "BREAK", i
+        break
+    return i
+
+def for_from():
+    """
+    >>> for_from()
+    CONTINUE 0
+    CONTINUE 5
+    BREAK 6
+    6
+    """
+    i = -1
+    for L in [[], range(5), range(10)]:
+        for i from 0 <= i < len(L):
+            if i > 5:
+                break
+        else:
+            print "CONTINUE", i
+            continue
+        print "BREAK", i
         break
     return i
