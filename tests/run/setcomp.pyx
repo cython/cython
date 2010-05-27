b2b9705f88809977e773ca565ff2f6e87a4d88bd
Stefan Behnel <scoder@users.berlios.de>
2010-05-27 15:34:15 +0200

    clean up comprehensions to bring them closer to generator expressions, make their scoping behaviour configurable remove optimisations for set([...]) and dict([...]) as they do not take side-effects into account: unhashable items lead to pre-mature exit from the loop instead, transform set(genexp), list(genexp) and dict(genexp) into inlined comprehensions that do not leak loop variables

--- tests/run/setcomp.pyx
+++ tests/run/setcomp.pyx
@@ -1,39 +1,38 @@
-__doc__ = u"""
->>> type(smoketest_set()) is not list
-True
->>> type(smoketest_set()) is _set
-True
->>> type(smoketest_list()) is _set
-True
-
->>> sorted(smoketest_set())
-[0, 4, 8]
->>> sorted(smoketest_list())
-[0, 4, 8]
-
->>> list(typed())
-[A, A, A]
->>> sorted(iterdict())
-[1, 2, 3]
-"""
 
 cimport cython
 
 # Py2.3 doesn't have the set type, but Cython does :)
 _set = set
 
-def smoketest_set():
+def setcomp():
+    """
+    >>> type(setcomp()) is not list
+    True
+    >>> type(setcomp()) is _set
+    True
+    >>> sorted(setcomp())
+    [0, 4, 8]
+    """
     return { x*2
              for x in range(5)
              if x % 2 == 0 }
 
-@cython.test_fail_if_path_exists("//SimpleCallNode//ComprehensionNode")
-@cython.test_assert_path_exists("//ComprehensionNode",
-                                "//ComprehensionNode//ComprehensionAppendNode")
-def smoketest_list():
-    return set([ x*2
+@cython.test_fail_if_path_exists(
+    "//GeneratorExpressionNode",
+    "//SimpleCallNode")
+@cython.test_assert_path_exists(
+    "//ComprehensionNode",
+    "//ComprehensionNode//ComprehensionAppendNode")
+def genexp_set():
+    """
+    >>> type(genexp_set()) is _set
+    True
+    >>> sorted(genexp_set())
+    [0, 4, 8]
+    """
+    return set( x*2
                  for x in range(5)
-                 if x % 2 == 0 ])
+                 if x % 2 == 0 )
 
 cdef class A:
     def __repr__(self): return u"A"
@@ -41,10 +40,18 @@ cdef class A:
     def __hash__(self): return id(self) % 65536
 
 def typed():
+    """
+    >>> list(typed())
+    [A, A, A]
+    """
     cdef A obj
     return {obj for obj in {A(), A(), A()}}
 
 def iterdict():
+    """
+    >>> sorted(iterdict())
+    [1, 2, 3]
+    """
     cdef dict d = dict(a=1,b=2,c=3)
     return {d[key] for key in d}
 
