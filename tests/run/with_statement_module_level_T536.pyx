386acf74211610057f727431a1971bcd2f3cbf62
Stefan Behnel <scoder@users.berlios.de>
2010-06-01 08:55:30 +0200

    fix test case

--- tests/run/with_statement_module_level_T536.pyx
+++ tests/run/with_statement_module_level_T536.pyx
@@ -3,7 +3,7 @@ __doc__ = """
 >>> inner_result
 ['ENTER']
 >>> result
-EXIT [None, None, None]
+['ENTER', 'EXIT (None, None, None)']
 """
 
 result = []
@@ -12,7 +12,7 @@ class ContextManager(object):
     def __enter__(self):
         result.append("ENTER")
     def __exit__(self, *values):
-        result.append("EXIT [%s]" % values)
+        result.append("EXIT %r" % (values,))
 
 with ContextManager() as c:
     inner_result = result[:]
