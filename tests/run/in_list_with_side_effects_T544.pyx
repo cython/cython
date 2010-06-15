18fecf66876250640d7415408f9130a537ee9294
Stefan Behnel <scoder@users.berlios.de>
2010-06-15 21:55:52 +0200

    fix bug #544: handle side-effects in flattened in-list tests correctly

---
+++ tests/run/in_list_with_side_effects_T544.pyx
@@ -0,0 +1,25 @@
+
+def count(i=[0]):
+    i[0] += 1
+    return i[0]
+
+def test(x):
+    """
+    >>> def py_count(i=[0]):
+    ...     i[0] += 1
+    ...     return i[0]
+    >>> 1 in (py_count(), py_count(), py_count(), py_count())
+    True
+    >>> 4 in (py_count(), py_count(), py_count(), py_count())
+    False
+    >>> 12 in (py_count(), py_count(), py_count(), py_count())
+    True
+
+    >>> test(1)
+    True
+    >>> test(4)
+    False
+    >>> test(12)
+    True
+    """
+    return x in (count(), count(), count(), count())
