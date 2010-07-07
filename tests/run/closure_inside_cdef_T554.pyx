0ce99d24a95e31e178c8f56c674673ebbf27b769
Robert Bradshaw <robertwb@math.washington.edu>
2010-07-07 08:35:35 -0700

    Test for #554, closures inside cdef block.

---
+++ tests/run/closure_inside_cdef_T554.pyx
@@ -0,0 +1,10 @@
+def call_f(x):
+    """
+    >>> call_f(2)
+    4
+    """
+    return f(x)
+
+cdef f(x):                # def  here => works fine
+   def g(y): return y*x  # cdef here => compile error
+   return g(x)           # faults@ INCREF(.*cur_scope->.*v_x
