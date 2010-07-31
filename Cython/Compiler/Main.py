898f06741736b1258b107db3f3c2f290051140d3
Robert Bradshaw <robertwb@math.washington.edu>
2010-07-31 14:10:52 -0700

    Disable FlattenInListTransform

--- Cython/Compiler/Main.py
+++ Cython/Compiler/Main.py
@@ -138,7 +138,7 @@ class Context(object):
             _align_function_definitions,
             MarkClosureVisitor(self),
             ConstantFolding(),
-            FlattenInListTransform(),
+#            FlattenInListTransform(),
             WithTransform(self),
             DecoratorTransform(self),
             AnalyseDeclarationsTransform(self),
