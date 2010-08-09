680b31f67bdc47c921bc84854f2e21b4d844931e
Stefan Behnel <scoder@users.berlios.de>
2010-08-09 20:28:24 +0200

    re-enable tree assertions in tests

--- runtests.py
+++ runtests.py
@@ -329,8 +329,7 @@ class CythonCompileTestCase(unittest.TestCase):
             use_listing_file = False,
             cplus = self.language == 'cpp',
             generate_pxi = False,
-#            evaluate_tree_assertions = True,
-            evaluate_tree_assertions = False,
+            evaluate_tree_assertions = True,
             )
         cython_compile(source, options=options,
                        full_module_name=module)
