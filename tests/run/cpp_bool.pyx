758080381b6148f70bca9c674c002e0d1312e4a5
Robert Bradshaw <robertwb@math.washington.edu>
2010-08-11 12:53:25 -0700

    cpp_bool test (failing)

---
+++ tests/run/cpp_bool.pyx
@@ -0,0 +1,18 @@
+from libcpp cimport bool
+
+def test_bool(bool a):
+    """
+    >>> test_bool(True)
+    True
+    >>> test_bool(1)
+    True
+    >>> test_bool(0)
+    False
+    >>> test_bool(100)
+    True
+    >>> test_bool(None)
+    False
+    >>> test_bool([])
+    False
+    """
+    return a
