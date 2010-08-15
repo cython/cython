87825a27453dc5ab3c88a3f6d8651b307cdf3182
Robert Bradshaw <robertwb@math.washington.edu>
2010-08-15 00:15:20 -0700

    Special method docstring tests.

---
+++ tests/run/special_method_docstrings.pyx
@@ -0,0 +1,39 @@
+cdef class A:
+    """
+    >>> A.__init__.__doc__
+    'A.__init__ docstring'
+    >>> A.__len__.__doc__
+    'A.__len__ docstring'
+    >>> A.__add__.__doc__
+    'A.__add__ docstring'
+    """
+    def __init__(self):
+        "A.__init__ docstring"
+    def __len__(self):
+        "A.__len__ docstring"
+    def __add__(self, other):
+        "A.__add__ docstring"
+
+cdef class B(A):
+    """
+    >>> B.__init__.__doc__
+    'A.__init__ docstring'
+    >>> B.__len__.__doc__
+    'B.__len__ docstring'
+    >>> B.__add__.__doc__
+    'A.__add__ docstring'
+    """
+    def __len__(self):
+        "B.__len__ docstring"
+
+class C(A):
+    """
+    >>> C.__init__.__doc__
+    'A.__init__ docstring'
+    >>> C.__len__.__doc__
+    'C.__len__ docstring'
+    >>> C.__add__.__doc__
+    'A.__add__ docstring'
+    """
+    def __len__(self):
+        "C.__len__ docstring"
