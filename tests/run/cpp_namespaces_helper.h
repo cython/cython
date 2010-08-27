5edc00e7feed5fe5ba5764901a55ef9bb5f7cb9e
Robert Bradshaw <robertwb@math.washington.edu>
2010-08-27 00:43:25 -0700

    C++ namespace tests.

---
+++ tests/run/cpp_namespaces_helper.h
@@ -0,0 +1,25 @@
+namespace outer {
+    
+    int x = 10;
+    
+    int outer_value = 10;
+    
+    namespace inner {
+        
+        int x = 100;
+        
+        int inner_value = 100;
+        
+    }
+    
+}
+
+namespace A {
+    
+    typedef int A_t;
+    
+    A_t A_func(A_t first, A_t second) {
+        return first + second;
+    }
+    
+}
