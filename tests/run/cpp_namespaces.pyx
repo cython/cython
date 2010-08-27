1fda8456e0927327835759c8361d152970a36c86
Robert Bradshaw <robertwb@math.washington.edu>
2010-08-27 00:43:25 -0700

    C++ namespace tests.

---
+++ tests/run/cpp_namespaces.pyx
@@ -0,0 +1,28 @@
+cdef extern from "cpp_namespaces_helper.h" namespace "A":
+    ctypedef int A_t
+    A_t A_func(A_t first, A_t)
+
+cdef extern from "cpp_namespaces_helper.h" namespace "outer":
+    int outer_value
+
+cdef extern from "cpp_namespaces_helper.h" namespace "outer::inner":
+    int inner_value
+
+def test_function(x, y):
+    """
+    >>> test_function(1, 2)
+    3
+    >>> test_function(9, 16)
+    25
+    """
+    return A_func(x, y)
+
+def test_nested():
+    """
+    >>> test_nested()
+    10
+    100
+    """
+    print outer_value
+    print inner_value
+
