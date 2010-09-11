7559edf1a14ff70fd0a79116352315b1bfba6ba8
Robert Bradshaw <robertwb@math.washington.edu>
2010-09-11 14:48:32 -0700

    Some error tests for c array iteration.

--- tests/errors/e_slice.pyx
+++ tests/errors/e_slice.pyx
@@ -1,10 +1,24 @@
 def f(obj2):
-	cdef int *ptr1
-	obj1 = obj2[ptr1::] # error
-	obj1 = obj2[:ptr1:] # error
-	obj1 = obj2[::ptr1] # error
+    cdef int *ptr1
+    obj1 = obj2[ptr1::] # error
+    obj1 = obj2[:ptr1:] # error
+    obj1 = obj2[::ptr1] # error
+    
+cdef int a
+cdef int* int_ptr
+
+for a in int_ptr:
+    pass
+for a in int_ptr[2:]:
+    pass
+for a in int_ptr[2:2:a]:
+    pass
+    
 _ERRORS = u"""
-3:17: Cannot convert 'int *' to Python object
-4:18: Cannot convert 'int *' to Python object
-5:19: Cannot convert 'int *' to Python object
+3:20: Cannot convert 'int *' to Python object
+4:21: Cannot convert 'int *' to Python object
+5:22: Cannot convert 'int *' to Python object
+10:16: C array iteration requires known end index
+12:16: C array iteration requires known end index
+14:22: C array iteration requires known step size and end index
 """
