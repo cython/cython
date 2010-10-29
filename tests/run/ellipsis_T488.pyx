305e7ba8b5fb93c80f31937652b620c726d869d8
Stefan Behnel <scoder@users.berlios.de>
2010-10-29 17:17:39 +0200

    Index: Cython/Compiler/Parsing.py

---
+++ tests/run/ellipsis_T488.pyx
@@ -0,0 +1,20 @@
+"""
+>>> test()
+"""
+def test():
+    x = ...
+    assert x is Ellipsis
+
+    d = {}
+    d[...] = 1
+    assert d[...] == 1
+    del d[...]
+    assert ... not in d
+
+    d[..., ...] = 1
+    assert d[..., ...] == 1
+    assert d[..., Ellipsis] == 1
+    assert (Ellipsis, Ellipsis) in d
+    del d[..., ...]
+    assert (Ellipsis, Ellipsis) not in d
+
