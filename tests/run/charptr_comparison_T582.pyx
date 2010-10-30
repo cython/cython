ff27eac9af38bbaca3273c5a8902c3f5463991a0
Stefan Behnel <scoder@users.berlios.de>
2010-10-30 22:19:11 +0200

    make bytes the common type of char* and bytes literal in comparisons (ticket #582)

--- tests/run/charptr_comparison_T582.pyx
+++ tests/run/charptr_comparison_T582.pyx
@@ -1,6 +1,85 @@
 
 cimport cython
 
+################################################################################
+## plain char*
+
+@cython.test_assert_path_exists('//SingleAssignmentNode')
+#@cython.test_fail_if_path_exists('//SingleAssignmentNode//CoerceFromPyTypeNode')
+def charptr_equals_literal(char* s):
+    """
+    >>> charptr_equals_literal('abc'.encode('ASCII'))
+    True
+    >>> charptr_equals_literal('aabc'.encode('ASCII'))
+    False
+    >>> charptr_equals_literal('abcx'.encode('ASCII'))
+    False
+    >>> charptr_equals_literal('bcx'.encode('ASCII'))
+    False
+    """
+    cdef bint result = (s == b"abc")
+    return result
+
+def charptr_gt_literal(char* s):
+    """
+    >>> charptr_gt_literal('abc'.encode('ASCII'))
+    False
+    >>> charptr_gt_literal('aabc'.encode('ASCII'))
+    False
+    >>> charptr_gt_literal('abcx'.encode('ASCII'))
+    True
+    >>> charptr_gt_literal('bcx'.encode('ASCII'))
+    True
+    """
+    cdef bint result = (s > b"abc")
+    return result
+
+def charptr_lt_literal(char* s):
+    """
+    >>> charptr_lt_literal('abc'.encode('ASCII'))
+    False
+    >>> charptr_lt_literal('aabc'.encode('ASCII'))
+    True
+    >>> charptr_lt_literal('abcx'.encode('ASCII'))
+    False
+    >>> charptr_lt_literal('bcx'.encode('ASCII'))
+    False
+    """
+    cdef bint result = (s < b"abc")
+    return result
+
+def charptr_ge_literal(char* s):
+    """
+    >>> charptr_ge_literal('abc'.encode('ASCII'))
+    True
+    >>> charptr_ge_literal('aabc'.encode('ASCII'))
+    False
+    >>> charptr_ge_literal('abcx'.encode('ASCII'))
+    True
+    >>> charptr_ge_literal('bcx'.encode('ASCII'))
+    True
+    """
+    cdef bint result = (s >= b"abc")
+    return result
+
+def charptr_le_literal(char* s):
+    """
+    >>> charptr_le_literal('abc'.encode('ASCII'))
+    True
+    >>> charptr_le_literal('aabc'.encode('ASCII'))
+    True
+    >>> charptr_le_literal('abcx'.encode('ASCII'))
+    False
+    >>> charptr_le_literal('bcx'.encode('ASCII'))
+    False
+    """
+    cdef bint result = (s <= b"abc")
+    return result
+
+
+################################################################################
+## slices
+
 @cython.test_assert_path_exists('//SingleAssignmentNode')
 #FIXME: optimise me!
 #@cython.test_fail_if_path_exists('//SingleAssignmentNode//CoerceFromPyTypeNode')
