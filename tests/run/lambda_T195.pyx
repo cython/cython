5fdebcd7a375d917d64e44081cf7111d839fd3bd
Stefan Behnel <scoder@users.berlios.de>
2010-11-02 14:27:17 +0100

    fix *args/**kwargs in lambda function declaration: was messed up with annotation syntax

--- tests/run/lambda_T195.pyx
+++ tests/run/lambda_T195.pyx
@@ -76,3 +76,35 @@ def make_typed_lambda_lambda(int x):
     """
     return lambda int y : \
            lambda int z:x+y+z
+
+def pass_lambda(f):
+    """
+    >>> def f(a, lfunc): return lfunc(a,2)
+    >>> pass_lambda(f)
+    12
+    """
+    return f(1, lambda a, b : a*10+b)
+
+def pass_lambda_with_args(f):
+    """
+    >>> def f(a, lfunc): return lfunc(a,2,3)
+    >>> pass_lambda_with_args(f)
+    123
+    """
+    return f(1, lambda a, *args : (a*10 + args[0])*10 + args[1])
+
+def pass_lambda_with_args_kwargs(f):
+    """
+    >>> def f(a, lfunc): return lfunc(a,2,3, b=4)
+    >>> pass_lambda_with_args_kwargs(f)
+    1234
+    """
+    return f(1, lambda a, *args, **kwargs : ((a*10 + args[0])*10 + args[1])*10 + kwargs['b'])
+
+def pass_lambda_with_args_kwargs_kwonly_args(f):
+    """
+    >>> def f(a, lfunc): return lfunc(a,2,3, b=4, c=5)
+    >>> pass_lambda_with_args_kwargs_kwonly_args(f)
+    12345
+    """
+    return f(1, lambda a, *args, b, **kwargs : (((a*10 + args[0])*10 + args[1])*10 + b)*10 + kwargs['c'])
