8ee0c69fe28517bd620733477d049a9c973a64e5
Stefan Behnel <scoder@users.berlios.de>
2010-11-03 21:44:37 +0100

    test cleanup

--- tests/run/metaclass.pyx
+++ tests/run/metaclass.pyx
@@ -1,12 +1,13 @@
-"""
->>> obj = Foo()
->>> obj.metaclass_was_here
-True
-"""
+
 class Base(type):
     def __new__(cls, name, bases, attrs):
         attrs['metaclass_was_here'] = True
         return type.__new__(cls, name, bases, attrs)
 
 class Foo(object):
+    """
+    >>> obj = Foo()
+    >>> obj.metaclass_was_here
+    True
+    """
     __metaclass__ = Base
