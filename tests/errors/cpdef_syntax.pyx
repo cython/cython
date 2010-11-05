d7ffd39be3d830395f9ee92cce983753d6c7f702
Stefan Behnel <scoder@users.berlios.de>
2010-11-05 08:19:34 +0100

    slightly more telling error message on generic syntax errors

--- tests/errors/cpdef_syntax.pyx
+++ tests/errors/cpdef_syntax.pyx
@@ -5,5 +5,5 @@ cpdef nogil class test: pass
 _ERRORS = u"""
  2: 6: cdef blocks cannot be declared cpdef
  3: 6: cdef blocks cannot be declared cpdef
- 3:12: Expected ':'
+ 3:12: Expected ':', found 'class'
 """
