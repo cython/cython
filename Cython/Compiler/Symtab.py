335bc37af8ff00e8b9bfda11dd2ec7c463b146fe
Stefan Behnel <scoder@users.berlios.de>
2010-11-08 10:14:32 +0100

    trial fix for ticket #583: method signatures of overridden C methods in pure mode

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -1559,7 +1559,8 @@ class CClassScope(ClassScope):
                     error(pos, "'%s' already defined" % name)
                 #print "CClassScope.declare_cfunction: checking signature" ###
                 if type.same_c_signature_as(entry.type, as_cmethod = 1) and type.nogil == entry.type.nogil:
-                    pass
+                    # may still need to adjust the type of 'self' => simply override the type
+                    entry.type = type
                 elif type.compatible_signature_with(entry.type, as_cmethod = 1) and type.nogil == entry.type.nogil:
                     entry = self.add_cfunction(name, type, pos, cname or name, visibility='ignore', modifiers=modifiers)
                     defining = 1
