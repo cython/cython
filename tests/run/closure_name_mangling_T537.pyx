09cf6663537d59f581e1ad8514e7ef52c1a850dd
Stefan Behnel <scoder@users.berlios.de>
2010-11-21 08:19:02 +0100

    test for ticket #537

---
+++ tests/run/closure_name_mangling_T537.pyx
@@ -0,0 +1,23 @@
+
+__doc__ = u"""
+>>> f1 = nested1()
+>>> f2 = nested2()
+>>> f1 == f2      # inner functions (f)
+False
+>>> f1() == f2()  # inner-inner functions (g)
+False
+"""
+
+def nested1():
+   def f():
+      def g():
+         pass
+      return g
+   return f
+
+def nested2():
+   def f():
+      def g():
+         pass
+      return g
+   return f
