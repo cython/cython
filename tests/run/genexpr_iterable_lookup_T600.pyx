2789351e6638d3ca69aec102504b092e74205c05
Stefan Behnel <scoder@users.berlios.de>
2010-11-25 19:18:06 +0100

    extended test cases for tickets #600 and #598

--- tests/run/genexpr_iterable_lookup_T600.pyx
+++ tests/run/genexpr_iterable_lookup_T600.pyx
@@ -16,3 +16,18 @@ def list_genexpr_iterable_lookup():
     result = list( x*2 for x in x if x % 2 == 0 )
     assert x == (0,1,2,3,4,5)
     return result
+
+@cython.test_assert_path_exists('//ComprehensionNode')
+@cython.test_fail_if_path_exists('//SimpleCallNode')
+def genexpr_iterable_in_closure():
+    """
+    >>> genexpr_iterable_in_closure()
+    [0, 4, 8]
+    """
+    x = 'abc'
+    def f():
+        return x
+    result = list( x*2 for x in x if x % 2 == 0 )
+    assert x == 'abc' # don't leak in Py3 code
+    assert f() == 'abc' # don't leak in Py3 code
+    return result
