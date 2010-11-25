904e2f00594846c21b3ceeb7ca53fd4c9daf47ce
Stefan Behnel <scoder@users.berlios.de>
2010-11-25 19:18:06 +0100

    extended test cases for tickets #600 and #598

--- tests/run/list_comp_in_closure_T598.pyx
+++ tests/run/list_comp_in_closure_T598.pyx
@@ -25,3 +25,17 @@ def genexpr_in_closure():
     assert x == 'abc' # don't leak in Py3 code
     assert f() == 'abc' # don't leak in Py3 code
     return result
+
+def genexpr_scope_in_closure():
+    """
+    >>> genexpr_scope_in_closure()
+    [0, 4, 8]
+    """
+    i = 2
+    x = 'abc'
+    def f():
+        return i, x
+    result = list( x*i for x in range(5) if x % 2 == 0 )
+    assert x == 'abc' # don't leak in Py3 code
+    assert f() == 2,'abc' # don't leak in Py3 code
+    return result
