bcf5763f15f95ea78bf6b6a08e327545d1088c4e
Stefan Behnel <scoder@users.berlios.de>
2010-12-11 01:21:20 +0100

    disable generators test in Py2.4 - depends on GeneratorExit and potentially other stuff

--- runtests.py
+++ runtests.py
@@ -62,6 +62,7 @@ VER_DEP_MODULES = {
                                           ]),
     (2,5) : (operator.lt, lambda x: x in ['run.any',
                                           'run.all',
+                                          'run.generators',
                                           ]),
     (2,6) : (operator.lt, lambda x: x in ['run.print_function',
                                           'run.cython3',
