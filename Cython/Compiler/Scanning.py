d8813f05292b70bbb36855de845a8ed2cc160b43
Stefan Behnel <scoder@users.berlios.de>
2010-12-17 00:19:19 +0100

    manual merge of Haoyu's nonlocal implementation

--- Cython/Compiler/Scanning.py
+++ Cython/Compiler/Scanning.py
@@ -36,7 +36,7 @@ def get_lexicon():
 #------------------------------------------------------------------
 
 py_reserved_words = [
-    "global", "def", "class", "print", "del", "pass", "break",
+    "global", "nonlocal", "def", "class", "print", "del", "pass", "break",
     "continue", "return", "raise", "import", "exec", "try",
     "except", "finally", "while", "if", "elif", "else", "for",
     "in", "assert", "and", "or", "not", "is", "in", "lambda",
