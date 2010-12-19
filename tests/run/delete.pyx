a619863d8e2687339df8d379b6e232e1e0295949
Stefan Behnel <scoder@users.berlios.de>
2010-12-19 19:46:22 +0100

    test cleanup

--- tests/run/delete.pyx
+++ tests/run/delete.pyx
@@ -1,27 +1,12 @@
-"""
->>> a = A()
->>> a.f()
-[2, 1]
->>> a.g()
-(False, True)
->>> del_item({1: 'a', 2: 'b'}, 1)
-{2: 'b'}
->>> del_item(list(range(10)), 2)
-[0, 1, 3, 4, 5, 6, 7, 8, 9]
 
->>> del_dict({1: 'a', 2: 'b'}, 1)
-{2: 'b'}
->>> del_list(list(range(5)), 3)
-[0, 1, 2, 4]
->>> del_int(list(range(5)), 3)
-[0, 1, 2, 4]
->>> del_list_int(list(range(5)), 3)
-[0, 1, 2, 4]
->>> del_int({-1: 'neg', 1: 'pos'}, -1)
-{1: 'pos'}
-"""
-
-class A:
+class A(object):
+    """
+    >>> a = A()
+    >>> a.f()
+    [2, 1]
+    >>> a.g()
+    (False, True)
+    """
     def f(self):
         self.refs = [3,2,1]
         del self.refs[0]
@@ -33,21 +18,45 @@ class A:
         return (hasattr(self, u"a"), hasattr(self, u"g"))
 
 def del_item(L, o):
+    """
+    >>> del_item({1: 'a', 2: 'b'}, 1)
+    {2: 'b'}
+    >>> del_item(list(range(10)), 2)
+    [0, 1, 3, 4, 5, 6, 7, 8, 9]
+    """
     del L[o]
     return L
 
 def del_dict(dict D, o):
+    """
+    >>> del_dict({1: 'a', 2: 'b'}, 1)
+    {2: 'b'}
+    """
     del D[o]
     return D
 
 def del_list(list L, o):
+    """
+    >>> del_list(list(range(5)), 3)
+    [0, 1, 2, 4]
+    """
     del L[o]
     return L
 
 def del_int(L, int i):
+    """
+    >>> del_int(list(range(5)), 3)
+    [0, 1, 2, 4]
+    >>> del_int({-1: 'neg', 1: 'pos'}, -1)
+    {1: 'pos'}
+    """
     del L[i]
     return L
 
 def del_list_int(L, int i):
+    """
+    >>> del_list_int(list(range(5)), 3)
+    [0, 1, 2, 4]
+    """
     del L[i]
     return L
