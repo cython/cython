72fd96164cb13013565a6145078bb64456b47cec
Stefan Behnel <scoder@users.berlios.de>
2010-12-27 10:05:37 +0100

    always INCREF Python arguments in 'with gil' C functions as we cannot know who owns them

--- tests/run/owned_arg_refs.pyx
+++ tests/run/owned_arg_refs.pyx
@@ -15,6 +15,24 @@ def test_ext_type_attr():
     owner.x = ''.join("abc%d" % 5) # non-interned object
     return call_me_with_owner(owner, owner.x)
 
+
+cdef void call_me_without_gil(Owner owner, x) with gil:
+    owner.x = "def" # overwrite external reference
+    print x         # crashes if x is not owned by function or caller
+
+def test_ext_type_attr_nogil():
+    """
+    >>> test_ext_type_attr_nogil()
+    abc5
+    """
+    owner = Owner()
+    owner.x = ''.join("abc%d" % 5) # non-interned object
+    with nogil:
+        call_me_without_gil(owner, owner.x)
+
+
+# the following isn't dangerous as long as index access uses temps
+
 cdef call_me_with_list(list l, x):
     l[:] = [(1,2), (3,4)] # overwrite external reference
     return x              # crashes if x is not owned by function or caller
