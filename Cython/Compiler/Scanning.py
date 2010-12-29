c6be9c54a42995f82fcfdf5bdbf0d1e3d5f4a88e
Stefan Behnel <scoder@users.berlios.de>
2010-12-29 09:07:02 +0100

    ticket #635: use relative paths in generated C code position comments

--- Cython/Compiler/Scanning.py
+++ Cython/Compiler/Scanning.py
@@ -166,8 +166,9 @@ class FileSourceDescriptor(SourceDescriptor):
     optional name argument and will be passed back when asking for
     the position()-tuple.
     """
-    def __init__(self, filename):
+    def __init__(self, filename, path_description=None):
         filename = Utils.decode_filename(filename)
+        self.path_description = path_description or filename
         self.filename = filename
         self.set_file_type_from_name(filename)
         self._cmp_name = filename
@@ -180,7 +181,7 @@ class FileSourceDescriptor(SourceDescriptor):
             require_normalised_newlines=False)
 
     def get_description(self):
-        return self.filename
+        return self.path_description
 
     def get_filenametable_entry(self):
         return self.filename
