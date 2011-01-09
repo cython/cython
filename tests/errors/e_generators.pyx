30e9845dc703fa44297bb73985d0b02db1f28ac9
Vitja Makarov <vitja.makarov@gmail.com>
2011-01-09 12:41:08 +0300

    Add support for return with no value inside generator

--- tests/errors/e_generators.pyx
+++ tests/errors/e_generators.pyx
@@ -6,10 +6,6 @@ def bar(a):
     return 0
     yield
 
-def xxx():
-    yield
-    return
-
 yield
 
 class Foo:
@@ -18,7 +14,6 @@ class Foo:
 _ERRORS = u"""
 3:4: 'return' with argument inside generator
 7:4: 'yield' outside function
-11:4: 'return' inside generators not yet supported
-13:0: 'yield' not supported here
-16:4: 'yield' not supported here
+9:0: 'yield' not supported here
+12:4: 'yield' not supported here
 """
