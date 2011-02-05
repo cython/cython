a34aaa4c14ceaf89db08303fc5d4c46b6a8e79cd
Stefan Behnel <scoder@users.berlios.de>
2011-02-05 19:21:47 +0100

    error handling fixes for argument unpacking when args/kwargs are in closures

---
+++ tests/run/args_unpacking_in_closure_T658.pyx
@@ -0,0 +1,14 @@
+
+def outer(int x, *args, **kwargs):
+    """
+    >>> inner = outer(1, 2, a=3)
+    >>> inner()
+    (1, (2,), {'a': 3})
+
+    >>> inner = outer('abc', 2, a=3)
+    Traceback (most recent call last):
+    TypeError: an integer is required
+    """
+    def inner():
+        return x, args, kwargs
+    return inner
