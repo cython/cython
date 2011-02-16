541b01af8261e6cf05240a6d62c4eec3d9762182
Lisandro Dalcin <dalcinl@gmail.com>
2011-02-16 20:11:05 -0300

    enable embedded testcase in Python 3

--- Demos/embed/assert_equal.py
+++ Demos/embed/assert_equal.py
@@ -1,6 +1,12 @@
 import sys
-if open(sys.argv[1]).read() != open(sys.argv[2]).read():
-    print "Files differ"
-    sys.exit(1)
-else:
-    print "Files identical"
+f1 = open(sys.argv[1])
+f2 = open(sys.argv[2])
+try:
+    if f1.read() != f2.read():
+        print ("Files differ")
+        sys.exit(1)
+    else:
+        print ("Files identical")
+finally:
+    f1.close()
+    f2.close()
