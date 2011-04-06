2ee3a0ec7cbe2121e0deabc5f89fff0dd649f98f
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-04-06 20:50:26 +0200

    cython.py: Always put Cython in sys.path

--- cython.py
+++ cython.py
@@ -4,6 +4,13 @@
 
 if __name__ == '__main__':
 
+    import os
+    import sys
+    
+    # Make sure we import the right Cython
+    cythonpath, _ = os.path.split(os.path.realpath(__file__))
+    sys.path.insert(0, cythonpath)
+
     from Cython.Compiler.Main import main
     main(command_line = 1)
 
