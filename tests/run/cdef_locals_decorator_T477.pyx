4c333a898f5de90e828a14f594b5a35609ba438f
Lisandro Dalcin <dalcinl@gmail.com>
2011-04-07 10:47:10 -0300

    more tests for #477

--- tests/run/cdef_locals_decorator_T477.pyx
+++ tests/run/cdef_locals_decorator_T477.pyx
@@ -15,3 +15,17 @@ def test():
     True
     """
     return func(2)
+
+def test_defval(x=None):
+    """
+    >>> test_defval()
+    0.0
+    >>> test_defval(1)
+    1.0
+    >>> test_defval(2.0)
+    4.0
+    """
+    if x is None:
+        return func_defval()
+    else:
+        return func_defval(x)
