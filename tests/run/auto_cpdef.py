368ddfc3a4c8ac95bc9aeb4cc2a2adfe1b45ee74
Stefan Behnel <scoder@users.berlios.de>
2011-04-13 21:25:03 +0200

    fix #683: allow overriding C-API mapped builtin functions with (auto-)cpdef functions

---
+++ tests/run/auto_cpdef.py
@@ -0,0 +1,24 @@
+# cython: auto_cpdef=True
+# mode:run
+# tag: directive,auto_cpdef
+
+import cython
+
+def str(arg):
+    """
+    This is a bit evil - str gets mapped to a C-API function and is
+    being redefined here.
+
+    >>> print(str('TEST'))
+    STR
+    """
+    return 'STR'
+
+@cython.test_assert_path_exists('//SimpleCallNode[@function.type.is_cfunction = True]')
+@cython.test_fail_if_path_exists('//SimpleCallNode[@function.type.is_builtin_type = True]')
+def call_str(arg):
+    """
+    >>> print(call_str('TEST'))
+    STR
+    """
+    return str(arg)
