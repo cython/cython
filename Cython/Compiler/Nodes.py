3f130b73ec913d6685ddd32a046e229f3a7c627b
Mark Florisson <markflorisson88@gmail.com>
2011-04-20 13:27:49 +0200

    Fix Python 3 compatibility

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -5999,6 +5999,9 @@ class ParallelRangeNode(ParallelStatNode):
         self.is_nogil = self.kwargs.pop('nogil', False)
         self.schedule = self.kwargs.pop('schedule', None)
 
+        if hasattr(self.schedule, 'decode'):
+            self.schedule = self.schedule.decode('ascii')
+
         if self.schedule not in (None, 'static', 'dynamic', 'guided',
                                  'runtime'):
             error(self.pos, "Invalid schedule argument to prange: %r" %
