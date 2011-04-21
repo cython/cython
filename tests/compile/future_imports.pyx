094bda2fbcdc4360327b23e38dd3c022b106cba2
Stefan Behnel <scoder@users.berlios.de>
2011-04-21 10:30:40 +0200

    __future__ imports syntax test

---
+++ tests/compile/future_imports.pyx
@@ -0,0 +1,9 @@
+# mode: compile
+
+from __future__ import nested_scopes
+
+from __future__ import with_statement
+
+pass
+
+from __future__ import nested_scopes ; from __future__ import nested_scopes
