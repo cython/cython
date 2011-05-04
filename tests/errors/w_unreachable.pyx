228be0ade893b5359f841f2549c1ee6b4205733f
Vitja Makarov <vitja.makarov@gmail.com>
2011-05-04 16:01:49 +0400

    Unreachable code tests

---
+++ tests/errors/w_unreachable.pyx
@@ -0,0 +1,31 @@
+# mode: error
+# tag: werror
+
+def simple_return():
+    return
+    print 'Where am I?'
+
+def simple_loops(*args):
+    for i in args:
+        continue
+        print 'Never be here'
+
+    while True:
+        break
+        print 'Never be here'
+
+def conditional(a, b):
+    if a:
+        return 1
+    elif b:
+        return 2
+    else:
+        return 3
+    print 'oops'
+
+_ERRORS = """
+6:4: Unreachable code
+11:8: Unreachable code
+15:8: Unreachable code
+24:4: Unreachable code
+"""
