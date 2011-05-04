be855a5c2bc88310a824c0dfb37744841e70c3c3
Vitja Makarov <vitja.makarov@gmail.com>
2011-05-04 10:32:58 +0400

    Add testcase for closure variable deletion

--- tests/errors/e_del.pyx
+++ tests/errors/e_del.pyx
@@ -12,9 +12,18 @@ def f(a):
     del j # error: deletion of non-Python object
     del x[i] # error: deletion of non-Python object
     del s.m # error: deletion of non-Python object
+
+def outer(a):
+    def inner():
+        print a
+    del a
+    return inner()
+
+
 _ERRORS = u"""
 10:9: Cannot assign to or delete this
 11:48: Deletion of non-Python, non-C++ object
 13:9: Deletion of non-Python, non-C++ object
 14:9: Deletion of non-Python, non-C++ object
+19:9: can not delete variable 'a' referenced in nested scope
 """
