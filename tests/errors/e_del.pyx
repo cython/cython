bef77ee890d064cf2a966fd331a908f13e6e964b
Vitja Makarov <vitja.makarov@gmail.com>
2011-05-04 10:32:36 +0400

    Remove tabs

--- tests/errors/e_del.pyx
+++ tests/errors/e_del.pyx
@@ -1,20 +1,20 @@
 # mode: error
 
 cdef struct S:
-	int m
+    int m
 
 def f(a):
-	cdef int i, x[2]
-	cdef S s
-	global j
-	del f() # error
-	del i # error: deletion of non-Python object
-	del j # error: deletion of non-Python object
-	del x[i] # error: deletion of non-Python object
-	del s.m # error: deletion of non-Python object
+    cdef int i, x[2]
+    cdef S s
+    global j
+    del f() # error
+    del i # error: deletion of non-Python object
+    del j # error: deletion of non-Python object
+    del x[i] # error: deletion of non-Python object
+    del s.m # error: deletion of non-Python object
 _ERRORS = u"""
-10:6: Cannot assign to or delete this
-11:45: Deletion of non-Python, non-C++ object
-13:6: Deletion of non-Python, non-C++ object
-14:6: Deletion of non-Python, non-C++ object
+10:9: Cannot assign to or delete this
+11:48: Deletion of non-Python, non-C++ object
+13:9: Deletion of non-Python, non-C++ object
+14:9: Deletion of non-Python, non-C++ object
 """
