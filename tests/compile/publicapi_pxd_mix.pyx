f96286b31423a9bd8508609aeaf6aa0b408de5f4
Lisandro Dalcin <dalcinl@gmail.com>
2011-05-19 18:17:32 +0300

    More work on pxd/public/api import/export

--- tests/compile/publicapi_pxd_mix.pyx
+++ tests/compile/publicapi_pxd_mix.pyx
@@ -8,10 +8,12 @@ cdef class Bar1: pass
 cdef class Bar2: pass
 cdef class Bar3: pass
 
+cdef            void bar0(): pass
 cdef public     void bar1(): pass
 cdef        api void bar2(): pass
 cdef public api void bar3(): pass
 
+cdef            void* spam0(object o) except NULL: return NULL
 cdef public     void* spam1(object o) except NULL: return NULL
 cdef        api void* spam2(object o) nogil except NULL: return NULL
 cdef public api void* spam3(object o) except NULL with gil: return NULL
