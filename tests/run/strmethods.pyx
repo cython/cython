501405a3a0787dfe575663a476756d1cb172ad21
John Ehresman <jpe@wingware.com>
2011-05-25 15:24:57 -0400

    Initial implementation of startswith & endswith for str
    --HG--
    extra : rebase_source : dad9efaf7c3e1e4f328c64147c913ca258421b2b

---
+++ tests/run/strmethods.pyx
@@ -0,0 +1,47 @@
+cimport cython
+
+@cython.test_assert_path_exists(
+    "//PythonCapiCallNode")
+def str_startswith(str s, sub, start=None, stop=None):
+    """
+    >>> str_startswith('a', 'a')
+    True
+    >>> str_startswith('a', 'b')
+    False
+    >>> str_startswith('a', ('a', 'b'))
+    True
+    >>> str_startswith('a', 'a', 1)
+    False
+    >>> str_startswith('a', 'a', 0, 0)
+    False
+    """
+
+    if start is None:
+      return s.startswith(sub)
+    elif stop is None:
+      return s.startswith(sub, start)
+    else:
+      return s.startswith(sub, start, stop)
+
+@cython.test_assert_path_exists(
+    "//PythonCapiCallNode")
+def str_endswith(str s, sub, start=None, stop=None):
+    """
+    >>> str_endswith('a', 'a')
+    True
+    >>> str_endswith('a', 'b')
+    False
+    >>> str_endswith('a', ('a', 'b'))
+    True
+    >>> str_endswith('a', 'a', 1)
+    False
+    >>> str_endswith('a', 'a', 0, 0)
+    False
+    """
+
+    if start is None:
+      return s.endswith(sub)
+    elif stop is None:
+      return s.endswith(sub, start)
+    else:
+      return s.endswith(sub, start, stop)
