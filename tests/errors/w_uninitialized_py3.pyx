ee285b53b8554c09173e0d36d9aa3aff591974d1
Vitja Makarov <vitja.makarov@gmail.com>
2011-05-28 15:46:44 +0400

    Don't use print() inside w_unitialized_py3 test, that doesn't work for python < 2.6

--- tests/errors/w_uninitialized_py3.pyx
+++ tests/errors/w_uninitialized_py3.pyx
@@ -2,20 +2,23 @@
 # mode: error
 # tag: werror
 
+def ref(obj):
+    pass
+
 def list_comp(a):
     r = [i for i in a]
-    print(i)
+    ref(i)
     i = 0
     return r
 
 def dict_comp(a):
     r = {i: j for i, j in a}
-    print(i)
+    ref(i)
     i = 0
     return r
 
 
 _ERRORS = """
-7:11: local variable 'i' referenced before assignment
-13:11: local variable 'i' referenced before assignment
+10:9: local variable 'i' referenced before assignment
+16:9: local variable 'i' referenced before assignment
 """
