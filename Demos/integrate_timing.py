f9a1e09b46b9153db294fa888d7b56b1a52ef8d8
Robert Bradshaw <robertwb@math.washington.edu>
2011-06-08 22:45:58 -0700

    Integrate test from quickstart.

---
+++ Demos/integrate_timing.py
@@ -0,0 +1,13 @@
+import timeit
+
+import integrate0, integrate1, integrate2
+
+number = 10
+py_time = None
+for m in ('integrate0', 'integrate1', 'integrate2'):
+    print m
+    t = min(timeit.repeat("integrate_f(0.0, 10.0, 10000000)", "from %s import integrate_f" % m, number=number))
+    if py_time is None:
+        py_time = t
+    print "    ", t / number, "s"
+    print "    ", py_time / t
