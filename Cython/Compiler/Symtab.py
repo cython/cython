1612aab0b105eb8c7f6cafc821b285cd6d72ecf8
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-07-11 12:08:38 +0200

    cleanup: Remove unused fields from Symtab
    A grep indicated these are no longer used

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -209,7 +209,6 @@ class Scope(object):
     # cfunc_entries     [Entry]            C function entries
     # c_class_entries   [Entry]            All extension type entries
     # cname_to_entry    {string : Entry}   Temp cname to entry mapping
-    # int_to_entry      {int : Entry}      Temp cname to entry mapping
     # return_type       PyrexType or None  Return type of function owning scope
     # is_builtin_scope  boolean            Is the builtin scope of Python/Cython
     # is_py_class_scope boolean            Is a Python class scope
@@ -221,7 +220,6 @@ class Scope(object):
     # scope_prefix      string             Disambiguator for C names
     # in_cinclude       boolean            Suppress C declaration code
     # qualified_name    string             "modname" or "modname.classname"
-    # pystring_entries  [Entry]            String const entries newly used as
     #                                        Python strings in this scope
     # nogil             boolean            In a nogil section
     # directives       dict                Helper variable for the recursive
@@ -271,7 +269,6 @@ class Scope(object):
         self.identifier_to_entry = {}
         self.num_to_entry = {}
         self.obj_to_entry = {}
-        self.pystring_entries = []
         self.buffer_entries = []
         self.lambda_defs = []
         self.return_type = None
