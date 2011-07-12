7a0f63b2ec6c3fcd9764763b997c6e3ae1028e4f
Dag Sverre Seljebotn <dagss@student.matnat.uio.no>
2011-07-12 12:30:49 +0200

    Test for problem with CythonUtilityCode overwriting __test__

--- tests/run/cythonscope.pyx
+++ tests/run/cythonscope.pyx
@@ -1,11 +1,3 @@
-"""
->>> f()
-hello from cython scope, value=4
-hello from cython.view scope, value=4
-hello from cython scope, value=3
-hello from cython.view scope, value=3
-"""
-
 cimport cython
 
 from cython cimport _testscope as tester
@@ -13,6 +5,13 @@ from cython.view cimport _testscope as viewtester
 
 
 def f():
+    """
+    >>> f()
+    hello from cython scope, value=4
+    hello from cython.view scope, value=4
+    hello from cython scope, value=3
+    hello from cython.view scope, value=3
+    """
     print cython._testscope(4)
     print cython.view._testscope(4)
     print tester(3)
