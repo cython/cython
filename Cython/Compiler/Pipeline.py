0daf725086bda1dddf31b24387bcfb2414d18301
Mark Florisson <markflorisson88@gmail.com>
2011-07-15 09:57:31 +0200

    Support cname() decorators in CythonUtilityCode

--- Cython/Compiler/Pipeline.py
+++ Cython/Compiler/Pipeline.py
@@ -237,6 +237,26 @@ def create_pyx_as_pxd_pipeline(context, result):
     pipeline.append(fake_pxd)
     return pipeline
 
+def insert_into_pipeline(pipeline, transform, before=None, after=None):
+    """
+    Insert a new transform into the pipeline after or before an instance of
+    the given class. e.g.
+
+        pipeline = insert_into_pipeline(pipeline, transform,
+                                        after=AnalyseDeclarationsTransform)
+    """
+    assert before or after
+
+    cls = before or after
+    for i, t in enumerate(pipeline):
+        if isinstance(t, cls):
+            break
+
+    if after:
+        i += 1
+
+    return pipeline[:i] + [transform] + pipeline[i:]
+
 #
 # Running a pipeline
 #
