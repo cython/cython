0d64b7d3692b1a878d57c5b378fd36c859fb4784
Stefano Sanfilippo <girdle.of.hippolyte@gmail.com>
2011-08-07 19:36:12 +0200

    Turned flags structure into a readable dictionary.

--- Cython/Compiler/CmdLine.py
+++ Cython/Compiler/CmdLine.py
@@ -2,7 +2,99 @@
 #   Cython - Command Line Parsing
 #
 
+options = {
+    'default' : {
+        ('-V', '--version') : {
+            'dest' : 'show_version', 'const' : 1, 'action' : 'store_const',
+            'help' : 'Display version number of cython compiler'}},
+    'environment setup': {
+        ('-l', '--create-listing') : {
+            'dest' : 'use_listing_file', 'const' : 1, 'action' : 'store_const',
+            'help' : 'Write error messages to a listing file'},
+        ('-I', '--include-dir') : {
+            'dest' : 'include_path', 'action' : 'append',
+            'help' : '''Search for include files in named directory
+                        (multiple include directories are allowed).'''},
+        ('-o', '--output-file') : {
+            'dest' : 'output_file',
+            'help' : 'Specify name of generated C file'},
+        ('-w', '--working') : {
+            'dest' : 'working_path',
+            'help' : '''Sets the working directory for Cython 
+                        (the directory modules are searched from)'''}},
+    'compilation' : {
+        ('-t', '--timestamps') : {
+            'dest' : 'timestamps', 'const' : 1, 'action' : 'store_const',
+            'help' : 'Only compile newer source files (implied)'},
+        ('-f', '--force') : {
+            'dest' : 'timestamps', 'const' : 0, 'action' : 'store_const',
+            'help' : 'Compile all source files (overrides -t)'}},
+    'debugging' : {
+        ('--cleanup') : {
+            'dest' : 'generate_cleanup_code', 'type' : int,
+            'help' : '''Release interned objects on python exit, for memory debugging.
+                        Level indicates aggressiveness, default 0 releases nothing.'''},
+        ('--gdb') : {
+            'dest' : 'gdb_debug', 'action' : 'store_true',
+            'help' : 'Output debug information for cygdb'}},
+    'recursive compilation': {
+        ('-r', '--recursive') : {
+            'dest' : 'recursive', 'const' : 1, 'action' : 'store_const',
+            'help' : 'Recursively find and compile dependencies (implies -t)'},
+        ('-q', '--quiet') : {
+            'dest' : 'quiet', 'const' : 1, 'action' : 'store_const',
+            'help' : "Don't print module names in recursive mode" },
+        ('-v', '--verbose') : {
+            'dest' : 'verbose', 'const' : 1, 'action' : 'append_const',
+            'help' : 'Be verbose, print file names on multiple compilation' }},
+    'code annotation' : {
+        ('-a', '--annotate') : {
+            'dest' : 'annotate', 'action' : 'store_true',
+            'help' : 'Produce a colorized HTML version of the source.' },
+        ('--line-directives') : {
+            'dest' : 'emit_linenums', 'action' : 'store_true',
+            'help' : 'Produce #line directives pointing to the .pyx source' }},
+    'code generation' : {
+        ('--embed') : {
+            'dest' : 'embed', 'const' : 'main', 'nargs' : '?',
+            'help' : 'Generate a main() function that embeds the Python interpreter.'},
+        ('-+', '--cplus') : {
+            'dest' : 'cplus', 'const' : 1, 'action' : 'store_const',
+            'help' : 'Output a C++ rather than C file.' },
+        ('--no-c-in-traceback') : {
+            'dest' : 'c_line_in_traceback', 'action' : 'store_false',
+            'help' : '' }, #FIXME add help
+        ('-2') : {
+            'dest' : 'language_level', 'const' : 2, 'action' : 'store_const',
+            'help' : 'Compile based on Python-2 syntax and code semantics.'},
+        ('-3') : {
+            'dest' : 'language_level', 'const' : 3, 'action' : 'store_const',
+            'help' : 'Compile based on Python-3 syntax and code semantics.'}}
+}
+
+epilog = '''Cython (http://cython.org) is a compiler for code written in the
+    Cython language.  Cython is based on Pyrex by Greg Ewing.
+    RECURSIVE COMPILATION IS STILL BROKEN.'''
+
+class ArgsError(Exception):
+    pass
+
+
 import argparse
+
+parser = argparse.ArgumentParser(epilog=epilog)
+
+for grouptitle, flaglist in options.iteritems():
+    group = parser.add_argument_group(title=grouptitle)
+    for flag, parameters in flaglist.iteritems():
+        print flag
+        try:
+            group.add_argument(flag, **parameters)
+        except ValueError:
+            group.add_argument(*flag, **parameters)
+
+parser.add_argument('sources', help='Source file(s) to be compiled.', nargs='*')
+
 
 from Cython.Compiler.Main import CompilationOptions, default_options
 import Options
@@ -10,100 +102,34 @@ import Options
 import os
 import sys
 
-parser = argparse.ArgumentParser(epilog=
-    '''Cython (http://cython.org) is a compiler for code written in the
-    Cython language.  Cython is based on Pyrex by Greg Ewing.''')
-
-# General options
-parser.add_argument('-V', '--version', dest='show_version', const=1,
-    action='store_const', help='Display version number of cython compiler')
-
-# Environment setup: include dirs, working directory &co.
-working = parser.add_argument_group(title='Environment setup')
-
-working.add_argument('-l', '--create-listing', dest='use_listing_file',
-    const=1, action='store_const', help='Write error messages to a listing file')
-working.add_argument('-I', '--include-dir', dest='include_path', action='append',
-    help='Search for include files in named directory (multiple include directories are allowed).')
-working.add_argument('-o', '--output-file', dest='output_file',
-    help='Specify name of generated C file')
-working.add_argument('-w', '--working', dest='working_path',
-    help='Sets the working directory for Cython (the directory modules are searched from)')
-
-# Compilation: what files to compile, based on timestamp
-compilation = parser.add_argument_group(title='Compilation')
-
-compilation.add_argument('-t', '--timestamps', dest='timestamps', const=1,
-    action='store_const', help='Only compile newer source files (implied)')
-compilation.add_argument('-f', '--force', dest='timestamps', const=0,
-    action='store_const', help='Compile all source files (overrides -t)')
-
-# Debugging options (both for compiler and generated files)
-debug = parser.add_argument_group(title='Debugging')
-
-# CLEANUP
-debug.add_argument('--gdb', dest='gdb_debug', action='store_true',
-    help='Output debug information for cygdb')
-
-# Recursion options
-recurse = parser.add_argument_group(title='Recursive compilation')
-
-recurse.add_argument('-r', '--recursive', dest='recursive', const=1,
-    action='store_const', help='Recursively find and compile dependencies (implies -t) BROKEN!!!') #FIXME
-recurse.add_argument('-q', '--quiet', dest='quiet', const=1,
-    action='store_const', help="Don't print module names in recursive mode")
-recurse.add_argument('-v', '--verbose', dest='verbose', const=1,
-    action='append_const', help='Be verbose, print file names on multiple compilation')
-
-# Code annotation
-annotate = parser.add_argument_group(title='Code annotation')
-
-annotate.add_argument('-a', '--annotate', dest='annotate', action='store_true',
-    help='Produce a colorized HTML version of the source.')
-annotate.add_argument('--line-directives', dest='emit_linenums', action='store_true',
-    help='Produce #line directives pointing to the .pyx source')
-
-# Code generation options
-codegen = parser.add_argument_group(title='Code generation')
-
-#DOCSTRINGS
-codegen.add_argument('--embed', dest='embed', const='main', nargs='?',
-    help='Generate a main() function that embeds the Python interpreter.')
-codegen.add_argument('-+', '--cplus', dest='cplus', const=1, action='store_const',
-    help='Output a C++ rather than C file.')
-codegen.add_argument('--no-c-in-traceback', dest='c_line_in_traceback',
-    action='store_false', help='') #FIXME add help
-codegen.add_argument('-2', dest='language_level', const=2, action='store_const',
-    help='Compile based on Python-2 syntax and code semantics.')
-codegen.add_argument('-3', dest='language_level', const=3, action='store_const',
-    help='Compile based on Python-3 syntax and code semantics.')
-
-#Source files
-parser.add_argument('sources', help='Source file(s) to be compiled.', nargs='*')
-
-class ArgsError(Exception):
-    pass
-
 def parse_command_line(args = sys.argv):
     options = CompilationOptions(default_options)
     parser.parse_args(args, namespace=options)
+    sources = options.sources
+    del options.sources # FIXME
 
 #    options.verbose = len(options.verbose)
     if options.gdb_debug:
         options.output_dir = os.curdir
 
-    if options.output_file and len(sources) > 1:
-        raise ArgsError('Only one source file allowed when using -o')
+    if not sources:
+        raise ArgsError('no source file specified.')
 
-    if not len(sources) == 0 and not options.show_version:
+    if options.output_file and len(sources) > 1:
+        raise ArgsError('only one source file allowed when using -o')
+
+    if not len(options.sources) == 0 and not options.show_version:
         pass #FIXME
 
-    if Options.embed and len(sources) > 1:
-        raise ArgsError('Only one source file allowed when using -embed')
+    if options.embed and len(sources) > 1:
+        raise ArgsError('only one source file allowed when using -embed')
 
-#    return options, sources
-
+    #FIXME
+    Options.embed = options.embed
     print vars(options)
+
+    return options, sources
+
 
 if __name__ == '__main__':
     parse_command_line(sys.argv[1:])
@@ -118,7 +144,26 @@ if __name__ == '__main__':
 
 
 
-
+#-            elif option in ("-p", "--embed-positions"):
+#-                Options.embed_pos_in_docstring = 1
+#-            elif option in ("-z", "--pre-import"):
+#-                Options.pre_import = pop_arg()
+#-            elif option == "--cleanup":
+#-                Options.generate_cleanup_code = int(pop_arg())
+#-            elif option in ("-D", "--no-docstrings"):
+#-                Options.docstrings = False
+#-            elif option in ("-a", "--annotate"):
+#-                Options.annotate = True
+#-            elif option == "--convert-range":
+#-                Options.convert_range = True
+#-            elif option == "--fast-fail":
+#-                Options.fast_fail = True
+#-            elif option in ('-Werror', '--warning-errors'):
+#-                Options.warning_errors = True
+#-            elif option == "--disable-function-redefinition":
+#-                Options.disable_function_redefinition = True
+#-            elif option == "--old-style-globals":
+#-                Options.old_style_globals = True
 
 usage = """\
   -p, --embed-positions          If specified, the positions in Cython files of each
