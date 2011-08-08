de7c88de591e160787c5728f03427ce9d6e90bf6
Stefano Sanfilippo <girdle.of.hippolyte@gmail.com>
2011-08-08 15:27:52 +0200

    Added optparse fallback.

--- Cython/Compiler/CmdLine.py
+++ Cython/Compiler/CmdLine.py
@@ -1,5 +1,5 @@
 #
-#   Copyright 2011 Stefano Sanfilippo
+#   Copyright 2011 Stefano Sanfilippo <satufk on GitHub>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ menu = {
             'version' : 'Cython version %s' % version,
             'help' : 'Display version number of cython compiler'},
         ('sources') : {
-            'nargs' : '*',
+            'nargs' : '+', 'metavar' : 'sourcefile.{py,pyx}',
             'help' : 'Source file(s) to be compiled.' }},
     'environment setup': {
         ('-l', '--create-listing') : {
@@ -70,6 +70,13 @@ menu = {
         ('--gdb') : {
             'dest' : 'gdb_debug', 'action' : 'store_true',
             'help' : 'Output debug information for cygdb'}},
+    'Python compatibility options' : {
+        ('-2') : {
+            'dest' : 'language_level', 'const' : 2, 'action' : 'store_const',
+            'help' : 'Compile based on Python-2 syntax and code semantics.'},
+        ('-3') : {
+            'dest' : 'language_level', 'const' : 3, 'action' : 'store_const',
+            'help' : 'Compile based on Python-3 syntax and code semantics.'}},
     'recursive compilation': {
         ('-r', '--recursive') : {
             'dest' : 'recursive', 'const' : 1, 'action' : 'store_const',
@@ -100,7 +107,7 @@ menu = {
                         function definition is embedded in its docstring.''' },
         ('--no-c-in-traceback') : {
             'dest' : 'c_line_in_traceback', 'action' : 'store_false',
-            'help' : '' }, #FIXME
+            'help' : 'Omit C source code line when printing tracebacks.' },
         ('--convert-range') : {
             'dest' : 'convert_range', 'action' : 'store_true',
             'help' : '''Convert `for x in range():` statements in pure C for(),
@@ -110,26 +117,30 @@ menu = {
             'help' : 'Strip docstrings from the compiled module.' },
         ('--disable-function-redefinition') : {
             'dest' : 'disable_function_redefinition', 'action' : 'store_true',
-            'help' : '' }, #FIXME
+            'help' : 'For legacy code only, needed for some circular imports.' },
         ('--old-style-globals') : {
             'dest' : 'old_style_globals', 'action' : 'store_true',
-            'help' : '' }}, #FIXME
-    'Python compatibility options' : {
-        ('-2') : {
-            'dest' : 'language_level', 'const' : 2, 'action' : 'store_const',
-            'help' : 'Compile based on Python-2 syntax and code semantics.'},
-        ('-3') : {
-            'dest' : 'language_level', 'const' : 3, 'action' : 'store_const',
-            'help' : 'Compile based on Python-3 syntax and code semantics.'}},
-#    'experimental options (MacOS X only)' : {
+            'help' : '''Makes globals() give the first non-Cython module
+                        globals in the call stack. For SAGE compatibility''' }},
+   'internal options' : {
+       ('--directive', '-X') : {
+           'dest' : 'compiler_directives', 'action' : 'append',
+           'metavar' : '<name>=<value>,', 'nargs' : '+',
+           'help' : 'Overrides a #pragma compiler directive'},
+       ('--debug') : {
+            'dest' : 'debug_flags', 'action' : 'append',
+            'metavar' : '<flag>', 'nargs' : '+',
+            'help' : "Sets Cython's internal debug options"}},
+#    'experimental options' : { # MacOS X only
 #        ('-C', '--compile') : {
 #            'dest' : 'compile', 'action' : 'store_true',
 #            'help' : 'Compile generated .c file to .o file' },
 #        ('--link') : {
 #            'dest' : 'link', 'action' : 'store_true',
 #            'help' : '''Link .o file to produce extension module (implies -C)
-#                        Additional .o files to link may be supplied when using -X.''' }}
+#                        Additional .o files to link may be supplied when using -X.''' }},
 }
+
 
 epilog = '''\
 Cython (http://cython.org) is a compiler for code written
@@ -138,56 +149,9 @@ in the Cython language. Cython is based on Pyrex by Greg Ewing.
 WARNING: RECURSIVE COMPILATION IS STILL BROKEN
 (see http://trac.cython.org/cython_trac/ticket/379).'''
 
-
-from Cython.Compiler.Main import CompilationOptions, default_options
-import argparse
 import os
 
-class BasicParser:
-    def error(self, message):
-        pass
-
-    def refine(self, options):
-        sources = options.sources
-        del options.sources
-
-        if options.gdb_debug:
-            options.output_dir = os.curdir
-        if not sources:
-            self.error('no source file specified.')
-        if options.output_file and len(sources) > 1:
-            self.error('only one source file allowed when using -o')
-        if not len(sources) == 0 and not options.show_version:
-            self.error('malformed command line.')
-        if options.embed and len(sources) > 1:
-            self.error('only one source file allowed when using --embed')
-
-        return options, sources
-
-
-class Parser(BasicParser, argparse.ArgumentParser):
-    def __init__(self):
-        format = argparse.RawDescriptionHelpFormatter
-        argparse.ArgumentParser.__init__(self, epilog=epilog, formatter_class=format)
-        newgroup = lambda t: self.add_argument_group(title=t)
-        for grouptitle, flaglist in menu.iteritems():
-            group = self if grouptitle == 'default' else newgroup(grouptitle)
-            for flag, parameters in flaglist.iteritems():
-                try:
-                    group.add_argument(flag, **parameters)
-                except ValueError:
-                    group.add_argument(*flag, **parameters)
-
-    def parse(self, args):
-        options = CompilationOptions(default_options)
-        parser.parse_args(args, namespace=options)
-        return self.refine(options)
-
-
-parser = Parser()
-
-
-def XXX_compatibility_assign_variables(options):
+def XXX_assign_variables(options):
     for option in ['embed', 'embed_pos_in_docstring', 'pre_import',
                     'generate_cleanup_code', 'docstrings', 'annotate',
                     'convert_range', 'fast_fail', 'warning_errors',
@@ -200,41 +164,112 @@ def XXX_compatibility_assign_variables(options):
         except KeyError:
             pass
 
-def parse_command_line(args):
-    options, sources = parser.parse(args)
-    XXX_compatibility_assign_variables(options)
-    return options, sources
+def XXX_parse_debug_flags(parser, flags, options):
+    for option in flags:
+        if option.startswith('--debug'):
+            option = option[2:].replace('-', '_')
+            import DebugFlags
+            if option in dir(DebugFlags):
+                setattr(DebugFlags, option, True)
+            else:
+                parser.error('unknown debug flag: %s' % option)
+        elif option.startswith('-X'):
+            if option[2:].strip():
+                x_args = option[2:]
+            else:
+                x_args = pop_arg()
+            try:
+                options.compiler_directives = Options.parse_directive_list(
+                    x_args, relaxed_bool=True,
+                    current_settings=options.compiler_directives)
+            except ValueError, e:
+                parser.error('error in compiler directive: %s' % e.args[0])
 
+class BasicParser:
+    def refine(self, results):
+        options, extra = results
+        sources = options.sources
+        del options.sources
+
+        if options.gdb_debug:
+            options.output_dir = os.curdir
+        if not sources:
+            self.error('no source file specified.')
+        if options.output_file and len(sources) > 1:
+            self.error('only one source file allowed when using -o')
+        if options.embed and len(sources) > 1:
+            self.error('only one source file allowed when using --embed')
+
+        XXX_parse_debug_flags(self, extra, options)
+        XXX_assign_variables(options)
+
+        return options, sources
+
+
+from Main import CompilationOptions, default_options #Cython.Compiler.
 import sys
 
-if __name__ == '__main__':
-    parse_command_line(sys.argv[1:])
+if sys.version_info >= (2, 7):
+    import argparse
 
-# TODO
-#"""
-#  -X, --directive <name>=<value>[,<name=value,...] Overrides a compiler directive
-#"""
-#    class split(argparse.Action):
-#        def __call__(self, parser, namespace, values, option_string=None):
-#           pass
-#
-#           if option == "--directive" or option.startswith('-X'):
-#                if option.startswith('-X') and option[2:].strip():
-#                    x_args = option[2:]
-#                else:
-#                    x_args = pop_arg()
-#                try:
-#                    options.compiler_directives = Options.parse_directive_list(
-#                        x_args, relaxed_bool=True,
-#                        current_settings=options.compiler_directives)
-#                except ValueError, e:
-#                    sys.stderr.write("Error in compiler directive: %s\n" % e.args[0])
-#                    sys.exit(1)
-#            elif option.startswith('--debug'):
-#                option = option[2:].replace('-', '_')
-#                import DebugFlags
-#                if option in dir(DebugFlags):
-#                    setattr(DebugFlags, option, True)
-#                else:
-#                    sys.stderr.write("Unknown debug flag: %s\n" % option)
-#                    bad_usage()
+    class Parser(BasicParser, argparse.ArgumentParser):
+        def __init__(self):
+            argparse.ArgumentParser.__init__(self, epilog=epilog,
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                fromfile_prefix_chars='@')
+            newgroup = lambda t: self.add_argument_group(title=t)
+            for grouptitle, flaglist in menu.iteritems():
+                group = self if grouptitle == 'default' else newgroup(grouptitle)
+                for flag, parameters in flaglist.iteritems():
+                    if isinstance(flag, str):
+                        group.add_argument(flag, **parameters)
+                    else:
+                        group.add_argument(*flag, **parameters)
+
+        def parse(self, args):
+            options = CompilationOptions(default_options)
+            results = parser.parse_known_args(args, namespace=options)
+            return self.refine(results)
+
+else:
+    import optparse
+
+    class Parser(BasicParser, optparse.OptionParser):
+        def __init__(self):
+            optparse.OptionParser.__init__(self, epilog=epilog,
+                version=menu['default'][('-V', '--version')]['version'],
+                usage="%prog [options] sourcefile.{pyx,py} ...")
+            newgroup = lambda t: optparse.OptionGroup(self, t)
+            for grouptitle, flaglist in menu.iteritems():
+                group = self if grouptitle == 'default' else newgroup(grouptitle)
+                for flag, parameters in flaglist.iteritems():
+                    try:
+                        if isinstance(flag, str):
+                            group.add_option(flag, **parameters)
+                        else:
+                            group.add_option(*flag, **parameters)
+                    except optparse.OptionError:
+                        pass
+
+                if group is not self: self.add_option_group(group)
+
+        def parse(self, args):
+            options, sources = parser.parse_args(args, values=CompilationOptions)
+            options.sources = sources
+            options, sources = self.refine((options, sources))
+            return CompilationOptions(options), sources
+
+
+parser = Parser()
+
+
+def parse_command_line(args):
+    return parser.parse(args)
+
+
+if __name__ == '__main__':
+    import sys
+    options, sources = parse_command_line(sys.argv[1:])
+    print vars(options)
+    print Options.embed
+    print sources
