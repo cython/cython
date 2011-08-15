ffea991b5eaeaf58e4cef2fa7c2e75613ee5f340
Stefan Behnel <scoder@users.berlios.de>
2011-08-15 19:45:11 +0200

    fix ticket #712: sequence unpacking of known types crashed the compiler

---
+++ tests/run/tupleunpack_T712.pyx
@@ -0,0 +1,18 @@
+# mode: run
+# ticket: 712
+
+def single_from_string():
+    """
+    >>> print(single_from_string())
+    a
+    """
+    (a,) = 'a'
+    return a
+
+def single_from_set():
+    """
+    >>> print(single_from_set())
+    a
+    """
+    (a,) = set(["a"])
+    return a
