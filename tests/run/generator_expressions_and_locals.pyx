2ab3982735bb75119aedcb14a002d30d08c9a1fe
Stefan Behnel <scoder@users.berlios.de>
2011-08-30 18:24:52 +0200

    fix compiler crash when using locals() and generator expressions in the same function
    --HG--
    extra : transplant_source : %D0%C1g%D5%EA%CE%FE%C36%A5%28%D25J%DF%AD%FB%A3%A3%B2

---
+++ tests/run/generator_expressions_and_locals.pyx
@@ -0,0 +1,10 @@
+# mode: run
+# tag: genexpr, locals
+
+def genexpr_not_in_locals():
+    """
+    >>> genexpr_not_in_locals()
+    {'t': (0, 1, 4, 9, 16, 25, 36, 49, 64, 81)}
+    """
+    t = tuple(x*x for x in range(10))
+    return locals()
