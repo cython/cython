847c2ffbc551d8b69ae13399d62d69d25c048a2e
Vladimir Cerny <blackvladimir@gmail.com>
2011-10-15 20:34:12 +0200

    overloading on tamplate types

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -2613,7 +2613,7 @@ class CppClassType(CType):
         if other_type.is_cpp_class:
             if self == other_type:
                 return 1
-            elif self.template_type and self.template_type == other_type.template_type:
+            elif self.template_type and other_type.template_type:
                 if self.templates == other_type.templates:
                     return 1
                 for t1, t2 in zip(self.templates, other_type.templates):
