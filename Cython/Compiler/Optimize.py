108ab86b2f7584aa32d8625b5bfd67ad22be380e
Robert Bradshaw <robertwb@gmail.com>
2011-10-20 23:47:06 -0700

    Move list_pop to external utility code.

--- Cython/Compiler/Optimize.py
+++ Cython/Compiler/Optimize.py
@@ -15,7 +15,7 @@ import Symtab
 import Options
 import Naming
 
-from Code import UtilityCode
+from Code import UtilityCode, ContentHashingUtilityCode
 from StringEncoding import EncodedString, BytesLiteral
 from Errors import error
 from ParseTreeTransforms import SkipDeclarations
@@ -31,6 +31,9 @@ try:
     from __builtin__ import basestring
 except ImportError:
     basestring = str # Python 3
+
+def load_c_utility(name):
+    return ContentHashingUtilityCode.load(name, "Optimize.c")
 
 class FakePythonEnv(object):
     "A fake environment for creating type test nodes etc."
@@ -2321,7 +2324,7 @@ class OptimizeBuiltinCalls(Visitor.EnvTransform):
                         args = args,
                         may_return_none = True,
                         is_temp = node.is_temp,
-                        utility_code = pop_index_utility_code
+                        utility_code = load_c_utility("pop_index")
                         )
 
         return node
@@ -3212,55 +3215,6 @@ static CYTHON_INLINE PyObject* __Pyx_PyObject_Pop(PyObject* L) {
 }
 """,
 impl = ""
-)
-
-pop_index_utility_code = UtilityCode(
-proto = """
-static PyObject* __Pyx_PyObject_PopIndex(PyObject* L, Py_ssize_t ix);
-""",
-impl = """
-static PyObject* __Pyx_PyObject_PopIndex(PyObject* L, Py_ssize_t ix) {
-    PyObject *r, *m, *t, *py_ix;
-#if PY_VERSION_HEX >= 0x02040000
-    if (likely(PyList_CheckExact(L))) {
-        Py_ssize_t size = PyList_GET_SIZE(L);
-        if (likely(size > (((PyListObject*)L)->allocated >> 1))) {
-            if (ix < 0) {
-                ix += size;
-            }
-            if (likely(0 <= ix && ix < size)) {
-                Py_ssize_t i;
-                PyObject* v = PyList_GET_ITEM(L, ix);
-                Py_SIZE(L) -= 1;
-                size -= 1;
-                for(i=ix; i<size; i++) {
-                    PyList_SET_ITEM(L, i, PyList_GET_ITEM(L, i+1));
-                }
-                return v;
-            }
-        }
-    }
-#endif
-    py_ix = t = NULL;
-    m = __Pyx_GetAttrString(L, "pop");
-    if (!m) goto bad;
-    py_ix = PyInt_FromSsize_t(ix);
-    if (!py_ix) goto bad;
-    t = PyTuple_New(1);
-    if (!t) goto bad;
-    PyTuple_SET_ITEM(t, 0, py_ix);
-    py_ix = NULL;
-    r = PyObject_CallObject(m, t);
-    Py_DECREF(m);
-    Py_DECREF(t);
-    return r;
-bad:
-    Py_XDECREF(m);
-    Py_XDECREF(t);
-    Py_XDECREF(py_ix);
-    return NULL;
-}
-"""
 )
 
 
---
+++ Cython/Utility/Optimize.c
@@ -0,0 +1,47 @@
+/////////////// pop_index.proto ///////////////
+
+static PyObject* __Pyx_PyObject_PopIndex(PyObject* L, Py_ssize_t ix);
+
+/////////////// pop_index.proto ///////////////
+
+static PyObject* __Pyx_PyObject_PopIndex(PyObject* L, Py_ssize_t ix) {
+    PyObject *r, *m, *t, *py_ix;
+#if PY_VERSION_HEX >= 0x02040000
+    if (likely(PyList_CheckExact(L))) {
+        Py_ssize_t size = PyList_GET_SIZE(L);
+        if (likely(size > (((PyListObject*)L)->allocated >> 1))) {
+            if (ix < 0) {
+                ix += size;
+            }
+            if (likely(0 <= ix && ix < size)) {
+                Py_ssize_t i;
+                PyObject* v = PyList_GET_ITEM(L, ix);
+                Py_SIZE(L) -= 1;
+                size -= 1;
+                for(i=ix; i<size; i++) {
+                    PyList_SET_ITEM(L, i, PyList_GET_ITEM(L, i+1));
+                }
+                return v;
+            }
+        }
+    }
+#endif
+    py_ix = t = NULL;
+    m = __Pyx_GetAttrString(L, "pop");
+    if (!m) goto bad;
+    py_ix = PyInt_FromSsize_t(ix);
+    if (!py_ix) goto bad;
+    t = PyTuple_New(1);
+    if (!t) goto bad;
+    PyTuple_SET_ITEM(t, 0, py_ix);
+    py_ix = NULL;
+    r = PyObject_CallObject(m, t);
+    Py_DECREF(m);
+    Py_DECREF(t);
+    return r;
+bad:
+    Py_XDECREF(m);
+    Py_XDECREF(t);
+    Py_XDECREF(py_ix);
+    return NULL;
+}
