bd3da3c1b03b8484e152a4e6ad7d201a2d4ea3c3
Stefan Behnel <stefan_ml@behnel.de>
2011-11-15 21:29:05 +0100

    strip a comment from the generated C file

--- Cython/Utility/StringTools.c
+++ Cython/Utility/StringTools.c
@@ -90,7 +90,8 @@ static CYTHON_INLINE int __Pyx_PyUnicode_Equals(PyObject* s1, PyObject* s2, int 
 //////////////////// UnicodeEquals ////////////////////
 
 static CYTHON_INLINE int __Pyx_PyUnicode_Equals(PyObject* s1, PyObject* s2, int equals) {
-    if (s1 == s2) {   /* as done by PyObject_RichCompareBool(); also catches the (interned) empty string */
+    if (s1 == s2) {
+        /* as done by PyObject_RichCompareBool(); also catches the (interned) empty string */
         return (equals == Py_EQ);
     } else if (PyUnicode_CheckExact(s1) & PyUnicode_CheckExact(s2)) {
         #ifdef CYTHON_PEP393_ENABLED
