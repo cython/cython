b3303fa7a1df1bebb8e163faefc858d90760eccd
Mark Florisson <markflorisson88@gmail.com>
2012-02-04 13:02:26 +0000

    Fix error test

--- tests/errors/e_bufaccess2.pyx
+++ tests/errors/e_bufaccess2.pyx
@@ -30,7 +30,6 @@ def withnogil_acquire(x):
 
 _ERRORS = u"""
  3: 9: 'nothing' is not a type identifier
-12:11: Cannot check buffer index bounds without gil; use boundscheck(False) directive
 24:11: Cannot access buffer with object dtype without gil
 24:11: Assignment of Python object not allowed without gil
 29:12: Assignment of Python object not allowed without gil
