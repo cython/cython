a03a562d980f20ea96268529fc576e2cc78ea70b
Mark Florisson <markflorisson88@gmail.com>
2012-02-15 11:13:31 +0000

    Fix error test

--- tests/errors/e_invalid_num_threads.pyx
+++ tests/errors/e_invalid_num_threads.pyx
@@ -28,7 +28,6 @@ for i in prange(10, nogil=True, num_threads=2):
     pass
 
 _ERRORS = u"""
-e_invalid_num_threads.pyx:8:33: Coercion from Python not allowed without the GIL
 e_invalid_num_threads.pyx:12:20: argument to num_threads must be greater than 0
 e_invalid_num_threads.pyx:19:19: num_threads already declared in outer section
 e_invalid_num_threads.pyx:23:19: num_threads must be declared in the parent parallel section
