72ef3640409b4963586a2a7e92fe9f7162e8e157
Stefan Behnel <stefan_ml@behnel.de>
2012-04-10 07:17:02 +0200

    avoid unnecessarily unsafe code in test: don't take char* from global Python variable

--- tests/run/unsigned_char_ptr_bytes_conversion_T359.pyx
+++ tests/run/unsigned_char_ptr_bytes_conversion_T359.pyx
@@ -1,22 +1,24 @@
 # ticket: 359
 
-__doc__ = """
->>> print(py_string1.decode('iso8859-1'))
-test toast taste
->>> print(py_string2.decode('iso8859-1'))
-test toast taste
->>> print(py_string3.decode('iso8859-1'))
-test toast taste
-"""
-
 cdef unsigned char* some_c_unstring = 'test toast taste'
 
-py_string1 = some_c_unstring
+def test_uchar_conversion():
+    """
+    >>> py_string1, py_string2, py_string3 = test_uchar_conversion()
+    >>> print(py_string1.decode('iso8859-1'))
+    test toast taste
+    >>> print(py_string2.decode('iso8859-1'))
+    test toast taste
+    >>> print(py_string3.decode('iso8859-1'))
+    test toast taste
+    """
 
-cdef unsigned char* c_unstring_from_py = py_string1
+    cdef object py_string1 = some_c_unstring
 
-py_string2 = c_unstring_from_py
+    cdef unsigned char* c_unstring_from_py = py_string1
+    cdef object py_string2 = c_unstring_from_py
 
-cdef char* c_string_from_py = py_string2
+    cdef char* c_string_from_py = py_string2
+    cdef object py_string3 = c_string_from_py
 
-py_string3 = c_string_from_py
+    return py_string1, py_string2, py_string3
