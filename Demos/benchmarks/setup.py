7af9d4864e73ecc91f4d6f4556356430a0af357d
Stefan Behnel <stefan_ml@behnel.de>
2012-07-05 19:51:36 +0200

    generate annotated HTML from benchmark sources

--- Demos/benchmarks/setup.py
+++ Demos/benchmarks/setup.py
@@ -3,5 +3,5 @@ from Cython.Build import cythonize
 
 setup(
   name = 'benchmarks',
-  ext_modules = cythonize("*.py", language_level=3),
+  ext_modules = cythonize("*.py", language_level=3, annotate=True),
 )
