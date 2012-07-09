dad7c3e2d9512d8725d83ec9d6b4185a9f32d253
Stefan Behnel <stefan_ml@behnel.de>
2012-07-09 12:59:31 +0200

    suppress 'unused argument' warning in test code

--- tests/run/arithmetic_analyse_types_helper.h
+++ tests/run/arithmetic_analyse_types_helper.h
@@ -8,18 +8,21 @@ struct longlong_return { char *msg; };
 
 short_return f(short arg) {
     short_return val;
+    arg++;
     val.msg = (char*)"short called";
     return val;
 }
 
 int_return f(int arg) {
     int_return val;
+    arg++;
     val.msg = (char*)"int called";
     return val;
 }
 
 longlong_return f(long long arg) {
     longlong_return val;
+    arg++;
     val.msg = (char*)"long long called";
     return val;
 }
