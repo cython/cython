45b023f8f70f246f6b31c1b43661d4374429cb4a
Stefan Behnel <stefan_ml@behnel.de>
2012-07-24 18:40:51 +0200

    require Py2.4+

--- Cython/Compiler/Main.py
+++ Cython/Compiler/Main.py
@@ -3,8 +3,8 @@
 #
 
 import os, sys, re, codecs
-if sys.version_info[:2] < (2, 3):
-    sys.stderr.write("Sorry, Cython requires Python 2.3 or later\n")
+if sys.version_info[:2] < (2, 4):
+    sys.stderr.write("Sorry, Cython requires Python 2.4 or later\n")
     sys.exit(1)
 
 import Errors
