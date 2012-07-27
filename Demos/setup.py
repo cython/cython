1b05e8bde21202f68aa3349b2610af919d581baa
Stefan Behnel <stefan_ml@behnel.de>
2012-07-27 19:20:24 +0200

    fix setup.py script in Demos dir

--- Demos/setup.py
+++ Demos/setup.py
@@ -13,9 +13,9 @@ ext_modules = cythonize("**/*.pyx", exclude="numpy_*.pyx")
 # Only compile the following if numpy is installed.
 try:
     from numpy.distutils.misc_util import get_numpy_include_dirs
-    numpy_demo = Extension("*",
-                           ["numpy_*.pyx"],
-                           include_dirs=get_numpy_include_dirs())
+    numpy_demo = [Extension("*",
+                            ["numpy_*.pyx"],
+                            include_dirs=get_numpy_include_dirs())]
     ext_modules.extend(cythonize(numpy_demo))
 except ImportError:
     pass
