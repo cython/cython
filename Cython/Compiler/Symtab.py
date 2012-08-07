31e7360087615d528f4aa7e2413e52aac99bf2b4
Stefan Behnel <stefan_ml@behnel.de>
2012-08-07 14:00:36 +0200

    restored code in Symtab.py that fixed the module name for package compilation - still needed

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -971,7 +971,13 @@ class ModuleScope(Scope):
         self.parent_module = parent_module
         outer_scope = Builtin.builtin_scope
         Scope.__init__(self, name, outer_scope, parent_module)
-        self.module_name = EncodedString(name)
+        if name != "__init__":
+            self.module_name = name
+        else:
+            # Treat Spam/__init__.pyx specially, so that when Python loads
+            # Spam/__init__.so, initSpam() is defined.
+            self.module_name = parent_module.module_name
+        self.module_name = EncodedString(self.module_name)
         self.context = context
         self.module_cname = Naming.module_cname
         self.module_dict_cname = Naming.moddict_cname
