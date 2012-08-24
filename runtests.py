29d47edab491c7baa14a0a6130c51587c122ce87
Stefan Behnel <stefan_ml@behnel.de>
2012-08-24 14:54:21 +0200

    set initial __file__ value in pyregr test modules

--- runtests.py
+++ runtests.py
@@ -902,7 +902,9 @@ class CythonPyregrTestCase(CythonRunTestCase):
         CythonRunTestCase.setUp(self)
         from Cython.Compiler import Options
         Options.error_on_unknown_names = False
-        Options.directive_defaults.update(dict(binding=True, always_allow_keywords=True))
+        Options.directive_defaults.update(dict(
+            binding=True, always_allow_keywords=True,
+            set_initial_path="SOURCEFILE"))
 
     def _run_unittest(self, result, *classes):
         """Run tests from unittest.TestCase-derived classes."""
