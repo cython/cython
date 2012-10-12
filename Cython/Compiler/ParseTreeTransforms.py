f675f1ceb07ea7a56882d5e5f460569eb5020289
Stefan Behnel <stefan_ml@behnel.de>
2012-10-12 09:23:14 +0200

    fix 'cython.compiled' flag

--- Cython/Compiler/ParseTreeTransforms.py
+++ Cython/Compiler/ParseTreeTransforms.py
@@ -2413,7 +2413,7 @@ class TransformBuiltinMethods(EnvTransform):
         if attribute:
             if attribute == u'compiled':
                 node = ExprNodes.BoolNode(node.pos, value=True)
-            if attribute == u'__version__':
+            elif attribute == u'__version__':
                 import Cython
                 node = ExprNodes.StringNode(node.pos, value=EncodedString(Cython.__version__))
             elif attribute == u'NULL':
