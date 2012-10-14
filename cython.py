f9b33c46b9d238654e5ac375fd0e3a2e53bde60c
Mark Florisson <markflorisson88@gmail.com>
2012-10-14 16:44:07 +0100

    Support Cython under pdb for python 2.4

--- cython.py
+++ cython.py
@@ -8,10 +8,14 @@ if __name__ == '__main__':
 
     import os
     import sys
-    
-    # Make sure we import the right Cython
-    cythonpath, _ = os.path.split(os.path.realpath(__file__))
-    sys.path.insert(0, cythonpath)
+
+    try:
+        # Make sure we import the right Cython
+        cythonpath, _ = os.path.split(os.path.realpath(__file__))
+        sys.path.insert(0, cythonpath)
+    except NameError:
+        # In pdb in python 2.4, __file__ is not defined...
+        pass
 
     from Cython.Compiler.Main import main
     main(command_line = 1)
