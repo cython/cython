cf20c0d9454a573f373d5de0526e0d4a1c2605b5
Stefan Behnel <stefan_ml@behnel.de>
2012-11-03 23:36:25 +0100

    added test for self-reimport at module init time
    --HG--
    extra : transplant_source : Y%1C%3A%07%2C%60%B0%F0.%EA%B1%23%3F%0E%20%89%13%C5%C24

---
+++ tests/run/reimport.pyx
@@ -0,0 +1,13 @@
+# mode: run
+# tags: import
+
+# reimports at module init time used to be a problem in Py3
+import reimport
+
+def test():
+    """
+    >>> test()
+    True
+    """
+    import sys
+    return reimport in sys.modules.values()
