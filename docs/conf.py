793e473471901fd477b0beffdd046825fb2822e8
Stefan Behnel <stefan_ml@behnel.de>
2012-12-16 18:19:03 +0100

    include PDF-Builder in Sphinx conf (if installed)

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,17 @@ highlight_language = 'cython'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['ipython_console_highlighting', 'cython_highlighting', 'sphinx.ext.pngmath', 'sphinx.ext.todo', 'sphinx.ext.intersphinx']
+extensions = [
+    'ipython_console_highlighting',
+    'cython_highlighting',
+    'sphinx.ext.pngmath',
+    'sphinx.ext.todo',
+    'sphinx.ext.intersphinx'
+    ]
+
+try: import rst2pdf
+except ImportError: pass
+else: extensions.append('rst2pdf.pdfbuilder')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
