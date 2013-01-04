51f3877ee1c4919f3cd570812aef884d913328bb
Stefan Behnel <stefan_ml@behnel.de>
2013-01-04 07:29:34 +0100

    fix merge conflict

--- Cython/__init__.py
+++ Cython/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from Cython.Shadow import __version__
-=======
-__version__ = "0.17.4"
->>>>>>> bugs
 
 # Void cython.* directives (for case insensitive operating systems).
 from Cython.Shadow import *
