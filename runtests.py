b82127ccbc72459f3b2f9a4a10880c2f0afa6d85
Stefan Behnel <stefan_ml@behnel.de>
2013-02-16 18:46:34 +0100

    disable dict.setdefault() test in Py2.4 which tests for a bug in that version

--- runtests.py
+++ runtests.py
@@ -204,6 +204,7 @@ VER_DEP_MODULES = {
                                           'run.relativeimport_star_T542',
                                           'run.initial_file_path',  # relative import
                                           'run.pynumber_subtype_conversion',  # bug in Py2.4
+                                          'run.dict_setdefault',
                                           ]),
     (2,6) : (operator.lt, lambda x: x in ['run.print_function',
                                           'run.language_level', # print function
