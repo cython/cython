c666fe526a645cbc3a53c83d11e962b7d6e1a16a
zaur <aintellimath@gmail.com>
2013-02-19 12:48:20 +0300

    Add none check for unicode slicing, remove duplicate code, add test for non-ascii unicode strings and None

--- tests/run/unicode_slicing.pyx
+++ tests/run/unicode_slicing.pyx
@@ -1,9 +1,19 @@
-# coding=utf-8
+# coding: utf-8
+
 __doc__ = u"""
     >>> do_slice(u'abcdef', 2, 3)
-    (u'c', u'cdef', u'ab', u'abcdef')
+    (u'c', u'cdef', u'ab', u'abcdef', u'cdef', u'ab', u'abcdef')
     >>> do_slice(u'abcdef', 0, 5)
-    (u'abcde', u'abcdef', u'', u'abcdef')
+    (u'abcde', u'abcdef', u'', u'abcdef', u'abcdef', u'', u'abcdef')
+    >>> do_slice(u'aАbБcСdДeЕfФ', 2, 8)
+    (u'bБcСdД', u'bБcСdДeЕfФ', u'aА', u'aАbБcСdДeЕfФ', u'bБcСdДeЕfФ', u'aА', u'aАbБcСdДeЕfФ')
+    >>> do_slice(u'aАbБcСdДeЕfФ', 2, 8)
+    (u'bБcСdД', u'bБcСdДeЕfФ', u'aА', u'aАbБcСdДeЕfФ', u'bБcСdДeЕfФ', u'aА', u'aАbБcСdДeЕfФ')
+    >>> do_slice(u'АБСДЕФ', 2, 4)
+    (u'СД', u'СДЕФ', u'АБ', u'АБСДЕФ', u'СДЕФ', u'АБ', u'АБСДЕФ')
+    >>> do_slice(None, 2, 4)
+    Traceback (most recent call last):    
+    TypeError: 'NoneType' object is not slicable
 """
 
 import sys
@@ -12,5 +22,5 @@ if sys.version_info[0] >= 3:
     __doc__ = __doc__.replace(u"(u'", u"('").replace(u" u'", u" '")
 
 def do_slice(unicode s, int i, int j):
-    return s[i:j], s[i:], s[:i], s[:]
+    return s[i:j], s[i:], s[:i], s[:], s[i:None], s[None:i], s[None:None]
 
