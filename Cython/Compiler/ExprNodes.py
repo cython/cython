9233ad7c3a64515634b338009bd61abd4cd037b8
Stefan Behnel <stefan_ml@behnel.de>
2013-03-04 07:06:21 +0100

    remove redundant utility code from ExprNodes.py

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -10372,26 +10372,7 @@ class DocstringRefNode(ExprNode):
 #
 #------------------------------------------------------------------------------------
 
-get_name_interned_utility_code = UtilityCode(
-proto = """
-static PyObject *__Pyx_GetName(PyObject *dict, PyObject *name); /*proto*/
-""",
-impl = """
-static PyObject *__Pyx_GetName(PyObject *dict, PyObject *name) {
-    PyObject *result;
-    result = PyObject_GetAttr(dict, name);
-    if (!result) {
-        if (dict != %(BUILTINS)s) {
-            PyErr_Clear();
-            result = PyObject_GetAttr(%(BUILTINS)s, name);
-        }
-        if (!result) {
-            PyErr_SetObject(PyExc_NameError, name);
-        }
-    }
-    return result;
-}
-""" % {'BUILTINS' : Naming.builtins_cname})
+get_name_interned_utility_code = UtilityCode.load_cached("GetGlobalName", "ObjectHandling.c")
 
 #------------------------------------------------------------------------------------
 
