5e075713e97665cfc6d213023dd6c6816ae59910
Stefan Behnel <stefan_ml@behnel.de>
2013-03-10 12:59:58 +0100

    exclude setup.py from compiled modules

--- Demos/benchmarks/setup.py
+++ Demos/benchmarks/setup.py
@@ -8,5 +8,6 @@ directives = {
 setup(
   name = 'benchmarks',
   ext_modules = cythonize("*.py", language_level=3, annotate=True,
-                          compiler_directives=directives),
+                          compiler_directives=directives,
+                          exclude="setup.py"),
 )
