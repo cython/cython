16cb9327472b05ec32e52b3567413e481d8646d2
Robert Bradshaw <robertwb@gmail.com>
2013-03-25 23:38:07 -0700

    Const temp regression test.

--- tests/compile/const_decl.pyx
+++ tests/compile/const_decl.pyx
@@ -5,3 +5,7 @@ cdef const_args(const int a, const int *b, const (int*) c):
     print b[0]
     b = NULL    # OK, the pointer itself is not const
     c[0] = 4    # OK, the value is not const
+
+def call_const_args(x):
+    cdef int k = x
+    const_args(x, &k, &k)
