ab6d03c2f486b144fe7b791ab3006daed111e79c
Stefan Behnel <stefan_ml@behnel.de>
2013-05-09 08:56:03 +0200

    update setup.py script to use cythonize()
    --HG--
    extra : rebase_source : 6494c60e7f79a9cc85fe72a7adf325557f6de2be

--- Demos/callback/Setup.py
+++ Demos/callback/Setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 setup(
   name = 'callback',
-  ext_modules=[ 
+  ext_modules=cythonize([
     Extension("cheese", ["cheese.pyx", "cheesefinder.c"]),
-    ],
-  cmdclass = {'build_ext': build_ext}
+    ]),
 )
