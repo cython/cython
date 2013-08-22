fd318ca5dc2418ebdbc704d9d10321390644ccf8
Matěj Laitl <matej@laitl.cz>
2013-08-22 17:55:07 +0200

    Add testcase for cimport+alias+subclass bug
    The test may require moving into tests/compile after the bug is fixed.

---
+++ tests/run/cimport_alias_subclass.pyx
@@ -0,0 +1,13 @@
+cimport cimport_alias_subclass_helper as cash
+
+cdef class Derived(cash.Base):
+    cdef bint foo(self):
+        print "Hello"
+
+def run():
+    """
+    >>> run()
+    Hello
+    """
+    d = Derived()
+    d.foo()
