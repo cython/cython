3e799f2eeacbb5ef4e8ab4a17e72a9f672946c6b
Stefan Behnel <stefan_ml@behnel.de>
2013-11-17 11:59:35 +0100

    fix test

--- tests/run/genexpr_iterable_lookup_T600.pyx
+++ tests/run/genexpr_iterable_lookup_T600.pyx
@@ -26,12 +26,12 @@ def list_genexpr_iterable_lookup():
 def genexpr_iterable_in_closure():
     """
     >>> genexpr_iterable_in_closure()
-    [0, 4, 8]
+    ['aa', 'cc']
     """
     x = 'abc'
     def f():
         return x
-    result = list( x*2 for x in x if x % 2 == 0 )
+    result = list( x*2 for x in x if x != 'b' )
     assert x == 'abc' # don't leak in Py3 code
     assert f() == 'abc' # don't leak in Py3 code
     return result
