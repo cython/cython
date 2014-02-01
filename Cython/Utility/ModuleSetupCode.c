2fe89038f7a5a86ae569215ef8a7a8d55158b5ea
Stefan Behnel <stefan_ml@behnel.de>
2014-02-01 11:37:35 +0100

    code objects: add number of kwonly args to number of regular arguments in Py2 (which does not support kwonly arguments for Python functions) to make sure they are still visible for introspection

--- Cython/Utility/ModuleSetupCode.c
+++ Cython/Utility/ModuleSetupCode.c
@@ -108,7 +108,7 @@
 #if PY_MAJOR_VERSION < 3
   #define __Pyx_BUILTIN_MODULE_NAME "__builtin__"
   #define __Pyx_PyCode_New(a, k, l, s, f, code, c, n, v, fv, cell, fn, name, fline, lnos) \
-          PyCode_New(a, l, s, f, code, c, n, v, fv, cell, fn, name, fline, lnos)
+          PyCode_New(a+k, l, s, f, code, c, n, v, fv, cell, fn, name, fline, lnos)
   #define __Pyx_DefaultClassType PyClass_Type
 #else
   #define __Pyx_BUILTIN_MODULE_NAME "builtins"
