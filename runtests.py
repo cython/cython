98d604922da969a06e36ad8ae339cb7f0ad06bc4
Stefan Behnel <stefan_ml@behnel.de>
2014-02-01 15:25:12 +0100

    add test for cyfunction signature introspection in Py3.4
    --HG--
    extra : transplant_source : G%E0%FEP%AA%EC%9B%8B%06G4%C1x%17%F5Q4F%3Ep

--- runtests.py
+++ runtests.py
@@ -266,6 +266,8 @@ VER_DEP_MODULES = {
                                         'run.special_methods_T561_py2']),
     (3,3) : (operator.lt, lambda x: x in ['build.package_compilation',
                                           ]),
+    (3,4,0,'beta',3) : (operator.le, lambda x: x in ['run.py34_signature',
+                                          ]),
 }
 
 # files that should not be converted to Python 3 code with 2to3
