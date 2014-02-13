ee6c58e1363f3632b86b2c9bcedce8148a0f910e
Robert Bradshaw <robertwb@gmail.com>
2014-02-13 13:07:02 -0800

    Disable tests to "new" for the ancient boxen CI gcc.

--- tests/run/cpp_stl_conversion.pyx
+++ tests/run/cpp_stl_conversion.pyx
@@ -1,11 +1,12 @@
 # tag: cpp
-# distutils: extra_compile_args=-std=c++0x
+## Re-enable once our buildbot gcc is upgraded to a less-than-seven year old release.
+## distutils: extra_compile_args=-std=c++0x
 
 import sys
 from libcpp.map cimport map
-from libcpp.unordered_map cimport unordered_map
+#from libcpp.unordered_map cimport unordered_map
 from libcpp.set cimport set as cpp_set
-from libcpp.unordered_set cimport unordered_set
+#from libcpp.unordered_set cimport unordered_set
 from libcpp.string cimport string
 from libcpp.pair cimport pair
 from libcpp.vector cimport vector
@@ -143,17 +144,17 @@ def test_set(o):
     cdef cpp_set[long] s = o
     return s
 
-def test_unordered_set(o):
-    """
-    >>> sorted(test_unordered_set([1, 2, 3]))
-    [1, 2, 3]
-    >>> sorted(test_unordered_set([1, 2, 3, 3]))
-    [1, 2, 3]
-    >>> type(test_unordered_set([])) is py_set
-    True
-    """
-    cdef unordered_set[long] s = o
-    return s
+#def test_unordered_set(o):
+#    """
+#    >>> sorted(test_unordered_set([1, 2, 3]))
+#    [1, 2, 3]
+#    >>> sorted(test_unordered_set([1, 2, 3, 3]))
+#    [1, 2, 3]
+#    >>> type(test_unordered_set([])) is py_set
+#    True
+#    """
+#    cdef unordered_set[long] s = o
+#    return s
 
 def test_map(o):
     """
@@ -163,13 +164,13 @@ def test_map(o):
     cdef map[int, double] m = o
     return m
 
-def test_unordered_map(o):
-    """
-    >>> test_map({1: 1.0, 2: 0.5, 3: 0.25})
-    {1: 1.0, 2: 0.5, 3: 0.25}
-    """
-    cdef unordered_map[int, double] m = o
-    return m
+#def test_unordered_map(o):
+#    """
+#    >>> test_map({1: 1.0, 2: 0.5, 3: 0.25})
+#    {1: 1.0, 2: 0.5, 3: 0.25}
+#    """
+#    cdef unordered_map[int, double] m = o
+#    return m
 
 def test_nested(o):
     """
