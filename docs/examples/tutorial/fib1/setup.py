fb863d295c224cff6cf94d8f062dc6e64474bb4e
Stefan Behnel <stefan_ml@behnel.de>
2014-02-23 08:09:51 +0100

    use cythonize() instead of build_ext override in tutorial examples

--- docs/examples/tutorial/fib1/setup.py
+++ docs/examples/tutorial/fib1/setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("fib", ["fib.pyx"])]
+    ext_modules=cythonize("fib.pyx"),
 )
-
