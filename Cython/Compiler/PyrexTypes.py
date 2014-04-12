8939521be140e59247c5aa55b1d3e002c462a96e
Stefan Behnel <stefan_ml@behnel.de>
2014-04-12 09:21:37 +0200

    utility code construction for struct conversion requires constructing the field conversion functions as well
    --HG--
    extra : amend_source : af016229b6c8c40168070e4d3a9581ed12500d3f

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -2879,8 +2879,7 @@ class CStructOrUnionType(CType):
 
         if self._convert_to_py_code is None:
             for member in self.scope.var_entries:
-                if (not member.type.to_py_function and
-                        not member.type.create_to_py_utility_code(env)):
+                if not member.type.create_to_py_utility_code(env):
                     self.to_py_function = None
                     self._convert_to_py_code = False
                     return False
@@ -2898,8 +2897,7 @@ class CStructOrUnionType(CType):
 
         if self._convert_from_py_code is None:
             for member in self.scope.var_entries:
-                if (not member.type.from_py_function and
-                        not member.type.create_from_py_utility_code(env)):
+                if not member.type.create_from_py_utility_code(env):
                     self.from_py_function = None
                     self._convert_from_py_code = False
                     return False
