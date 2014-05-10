4ca95f8002857b071be29a051271963b07fab38d
Stefan Behnel <stefan_ml@behnel.de>
2014-05-10 17:26:28 +0200

    minor code cleanup
    --HG--
    extra : transplant_source : M%D5%2C%C2%15%9E%3C%BC%3C%89%25n%95%DF%5C%B8%B5%C5%B8%D9

--- Cython/Utility/TypeConversion.c
+++ Cython/Utility/TypeConversion.c
@@ -130,12 +130,12 @@ static int __Pyx_init_sys_getdefaultencoding_params(void) {
     PyObject* default_encoding = NULL;
     char* default_encoding_c;
     sys = PyImport_ImportModule("sys");
-    if (sys == NULL) goto bad;
+    if (!sys) goto bad;
     default_encoding = PyObject_CallMethod(sys, (char*) (const char*) "getdefaultencoding", NULL);
-    if (default_encoding == NULL) goto bad;
+    if (!default_encoding) goto bad;
     default_encoding_c = PyBytes_AS_STRING(default_encoding);
     __PYX_DEFAULT_STRING_ENCODING = (char*) malloc(strlen(default_encoding_c));
-    if (__PYX_DEFAULT_STRING_ENCODING == NULL) goto bad;
+    if (!__PYX_DEFAULT_STRING_ENCODING) goto bad;
     strcpy(__PYX_DEFAULT_STRING_ENCODING, default_encoding_c);
     Py_DECREF(sys);
     Py_DECREF(default_encoding);
