8c42993bdb0dad7f25a515d0ce237ce5589260f6
Stefan Behnel <stefan_ml@behnel.de>
2014-06-29 14:25:30 +0200

    use nicer Sphinx scheme and include Cython logo in docs

--- docs/conf.py
+++ docs/conf.py
@@ -138,7 +138,7 @@ intersphinx_mapping = {'python': ('http://docs.python.org/3/', None)}
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -157,12 +157,12 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "cythonlogo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
