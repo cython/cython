e4fb167197e9cb0cbe6f622e4bb7a6297bc78cfd
Stefan Behnel <stefan_ml@behnel.de>
2014-07-27 21:32:04 +0200

    clean up test

--- tests/run/posix_resource.pyx
+++ tests/run/posix_resource.pyx
@@ -1,27 +1,37 @@
 # tag: posix
-from posix.unistd    cimport *
+
+from posix.unistd cimport *
 from posix.resource cimport *
+
 
 def test_getpriority():
     """
     >>> test_getpriority()
+    0
     """
     ret = getpriority(PRIO_PROCESS, getpid())
-    assert ret == 0
+    return ret
+
 
 def test_getrlimit():
     """
     >>> test_getrlimit()
+    0
+    True
     """
     cdef rlimit rlim
+    rlim.rlim_cur = 0
+
     ret = getrlimit(RLIMIT_CPU, &rlim)
-    assert ret == 0
-    assert rlim.rlim_cur != 0
+    print(ret)
+    return rlim.rlim_cur != 0
+
 
 def test_getrusage():
     """
     >>> test_getrusage()
+    0
     """
     cdef rusage r
     ret = getrusage(RUSAGE_SELF, &r)
-    assert ret == 0
+    return ret
