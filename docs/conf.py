b4aff54dbe60d8050acabaadc7c5b0cc352221a1
Stefan Behnel <stefan_ml@behnel.de>
2014-09-15 22:57:03 +0200

    docs: make logo integration work with older Sphinx versions

--- docs/conf.py
+++ docs/conf.py
@@ -163,12 +163,12 @@ except (ImportError, AttributeError):
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "cythonlogo.png"
+html_logo = "_static/cythonlogo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "favicon.ico"
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
