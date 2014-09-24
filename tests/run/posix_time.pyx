019f8a6621f5314ebaac40ce59ac0d73efbe665b
Stefan Behnel <stefan_ml@behnel.de>
2014-09-24 17:41:15 +0200

    split posix time tests in the same way as posix/time.pxd file before

--- tests/run/posix_time.pyx
+++ tests/run/posix_time.pyx
@@ -3,35 +3,6 @@ from libc.stdlib  cimport getenv
 from posix.stdlib cimport setenv, unsetenv
 from posix.time cimport *
 
-def test_itimer(sec, usec):
-    """
-    >>> test_itimer(10, 2)
-    (10, 2)
-    """
-    cdef itimerval t, gtime
-
-    t.it_interval.tv_sec = sec
-    t.it_interval.tv_usec = usec
-    t.it_value.tv_sec = sec
-    t.it_value.tv_usec = usec
-    ret = setitimer(ITIMER_REAL, &t, NULL)
-    assert ret == 0
-    ret = getitimer(ITIMER_REAL, &gtime)
-    assert ret == 0
-    t.it_interval.tv_sec = 0
-    t.it_interval.tv_usec = 0
-    t.it_value.tv_sec = 0
-    t.it_value.tv_usec = 0
-    ret = setitimer(ITIMER_REAL, &t, NULL)
-    return gtime.it_interval.tv_sec, gtime.it_interval.tv_usec
-
-def test_gettimeofday():
-    """
-    >>> test_gettimeofday()
-    """
-    cdef timeval t
-    ret = gettimeofday(&t, NULL)
-    assert ret == 0
 
 def test_time():
     """
@@ -42,6 +13,7 @@ def test_time():
     assert t1 != 0
     t1 = time(&t2)
     assert t1 == t2
+
 
 def test_mktime():
     """
