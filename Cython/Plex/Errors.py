73aa3651c71a147ba92e1b99dd0361a11126cc2a
Stefan Behnel <stefan_ml@behnel.de>
2014-10-10 14:25:04 +0200

    simplify code

--- Cython/Plex/Errors.py
+++ Cython/Plex/Errors.py
@@ -50,5 +50,5 @@ class UnrecognizedInput(PlexError):
         self.state_name = state_name
 
     def __str__(self):
-        return ("'%s', line %d, char %d: Token not recognised in state %s" % (
-            self.position + (repr(self.state_name),)))
+        return ("'%s', line %d, char %d: Token not recognised in state %r" % (
+            self.position + (self.state_name,)))
