0a363e25082663bda28c7674c2da3f71f237e041
Stefan Behnel <stefan_ml@behnel.de>
2014-10-10 08:53:40 +0200

    extend test

--- tests/errors/pxd_signature_mismatch.pxd
+++ tests/errors/pxd_signature_mismatch.pxd
@@ -8,3 +8,7 @@ cdef int wrong_exception_check(int x, int y) except 0
 cdef int wrong_exception_value(int x, int y) except 0
 
 cdef int wrong_exception_value_check(int x, int y) except 0
+
+cdef int inherit_exception_value(int x, int y) except 0
+
+cdef int inherit_exception_check(int x, int y) except *
--- tests/errors/pxd_signature_mismatch.pyx
+++ tests/errors/pxd_signature_mismatch.pyx
@@ -2,19 +2,25 @@
 # tag: pxd
 
 cdef int wrong_args(int x, int y):
-    return 0
+    return 2
 
 cdef int wrong_return_type(int x, int y):
-    return 0
+    return 2
 
 cdef int wrong_exception_check(int x, int y) except? 0:
-    return 0
+    return 2
 
 cdef int wrong_exception_value(int x, int y) except 1:
-    return 0
+    return 2
 
 cdef int wrong_exception_value_check(int x, int y) except? 1:
-    return 0
+    return 2
+
+cdef int inherit_exception_value(int x, int y):
+    return 2
+
+cdef int inherit_exception_check(int x, int y):
+    return 2
 
 
 _ERRORS = """
@@ -23,4 +29,6 @@ _ERRORS = """
 10:5: Function signature does not match previous declaration
 13:5: Function signature does not match previous declaration
 16:5: Function signature does not match previous declaration
+19:5: Function signature does not match previous declaration
+22:5: Function signature does not match previous declaration
 """
