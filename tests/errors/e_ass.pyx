d84f7744c7502eb9bd98daccbc0469f7f9988e23
Stefan Behnel <stefan_ml@behnel.de>
2014-10-25 12:21:13 +0200

    remove error test that is no longer an error

--- tests/errors/e_ass.pyx
+++ tests/errors/e_ass.pyx
@@ -14,7 +14,7 @@ ctypedef int[1] int_array
 
 
 cdef int_array x, y
-x = y # error
+x = y # not an error anymore
 
 cdef int_array *x_ptr = &x
 x_ptr[0] = y # error
@@ -24,8 +24,8 @@ cdef class A:
     def __init__(self):
         self.value = x # error
 
+
 _ERRORS = u"""
-17:2: Assignment to non-lvalue 'x'
 20:5: Assignment to non-lvalue of type 'int_array'
 25:12: Assignment to non-lvalue of type 'int_array'
 7:19: Cannot assign type 'char *' to 'int'
