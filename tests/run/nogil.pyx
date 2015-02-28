fc79443196b25f240e45f4c3f7a6fe42b75e336e
Stefan Behnel <stefan_ml@behnel.de>
2015-02-28 13:53:42 +0100

    allow "unraisable" warnings in nogil functions

--- tests/run/nogil.pyx
+++ tests/run/nogil.pyx
@@ -1,3 +1,11 @@
+# mode: run
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
 def test(int x):
     """
     >>> test(5)
@@ -35,3 +43,23 @@ def test_nogil_exception_propagation():
     """
     with nogil:
         nogil_func()
+
+
+cdef int write_unraisable() nogil:
+    with gil:
+        raise ValueError()
+
+
+def test_unraisable():
+    """
+    >>> print(test_unraisable())  # doctest: +ELLIPSIS
+    Exception...ignored...
+    """
+    import sys
+    old_stderr = sys.stderr
+    stderr = sys.stderr = StringIO()
+    try:
+        write_unraisable()
+    finally:
+        sys.stderr = old_stderr
+    return stderr.getvalue().strip()
