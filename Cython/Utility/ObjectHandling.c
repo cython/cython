35acc9d6305de9d38d3863843f0779a5674e4c5f
Stefan Behnel <stefan_ml@behnel.de>
2015-04-10 12:00:14 +0200

    minor utility code cleanup to avoid duplication

--- Cython/Utility/ObjectHandling.c
+++ Cython/Utility/ObjectHandling.c
@@ -480,12 +480,11 @@ static CYTHON_INLINE int __Pyx_PyObject_SetSlice(
 /////////////// SliceObject ///////////////
 
 {{if access == 'Get'}}
-static CYTHON_INLINE PyObject* __Pyx_PyObject_GetSlice(
-        PyObject* obj, Py_ssize_t cstart, Py_ssize_t cstop,
+static CYTHON_INLINE PyObject* __Pyx_PyObject_GetSlice(PyObject* obj,
 {{else}}
-static CYTHON_INLINE int __Pyx_PyObject_SetSlice(
-        PyObject* obj, PyObject* value, Py_ssize_t cstart, Py_ssize_t cstop,
+static CYTHON_INLINE int __Pyx_PyObject_SetSlice(PyObject* obj, PyObject* value,
 {{endif}}
+        Py_ssize_t cstart, Py_ssize_t cstop,
         PyObject** _py_start, PyObject** _py_stop, PyObject** _py_slice,
         int has_cstart, int has_cstop, CYTHON_NCP_UNUSED int wraparound) {
 #if CYTHON_COMPILING_IN_CPYTHON
