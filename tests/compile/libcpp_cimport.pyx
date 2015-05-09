d3f669a20f26ebf9f85fdd13a683e6af03cd804a
da_woods <n/a>
2015-05-09 11:23:51 +0100

    Fix the pxd file for libcpp.complex
    Previously it could not be cimported
    Also added a test case (for cimporting the whole of the libcpp
    standard library)

---
+++ tests/compile/libcpp_cimport.pyx
@@ -0,0 +1,21 @@
+# tag: cpp
+# mode: compile
+
+# test that we can cimport the c++ standard library
+# (don't run it)
+cimport libcpp.algorithm
+cimport libcpp.cast
+cimport libcpp.complex 
+cimport libcpp.deque
+cimport libcpp.list
+cimport libcpp.map
+cimport libcpp.pair
+cimport libcpp.queue
+cimport libcpp.set
+cimport libcpp.stack
+cimport libcpp.string
+cimport libcpp.unordered_map
+cimport libcpp.unordered_set
+cimport libcpp.utility
+cimport libcpp.vector
+cimport libcpp
