86e741dca8cf9d739cb3cfb05e3ac3205ee4bc2f
Stefan Behnel <stefan_ml@behnel.de>
2015-07-26 15:21:22 +0200

    go back to using StringIO for XML test reporter in Py2

--- Cython/Tests/xmlrunner.py
+++ Cython/Tests/xmlrunner.py
@@ -44,8 +44,11 @@ import os
 import sys
 import time
 from unittest import TestResult, _TextTestResult, TextTestRunner
-from io import StringIO
 import xml.dom.minidom
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO  # doesn't accept 'str' in Py2
 
 
 class XMLDocument(xml.dom.minidom.Document):
@@ -332,9 +335,10 @@ class _XMLTestResult(_TextTestResult):
 class XMLTestRunner(TextTestRunner):
     """A test runner class that outputs the results in JUnit like XML files.
     """
-    def __init__(self, output='.', stream=sys.stderr, descriptions=True, \
-        verbose=False, elapsed_times=True):
+    def __init__(self, output='.', stream=None, descriptions=True, verbose=False, elapsed_times=True):
         "Create a new instance of XMLTestRunner."
+        if stream is None:
+            stream = sys.stderr
         verbosity = (1, 2)[verbose]
         TextTestRunner.__init__(self, stream, descriptions, verbosity)
         self.output = output
