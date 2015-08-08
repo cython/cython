2e32985eca1e4094f36ae98fc85f965e921b6d49
Robert Bradshaw <robertwb@gmail.com>
2015-08-07 21:30:46 -0700

    Add test for unique_ptr.

---
+++ tests/run/cpp_smart_ptr.pyx
@@ -0,0 +1,20 @@
+# mode: run
+# tag: cpp
+
+from libcpp.memory cimport unique_ptr, shared_ptr
+
+cdef extern from "cpp_smart_ptr_helper.h":
+    cdef cppclass CountAllocDealloc:
+        CountAllocDealloc(int*, int*)
+
+def test_unique_ptr():
+    """
+    >>> test_unique_ptr()
+    """
+    cdef int alloc_count = 0, dealloc_count = 0
+    cdef unique_ptr[CountAllocDealloc] x_ptr
+    x_ptr.reset(new CountAllocDealloc(&alloc_count, &dealloc_count))
+    assert alloc_count == 1
+    x_ptr.reset()
+    assert alloc_count == 1
+    assert dealloc_count == 1
