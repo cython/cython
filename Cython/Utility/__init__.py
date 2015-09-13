3489c2211f037fd4a40be5d48459223be464f007
Stefan Behnel <stefan_ml@behnel.de>
2015-09-13 10:34:29 +0200

    explicitly cast digits during PyLong conversion to avoid relying on potentially unsafe C coercion (fixes ticket #865)

--- Cython/Utility/__init__.py
+++ Cython/Utility/__init__.py
@@ -6,8 +6,8 @@ def pylong_join(count, digits_ptr='digits', join_type='unsigned long'):
 
     (((d[2] << n) | d[1]) << n) | d[0]
     """
-    return ('(' * (count * 2) + "(%s)" % join_type + ' | '.join(
-        "%s[%d])%s)" % (digits_ptr, _i, " << PyLong_SHIFT" if _i else '')
+    return ('(' * (count * 2) + ' | '.join(
+        "(%s)%s[%d])%s)" % (join_type, digits_ptr, _i, " << PyLong_SHIFT" if _i else '')
         for _i in range(count-1, -1, -1)))
 
 
