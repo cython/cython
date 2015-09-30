8f692c662130ebe61673c44493b882c2260cb792
Stefan Behnel <stefan_ml@behnel.de>
2015-09-30 22:58:33 +0200

    delete dead test code

--- tests/run/pstats_profile_test.pyx
+++ tests/run/pstats_profile_test.pyx
@@ -44,13 +44,6 @@ __doc__ = u"""
     ...    pass
 """
 
-import sys
-if sys.version_info < (2,5):
-    # disable in earlier versions
-    __doc__ = """
->>> # nothing to test here ...
-"""
-
 cimport cython
 
 def test_profile(long N):
