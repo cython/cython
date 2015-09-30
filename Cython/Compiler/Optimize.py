aed0ff79e394c46deff24898b1c3aa31fe546048
Stefan Behnel <stefan_ml@behnel.de>
2015-09-30 15:27:59 +0200

    move extensive node building code for range step calculation into a separate method

--- Cython/Compiler/Optimize.py
+++ Cython/Compiler/Optimize.py
@@ -743,67 +743,8 @@ class IterationTransform(Visitor.EnvTransform):
                 else:
                     # evaluate the same expression as above at runtime
                     bound2_ref_node = UtilNodes.LetRefNode(bound2)
-                    spanning_type = PyrexTypes.spanning_type(bound1.type, bound2.type)
-                    if step.type.is_int and abs_step < 0x7FFF:
-                        # Avoid loss of integer precision warnings.
-                        spanning_step_type = PyrexTypes.spanning_type(spanning_type, PyrexTypes.c_int_type)
-                    else:
-                        spanning_step_type = PyrexTypes.spanning_type(spanning_type, step.type)
-
-                    if step_value < 0:
-                        begin_value = bound2_ref_node
-                        end_value = bound1
-                        final_op = '-'
-                    else:
-                        begin_value = bound1
-                        end_value = bound2_ref_node
-                        final_op = '+'
-
-                    bound1 = ExprNodes.binop_node(
-                        bound1.pos,
-                        operand1=ExprNodes.binop_node(
-                            bound1.pos,
-                            operand1=bound2_ref_node,
-                            operator=final_op,  # +/-
-                            operand2=ExprNodes.MulNode(
-                                bound1.pos,
-                                operand1=ExprNodes.IntNode(
-                                    bound1.pos,
-                                    value=str(abs_step),
-                                    constant_result=abs_step,
-                                    type=spanning_step_type),
-                                operator='*',
-                                operand2=ExprNodes.DivNode(
-                                    bound1.pos,
-                                    operand1=ExprNodes.SubNode(
-                                        bound1.pos,
-                                        operand1=ExprNodes.SubNode(
-                                            bound1.pos,
-                                            operand1=begin_value,
-                                            operator='-',
-                                            operand2=end_value,
-                                            type=spanning_type),
-                                        operator='-',
-                                        operand2=ExprNodes.IntNode(
-                                            bound1.pos,
-                                            value='1',
-                                            constant_result=1),
-                                        type=spanning_step_type),
-                                    operator='//',
-                                    operand2=ExprNodes.IntNode(
-                                        bound1.pos,
-                                        value=str(abs_step),
-                                        constant_result=abs_step,
-                                        type=spanning_step_type),
-                                    type=spanning_step_type),
-                                type=spanning_step_type),
-                            type=spanning_step_type),
-                        operator=final_op,  # +/-
-                        operand2=ExprNodes.IntNode(
-                            bound1.pos,
-                            value='1',
-                            constant_result=1),
-                        type=spanning_type)
+                    bound1 = self._build_range_step_calculation(
+                        bound1, bound2_ref_node, step, step_value)
 
         if step_value < 0:
             step_value = -step_value
@@ -831,6 +772,70 @@ class IterationTransform(Visitor.EnvTransform):
             for_node = UtilNodes.LetNode(bound2, for_node)
 
         return for_node
+
+    def _build_range_step_calculation(self, bound1, bound2_ref_node, step, step_value):
+        abs_step = abs(step_value)
+        spanning_type = PyrexTypes.spanning_type(bound1.type, bound2_ref_node.type)
+        if step.type.is_int and abs_step < 0x7FFF:
+            # Avoid loss of integer precision warnings.
+            spanning_step_type = PyrexTypes.spanning_type(spanning_type, PyrexTypes.c_int_type)
+        else:
+            spanning_step_type = PyrexTypes.spanning_type(spanning_type, step.type)
+        if step_value < 0:
+            begin_value = bound2_ref_node
+            end_value = bound1
+            final_op = '-'
+        else:
+            begin_value = bound1
+            end_value = bound2_ref_node
+            final_op = '+'
+
+        step_calculation_node = ExprNodes.binop_node(
+            bound1.pos,
+            operand1=ExprNodes.binop_node(
+                bound1.pos,
+                operand1=bound2_ref_node,
+                operator=final_op,  # +/-
+                operand2=ExprNodes.MulNode(
+                    bound1.pos,
+                    operand1=ExprNodes.IntNode(
+                        bound1.pos,
+                        value=str(abs_step),
+                        constant_result=abs_step,
+                        type=spanning_step_type),
+                    operator='*',
+                    operand2=ExprNodes.DivNode(
+                        bound1.pos,
+                        operand1=ExprNodes.SubNode(
+                            bound1.pos,
+                            operand1=ExprNodes.SubNode(
+                                bound1.pos,
+                                operand1=begin_value,
+                                operator='-',
+                                operand2=end_value,
+                                type=spanning_type),
+                            operator='-',
+                            operand2=ExprNodes.IntNode(
+                                bound1.pos,
+                                value='1',
+                                constant_result=1),
+                            type=spanning_step_type),
+                        operator='//',
+                        operand2=ExprNodes.IntNode(
+                            bound1.pos,
+                            value=str(abs_step),
+                            constant_result=abs_step,
+                            type=spanning_step_type),
+                        type=spanning_step_type),
+                    type=spanning_step_type),
+                type=spanning_step_type),
+            operator=final_op,  # +/-
+            operand2=ExprNodes.IntNode(
+                bound1.pos,
+                value='1',
+                constant_result=1),
+            type=spanning_type)
+        return step_calculation_node
 
     def _transform_dict_iteration(self, node, dict_obj, method, keys, values):
         temps = []
