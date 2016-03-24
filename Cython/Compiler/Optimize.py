9c7dac3bdda2fbae69a06eae5ca7791bf8144bdd
Stefan Behnel <stefan_ml@behnel.de>
2016-03-24 20:08:36 +0100

    improve comments

--- Cython/Compiler/Optimize.py
+++ Cython/Compiler/Optimize.py
@@ -1519,13 +1519,13 @@ class EarlyReplaceBuiltinCalls(Visitor.EnvTransform):
     def _handle_simple_function_all(self, node, pos_args):
         """Transform
 
-        _result = all(x for L in LL for x in L)
+        _result = all(p(x) for L in LL for x in L)
 
         into
 
         for L in LL:
             for x in L:
-                if not x:
+                if not p(x):
                     _result = False
                     break
             else:
@@ -1539,13 +1539,13 @@ class EarlyReplaceBuiltinCalls(Visitor.EnvTransform):
     def _handle_simple_function_any(self, node, pos_args):
         """Transform
 
-        _result = any(x for L in LL for x in L)
+        _result = any(p(x) for L in LL for x in L)
 
         into
 
         for L in LL:
             for x in L:
-                if x:
+                if p(x):
                     _result = True
                     break
             else:
