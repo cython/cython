de660bb02be12374f0b0607ce747f25b06718878
Stefan Behnel <stefan_ml@behnel.de>
2016-06-12 08:34:43 +0200

    fix test

--- tests/run/define_macro.pyx
+++ tests/run/define_macro.pyx
@@ -1,4 +1,4 @@
-#distutils: define_macros = DEFINE_NO_VALUE, DEFINE_WITH_VALUE=0
+#distutils: define_macros = DEFINE_NO_VALUE  DEFINE_WITH_VALUE=0
 
 cdef extern from "define_macro_helper.h" nogil:
     int VAL;
