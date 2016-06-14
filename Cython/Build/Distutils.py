9f0e25f72b581201233a2a814961c4bc9c77ba00
Robert Bradshaw <robertwb@gmail.com>
2016-06-14 10:22:04 -0700

    Simple cythonize build_ext.

---
+++ Cython/Build/Distutils.py
@@ -0,0 +1,13 @@
+import sys
+from .Dependencies import cythonize
+
+if 'setuptools' is sys.modules:
+    from setuptools.command import build_ext as _build_ext
+else:
+    from distutils.command import build_ext as _build_ext
+
+class build_ext(_build_ext.build_ext):
+    def finalize_options(self):
+        self.distribution.ext_modules[:] = cythonize(
+            self.distribution.ext_modules)
+        super(build_ext, self).finalize_options()
