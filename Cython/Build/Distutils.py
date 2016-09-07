76fc68acd2c5fe051ded254484c533f682d649e2
Robert Bradshaw <robertwb@gmail.com>
2016-09-07 09:29:54 -0700

    Move build_ext files around.
    The old build_ext is now named old_build_ext, replaced with the one from Cython/Build.

--- Cython/Build/Distutils.py
+++ Cython/Build/Distutils.py
@@ -1,13 +1,1 @@
-import sys
-from .Dependencies import cythonize
-
-if 'setuptools' in sys.modules:
-    from setuptools.command import build_ext as _build_ext
-else:
-    from distutils.command import build_ext as _build_ext
-
-class build_ext(_build_ext.build_ext, object):
-    def finalize_options(self):
-        self.distribution.ext_modules[:] = cythonize(
-            self.distribution.ext_modules)
-        super(build_ext, self).finalize_options()
+from Cython.Distutils.build_ext import build_ext
