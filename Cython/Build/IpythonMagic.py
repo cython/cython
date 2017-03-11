350f462a854cb5b4beb3d1f43587fbde6d60f6e1
syrte <styr.py@gmail.com>
2017-03-11 10:46:54 +0800

    pep 8 formatting

--- Cython/Build/IpythonMagic.py
+++ Cython/Build/IpythonMagic.py
@@ -87,7 +87,7 @@ from .Dependencies import cythonize
 class CythonMagics(Magics):
 
     def __init__(self, shell):
-        super(CythonMagics,self).__init__(shell)
+        super(CythonMagics, self).__init__(shell)
         self._reloads = {}
         self._code_cache = {}
         self._pyximport_installed = False
@@ -237,7 +237,7 @@ class CythonMagics(Magics):
             ...
         """
         args = magic_arguments.parse_argstring(self.cython, line)
-        code = cell if cell.endswith('\n') else cell+'\n'
+        code = cell if cell.endswith('\n') else cell + '\n'
         lib_dir = os.path.join(get_ipython_cache_dir(), 'cython')
         quiet = True
         key = code, line, sys.version_info, sys.executable, cython_version
@@ -275,14 +275,14 @@ class CythonMagics(Magics):
             with io.open(pyx_file, 'w', encoding='utf-8') as f:
                 f.write(code)
             extension = Extension(
-                name = module_name,
-                sources = [pyx_file] + c_src_files,
-                include_dirs = c_include_dirs,
-                library_dirs = args.library_dirs,
-                extra_compile_args = args.compile_args,
-                extra_link_args = args.link_args,
-                libraries = args.lib,
-                language = 'c++' if args.cplus else 'c',
+                name=module_name,
+                sources=[pyx_file] + c_src_files,
+                include_dirs=c_include_dirs,
+                library_dirs=args.library_dirs,
+                extra_compile_args=args.compile_args,
+                extra_link_args=args.link_args,
+                libraries=args.lib,
+                language='c++' if args.cplus else 'c',
             )
             build_extension = self._get_build_extension()
             try:
@@ -302,7 +302,7 @@ class CythonMagics(Magics):
 
         if not have_module:
             build_extension.build_temp = os.path.dirname(pyx_file)
-            build_extension.build_lib  = lib_dir
+            build_extension.build_lib = lib_dir
             build_extension.run()
             self._code_cache[key] = module_name
 
@@ -370,10 +370,10 @@ class CythonMagics(Magics):
         return html
 
 __doc__ = __doc__.format(
-                # rST doesn't see the -+ flag as part of an option list, so we
-                # hide it from the module-level docstring.
-                CYTHON_DOC = dedent(CythonMagics.cython.__doc__\
-                            .replace('-+, --cplus','--cplus    ')),
-                CYTHON_INLINE_DOC = dedent(CythonMagics.cython_inline.__doc__),
-                CYTHON_PYXIMPORT_DOC = dedent(CythonMagics.cython_pyximport.__doc__),
+    # rST doesn't see the -+ flag as part of an option list, so we
+    # hide it from the module-level docstring.
+    CYTHON_DOC=dedent(CythonMagics.cython.__doc__\
+                                  .replace('-+, --cplus', '--cplus    ')),
+    CYTHON_INLINE_DOC=dedent(CythonMagics.cython_inline.__doc__),
+    CYTHON_PYXIMPORT_DOC=dedent(CythonMagics.cython_pyximport.__doc__),
 )
