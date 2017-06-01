3392afe9883d2ac062c4f1bb2c96f996795a2466
Robert Bradshaw <robertwb@gmail.com>
2017-06-01 00:11:49 -0700

    Test for bad cimported module attributes.

---
+++ tests/errors/cimport_attributes.pyx
@@ -0,0 +1,25 @@
+# mode: error
+
+
+cimport libcpp
+print libcpp.no_such_attribute
+
+cimport libcpp.map
+print libcpp.map.no_such_attribute
+
+from libcpp cimport vector
+print vector.no_such_attribute
+
+from libcpp cimport vector as my_vector
+print my_vector.no_such_attribute
+
+from libcpp cimport vector as my_vector_with_shadow
+from libcpp import vector as my_vector_with_shadow
+print my_vector_with_shadow.no_such_attribute   # OK (if such a module existed at runtime)
+
+_ERRORS = u"""
+5:12: cimported module has no attribute 'no_such_attribute'
+8:16: cimported module has no attribute 'no_such_attribute'
+11:12: cimported module has no attribute 'no_such_attribute'
+14:15: cimported module has no attribute 'no_such_attribute'
+"""
