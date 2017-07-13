77970aa5767ed59c3b5f3910421d543f0895cca7
Stefan Behnel <stefan_ml@behnel.de>
2017-07-13 16:51:24 +0200

    extend fastcall test

--- tests/run/fastcall.pyx
+++ tests/run/fastcall.pyx
@@ -5,6 +5,8 @@ import sys
 import struct
 from collections import deque
 
+pack = struct.pack
+
 
 def deque_methods(v):
     """
@@ -16,6 +18,7 @@ def deque_methods(v):
     if sys.version_info >= (3, 5):
         d.insert(1, v)
     else:
+        # deque has no 2-args methods in older Python versions
         d.rotate(-1)
         d.appendleft(2)
         d.rotate(1)
@@ -30,13 +33,20 @@ def deque_methods(v):
 
 def struct_methods(v):
     """
-    >>> i, lf = struct_methods(2)
+    >>> i, lf, i2, f = struct_methods(2)
     >>> struct.unpack('i', i)
+    (2,)
+    >>> struct.unpack('i', i2)
     (2,)
     >>> struct.unpack('lf', lf)
     (2, 4.0)
+    >>> struct.unpack('f', f)
+    (2.0,)
     """
+    local_pack = pack
     return [
         struct.pack('i', v),
         struct.pack('lf', v, v*2),
+        pack('i', v),
+        local_pack('f', v),
     ]
