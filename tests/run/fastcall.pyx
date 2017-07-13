a081949051216cadfc8e053e02f378ee8da9ee94
Stefan Behnel <stefan_ml@behnel.de>
2017-07-13 11:42:52 +0200

    extend fastcall tests with more Python calls that use METH_FASTCALL in Py3.6+

--- tests/run/fastcall.pyx
+++ tests/run/fastcall.pyx
@@ -1,6 +1,7 @@
 # mode: run
 # tag: METH_FASTCALL
 
+import struct
 from collections import deque
 
 
@@ -19,3 +20,17 @@ def deque_methods(v):
     assert list(d) == [1,2,3,4]
 
     return list(d)
+
+
+def struct_methods(v):
+    """
+    >>> i, lf = struct_methods(2)
+    >>> struct.unpack('i', i)
+    (2,)
+    >>> struct.unpack('lf', lf)
+    (2, 4.0)
+    """
+    return [
+        struct.pack('i', v),
+        struct.pack('lf', v, v*2),
+    ]
