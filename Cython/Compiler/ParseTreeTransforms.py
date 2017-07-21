384f08f97baa4ee828777dacec61950fd00c543b
Stefan Behnel <stefan_ml@behnel.de>
2017-07-21 06:55:31 +0200

    Revert "Escape local variables in generated pickle helper functions to prevent conflicts with the user defined class name."
    This reverts commit a158967ed1056491c98cfd1f0f0d697c9e1a719a.

--- Cython/Compiler/ParseTreeTransforms.py
+++ Cython/Compiler/ParseTreeTransforms.py
@@ -1671,24 +1671,24 @@ if VALUE is not None:
             unpickle_func = TreeFragment(u"""
                 def %(unpickle_func_name)s(__pyx_type, long __pyx_checksum, __pyx_state):
                     if __pyx_checksum != %(checksum)s:
-                        from pickle import PickleError as __pyx_PickleError
-                        raise __pyx_PickleError("Incompatible checksums (%%s vs %(checksum)s = (%(members)s))" %% __pyx_checksum)
-                    __pyx_result = %(class_name)s.__new__(__pyx_type)
+                        from pickle import PickleError
+                        raise PickleError("Incompatible checksums (%%s vs %(checksum)s = (%(members)s))" %% __pyx_checksum)
+                    result = %(class_name)s.__new__(__pyx_type)
                     if __pyx_state is not None:
-                        %(unpickle_func_name)s__set_state(<%(class_name)s> __pyx_result, __pyx_state)
-                    return __pyx_result
+                        %(unpickle_func_name)s__set_state(<%(class_name)s> result, __pyx_state)
+                    return result
 
-                cdef %(unpickle_func_name)s__set_state(%(class_name)s __pyx_result, tuple __pyx_state):
+                cdef %(unpickle_func_name)s__set_state(%(class_name)s result, tuple __pyx_state):
                     %(assignments)s
-                    if hasattr(__pyx_result, '__dict__'):
-                        __pyx_result.__dict__.update(__pyx_state[%(num_members)s])
+                    if hasattr(result, '__dict__'):
+                        result.__dict__.update(__pyx_state[%(num_members)s])
                 """ % {
                     'unpickle_func_name': unpickle_func_name,
                     'checksum': checksum,
                     'members': ', '.join(all_members_names),
                     'class_name': node.class_name,
                     'assignments': '; '.join(
-                        '__pyx_result.%s = __pyx_state[%s]' % (v, ix)
+                        'result.%s = __pyx_state[%s]' % (v, ix)
                         for ix, v in enumerate(all_members_names)),
                     'num_members': len(all_members_names),
                 }, level='module', pipeline=[NormalizeTree(None)]).substitute({})
