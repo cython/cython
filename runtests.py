631e8b33a8ac11351e251aec69a354675d5a80c1
Stefan Behnel <stefan_ml@behnel.de>
2017-07-29 21:10:14 +0200

    test runner output fixes

--- runtests.py
+++ runtests.py
@@ -2141,11 +2141,11 @@ def runtests(options, cmd_args, coverage=None):
         test_runner = XMLTestRunner(output=xml_output_dir,
                                     verbose=options.verbosity > 0)
         if options.failfast:
-            sys.stderr.write("--failfast not supported with XML runner")
+            sys.stderr.write("--failfast not supported with XML runner\n")
     else:
         text_runner_options = {}
         if options.failfast and sys.version_info < (2, 7):
-            sys.stderr.write("--failfast not supported with Python < 2.7")
+            sys.stderr.write("--failfast not supported with Python < 2.7\n")
         else:
             text_runner_options['failfast'] = True
         test_runner = unittest.TextTestRunner(verbosity=options.verbosity, **text_runner_options)
