94d982d83c848ba2a7421c8a987849a163dd5511
Stefan Behnel <stefan_ml@behnel.de>
2017-07-31 23:57:15 +0200

    repair "except" on tuples

--- Cython/Utility/Exceptions.c
+++ Cython/Utility/Exceptions.c
@@ -35,6 +35,17 @@ static CYTHON_INLINE int __Pyx_PyErr_ExceptionMatchesInState(PyThreadState* tsta
     PyObject *exc_type = tstate->curexc_type;
     if (exc_type == err) return 1;
     if (unlikely(!exc_type)) return 0;
+    if (unlikely(PyTuple_Check(err))) {
+        Py_ssize_t i, n;
+        n = PyTuple_GET_SIZE(err);
+        for (i=0; i<n; i++) {
+            if (exc_type == PyTuple_GET_ITEM(err, i)) return 1;
+        }
+        for (i=0; i<n; i++) {
+            if (__Pyx_PyErr_GivenExceptionMatches(exc_type, PyTuple_GET_ITEM(err, i))) return 1;
+        }
+        return 0;
+    }
     return __Pyx_PyErr_GivenExceptionMatches(exc_type, err);
 }
 #endif
