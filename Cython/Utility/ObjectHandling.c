13ab4d8a1b20d8a4a85172af8e13d07f8e48d455
Stefan Behnel <stefan_ml@behnel.de>
2017-11-05 21:19:36 +0100

    Simplify C helper code.

--- Cython/Utility/ObjectHandling.c
+++ Cython/Utility/ObjectHandling.c
@@ -276,21 +276,23 @@ static CYTHON_INLINE PyObject *__Pyx_PyObject_GetItem(PyObject *obj, PyObject* k
 
 #if CYTHON_USE_TYPE_SLOTS
 static PyObject *__Pyx_PyObject_GetIndex(PyObject *obj, PyObject* index) {
+    PyObject *runerr;
+    Py_ssize_t key_value;
     PySequenceMethods *m = Py_TYPE(obj)->tp_as_sequence;
-    if (likely(m && m->sq_item)) {
-        PyObject *runerr;
-        Py_ssize_t key_value = __Pyx_PyIndex_AsSsize_t(index);
-        if (likely(key_value != -1 || !(runerr = PyErr_Occurred()))) {
-            return __Pyx_GetItemInt_Fast(obj, key_value, 0, 1, 1);
-        }
-
-        // Error handling code -- only manage OverflowError differently.
-        if (PyErr_GivenExceptionMatches(runerr, PyExc_OverflowError)) {
-            PyErr_Clear();
-            PyErr_Format(PyExc_IndexError, "cannot fit '%.200s' into an index-sized integer", Py_TYPE(index)->tp_name);
-        }
-    } else {
+    if (unlikely(!(m && m->sq_item))) {
         PyErr_Format(PyExc_TypeError, "'%.200s' object is not subscriptable", Py_TYPE(obj)->tp_name);
+        return NULL;
+    }
+
+    key_value = __Pyx_PyIndex_AsSsize_t(index);
+    if (likely(key_value != -1 || !(runerr = PyErr_Occurred()))) {
+        return __Pyx_GetItemInt_Fast(obj, key_value, 0, 1, 1);
+    }
+
+    // Error handling code -- only manage OverflowError differently.
+    if (PyErr_GivenExceptionMatches(runerr, PyExc_OverflowError)) {
+        PyErr_Clear();
+        PyErr_Format(PyExc_IndexError, "cannot fit '%.200s' into an index-sized integer", Py_TYPE(index)->tp_name);
     }
     return NULL;
 }
