fff85888bee93fed6ccf1a178e67480a06cbc44f
Stefan Behnel <stefan_ml@behnel.de>
2017-11-06 21:21:03 +0100

    Provide more information when a test assertion fails.

--- tests/run/numpy_subarray.pyx
+++ tests/run/numpy_subarray.pyx
@@ -21,7 +21,7 @@ def test_record_subarray():
 
     # Make sure the dtype looks like we expect
     assert descr.fields == {'a': (py_numpy.dtype('int32'), 0),
-                            'b': (py_numpy.dtype(('<f8', (3, 3))), 4)}
+                            'b': (py_numpy.dtype(('<f8', (3, 3))), 4)}, descr.fields
 
     # Make sure that HASSUBARRAY is working
     assert not np.PyDataType_HASSUBARRAY(descr)
@@ -29,9 +29,9 @@ def test_record_subarray():
     assert np.PyDataType_HASSUBARRAY(b_descr)
 
     # Make sure the direct field access works
-    assert <tuple>b_descr.subarray.shape == (3, 3)
+    assert <tuple>b_descr.subarray.shape == (3, 3), <tuple>b_descr.subarray.shape
 
     # Make sure the safe high-level helper function works
-    assert np.PyDataType_SHAPE(descr) == ()
-    assert np.PyDataType_SHAPE(a_descr) == ()
-    assert np.PyDataType_SHAPE(b_descr) == (3, 3)
+    assert np.PyDataType_SHAPE(descr) == (), np.PyDataType_SHAPE(descr)
+    assert np.PyDataType_SHAPE(a_descr) == (), np.PyDataType_SHAPE(a_descr)
+    assert np.PyDataType_SHAPE(b_descr) == (3, 3), np.PyDataType_SHAPE(b_descr)
