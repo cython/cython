757c030e15f8ed317b90271f184d13ef5651aebb
Antoine Pitrou <antoine@python.org>
2017-12-16 18:58:32 +0100

    Issue #2042: add optimized versions of set.remove() and set.discard()

--- Cython/Utility/Optimize.c
+++ Cython/Utility/Optimize.c
@@ -397,6 +397,69 @@ static CYTHON_INLINE int __Pyx_dict_iter_next(
 }
 
 
+/////////////// py_set_discard.proto ///////////////
+
+static CYTHON_INLINE int __Pyx_PySet_Discard(PyObject *set, PyObject *key); /*proto*/
+
+/////////////// py_set_discard ///////////////
+
+static CYTHON_INLINE int __Pyx_PySet_Discard(PyObject *set, PyObject *key) {
+    PyObject *tmpkey;
+    int rv;
+
+    rv = PySet_Discard(set, key);
+    /* Convert *key* to frozenset if necessary */
+    if (rv < 0) {
+        if (!PySet_Check(key) || !PyErr_ExceptionMatches(PyExc_TypeError))
+            return -1;
+        PyErr_Clear();
+        tmpkey = PyFrozenSet_New(key);
+        if (tmpkey == NULL)
+            return -1;
+        rv = PySet_Discard(set, tmpkey);
+        Py_DECREF(tmpkey);
+    }
+    return rv;
+}
+
+
+/////////////// py_set_remove.proto ///////////////
+
+static CYTHON_INLINE int __Pyx_PySet_Remove(PyObject *set, PyObject *key); /*proto*/
+
+/////////////// py_set_remove ///////////////
+
+static CYTHON_INLINE int __Pyx_PySet_Remove(PyObject *set, PyObject *key) {
+    PyObject *tmpkey;
+    int rv;
+
+    rv = PySet_Discard(set, key);
+
+    /* Convert *key* to frozenset if necessary */
+    if (rv < 0) {
+        if (!PySet_Check(key) || !PyErr_ExceptionMatches(PyExc_TypeError))
+            return -1;
+        PyErr_Clear();
+        tmpkey = PyFrozenSet_New(key);
+        if (tmpkey == NULL)
+            return -1;
+        rv = PySet_Discard(set, tmpkey);
+        Py_DECREF(tmpkey);
+    }
+    if (rv == 0) {
+        /* Not found */
+        PyObject *tup;
+        tup = PyTuple_Pack(1, key);
+        if (!tup)
+            return -1;
+        PyErr_SetObject(PyExc_KeyError, tup);
+        Py_DECREF(tup);
+        return -1;
+    }
+    return 0;
+}
+
+
 /////////////// unicode_iter.proto ///////////////
 
 static CYTHON_INLINE int __Pyx_init_unicode_iteration(
