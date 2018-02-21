73914beb751f79afe45f2e031d0a3617b58fac5f
Myles Hollowed <mylesthollowed@gmail.com>
2018-02-20 21:54:24 -0800

    Add test coverage for build / usage / teardown

--- docs/examples/tutorial/rectangle_cpp/rect.pyx
+++ docs/examples/tutorial/rectangle_cpp/rect.pyx
@@ -36,10 +36,34 @@ cdef class PyRectangle:
     @property
     def x0(self):
         return self.c_rect.x0
-
     @x0.setter
     def x0(self, x0):
         self.c_rect.x0 = x0
+
+    # Attribute access
+    @property
+    def x1(self):
+        return self.c_rect.x1
+    @x1.setter
+    def x1(self, x1):
+        self.c_rect.x1 = x1
+
+
+    # Attribute access
+    @property
+    def y0(self):
+        return self.c_rect.y0
+    @y0.setter
+    def y0(self, y0):
+        self.c_rect.y0 = y0
+
+    # Attribute access
+    @property
+    def y1(self):
+        return self.c_rect.y1
+    @y1.setter
+    def y1(self, y1):
+        self.c_rect.y1 = y1
 
 
 def main():
