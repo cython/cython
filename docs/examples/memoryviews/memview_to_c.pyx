ae0d90403b6cdd12c114083b7cd074e5b687542a
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-05-26 10:27:23 +0200

    To fix the tests, a "cdef extern from" was used.

--- docs/examples/memoryviews/memview_to_c.pyx
+++ docs/examples/memoryviews/memview_to_c.pyx
@@ -1,5 +1,6 @@
-# distutils: sources = C_func_file.c
-# distutils: include_dirs = ./
+cdef extern from "C_func_file.c":
+    # C is include here so that it doesn't need to be compiled externally
+    pass
 
 cdef extern from "C_func_file.h":
     void multiply_by_10_in_C(double *, unsigned int)
