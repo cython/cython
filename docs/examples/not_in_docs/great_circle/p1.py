208c3553f19a6cbbd2a6d986f881bb6c32e2aaa6
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-05-30 11:31:56 +0200

    Put back the great_circle files in the examples/not_in_docs

---
+++ docs/examples/not_in_docs/great_circle/p1.py
@@ -0,0 +1,12 @@
+import math
+
+def great_circle(lon1, lat1, lon2, lat2):
+    radius = 3956 # miles
+    x = math.pi/180.0
+
+    a = (90.0 - lat1)*x
+    b = (90.0 - lat2)*x
+    theta = (lon2 - lon1)*x
+    c = math.acos(math.cos(a)*math.cos(b) + math.sin(a)*math.sin(b)*math.cos(theta))
+
+    return radius*c
