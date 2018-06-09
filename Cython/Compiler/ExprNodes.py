af96ff712929e5fa2fe8bf3e2ab4a50aebdfdd5d
Stefan Behnel <stefan_ml@behnel.de>
2018-06-09 07:16:43 +0200

    Simplify name lookup when analysing types by name.

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -1376,9 +1376,7 @@ def _analyse_name_as_type(name, pos, env):
     if type is not None:
         return type
 
-    global_entry = env.global_scope().lookup_here(name)
-    if global_entry is None:
-        global_entry = env.builtin_scope().lookup_here(name)
+    global_entry = env.global_scope().lookup(name)
     if global_entry and global_entry.type and (
             global_entry.type.is_extension_type
             or global_entry.type.is_struct_or_union
