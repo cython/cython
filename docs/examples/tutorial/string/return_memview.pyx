d17d4e96f7633a7b7848f2136102c5c58b91414e
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-06-20 23:41:22 +0200

    Put a dummy processing in the function rather than asking the user for decision.

--- docs/examples/tutorial/string/return_memview.pyx
+++ docs/examples/tutorial/string/return_memview.pyx
@@ -1,7 +1,9 @@
-def process_byte_data(unsigned char[:] data, bint return_all):
-    # ... process the data
+def process_byte_data(unsigned char[:] data):
+    # ... process the data, here, dummy processing.
+    cdef bint return_all = (data[0] == 108)
+
     if return_all:
         return bytes(data)
     else:
         # example for returning a slice
-        return bytes(data[5:35])
+        return bytes(data[5:7])
