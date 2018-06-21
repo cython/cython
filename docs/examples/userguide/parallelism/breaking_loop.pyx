a6ee14bbe3f2ed9556dd02be9ae1910d3e2b6b0f
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-06-21 19:59:11 +0200

    Moving an example from parallelism.rst to the examples directory for testing.

---
+++ docs/examples/userguide/parallelism/breaking_loop.pyx
@@ -0,0 +1,13 @@
+from cython.parallel import prange
+
+cdef int func(Py_ssize_t n):
+    cdef Py_ssize_t i
+
+    for i in prange(n, nogil=True):
+        if i == 8:
+            with gil:
+                raise Exception()
+        elif i == 4:
+            break
+        elif i == 2:
+            return i
