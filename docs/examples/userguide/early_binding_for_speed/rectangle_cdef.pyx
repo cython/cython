5472ad789de747d8329db296d463e856d5015e98
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-06-22 17:51:01 +0200

    Added examples from early_binding_for_speed.rst to the examples directory.

---
+++ docs/examples/userguide/early_binding_for_speed/rectangle_cdef.pyx
@@ -0,0 +1,22 @@
+cdef class Rectangle:
+    cdef int x0, y0
+    cdef int x1, y1
+
+    def __init__(self, int x0, int y0, int x1, int y1):
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
+
+    cdef int _area(self):
+        area = (self.x1 - self.x0) * (self.y1 - self.y0)
+        if area < 0:
+            area = -area
+        return area
+
+    def area(self):
+        return self._area()
+
+def rectArea(x0, y0, x1, y1):
+    rect = Rectangle(x0, y0, x1, y1)
+    return rect.area()
