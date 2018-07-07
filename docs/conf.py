0030408ac7219ea21352d9ab0b71cfcbab90de4c
Stefan Behnel <stefan_ml@behnel.de>
2018-07-07 21:30:25 +0200

    Try to make the project sources available to Sphinx autodoc. See #2180.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ YEAR = datetime.date.today().strftime('%Y')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.append(os.path.abspath('sphinxext'))
 
 # Import support for ipython console session syntax highlighting (lives
