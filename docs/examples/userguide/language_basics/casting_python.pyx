46e23ffe869f347c6d60a3df5fc9ee07d5c4bc73
gabrieldemarmiesse <gabriel.demarmiesse@teraki.com>
2018-07-11 17:41:15 +0200

    Fixing the casting_python test under windows.

--- docs/examples/userguide/language_basics/casting_python.pyx
+++ docs/examples/userguide/language_basics/casting_python.pyx
@@ -1,14 +1,13 @@
 from cpython.ref cimport PyObject
-from libc.stdint cimport uintptr_t
 
 python_string = "foo"
 
 cdef void* ptr = <void*>python_string
-cdef uintptr_t adress_in_c = <uintptr_t>ptr
+cdef Py_ssize_t adress_in_c = <Py_ssize_t>ptr
 address_from_void = adress_in_c        # address_from_void is a python int
 
 cdef PyObject* ptr2 = <PyObject*>python_string
-cdef uintptr_t address_in_c2 = <uintptr_t>ptr2
+cdef Py_ssize_t address_in_c2 = <Py_ssize_t>ptr2
 address_from_PyObject = address_in_c2  # address_from_PyObject is a python int
 
 assert address_from_void == address_from_PyObject == id(python_string)
