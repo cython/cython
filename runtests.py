3d4b322bba6fa54d0c15224cd54b4a8bda3006d7
Stefan Behnel <stefan_ml@behnel.de>
2018-08-20 21:46:03 +0200

    Exclude a test that seems platform specific (at least, it fails on MacOS-X for now obvious reason).

--- runtests.py
+++ runtests.py
@@ -227,6 +227,12 @@ def exclude_extension_in_pyver(*versions):
     return check
 
 
+def exclude_extension_on_platform(*platforms):
+    def check(ext):
+        return EXCLUDE_EXT if sys.platform in platforms else ext
+    return check
+
+
 def update_linetrace_extension(ext):
     ext.define_macros.append(('CYTHON_TRACE', 1))
     return ext
@@ -373,6 +379,7 @@ EXT_EXTRAS = {
     'tag:cpp11': update_cpp11_extension,
     'tag:trace' : update_linetrace_extension,
     'tag:bytesformat':  exclude_extension_in_pyver((3, 3), (3, 4)),  # no %-bytes formatting
+    'tag:no-macos':  exclude_extension_on_platform('darwin'),
 }
 
 
