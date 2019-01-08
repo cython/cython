60700546582a212fbc95de3c79cd3d7adb06daf0
Stefan Behnel <stefan_ml@behnel.de>
2019-01-08 18:41:28 +0100

    Code formatting.

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -318,8 +318,7 @@ def p_typecast(s):
     is_memslice = isinstance(base_type, Nodes.MemoryViewSliceTypeNode)
     is_template = isinstance(base_type, Nodes.TemplatedTypeNode)
     is_const_volatile = isinstance(base_type, Nodes.CConstOrVolatileTypeNode)
-    if (not is_memslice and not is_template and not is_const_volatile
-        and base_type.name is None):
+    if not is_memslice and not is_template and not is_const_volatile and base_type.name is None:
         s.error("Unknown type")
     declarator = p_c_declarator(s, empty = 1)
     if s.sy == '?':
@@ -330,8 +329,7 @@ def p_typecast(s):
     s.expect(">")
     operand = p_factor(s)
     if is_memslice:
-        return ExprNodes.CythonArrayNode(pos, base_type_node=base_type,
-                                         operand=operand)
+        return ExprNodes.CythonArrayNode(pos, base_type_node=base_type, operand=operand)
 
     return ExprNodes.TypecastNode(pos,
         base_type = base_type,
