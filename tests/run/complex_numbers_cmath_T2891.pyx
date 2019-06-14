22b75681674ce93516d3bfd806669811c1b050e0
Jerome Kieffer <jerome.kieffer@terre-adelie.org>
2019-06-14 19:02:17 +0200

    Try a test which is less sensitive signed zeros

--- tests/run/complex_numbers_cmath_T2891.pyx
+++ tests/run/complex_numbers_cmath_T2891.pyx
@@ -3,24 +3,13 @@
 
 cdef extern from "complex_numbers_c99_T398.h": pass
 
-from libc.math cimport M_PI
 from libc.complex cimport cimag, creal, cabs, carg
 
 
 def test_decomposing(double complex z):
     """
-    >>> test_decomposing(2j)
-    (0.0, 2.0, 2.0, 0.5)
-
-    >>> test_decomposing(2)
-    (2.0, 0.0, 2.0, 0.0)
-
-    >>> test_decomposing(-2j)
-    (-0.0, -2.0, 2.0, -0.5)
-
-    >>> test_decomposing(-2)
-    (-2.0, 0.0, 2.0, 1.0)
-
+    >>> test_decomposing(3+4j)
+    (3.0, 4.0, 5.0, 0.9272952180016122)
     """
-     
-    return (creal(z), cimag(z), cabs(z), carg(z)/M_PI)
+
+    return (creal(z), cimag(z), cabs(z), carg(z))
