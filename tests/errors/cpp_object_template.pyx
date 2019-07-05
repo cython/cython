c94695421ea2569c3c9d90ecfee62f3016bca52d
gsamain <gwenael.samain@nexedi.com>
2019-07-05 09:24:36 +0000

    Add cpp tag to some cpp error tests

--- tests/errors/cpp_object_template.pyx
+++ tests/errors/cpp_object_template.pyx
@@ -1,4 +1,5 @@
 # mode: error
+# tag: cpp
 
 from libcpp.vector cimport vector
 
@@ -12,6 +13,6 @@ def main():
     va.push_back(A())
 
 _ERRORS = u"""
-9:16: Python object type 'Python object' cannot be used as a template argument
-11:16: Python object type 'A' cannot be used as a template argument
+10:16: Python object type 'Python object' cannot be used as a template argument
+12:16: Python object type 'A' cannot be used as a template argument
 """
