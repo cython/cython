e824b37afeee54f2c184698f9e96adfdae38cfd0
William Schwartz <wkschwartz@gmail.com>
2019-07-20 15:30:22 -0500

    Fix #3047: Add PyMem_(Raw)Calloc to cpython.mem
    CPython added PyMem_RawCalloc and PyMem_Calloc in version 3.5. I did not
    in include a version guard for 3.4 because PyObject_Calloc was also
    added in 3.5 but it's already in this file without a version guard.
    
    I updated comments for other functions to reflect PyMem_Calloc but not
    PyMem_RawCalloc to match comments' currently only referencing
    PyMem_Malloc and friends.
    
    I did not modify Cython/Utility/ModuleSetupCode.c, where
    PyMem_RawMalloc, PyMem_RawRealloc, and PyMem_RawFree are #defined for
    Python 3.3 because (a) I couldn't figure out where these were being used
    and (b) they're defined to their non-raw versions, which strikes me as
    dangerous since the Raw versions allow use without the GIL but the
    non-Raw versions do not.
    
    Finally, I updated the tests in cpython_capi.pyx. I'm never sure when
    compilers decide code is dead and just ignore it, so I tried actually to
    write to the memory and return data from it. I can't figure out how to
    run Cython's tests, so... fingers crossed.

--- tests/run/cpython_capi.pyx
+++ tests/run/cpython_capi.pyx
@@ -5,18 +5,36 @@ from cpython cimport mem
 from cpython.pystate cimport PyGILState_Ensure, PyGILState_Release, PyGILState_STATE
 
 
+cdef short _assert_calloc(short * s, int n):
+    """Assert array ``s`` of length ``n`` is zero and return 3."""
+    s[0] += 1
+    s[n - 1] += 3
+    assert not s[0] and not s[n - 1]
+    for i in range(1, n - 1):
+        assert not s[i]
+    return s[n - 1]
+
+
 def test_pymalloc():
     """
     >>> test_pymalloc()
     3
     """
+    cdef short i
+    cdef short* s = <short*> mem.PyMem_Calloc(10, sizeof(*s))
+    if not s:
+        raise MemoryError()
+    try:
+        i = _assert_calloc(s, 10)
+    finally:
+        mem.PyMem_Free(m)
     cdef char* m2
     cdef char* m = <char*> mem.PyMem_Malloc(20)
     assert m
     try:
         m[0] = 1
         m[1] = 2
-        m[2] = 3
+        m[2] = i
         m2 = <char*> mem.PyMem_Realloc(m, 10)
         assert m2
         m = m2
@@ -30,16 +48,25 @@ def test_pymalloc_raw():
     >>> test_pymalloc_raw()
     3
     """
+    cdef short i
+    cdef short* s
     cdef char* m
     cdef char* m2 = NULL
     with nogil:
+        s = <short*> mem.PyMem_RawCalloc(10, sizeof(*s))
+        if not s:
+            raise MemoryError()
+        try:
+            i = _assert_calloc(s, 10)
+        finally:
+            mem.PyMem_Free(m)
         m = <char*> mem.PyMem_RawMalloc(20)
         if not m:
             raise MemoryError()
         try:
             m[0] = 1
             m[1] = 2
-            m[2] = 3
+            m[2] = i
             m2 = <char*> mem.PyMem_RawRealloc(m, 10)
             if m2:
                 m = m2
