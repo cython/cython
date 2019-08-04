f019888cf953f4ede655e8d16d5969bf53694175
realead <egor.dranischnikow@googlemail.com>
2019-08-04 08:28:15 +0200

    Replace custom command line parser with argparse (GH-3001)

--- Cython/Build/Cythonize.py
+++ Cython/Build/Cythonize.py
@@ -120,33 +120,8 @@ def run_distutils(args):
 
 
 def create_args_parser():
-    from argparse import ArgumentParser, Action
-
-    class ParseDirectivesAction(Action):
-        def __call__(self, parser, namespace, values, option_string=None):
-            old_directives = dict(getattr(namespace, self.dest,
-                                          Options.get_directive_defaults()))
-            directives = Options.parse_directive_list(
-                values, relaxed_bool=True, current_settings=old_directives)
-            setattr(namespace, self.dest, directives)
-
-    class ParseOptionsAction(Action):
-        def __call__(self, parser, namespace, values, option_string=None):
-            options = dict(getattr(namespace, self.dest, {}))
-            for opt in values.split(','):
-                if '=' in opt:
-                    n, v = opt.split('=', 1)
-                    v = v.lower() not in ('false', 'f', '0', 'no')
-                else:
-                    n, v = opt, True
-                options[n] = v
-            setattr(namespace, self.dest, options)
-
-    class ParseCompileTimeEnvAction(Action):
-        def __call__(self, parser, namespace, values, option_string=None):
-            old_env = dict(getattr(namespace, self.dest, {}))
-            new_env = Options.parse_compile_time_env(values, current_settings=old_env)
-            setattr(namespace, self.dest, new_env)
+    from argparse import ArgumentParser
+    from ..Compiler.CmdLine import ParseDirectivesAction, ParseOptionsAction, ParseCompileTimeEnvAction
 
     parser = ArgumentParser()
 
