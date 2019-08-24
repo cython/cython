c9dfe708d3e7abad331c16bc866d25896d2735dd
Stefan Behnel <stefan_ml@behnel.de>
2019-08-24 10:39:09 +0200

    Clean up the Lexicon.py generation script and use f-strings to prevent accidentally running it with older Python versions.

--- bin/cython-generate-lexicon.py
+++ bin/cython-generate-lexicon.py
@@ -11,20 +11,22 @@
 #    --overwrite    to update the existing Lexicon.py file
 #    --here         to create a copy of Lexicon.py in the current directory
 
+import functools
+import re
+import os
 import sys
 from io import StringIO
-import os
-import functools
 
 # Make sure we import the right Cython
 cythonpath, _ = os.path.split(os.path.realpath(__file__)) # bin directory
 cythonpath, _ = os.path.split(cythonpath)
-if os.path.exists(os.path.join(cythonpath,"Cython")):
+if os.path.exists(os.path.join(cythonpath, "Cython")):
     sys.path.insert(0, cythonpath)
     print("Found (and using) local cython directory")
 # else we aren't in a development directory
 
 from Cython.Compiler import Lexicon
+
 
 def main():
     arg = '--overwrite'
@@ -37,34 +39,23 @@ def main():
 """)
         return
 
-    generated_code = StringIO()
-    print("# generated with:\n #", sys.implementation.name, sys.version, file=generated_code)
-    print(file=generated_code)
-    print(start_expression(), file=generated_code)
-    print(file=generated_code)
-    print(cont_expression(), file=generated_code)
-    print(file=generated_code)
-    generated_code = generated_code.getvalue()
+    generated_code = (
+        f"# generated with:\n"
+        f"# {sys.implementation.name} {sys.version.splitlines()[0].strip()}\n"
+        "\n"
+        f"{generate_character_sets()}\n"
+    )
 
-    output = StringIO()
-    mode = 0 # 1 when found generated section, 2 afterwards
     print("Reading file", Lexicon.__file__)
-    with open(Lexicon.__file__,'r') as f:
-        for line in f:
-            if mode != 1:
-                output.write(line)
-            else:
-                if line.strip() == "# END GENERATED CODE":
-                    mode = 2
-                    output.write(line)
-            if mode == 0:
-                if line.strip() == "# BEGIN GENERATED CODE":
-                    mode = 1
-                    output.write(generated_code)
+    with open(Lexicon.__file__, 'r') as f:
+        parts = re.split(r"(# (?:BEGIN|END) GENERATED CODE\n?)", f.read())
 
-    if mode != 2:
+    if len(parts) not in (4,5) or ' GENERATED CODE' not in parts[1] or ' GENERATED CODE' not in parts[3]:
         print("Warning: generated code section not found - code not inserted")
         return
+
+    parts[2] = generated_code
+    output = "".join(parts)
 
     if arg == "--here":
         outfile = "Lexicon.py"
@@ -73,23 +64,26 @@ def main():
         outfile = Lexicon.__file__
 
     print("Writing to file", outfile)
-    with open(outfile,'w') as f:
-        f.write(output.getvalue())
+    with open(outfile, 'w') as f:
+        f.write(output)
 
 
 # The easiest way to generate an appropriate character set is just to use the str.isidentifier method
 # An alternative approach for getting character sets is at https://stackoverflow.com/a/49332214/4657412
-@functools.lru_cache(None)
+@functools.lru_cache()
 def get_start_characters_as_number():
     return [ i for i in range(sys.maxunicode) if str.isidentifier(chr(i)) ]
+
 
 def get_continue_characters_as_number():
     return [ i for i in range(sys.maxunicode) if str.isidentifier('a'+chr(i)) ]
 
+
 def get_continue_not_start_as_number():
     start = get_start_characters_as_number()
     cont = get_continue_characters_as_number()
-    return sorted(set(cont)-set(start))
+    return sorted(set(cont) - set(start))
+
 
 def to_ranges(char_num_list):
     # Convert the large lists of character digits to
@@ -106,47 +100,32 @@ def to_ranges(char_num_list):
             if first_good_val == char_num_list[n-1]:
                 single_chars.append(chr(char_num_list[n-1]))
             else:
-                ranges.append(chr(first_good_val)+chr(char_num_list[n-1]))
+                ranges.append(chr(first_good_val) + chr(char_num_list[n-1]))
             first_good_val = char_num_list[n]
-    return single_chars, ranges
 
-def make_split_strings(chars, splitby=60):
-    out = []
-    for i in range(0, len(chars), splitby):
-        out.append('u"{}"'.format("".join(chars[i:i+splitby])))
-    return "\n    ".join(out)
+    return ''.join(single_chars), ''.join(ranges)
 
-def start_expression():
-    output = StringIO()
-    print("unicode_start_ch_any = (\n    ", end='', file=output)
 
-    single_chars, ranges = to_ranges(get_start_characters_as_number())
-    single_chars = "".join(single_chars)
-    ranges = "".join(ranges)
+def make_split_strings(chars, splitby=60, indent="    "):
+    lines = [f'u"{chars[i:i+splitby]}"' for i in range(0, len(chars), splitby)]
+    return indent + f"\n{indent}".join(lines)
 
-    print(make_split_strings(single_chars), end='', file=output)
-    print(")", file=output)
-    print("unicode_start_ch_range = (\n    ", end='', file=output)
-    print(make_split_strings(ranges), end='', file=output)
-    print(")", file=output)
 
-    return output.getvalue()
+def generate_character_sets():
+    declarations = []
+    for char_type, char_generator in [
+        ("unicode_start_ch", get_start_characters_as_number),
+        ("unicode_continuation_ch", get_continue_not_start_as_number),
+    ]:
+        for set_type, chars in zip(("any", "range"), to_ranges(char_generator())):
+            declarations.append(
+                f"{char_type}_{set_type} = (\n"
+                f"{make_split_strings(chars)}\n"
+                f")\n"
+            )
 
-def cont_expression():
-    output = StringIO()
-    print("unicode_continuation_ch_any = (\n    ", end='', file=output)
+    return "".join(declarations)
 
-    single_chars, ranges = to_ranges(get_continue_not_start_as_number())
-    single_chars = "".join(single_chars)
-    ranges = "".join(ranges)
-
-    print(make_split_strings(single_chars), end='', file=output)
-    print(")", file=output)
-    print("unicode_continuation_ch_range = (\n    ", end='', file=output)
-    print(make_split_strings(ranges), end='', file=output)
-    print(")", file=output)
-
-    return output.getvalue()
 
 if __name__ == "__main__":
     main()
