27f9ab8d24130c90637c9b01ec79aa92d0dde51f
da-woods <dw-git@d-woods.co.uk>
2019-09-01 20:56:48 +0100

    Fixed broken building using Extension on Python2

--- Cython/Build/Dependencies.py
+++ Cython/Build/Dependencies.py
@@ -783,7 +783,8 @@ def create_extension_list(patterns, exclude=None, ctx=None, aliases=None, quiet=
     create_extension = ctx.options.create_extension or default_create_extension
 
     for pattern in patterns:
-        pattern = encode_filename_in_py2(pattern)
+        if sys.version_info[0]==2 and isinstance(pattern, unicode):
+            pattern = encode_filename_in_py2(pattern)
         if isinstance(pattern, str):
             filepattern = pattern
             template = Extension(pattern, [])  # Fake Extension without sources
