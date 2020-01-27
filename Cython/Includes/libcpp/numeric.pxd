5a806e64cef4b0aba173dc316603e70ad836ade8
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-27 17:38:32 -0500

    Add second version of accumulate with binary operations.

--- Cython/Includes/libcpp/numeric.pxd
+++ Cython/Includes/libcpp/numeric.pxd
@@ -1,6 +1,11 @@
 cdef extern from "<numeric>" namespace "std" nogil:
     T inner_product[InputIt1, InputIt2, T](InputIt1 first1, InputIt1 last1, InputIt2 first2, T init)
 
+    T inner_product[InputIt1, InputIt2, T, BinaryOperation1, BinaryOperation2](InputIt1 first1, InputIt1 last1,
+                                                                               InputIt2 first2, T init,
+                                                                               BinaryOperation1 op1,
+                                                                               BinaryOperation2 op2)
+
     void iota[ForwardIt, T](ForwardIt first, ForwardIt last, T value)
 
     T accumulate[InputIt, T](InputIt first, InputIt last, T init)
--- tests/run/cpp_stl_numeric_ops.pyx
+++ tests/run/cpp_stl_numeric_ops.pyx
@@ -3,6 +3,20 @@
 
 from libcpp.numeric cimport inner_product, iota, accumulate
 from libcpp.vector cimport vector
+from libcpp cimport bool
+
+# Subtracts two integers.
+cdef int subtract_integers(int lhs, int rhs):
+    return lhs - rhs
+
+# Adds two integers.
+cdef int add_integers(int lhs, int rhs):
+    return lhs + rhs
+
+# Determines equality for two integers.
+# If lhs == rhs, returns true. Returns false otherwise.
+cdef bool is_equal(int lhs, int rhs):
+    return lhs == rhs
 
 def test_inner_product(vector[int] v1, vector[int] v2, int init):
     """
@@ -21,6 +35,19 @@ def test_inner_product_with_zero(vector[int] v1, vector[int] v2, int init):
     """
     return inner_product(v1.begin(), v1.end(), v2.begin(), init)
 
+def test_inner_product_with_bin_op(vector[int] v1, vector[int] v2, int init):
+    """
+    Test inner_product with two binary operations. In this case,
+    Looks at number of pairwise matches between v1 and v2.
+    [5, 1, 2, 3, 4]
+    [5, 4, 2, 3, 1]
+    There are 3 matches (5, 2, 3). So, 1 + 1 + 1 = 3.
+
+    >>> test_inner_product_with_bin_op([5, 1, 2, 3, 4], [5, 4, 2, 3, 1], 0)
+    3
+    """
+    return inner_product(v1.begin(), v1.end(), v2.begin(), init, add_integers, is_equal)
+
 def test_iota(vector[int] v, int value):
     """
     Test iota with beginning value of 0.
@@ -38,9 +65,6 @@ def test_iota_negative_init(vector[int] v, int value):
     """
     iota(v.begin(), v.end(), value)
     return v
-
-cdef int subtract_integers(int lhs, int rhs):
-    return lhs - rhs
 
 def test_accumulate_with_subtraction(vector[int] v, int init):
     """
