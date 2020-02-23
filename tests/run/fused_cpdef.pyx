9bdff2579795bffd3502060424c84f724a142993
Will <willchencontact@gmail.com>
2020-02-23 11:08:02 +0000

    Added test to ensure fused cpdef's can be called by the module itself during import.

--- tests/run/fused_cpdef.pyx
+++ tests/run/fused_cpdef.pyx
@@ -1,13 +1,17 @@
 cimport cython
+from Cython.Compiler.Naming import fused_cpdef_globalindex
+import sys
 
 cy = __import__("cython")
 
-cpdef func1(self, cython.integral x):
-    print "%s," % (self,),
+cpdef func1(self, cython.integral x, printto=None):
+    if printto is None:
+        printto = sys.stdout
+    print >> printto, "%s," % (self,),
     if cython.integral is int:
-        print 'x is int', x, cython.typeof(x)
+        print >> printto, 'x is int', x, cython.typeof(x)
     else:
-        print 'x is long', x, cython.typeof(x)
+        print >> printto, 'x is long', x, cython.typeof(x)
 
 
 class A(object):
@@ -18,7 +22,7 @@ class A(object):
 
 pyfunc = func1
 
-def test_fused_cpdef():
+def test_fused_cpdef(printto=None):
     """
     >>> test_fused_cpdef()
     None, x is int 2 int
@@ -33,21 +37,55 @@ def test_fused_cpdef():
     A, x is long 2 long
     A, x is long 2 long
     """
-    func1[int](None, 2)
-    func1[long](None, 2)
-    func1(None, 2)
+    if printto is None:
+        printto = sys.stdout
+    func1[int](None, 2, printto)
+    func1[long](None, 2, printto)
+    func1(None, 2, printto=printto)
 
-    print
+    print >> printto
 
-    pyfunc[cy.int](None, 2)
-    pyfunc(None, 2)
+    pyfunc[cy.int](None, 2, printto=printto)
+    pyfunc(None, 2, printto=printto)
 
-    print
+    print >> printto
 
-    A.meth[cy.int](A(), 2)
-    A.meth(A(), 2)
-    A().meth[cy.long](2)
-    A().meth(2)
+    A.meth[cy.int](A(), 2, printto=printto)
+    A.meth(A(), 2, printto=printto)
+    A().meth[cy.long](2, printto=printto)
+    A().meth(2, printto=printto)
+
+
+class fakestdout(object):
+    def __init__(self):
+        self.sequence = []
+    def write(self, content):
+        self.sequence.append(content)
+    def __str__(self):
+        return ''.join(self.sequence)
+
+midimport_run = fakestdout()
+try:
+    test_fused_cpdef(printto=midimport_run)
+except Exception as e:
+    print >> midimport_run, repr(e)
+
+def test_midimport_run():
+    """
+    >>> test_midimport_run()
+    None, x is int 2 int
+    None, x is long 2 long
+    None, x is long 2 long
+    <BLANKLINE>
+    None, x is int 2 int
+    None, x is long 2 long
+    <BLANKLINE>
+    A, x is int 2 int
+    A, x is long 2 long
+    A, x is long 2 long
+    A, x is long 2 long
+    """
+    print midimport_run
 
 
 def assert_raise(func, *args):
@@ -63,8 +101,8 @@ def test_badcall():
     >>> test_badcall()
     """
     assert_raise(pyfunc)
-    assert_raise(pyfunc, 1, 2, 3)
-    assert_raise(pyfunc[cy.int], 10, 11, 12)
+    assert_raise(pyfunc, 1, 2, None, 3)
+    assert_raise(pyfunc[cy.int], 10, 11, None, 12)
     assert_raise(pyfunc, None, object())
     assert_raise(A().meth)
     assert_raise(A.meth)
