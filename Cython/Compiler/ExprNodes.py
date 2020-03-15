f8e396843b52f5758bdffcbfe2088a6c4643c42d
da-woods <dw-git@d-woods.co.uk>
2020-03-15 17:45:39 +0000

    Only used PyUnicode_Concat on unicode objects
    https://github.com/cython/cython/issues/3426

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -11364,13 +11364,8 @@ class AddNode(NumBinopNode):
                 self, type1, type2)
 
     def py_operation_function(self, code):
-        is_unicode_concat = False
-        if isinstance(self.operand1, FormattedValueNode) or isinstance(self.operand2, FormattedValueNode):
-            is_unicode_concat = True
-        else:
-            type1, type2 = self.operand1.type, self.operand2.type
-            if type1 is unicode_type or type2 is unicode_type:
-                is_unicode_concat = type1.is_builtin_type and type2.is_builtin_type
+        type1, type2 = self.operand1.type, self.operand2.type
+        is_unicode_concat = type1 is unicode_type and type2 is unicode_type
 
         if is_unicode_concat:
             if self.operand1.may_be_none() or self.operand2.may_be_none():
