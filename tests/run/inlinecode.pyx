ad5f01a41177297cebfa21097df38898e135b9e8
will-ca <37680486+will-ca@users.noreply.github.com>
2020-03-17 02:36:19 +0000

    Update inlinecode.pyx

--- tests/run/inlinecode.pyx
+++ tests/run/inlinecode.pyx
@@ -12,6 +12,6 @@ def inline_langversion(language_level, a=5, b=2):
     >>> inline_langversion(3)
     2.5
     """
-    # Caching for inline code didn't always respect compiler directives.
+    # Caching for inline code didn't always respect language version.
     # https://github.com/cython/cython/issues/3419
     print(Shadow.inline(_inline_divcode, language_level=language_level, quiet=True)(a=a, b=b))
