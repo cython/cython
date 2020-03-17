e8ea61dba2387a519c53884fa16bc405e53d70da
da-woods <dw-git@d-woods.co.uk>
2020-03-17 07:44:15 +0000

    Corrected choice with str_type
    Better if unicode concat is done with more settings of str_type

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -11364,11 +11364,13 @@ class AddNode(NumBinopNode):
                 self, type1, type2)
 
     def py_operation_function(self, code):
-        language_level = code.globalstate.directives['language_level'] or '3str'
-        unicode_types = ((unicode_type,) if language_level in ('2', '3str')
-                         else (unicode_type, str_type))
+        language_level = code.funcstate.scope.global_scope().context.language_level
         type1, type2 = self.operand1.type, self.operand2.type
-        is_unicode_concat = type1 in unicode_types and type2 in unicode_types
+        # in Py2 str_type is bytes but this can be concatted to a unicode_type string.
+        # in Py3 str_type is unicode so can be concatted to unicode_type or str_type
+        is_unicode_concat = (type1 in (unicode_type, str_type) and type2 in ((unicode_type, str_type))
+                                and not (type1 is str_type and type2 is str_type
+                                         and language_level == 2))
 
         if is_unicode_concat:
             if self.operand1.may_be_none() or self.operand2.may_be_none():
