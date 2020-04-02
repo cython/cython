b0f8ad4707b76460d3c1fdb8954228837ebae422
Will Ayd <william.ayd@icloud.com>
2020-04-02 16:13:45 -0700

    Replaced mutable default argument

--- Cython/Compiler/FusedNode.py
+++ Cython/Compiler/FusedNode.py
@@ -592,6 +592,7 @@ class FusedCFuncDefNode(StatListNode):
         pyx_code.put_chunk(
             u"""
                 if not _fused_sigindex:
+                    _fused_sigindex = {}
                     for sig in <dict>signatures:
                         sigindex_node = _fused_sigindex
                         sig_series = sig.strip('()').split('|')
@@ -640,7 +641,7 @@ class FusedCFuncDefNode(StatListNode):
 
         pyx_code.put_chunk(
             u"""
-                def __pyx_fused_cpdef(signatures, args, kwargs, defaults, *, _fused_sigindex={}):
+                def __pyx_fused_cpdef(signatures, args, kwargs, defaults, *, _fused_sigindex=None):
                     # FIXME: use a typed signature - currently fails badly because
                     #        default arguments inherit the types we specify here!
 
