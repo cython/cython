b709334b7af7cbafa017a8f27dc9bbeafda5af94
Stefan Behnel <stefan_ml@behnel.de>
2020-04-23 14:37:44 +0200

    Remove unused import.

--- Cython/Compiler/AutoDocTransforms.py
+++ Cython/Compiler/AutoDocTransforms.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function
 from .Visitor import CythonTransform
 from .StringEncoding import EncodedString
 from . import Options
-from . import PyrexTypes, ExprNodes
+from . import PyrexTypes
 from ..CodeWriter import ExpressionWriter
 from .Errors import warning
 
