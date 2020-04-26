603329c4c01a6ad7c5fd0373f0e006fd4fd6b327
da-woods <dw-git@d-woods.co.uk>
2020-04-26 13:39:10 +0100

    Revert "Removed __pyx special case (no longer needed)"
    This reverts commit 1626810f88ce1a4eb9d7b20fc85031bf427a6c4a.

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -2033,6 +2033,9 @@ class ClassScope(Scope):
     #  doc    string or None   Doc string
 
     def mangle_class_private_name(self, name):
+        # a few utilitycode names need to specifically be ignored
+        if name and name.lower().startswith("__pyx_"):
+            return name
         if name and name.startswith('__') and not name.endswith('__'):
             name = EncodedString('_%s%s' % (self.class_name.lstrip('_'), name))
         return name
