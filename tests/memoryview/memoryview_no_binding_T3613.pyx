0b31797e24878b113986981230c73809480505cb
da-woods <dw-git@d-woods.co.uk>
2020-05-16 19:46:45 +0100

    Updated tests to add decorated functions

--- tests/memoryview/memoryview_no_binding_T3613.pyx
+++ tests/memoryview/memoryview_no_binding_T3613.pyx
@@ -7,5 +7,15 @@
 # inconsistent state where different directives were applied at different stages
 # of the pipeline
 
+import cython
+
 def f(double[:] a):
+    pass
+
+@cython.binding(False)
+def g(double[:] a):
+    pass
+
+@cython.binding(True)
+def g(double[:] a):
     pass
