765d97542beef6806e817d12a03b05d0b17a3d5f
scoder <stefan_ml@behnel.de>
2020-08-12 08:32:32 +0200

    Fix doctest syntax.

--- tests/run/time_pxd.pyx
+++ tests/run/time_pxd.pyx
@@ -19,15 +19,15 @@ def test_time():
 def test_localtime():
     """
     >>> ltp, ltc = test_localtime()
-    >>> assert ltp.tm_year == ltc.tm_year
-    >>> assert ltp.tm_mon == ltc.tm_mon
-    >>> assert ltp.tm_mday == ltc.tm_mday
-    >>> assert ltp.tm_hour == ltc.tm_hour
-    >>> assert ltp.tm_min == ltc.tm_min
-    >>> assert ltp.tm_sec == ltc.tm_sec
-    >>> assert ltp.tm_wday == ltc.tm_wday
-    >>> assert ltp.tm_yday == ltc.tm_yday
-    >>> assert ltp.tm_isdst == ltc.tm_isdst
+    >>> assert ltp['tm_year'] == ltc['tm_year']
+    >>> assert ltp['tm_mon'] == ltc['tm_mon']
+    >>> assert ltp['tm_mday'] == ltc['tm_mday']
+    >>> assert ltp['tm_hour'] == ltc['tm_hour']
+    >>> assert ltp['tm_min'] == ltc['tm_min']
+    >>> assert ltp['tm_sec'] == ltc['tm_sec']
+    >>> assert ltp['tm_wday'] == ltc['tm_wday']
+    >>> assert ltp['tm_yday'] == ltc['tm_yday']
+    >>> assert ltp['tm_isdst'] == ltc['tm_isdst']
     """
     ltp = time.localtime()
     ltc = ctime.localtime()
