10aab7d2df210d8d12cece7d624e4dc1b96a0cf5
scoder <stefan_ml@behnel.de>
2020-08-12 08:36:16 +0200

    Let doctest do its work rather than calling "assert".

--- tests/run/time_pxd.pyx
+++ tests/run/time_pxd.pyx
@@ -6,7 +6,12 @@ from cpython cimport time as ctime
 def test_time():
     """
     >>> tic1, tic2, tic3 = test_time()
-    >>> assert tic1 <= tic2 <= tic3
+    >>> tic1 <= tic3  # sanity check
+    True
+    >>> tic1 <= tic2
+    True
+    >>> tic2 <= tic3
+    True
     """
     # check that ctime.time() matches time.time() to within call-time tolerance
     tic1 = time.time()
@@ -19,15 +24,24 @@ def test_time():
 def test_localtime():
     """
     >>> ltp, ltc = test_localtime()
-    >>> assert ltp['tm_year'] == ltc['tm_year']
-    >>> assert ltp['tm_mon'] == ltc['tm_mon']
-    >>> assert ltp['tm_mday'] == ltc['tm_mday']
-    >>> assert ltp['tm_hour'] == ltc['tm_hour']
-    >>> assert ltp['tm_min'] == ltc['tm_min']
-    >>> assert ltp['tm_sec'] == ltc['tm_sec']
-    >>> assert ltp['tm_wday'] == ltc['tm_wday']
-    >>> assert ltp['tm_yday'] == ltc['tm_yday']
-    >>> assert ltp['tm_isdst'] == ltc['tm_isdst']
+    >>> ltp['tm_year'] == ltc['tm_year']
+    True
+    >>> ltp['tm_mon'] == ltc['tm_mon']
+    True
+    >>> ltp['tm_mday'] == ltc['tm_mday']
+    True
+    >>> ltp['tm_hour'] == ltc['tm_hour']
+    True
+    >>> ltp['tm_min'] == ltc['tm_min']
+    True
+    >>> ltp['tm_sec'] == ltc['tm_sec']
+    True
+    >>> ltp['tm_wday'] == ltc['tm_wday']
+    True
+    >>> ltp['tm_yday'] == ltc['tm_yday']
+    True
+    >>> ltp['tm_isdst'] == ltc['tm_isdst']
+    True
     """
     ltp = time.localtime()
     ltc = ctime.localtime()
