20783848ade2fb5faa006d6073b25afadb0a559a
Ashwin Srinath <shwina@users.noreply.github.com>
2020-09-05 08:22:21 -0400

    Parse into CppRvalueReferenceDeclaratorNode

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -2886,6 +2886,12 @@ def p_c_simple_declarator(s, ctx, empty, is_type, cmethod_flag,
                               cmethod_flag = cmethod_flag,
                               assignable = assignable, nonempty = nonempty)
         result = Nodes.CReferenceDeclaratorNode(pos, base = base)
+    elif s.sy == '&&':
+        s.next()
+        base = p_c_declarator(s, ctx, empty = empty, is_type = is_type,
+                              cmethod_flag = cmethod_flag,
+                              assignable = assignable, nonempty = nonempty)
+        result = Nodes.CppRvalueReferenceDeclaratorNode(pos, base = base)
     else:
         rhs = None
         if s.sy == 'IDENT':
