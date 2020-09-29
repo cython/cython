51c42086ecff9014de727a079fe5bdad8b4447fa
Ashwin Srinath <shwina@users.noreply.github.com>
2020-09-29 11:26:07 -0400

    Add `optional` arg to ExprNode._make_move_result_rhs

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -500,7 +500,10 @@ class ExprNode(Node):
         else:
             return self.calculate_result_code()
 
-    def _make_move_result_rhs(self, result, allow_move=True):
+    def _make_move_result_rhs(self, result, allow_move=True, optional=True):
+        if not optional:
+            self.has_temp_moved = True
+            return "std::move({0})".format(result)
         if (self.is_temp and allow_move and
                 self.type.is_cpp_class and not self.type.is_reference):
             self.has_temp_moved = True
@@ -513,11 +516,11 @@ class ExprNode(Node):
 
     def move_result_rhs_as(self, type):
         if type.is_rvalue_reference and self.is_temp:
-            self.has_temp_moved = True
-            return "std::move({0})".format(self.result_as(type))
-        allow_move = (type and not type.is_reference and not type.needs_refcounting)
-        return self._make_move_result_rhs(self.result_as(type),
-                                          allow_move=allow_move)
+            return self._make_move_result_rhs(self.result_as(type), optional=False)
+        else:
+            allow_move = (type and not type.is_reference and not type.needs_refcounting)
+            return self._make_move_result_rhs(self.result_as(type),
+                                              allow_move=allow_move)
 
     def pythran_result(self, type_=None):
         if is_pythran_supported_node_or_none(self):
