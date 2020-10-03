de7e48a0fe7062b542d3d3e4783733c0dff51ddf
scoder <stefan_ml@behnel.de>
2020-10-03 15:02:12 +0200

    Remove useless parentheses.

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -698,7 +698,7 @@ class MemoryViewSliceType(PyrexType):
         assert not dll_linkage
         from . import MemoryView
         base_code = StringEncoding.EncodedString(
-            (str(self)) if pyrex or for_display else MemoryView.memviewslice_cname)
+            str(self) if pyrex or for_display else MemoryView.memviewslice_cname)
         return self.base_declaration_code(
                 base_code,
                 entity_code)
