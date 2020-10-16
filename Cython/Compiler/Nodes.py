338465156e7f2b2f62999664b6d21a645474afb6
da-woods <dw-git@d-woods.co.uk>
2020-10-16 20:00:49 +0100

    Added a missing None-check

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -1078,7 +1078,7 @@ class CSimpleBaseTypeNode(CBaseTypeNode):
                         type = PyrexTypes.TemplatePlaceholderType(self.name)
                     else:
                         error(self.pos, "'%s' is not a type identifier" % self.name)
-        if type.is_fused and env.fused_to_specific:
+        if type and type.is_fused and env.fused_to_specific:
             type = type.specialize(env.fused_to_specific)
         if self.complex:
             if not type.is_numeric or type.is_complex:
