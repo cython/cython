fb90754ca40b1554690908156731f14fe6e1cda4
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2020-11-13 23:58:32 +0100

    [wip] try to find what are the returned strings

--- Cython/Build/Tests/TestIpythonMagic.py
+++ Cython/Build/Tests/TestIpythonMagic.py
@@ -158,13 +158,13 @@ class TestIPythonMagic(CythonTest):
         ip = self._ip
         with capture_output() as captured:
             ip.run_cell_magic('cython', '-3', compile_error_code)
-        self.assertTrue("error" in captured.stderr)
+        self.assertEqual("error", captured.stderr)
 
     def test_cython_link_error_shown(self):
         ip = self._ip
         with capture_output() as captured:
             ip.run_cell_magic('cython', '-3 -l=xxxxxxxx', code)
-        self.assertTrue("error" in captured.stderr)
+        self.assertEqual("error", captured.stderr)
 
     @skip_win32('Skip on Windows')
     def test_cython3_pgo(self):
