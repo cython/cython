8073ed277dd5b64c333436f25366ef334ebff97c
Max Bachmann <kontakt@maxbachmann.de>
2021-03-21 13:22:12 +0100

    Fix compiler warning about constant conditions in clang (GH-4053)
    clang warns about dead code when an if-statement always evaluates to false. This warning can be silenced using an extra '()'.

--- Cython/Compiler/Code.py
+++ Cython/Compiler/Code.py
@@ -2412,7 +2412,8 @@ class CCodeWriter(object):
         return self.error_goto_if("!%s" % cname, pos)
 
     def error_goto_if_neg(self, cname, pos):
-        return self.error_goto_if("%s < 0" % cname, pos)
+        # Add extra parentheses to silence clang warnings about constant conditions.
+        return self.error_goto_if("(%s < 0)" % cname, pos)
 
     def error_goto_if_PyErr(self, pos):
         return self.error_goto_if("PyErr_Occurred()", pos)
