dd4dfee39c5e1f00aa9fd6d23a54a1555cd20e1b
Victor Stinner <vstinner@python.org>
2021-03-30 18:14:03 +0200

    Make load_dynamic() compatible with Python 3.10 (GH-4045)
    Backport code from the master branch:
    
    * commit 4e785d425f27f079bf2f6bba1381217fac4f5da3
    * commit a9fb77681e41810052df0b41b25abb12738bb8f3

--- Cython/Build/Inline.py
+++ Cython/Build/Inline.py
@@ -35,14 +35,18 @@ if sys.version_info[0] < 3:
 else:
     to_unicode = lambda x: x
 
-if sys.version_info[:2] < (3, 3):
+if sys.version_info < (3, 5):
     import imp
     def load_dynamic(name, module_path):
         return imp.load_dynamic(name, module_path)
 else:
-    from importlib.machinery import ExtensionFileLoader
+    import importlib.util as _importlib_util
     def load_dynamic(name, module_path):
-        return ExtensionFileLoader(name, module_path).load_module()
+        spec = _importlib_util.spec_from_file_location(name, module_path)
+        module = _importlib_util.module_from_spec(spec)
+        # sys.modules[name] = module
+        spec.loader.exec_module(module)
+        return module
 
 class UnboundSymbols(EnvTransform, SkipDeclarations):
     def __init__(self):
