5f447e6979d91053ca0301b9b215cb1af32fdc42
Peng WeiKang <pengwk@pengwk.com>
2021-04-05 18:05:47 +0800

    fix invalid function call in test.

--- tests/run/dict_unpacking_not_for_arg_T4082.pyx
+++ tests/run/dict_unpacking_not_for_arg_T4082.pyx
@@ -4,7 +4,7 @@ cimport cython
 @cython.test_assert_path_exists('//MergedDictNode')
 def dict_unpacking_not_for_arg_return_a_copy():
     """
-    >>> dict_unpacking_with_a_copy()
+    >>> dict_unpacking_not_for_arg_return_a_copy()
     {'a': 1, 'b': 0}
     {'a': 0, 'b': 0}
     """
