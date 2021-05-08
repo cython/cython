b537f6e2e5a879fd6d0c93194c24f7214feda79f
Max Bachmann <kontakt@maxbachmann.de>
2021-05-08 22:40:41 +0200

    ignore unreferenced local function warning

--- Cython/Utility/ModuleSetupCode.c
+++ Cython/Utility/ModuleSetupCode.c
@@ -331,7 +331,7 @@
 # elif defined(__ICC) || (defined(__INTEL_COMPILER) && !defined(_MSC_VER))
 #   define CYTHON_UNUSED __attribute__ ((__unused__))
 # elif defined(_MSC_VER)
-#   define CYTHON_UNUSED __pragma(warning(suppress: 4100 4101))
+#   define CYTHON_UNUSED __pragma(warning(suppress: 4100 4101 4505))
 # else
 #   define CYTHON_UNUSED
 # endif
