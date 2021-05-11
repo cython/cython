73442b138315d2719e5302219ea11f0a2fad76c4
Zhenbo Li <3221521+Endle@users.noreply.github.com>
2021-05-11 10:15:20 +0800

    Update Cython/Compiler/Optimize.py
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Cython/Compiler/Optimize.py
+++ Cython/Compiler/Optimize.py
@@ -2588,7 +2588,6 @@ class OptimizeBuiltinCalls(Visitor.NodeRefCleanupMixin,
                 is_temp=node.is_temp,
                 py_name="set"))
 
-    # When calling frozenset(), we replace SimpleCallNode
     def _handle_simple_function_frozenset(self, node, function, pos_args):
         # empty frozenset is singleton, so we never try to free them
         if not pos_args:
