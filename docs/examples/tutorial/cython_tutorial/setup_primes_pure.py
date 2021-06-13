5dc05c7ca83f90570825c589d5049ace3ec61ac0
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-06-13 15:41:48 +0300

    cython_tutorial\setup_primes_pure.py: use new name convention

--- docs/examples/tutorial/cython_tutorial/setup_primes_pure.py
+++ docs/examples/tutorial/cython_tutorial/setup_primes_pure.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules=cythonize(
-        ['primes_pure.py',              # Cython code file with primes() function
+        ['primes.py',                   # Cython code file with primes() function
          'primes_python_compiled.py'],  # Python code file with primes() function
         annotate=True),                 # enables generation of the html annotation file
 )
