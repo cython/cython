e20369648b9e83441e4086b80d70e8ddc2c09d44
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2021-06-16 17:06:53 +0300

    math_function_2.py: use cython.locals to match the cython version

--- docs/examples/tutorial/cdef_classes/math_function_2.py
+++ docs/examples/tutorial/cdef_classes/math_function_2.py
@@ -1,6 +1,7 @@
 @cython.cclass
 class Function:
     @cython.ccall
+    @cython.locals(x=cython.double)
     @cython.exceptval(check=True)
-    def evaluate(self, x: cython.double) -> cython.double:
+    def evaluate(self, x) -> cython.double:
         return 0
