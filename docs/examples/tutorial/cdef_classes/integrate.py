0dcacdbd01e14a61f7caa434841bda1e46f54c05
0dminnimda <0dminnimda@gmail.com>
2021-06-17 05:21:23 +0300

    Create integrate.py

---
+++ docs/examples/tutorial/cdef_classes/integrate.py
@@ -0,0 +1,18 @@
+from cython.cimports.sin_of_square import Function, SinOfSquareFunction
+
+@cython.locals(f=Function, a=cython.double, b=cython.double, N=cython.int)
+def integrate(f, a, b, N):
+    i: cython.int
+
+    if f is None:
+        raise ValueError("f cannot be None")
+
+    s: cython.double = 0
+    dx: cython.double = (b - a) / N
+
+    for i in range(N):
+        s += f.evaluate(a + i * dx)
+
+    return s * dx
+
+print(integrate(SinOfSquareFunction(), 0, 1, 10000))
