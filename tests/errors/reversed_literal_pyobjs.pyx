32a9b5998388097ff70e13d489e93c4494feac3e
da-woods <dw-git@d-woods.co.uk>
2021-06-17 07:31:12 +0100

    Restored loops

--- tests/errors/reversed_literal_pyobjs.pyx
+++ tests/errors/reversed_literal_pyobjs.pyx
@@ -10,6 +10,15 @@ for i in reversed(range(j, [], 2)):
     pass
 for i in reversed(range(j, [], -2)):
     pass
+# code below is no longer a compile-time error (although won't run without an exception)
+for i in reversed(range({}, j, 2)):
+    pass
+for i in reversed(range({}, j, -2)):
+    pass
+for i in reversed(range(j, {}, 2)):
+    pass
+for i in reversed(range(j, {}, -2)):
+    pass
 
 _ERRORS = """
 5:24: Cannot coerce list to type 'long'
