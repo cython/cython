2d287a6191bbba1e120ba9240b24ce364c443204
Matti Picus <matti.picus@gmail.com>
2021-06-17 20:57:51 +0300

    Restore old PyEval_EvalCode() signature for older versions of PyPy (GH-4236)
    Partially reverts https://github.com/cython/cython/commit/1027db04fb7c5bbfea2b9bcdfce0b1f384e26a93

--- Cython/Utility/Builtins.c
+++ Cython/Utility/Builtins.c
@@ -120,7 +120,7 @@ static PyObject* __Pyx_PyExec3(PyObject* o, PyObject* globals, PyObject* locals)
                 "code object passed to exec() may not contain free variables");
             goto bad;
         }
-        #if CYTHON_COMPILING_IN_PYPY || PY_VERSION_HEX < 0x030200B1
+        #if PY_VERSION_HEX < 0x030200B1 || (CYTHON_COMPILING_IN_PYPY && PYPY_VERSION_NUM < 0x07030400)
         result = PyEval_EvalCode((PyCodeObject *)o, globals, locals);
         #else
         result = PyEval_EvalCode(o, globals, locals);
