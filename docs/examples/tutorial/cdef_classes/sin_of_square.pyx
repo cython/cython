e9dbabf4fc810563431a2eee7f7ee9b5e8777071
0dminnimda <0dminnimda@gmail.com>
2021-06-20 00:37:34 +0300

    math_function_2.pyx & sin_of_square.pyx: remove blank lines for alignment

--- docs/examples/tutorial/cdef_classes/sin_of_square.pyx
+++ docs/examples/tutorial/cdef_classes/sin_of_square.pyx
@@ -6,13 +6,11 @@ from libc.math cimport sin
 
 cdef class Function:
 
-
     cpdef double evaluate(self, double x) except *:
         return 0
 
 
 cdef class SinOfSquareFunction(Function):
-
 
     cpdef double evaluate(self, double x) except *:
         return sin(x ** 2)
