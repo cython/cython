85bd06eda66a0419c32d229a164650fb6972ef57
Matus Valo <matusvalo@users.noreply.github.com>
2021-06-28 21:56:18 +0200

    Don't specify type of variable when type can be automatically inferred.
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- docs/examples/userguide/language_basics/casting_python.py
+++ docs/examples/userguide/language_basics/casting_python.py
@@ -4,12 +4,16 @@ def main():
 
     python_string = "foo"
 
-    ptr: p_void = cython.cast(cython.p_void, python_string)
-    adress_in_c: Py_intptr_t= cython.cast(Py_intptr_t, ptr)
+    # Note that the variables below are automatically inferred
+    # as the correct pointer type that is assigned to them.
+    # They do not need to be typed explicitly.
+    
+    ptr = cython.cast(cython.p_void, python_string)
+    adress_in_c = cython.cast(Py_intptr_t, ptr)
     address_from_void = adress_in_c        # address_from_void is a python int
 
-    ptr2: cython.pointer(PyObject) = cython.cast(cython.pointer(PyObject), python_string)
-    address_in_c2: Py_intptr_t= cython.cast(Py_intptr_t, ptr2)
+    ptr2 = cython.cast(cython.pointer(PyObject), python_string)
+    address_in_c2 = cython.cast(Py_intptr_t, ptr2)
     address_from_PyObject = address_in_c2  # address_from_PyObject is a python int
 
     assert address_from_void == address_from_PyObject == id(python_string)
