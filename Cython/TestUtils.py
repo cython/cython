fecac80db32dd82c9ee7fa2903d33cd5fcd675fd
0dminnimda <0dminnimda@gmail.com>
2021-07-04 07:29:42 +0300

    Update TestUtils.py

--- Cython/TestUtils.py
+++ Cython/TestUtils.py
@@ -232,10 +232,10 @@ def unpack_source_tree(tree_file, workdir, cython_root):
     return workdir, header
 
 
-def fresh_cythonize(*args, language_level=3, **kwargs):
+def fresh_cythonize(*args, **kwargs):
     """Clear function caches and run cythonize"""
     clear_function_and_Dependencies_caches()
-    Dependencies.cythonize(*args, language_level=language_level, **kwargs)
+    Dependencies.cythonize(*args, **kwargs)
 
 
 def clear_function_and_Dependencies_caches():
