391e413c6c924801a054d141bcbf1e9cf6db90f4
da-woods <dw-git@d-woods.co.uk>
2021-07-11 11:27:06 +0100

    Rename GeneratorExpressionScope->ComprehensionScope
    I think this is a remenant of an earlier implementation, but as
    far as I can tell generator expressions *never* use
    GeneratorExpressionScope and in fact it's only used for
    comprehensions. This renaming is just to avoid some very
    confusing variable naming in #4254

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -8360,7 +8360,7 @@ class ScopedExprNode(ExprNode):
         if expr_scope is not None:
             self.expr_scope = expr_scope
         elif self.has_local_scope:
-            self.expr_scope = Symtab.GeneratorExpressionScope(outer_scope)
+            self.expr_scope = Symtab.ComprehensionScope(outer_scope)
         else:
             self.expr_scope = None
 
