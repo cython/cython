5a9cc83085319c945d3d3f557e9ae7aed3fd318e
Matus Valo <matusvalo@gmail.com>
2021-07-14 19:39:21 +0200

    Use explicitely cython module in examples

--- docs/examples/tutorial/external/keyword_args_call.py
+++ docs/examples/tutorial/external/keyword_args_call.py
@@ -1,8 +1,7 @@
-import cython
 from cython.cimports.strstr import strstr
 
 def main():
-    data: p_char = "hfvcakdfagbcffvschvxcdfgccbcfhvgcsnfxjh"
+    data: cython.p_char = "hfvcakdfagbcffvschvxcdfgccbcfhvgcsnfxjh"
 
-    pos: p_char = strstr(needle='akd', haystack=data)
+    pos = strstr(needle='akd', haystack=data)
     print(pos is not cython.NULL)
--- docs/examples/tutorial/external/keyword_args_call.pyx
+++ docs/examples/tutorial/external/keyword_args_call.pyx
@@ -1,7 +1,6 @@
 cdef extern from "string.h":
     char* strstr(const char *haystack, const char *needle)
 
-
 cdef char* data = "hfvcakdfagbcffvschvxcdfgccbcfhvgcsnfxjh"
 
 cdef char* pos = strstr(needle='akd', haystack=data)
