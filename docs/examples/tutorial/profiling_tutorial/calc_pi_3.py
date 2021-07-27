f73473aac9cbfd9988459eee151bd8632f3492c3
Matus Valo <matusvalo@users.noreply.github.com>
2021-07-27 14:25:06 +0200

    Cosmetic in calc_pi_3.py
    Co-authored-by: 0dminnimda <0dminnimda@gmail.com>

--- docs/examples/tutorial/profiling_tutorial/calc_pi_3.py
+++ docs/examples/tutorial/profiling_tutorial/calc_pi_3.py
@@ -6,11 +6,10 @@
 def recip_square(i: int) -> cython.double:
     return 1. / (i * i)
 
-def approx_pi(n: cython.int=10000000):
+def approx_pi(n: cython.int = 10000000):
     val: cython.double = 0.
     k: cython.int
     for k in range(1, n + 1):
         val += recip_square(k)
     return (6 * val) ** .5
-
 
