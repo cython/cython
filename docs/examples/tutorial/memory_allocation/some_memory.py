4b39d72f2026850323667e7bf182ee91a8bf450d
0dminnimda <0dminnimda@gmail.com>
2021-07-28 03:02:46 +0300

    Create some_memory.py + format some_memory.pyx in same way

--- docs/examples/tutorial/memory_allocation/some_memory.py
+++ docs/examples/tutorial/memory_allocation/some_memory.py
@@ -1,22 +1,22 @@
-from cpython.mem cimport PyMem_Malloc, PyMem_Realloc, PyMem_Free
+from cython.cimports.cpython.mem import PyMem_Malloc, PyMem_Realloc, PyMem_Free
 
+@cython.cclass
+class SomeMemory:
+    data: cython.p_double
 
-cdef class SomeMemory:
-    cdef double* data
-
-    def __cinit__(self, size_t number):
+    def __cinit__(self, number: size_t):
         # allocate some memory (uninitialised, may contain arbitrary data)
-        self.data = <double*> PyMem_Malloc(
-            number * sizeof(double))
+        self.data = cython.cast(cython.p_double, PyMem_Malloc(
+            number * cython.sizeof(cython.double)))
         if not self.data:
             raise MemoryError()
 
-    def resize(self, size_t new_number):
+    def resize(self, new_number: size_t):
         # Allocates new_number * sizeof(double) bytes,
         # preserving the current content and making a best-effort to
         # re-use the original data location.
-        mem = <double*> PyMem_Realloc(
-            self.data, new_number * sizeof(double))
+        mem = cython.cast(cython.p_double, PyMem_Realloc(
+            self.data, new_number * cython.sizeof(cython.double)))
         if not mem:
             raise MemoryError()
         # Only overwrite the pointer if the memory was really reallocated.
