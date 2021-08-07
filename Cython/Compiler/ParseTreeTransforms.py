ec8c0804f1a847e7f9b92210d756507afb6ae9b3
da-woods <dw-git@d-woods.co.uk>
2021-08-07 09:28:31 +0100

    Restore error when using arbitrary decorators on cdef functions (GH-4323)
    These were lost when cdef properties (for extern types) were introduced.
    
    Closes #4322 (based on my interpretation of the problem as an error-reporting issue)

--- Cython/Compiler/ParseTreeTransforms.py
+++ Cython/Compiler/ParseTreeTransforms.py
@@ -1123,6 +1123,7 @@ class InterpretCompilerDirectives(CythonTransform):
                 realdecs.append(dec)
         if realdecs and (scope_name == 'cclass' or
                          isinstance(node, (Nodes.CClassDefNode, Nodes.CVarDefNode))):
+            # Note - arbitrary C function decorators are caught later in DecoratorTransform
             raise PostParseError(realdecs[0].pos, "Cdef functions/classes cannot take arbitrary decorators.")
         node.decorators = realdecs[::-1] + both[::-1]
         # merge or override repeated directives
@@ -1438,7 +1439,14 @@ class DecoratorTransform(ScopeTrackingTransform, SkipDeclarations):
 
     def visit_CFuncDefNode(self, node):
         node = self.visit_FuncDefNode(node)
-        if self.scope_type != 'cclass' or self.scope_node.visibility != "extern" or not node.decorators:
+        if not node.decorators:
+            return node
+        elif self.scope_type != 'cclass' or self.scope_node.visibility != "extern":
+            # at the moment cdef functions are very restricted in what decorators they can take
+            # so it's simple to test for the small number of allowed decorators....
+            if not (len(node.decorators) == 1 and node.decorators[0].decorator.is_name and
+                    node.decorators[0].decorator.name == "staticmethod"):
+                error(node.decorators[0].pos, "Cdef functions cannot take arbitrary decorators.")
             return node
 
         ret_node = node
