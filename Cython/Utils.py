831cd720700b3de10bd4c23f3a3133732a6e0f40
0dminnimda <0dminnimda@gmail.com>
2021-08-12 17:15:52 +0300

    Utils.py: add _find_cache_attributes, clear_method_caches

--- Cython/Utils.py
+++ Cython/Utils.py
@@ -54,8 +54,30 @@ def cached_function(f):
     return wrapper
 
 
+def _find_cache_attributes(obj):
+    """The function iterates over the attributes of the object and,
+    if it finds the name of the cache, it returns it and the corresponding method name.
+    The method may not be present in the object.
+    """
+    for attr_name in dir(obj):
+        match = _METHOD_CACHE_PATTERN.match(attr_name)
+        if match is not None:
+            yield attr_name, match.group(1)
+
+
+def clear_method_caches(obj):
+    """Removes every cache found in the object,
+    if a corresponding method exists for that cache.
+    """
+    for cache_name, method_name in _find_cache_attributes(obj):
+        if hasattr(obj, method_name):
+            delattr(obj, cache_name)
+        # if there is no corresponding method,
+        # then this attribute was not created by our cached method
+
+
 def cached_method(f):
-    cache_name = '__%s_cache' % f.__name__
+    cache_name = _CACHE_METHOD_NAME.format(f.__name__)
 
     def wrapper(self, *args):
         cache = getattr(self, cache_name, None)
