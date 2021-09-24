7cfd34ce25bbdfc661d524d9fba8874a85b81b03
da-woods <dw-git@d-woods.co.uk>
2021-09-24 18:43:10 +0100

    Comment out include-guards
    And just make the unpickleability explicit

--- Cython/Utility/Coroutine.c
+++ Cython/Utility/Coroutine.c
@@ -1501,19 +1501,21 @@ static PyObject *__Pyx_CoroutineAwait_no_new(PyTypeObject *type, PyObject *args,
 }
 #endif
 
-#if PY_VERSION_HEX < 0x03060000 || CYTHON_USE_TYPE_SPECS
 // In earlier versions of Python an object with no __dict__ and not __slots__ is assumed
 // to be pickleable by default. Coroutine-wrappers have significant state so shouldn't be.
 // Therefore provide a default implementation.
 // Something similar applies to heaptypes (i.e. with type_specs) with protocols 0 and 1
-// even in more recent versions
+// even in more recent versions.
+// We are applying this to all Python versions (hence the commented out version guard)
+// to make the behaviour explicit.
+// #if PY_VERSION_HEX < 0x03060000 || CYTHON_USE_TYPE_SPECS
 static PyObject *__Pyx_CoroutineAwait_reduce_ex(__pyx_CoroutineAwaitObject *self, PyObject *arg) {
     CYTHON_UNUSED_VAR(arg);
     PyErr_Format(PyExc_TypeError, "cannot pickle '%.200s' object",
                          Py_TYPE(self)->tp_name);
     return NULL;
 }
-#endif
+// #endif
 
 static PyMethodDef __pyx_CoroutineAwait_methods[] = {
     {"send", (PyCFunction) __Pyx_CoroutineAwait_Send, METH_O,
@@ -1522,10 +1524,11 @@ static PyMethodDef __pyx_CoroutineAwait_methods[] = {
      (char*) PyDoc_STR("throw(typ[,val[,tb]]) -> raise exception in coroutine,\nreturn next yielded value or raise StopIteration.")},
     {"close", (PyCFunction) __Pyx_CoroutineAwait_Close, METH_NOARGS,
      (char*) PyDoc_STR("close() -> raise GeneratorExit inside coroutine.")},
-#if PY_VERSION_HEX < 0x03060000 || CYTHON_USE_TYPE_SPECS
+// only needed with type-specs of version<3.6, but included in all versions for clarity
+// #if PY_VERSION_HEX < 0x03060000 || CYTHON_USE_TYPE_SPECS
     {"__reduce_ex__", (PyCFunction) __Pyx_CoroutineAwait_reduce_ex, METH_O, 0},
     {"__reduce__", (PyCFunction) __Pyx_CoroutineAwait_reduce_ex, METH_NOARGS, 0},
-#endif
+// #endif
     {0, 0, 0, 0}
 };
 
