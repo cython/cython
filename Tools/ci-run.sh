6fd3d7f78fb3c90b415bae389128b954a4c243cd
0dminnimda <0dminnimda@gmail.com>
2021-10-09 01:20:21 +0300

    use new syntax for `!= ""` case

--- Tools/ci-run.sh
+++ Tools/ci-run.sh
@@ -133,7 +133,7 @@ if [[ $NO_CYTHON_COMPILE != "1" && $PYTHON_VERSION != "pypy"* ]]; then
   # COVERAGE can be either "" (empty or not set) or "1" (when we set it)
   # STACKLESS can be either  "" (empty or not set) or "true" (when we set it)
   # CYTHON_COMPILE_ALL can be either  "" (empty or not set) or "1" (when we set it)
-  if [[ $COVERAGE != "1" && $STACKLESS != "true" && "${BACKEND//*cpp*}" != "" &&
+  if [[ $COVERAGE != "1" && $STACKLESS != "true" && $BACKEND != *"cpp"* &&
         $CYTHON_COMPILE_ALL != "1" && $LIMITED_API == "" && $EXTRA_CFLAGS == "" ]]; then
     python setup.py bdist_wheel || exit 1
   fi
