cb46d9d1630998c0ed9a185241c23935ac71ab3d
0dminnimda <0dminnimda@gmail.com>
2021-10-09 17:41:08 +0300

    Create to_unicode.py

---
+++ docs/examples/tutorial/string/to_unicode.py
@@ -0,0 +1,21 @@
+from cython.cimports.cpython.version import PY_MAJOR_VERSION
+
+# @cython.cfunc  # https://github.com/cython/cython/issues/2529
+def _text(s) -> unicode:
+    if type(s) is unicode:
+        # Fast path for most common case(s).
+        return cython.cast(unicode, s)
+
+    elif PY_MAJOR_VERSION < 3 and isinstance(s, bytes):
+        # Only accept byte strings as text input in Python 2.x, not in Py3.
+        return cython.cast(bytes, s).decode('ascii')
+
+    elif isinstance(s, unicode):
+        # We know from the fast path above that 's' can only be a subtype here.
+        # An evil cast to <unicode> might still work in some(!) cases,
+        # depending on what the further processing does.  To be safe,
+        # we can always create a copy instead.
+        return unicode(s)
+
+    else:
+        raise TypeError("Could not convert to unicode.")
