a0571a69f503b1dfadc331c84b1a8522194053f5
da-woods <dw-git@d-woods.co.uk>
2021-10-17 18:51:08 +0100

    Import TextTestResult in test runner instead of _TextTestResult (GH-4415)
    All the versions we currently test are new enough that the alias is no longer necessary.

--- runtests.py
+++ runtests.py
@@ -1626,14 +1626,11 @@ class _FakeClass(object):
     def shortDescription(self):
         return self._shortDescription
 
-try: # Py2.7+ and Py3.2+
-    from unittest.runner import _TextTestResult
-except ImportError:
-    from unittest import _TextTestResult
+from unittest import TextTestResult
 
-class PartialTestResult(_TextTestResult):
+class PartialTestResult(TextTestResult):
     def __init__(self, base_result):
-        _TextTestResult.__init__(
+        TextTestResult.__init__(
             self, self._StringIO(), True,
             base_result.dots + base_result.showAll*2)
 
