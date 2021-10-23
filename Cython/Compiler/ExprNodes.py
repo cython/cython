7177e02fb8b35f9909bca67653db9f64fa5c75d7
da-woods <dw-git@d-woods.co.uk>
2021-10-23 08:37:55 +0100

    Used new "put_nullify"

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -882,11 +882,7 @@ class ExprNode(Node):
                 # postponed from self.generate_evaluation_code()
                 self.generate_subexpr_disposal_code(code)
                 self.free_subexpr_temps(code)
-            elif self.type.is_pyobject:
-                code.putln("%s = 0;" % self.result())
-            elif self.type.is_memoryviewslice:
-                code.putln("%s.memview = NULL;" % self.result())
-                code.putln("%s.data = NULL;" % self.result())
+            code.put_nullify(self.result(), self.type)
 
             if self.has_temp_moved:
                 code.globalstate.use_utility_code(
