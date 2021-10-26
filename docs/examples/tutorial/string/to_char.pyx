73507ac0e53d925f6f89e13471376d351a091eb9
0dminnimda <0dminnimda@gmail.com>
2021-10-27 01:36:47 +0300

    Update to_char.pyx

--- docs/examples/tutorial/string/to_char.pyx
+++ docs/examples/tutorial/string/to_char.pyx
@@ -1,6 +1,7 @@
 # define a global name for whatever char type is used in the module
 ctypedef unsigned char char_type
 
+
 cdef char_type[:] _chars(s):
     if isinstance(s, unicode):
         # encode to the specific encoding used inside of the module
