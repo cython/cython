ae872fcebb014e2a2dc033cf485d39a3dd4860a5
scoder <stefan_ml@behnel.de>
2021-11-29 09:24:00 +0100

    Undo accidental removal

--- Cython/Compiler/Pipeline.py
+++ Cython/Compiler/Pipeline.py
@@ -222,7 +222,7 @@ def create_pipeline(context, mode, exclude_classes=()):
         ConsolidateOverflowCheck(context),
         DropRefcountingTransform(),
         FinalOptimizePhase(context),
-        GilCheck()
+        GilCheck(),
         ]
     filtered_stages = []
     for s in stages:
