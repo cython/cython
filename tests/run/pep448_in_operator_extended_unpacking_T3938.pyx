6f466e8a9f47ee4df55b3a3f295269cf395c4909
Arvind <arvindnatarajan06@gmail.com>
2021-12-09 17:07:55 +0530

    fix the func name

--- tests/run/pep448_in_operator_extended_unpacking_T3938.pyx
+++ tests/run/pep448_in_operator_extended_unpacking_T3938.pyx
@@ -7,14 +7,14 @@ def t3938(x, l, m):
     >>> l = [1,2,3]
     >>> m = [4,5,6]
     >>> x = 1
-    >>> unpack_in_operator_starred_expressions(x, l, m)
+    >>> t3938(x, l, m)
     True
     >>> x = 10
-    >>> unpack_in_operator_starred_expressions(x, l, m)
+    >>> t3938(x, l, m)
     False
-    >>> unpack_in_operator_starred_expressions(x, l, [])
+    >>> t3938(x, l, [])
     False
-    >>> unpack_in_operator_starred_expressions(x, [], [])
+    >>> t3938(x, [], [])
     False
     """
     return x in [*l, *m]
