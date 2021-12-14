6c802335f6079a56b85e503f49f7c99106fe5f4e
Matus Valo <matusvalo@users.noreply.github.com>
2021-12-14 14:49:17 +0100

    Don't error when exception_check is set to True and return type is PyObject. (GH-4433)
    With Cython syntax, we ignore the exception_check when a Python object is returned.
    In pure Python mode, with a Python object type as return type, we reject it.
    Instead of raising an error, we now just reset exception_check to False.
    
    Found in https://github.com/cython/cython/issues/2529

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -712,6 +712,12 @@ class CFuncDeclaratorNode(CDeclaratorNode):
             env.add_include_file('new')         # for std::bad_alloc
             env.add_include_file('stdexcept')
             env.add_include_file('typeinfo')    # for std::bad_cast
+        elif return_type.is_pyobject and self.exception_check:
+            # Functions in pure Python mode default to always check return values for exceptions
+            # (equivalent to the "except*" declaration). In this case, the exception clause
+            # is silently ignored for functions returning a Python object.
+            self.exception_check = False
+
         if (return_type.is_pyobject
                 and (self.exception_value or self.exception_check)
                 and self.exception_check != '+'):
