ed6478f85d63f36ca9f396150781087a032aecdb
Matus Valo <matusvalo@users.noreply.github.com>
2021-12-14 14:34:52 +0100

    Use cythonize() in pyximport (GH-4339)
    Closes https://github.com/cython/cython/issues/2304

--- Cython/Distutils/build_ext.py
+++ Cython/Distutils/build_ext.py
@@ -12,14 +12,32 @@ else:
 
 
 class new_build_ext(_build_ext, object):
-    def finalize_options(self):
-        if self.distribution.ext_modules:
-            nthreads = getattr(self, 'parallel', None)  # -j option in Py3.5+
-            nthreads = int(nthreads) if nthreads else None
-            from Cython.Build.Dependencies import cythonize
-            self.distribution.ext_modules[:] = cythonize(
-                self.distribution.ext_modules, nthreads=nthreads, force=self.force)
-        super(new_build_ext, self).finalize_options()
+
+    user_options = _build_ext.user_options[:]
+    boolean_options = _build_ext.boolean_options[:]
+
+    user_options.extend([
+        ('cython-c-in-temp', None,
+             "put generated C files in temp directory"),
+        ])
+
+    boolean_options.extend([
+        'cython-c-in-temp'
+    ])
+
+    def initialize_options(self):
+        _build_ext.initialize_options(self)
+        self.cython_c_in_temp = 0
+
+    def build_extension(self, ext):
+        from Cython.Build.Dependencies import cythonize
+        if self.cython_c_in_temp:
+            build_dir = self.build_temp
+        else:
+            build_dir = None
+        new_ext = cythonize(ext,force=self.force, quiet=self.verbose == 0, build_dir=build_dir)[0]
+        ext.sources = new_ext.sources
+        super(new_build_ext, self).build_extension(ext)
 
 # This will become new_build_ext in the future.
 from .old_build_ext import old_build_ext as build_ext
