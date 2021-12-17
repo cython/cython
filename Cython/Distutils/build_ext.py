6a9df880f73d3678dc6208c346c990f8bc8a9e6a
Matus Valo <matusvalo@gmail.com>
2021-12-17 21:53:39 +0100

    Revert "Skip testing if setuptools is in sys.modules"
    This reverts commit f7b0f7b971513dfdc7e4f6234f7d7e8c9f788fe1.

--- Cython/Distutils/build_ext.py
+++ Cython/Distutils/build_ext.py
@@ -1,8 +1,14 @@
+import sys
 import os
 
-try:
-    from setuptools.command.build_ext import build_ext as _build_ext
-except ImportError:
+if 'setuptools' in sys.modules:
+    try:
+        from setuptools.command.build_ext import build_ext as _build_ext
+    except ImportError:
+        # We may be in the process of importing setuptools, which tries
+        # to import this.
+        from distutils.command.build_ext import build_ext as _build_ext
+else:
     from distutils.command.build_ext import build_ext as _build_ext
 
 try:
