59382f14f656670751b442a07dfc424bef564516
scoder <stefan_ml@behnel.de>
2021-12-18 20:06:32 +0100

    Adapt call to new function signature.
    Co-authored-by: da-woods <dw-git@d-woods.co.uk>

--- Cython/Compiler/ModuleNode.py
+++ Cython/Compiler/ModuleNode.py
@@ -1630,7 +1630,7 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
             "}")
 
     def generate_del_function(self, scope, code):
-        tp_slot = TypeSlots.get_slot_by_name("tp_finalize")
+        tp_slot = TypeSlots.get_slot_by_name("tp_finalize", scope.directives)
         slot_func_cname = scope.mangle_internal("tp_finalize")
         if tp_slot.slot_code(scope) != slot_func_cname:
             return  # never used
