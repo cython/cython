9d8d21c0c50baaa482b20a7d69f76c86125eef97
BlindChickens <jacques@quantsolutions.co.za>
2021-12-29 21:29:33 +0200

    Fix tab characters that slipped in

--- tests/errors/cpp_class_extern_overloaded_traits.pyx
+++ tests/errors/cpp_class_extern_overloaded_traits.pyx
@@ -4,33 +4,33 @@
 from libcpp cimport bool as bool_t
 
 cdef:
-	struct S1:
-		int i
+    struct S1:
+        int i
 
-	struct S2:
-		int i
+    struct S2:
+        int i
 
-	extern from "cpp_source.h":
-		cppclass testclass:
-			testclass()
-			void a()
-			void a(unsigned int i)
+    extern from "cpp_source.h":
+        cppclass testclass:
+            testclass()
+            void a()
+            void a(unsigned int i)
 
-	cppclass testclass2(testclass):
-		testclass2()
+    cppclass testclass2(testclass):
+        testclass2()
 
-	cppclass testclass3(testclass):
-		testclass3()
-		void a(S1 v)
+    cppclass testclass3(testclass):
+        testclass3()
+        void a(S1 v)
 
-	cppclass testclass4(testclass):
-		testclass4()
-		void a(S2 v)
+    cppclass testclass4(testclass):
+        testclass4()
+        void a(S2 v)
 
-	testclass t1 = testclass()
-	testclass2 t2 = testclass2()
-	testclass3 t3 = testclass3()
-	testclass4 t4 = testclass4()
+    testclass t1 = testclass()
+    testclass2 t2 = testclass2()
+    testclass3 t3 = testclass3()
+    testclass4 t4 = testclass4()
 
 cdef S1 s1
 cdef S2 s2
--- tests/run/cpp_class_extern_overloaded_traits.pyx
+++ tests/run/cpp_class_extern_overloaded_traits.pyx
@@ -4,53 +4,53 @@
 from libcpp cimport bool as bool_t
 
 cdef:
-	struct S1:
-		int i
+    struct S1:
+        int i
 
-	struct S2:
-		int i
+    struct S2:
+        int i
 
-	extern from *:
-	    """
-	    #include <stdio.h>
+    extern from *:
+        """
+        #include <stdio.h>
         #include <map>
 
         unsigned int i;
 
         void test(std::map<unsigned int, unsigned int> m) {
-	        printf("Map size: %d\n", m.size());
+            printf("Map size: %d\n", m.size());
         }
 
         class testclass {
         public:
-	        void a() {
-		        printf("testclass->a();\n");
-	        }
-	        void a(unsigned int i) {
-		        printf("testclass->a(%d);\n", i);
-	        }
+            void a() {
+                printf("testclass->a();\n");
+            }
+            void a(unsigned int i) {
+                printf("testclass->a(%d);\n", i);
+            }
         };
-	    """
-		cppclass testclass:
-			testclass()
-			void a()
-			void a(unsigned int i)
+        """
+        cppclass testclass:
+            testclass()
+            void a()
+            void a(unsigned int i)
 
-	cppclass testclass2(testclass):
-		testclass2()
+    cppclass testclass2(testclass):
+        testclass2()
 
-	cppclass testclass3(testclass):
-		testclass3()
-		void a(S1 v)
+    cppclass testclass3(testclass):
+        testclass3()
+        void a(S1 v)
 
-	cppclass testclass4(testclass):
-		testclass4()
-		void a(S2 v)
+    cppclass testclass4(testclass):
+        testclass4()
+        void a(S2 v)
 
-	testclass t1 = testclass()
-	testclass2 t2 = testclass2()
-	testclass3 t3 = testclass3()
-	testclass4 t4 = testclass4()
+    testclass t1 = testclass()
+    testclass2 t2 = testclass2()
+    testclass3 t3 = testclass3()
+    testclass4 t4 = testclass4()
 
 cdef S1 s1
 cdef S2 s2
