23356ea333deb6945d314d35766525f651d53d3a
Matus Valo <matusvalo@gmail.com>
2022-01-21 22:00:07 +0100

    Added warning bar when development docs is showed

--- docs/conf.py
+++ docs/conf.py
@@ -184,6 +184,10 @@ html_context = {
     'css_files': ['_static/css/tabs.css'],
 }
 
+development = True if release.startswith('3.0') else False
+
+html_context = {'development': development}
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
