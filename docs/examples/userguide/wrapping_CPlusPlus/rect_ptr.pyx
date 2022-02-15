9bfa2fd56134e1f4afce9d094b7b2d525136c729
Golden Rockefeller <golden.rockefeller@protonmail.com>
2022-02-14 20:33:13 -0500

    Use __cinit__ when necessary in the wrapping C++ examples

--- docs/examples/userguide/wrapping_CPlusPlus/rect_ptr.pyx
+++ docs/examples/userguide/wrapping_CPlusPlus/rect_ptr.pyx
@@ -5,7 +5,12 @@ from Rectangle cimport Rectangle
 cdef class PyRectangle:
     cdef Rectangle*c_rect  # hold a pointer to the C++ instance which we're wrapping
 
-    def __cinit__(self, int x0, int y0, int x1, int y1):
+    def __cinit__(self):
+        self.c_rect = new Rectangle()
+
+    def __init__(self, int x0, int y0, int x1, int y1):
+        if self.c_rect:
+            del self.c_rect
         self.c_rect = new Rectangle(x0, y0, x1, y1)
 
     def __dealloc__(self):
