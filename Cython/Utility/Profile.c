dbcd9ec586621c3e4c4599e7ca022dc122f8acb1
scoder <stefan_ml@behnel.de>
2022-03-07 22:44:26 +0100

    Avoid impact of Py_BUILD_CORE on "traceback.h".

--- Cython/Utility/Profile.c
+++ Cython/Utility/Profile.c
@@ -37,13 +37,13 @@
 
   #include "compile.h"
   #include "frameobject.h"
+  #include "traceback.h"
 #if PY_VERSION_HEX >= 0x030b00a6
   #ifndef Py_BUILD_CORE
     #define Py_BUILD_CORE 1
   #endif
   #include "internal/pycore_frame.h"
 #endif
-  #include "traceback.h"
 
   #if CYTHON_PROFILE_REUSE_FRAME
     #define CYTHON_FRAME_MODIFIER static
