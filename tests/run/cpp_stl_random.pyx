f017653f20cb3dbc476521df5b13fe085944bdea
Till Hoffmann <tillahoffmann@gmail.com>
2022-04-20 11:27:28 +0100

    Use print for doctest to handle python2 and python3.

--- tests/run/cpp_stl_random.pyx
+++ tests/run/cpp_stl_random.pyx
@@ -6,7 +6,7 @@ from libcpp.random cimport mt19937
 
 def mt19937_seed_test():
     """
-    >>> mt19937_seed_test()
+    >>> print(mt19937_seed_test())
     1608637542
     """
     cdef mt19937 rd = mt19937(42)
@@ -15,7 +15,7 @@ def mt19937_seed_test():
 
 def mt19937_reseed_test():
     """
-    >>> mt19937_reseed_test()
+    >>> print(mt19937_reseed_test())
     1608637542
     """
     cdef mt19937 rd
@@ -25,9 +25,11 @@ def mt19937_reseed_test():
 
 def mt19937_min_max():
     """
-    The minimum is zero and the maximum is 2 ** 32 - 1 because mt19937 is 32 bit.
-    >>> mt19937_min_max()
-    (0, 4294967295)
+    >>> x, y = mt19937_min_max()
+    >>> print(x)
+    0
+    >>> print(y)  # 2 ** 32 - 1 because mt19937 is 32 bit.
+    4294967295
     """
     cdef mt19937 rd
     return rd.min(), rd.max()
@@ -35,8 +37,11 @@ def mt19937_min_max():
 
 def mt19937_discard(z):
     """
-    >>> mt19937_discard(13)
-    (1972458954, 1972458954)
+    >>> x, y = mt19937_discard(13)
+    >>> print(x)
+    1972458954
+    >>> print(y)
+    1972458954
     """
     cdef mt19937 rd = mt19937(42)
     # Throw away z random numbers.
