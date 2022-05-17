483c3a5ddd97e3335842fac256d18b027d258cee
Matus Valo <matusvalo@gmail.com>
2022-05-17 22:53:47 +0200

    Fix failing e_relative_cimport

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -8569,9 +8569,14 @@ class FromCImportStatNode(StatNode):
         if not env.is_module_scope:
             error(self.pos, "cimport only allowed at module level")
             return
-        if self.relative_level and self.relative_level > len(env.qualified_name.split('.')):
-            error(self.pos, "relative cimport beyond main package is not allowed")
-            return
+        qualified_name_components = len(env.qualified_name.split('.'))
+        if self.relative_level:
+            if self.relative_level > qualified_name_components or (
+                    self.relative_level == qualified_name_components and not env.is_package):
+                # 1. case: importing beyond package: from .. import pkg
+                # 2. case: importing from same level but current dir is not package: from . import module
+                error(self.pos, "relative cimport beyond main package is not allowed")
+                return
         module_scope = env.find_module(self.module_name, self.pos, relative_level=self.relative_level)
         module_name = module_scope.qualified_name
         env.add_imported_module(module_scope)
