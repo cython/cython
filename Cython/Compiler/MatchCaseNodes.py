c6bda31bcb957027acea36a5c72e57b404713b72
da-woods <dw-git@d-woods.co.uk>
2022-06-19 19:03:10 +0100

    Remove a case that we were mishandling
    Difficult to handle or patterns with targets as a simple if
    statement

--- Cython/Compiler/MatchCaseNodes.py
+++ Cython/Compiler/MatchCaseNodes.py
@@ -141,7 +141,8 @@ class MatchCaseNode(Node):
         if self.guard:
             error(self.pos, "Cases with guards are currently not supported")
             return self
-        self.error(self.pos, "This case statement is not yet supported")
+        error(self.pos, "This case statement is not yet supported")
+        return self
 
     def generate_execution_code(self, code):
         error(self.pos, "This case statement is not yet supported")
@@ -326,7 +327,12 @@ class OrPatternNode(PatternNode):
             a.validate_irrefutable()
 
     def is_simple_value_comparison(self):
-        return all(a.is_simple_value_comparison() for a in self.alternatives)
+        return all(
+            # it turns out to be hard to generate correct assignment code
+            # for or patterns with targets
+            a.is_simple_value_comparison() and not a.get_targets()
+            for a in self.alternatives
+        )
 
     def get_simple_comparison_node(self, subject_node):
         assert self.is_simple_value_comparison()
