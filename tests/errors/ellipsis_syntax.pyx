dfb92f938bc1abe7260bb75453bac3b8d031cadd
0dminnimda <0dminnimda@gmail.com>
2022-06-30 14:21:45 +0300

    Create ellipsis_syntax.pyx

---
+++ tests/errors/ellipsis_syntax.pyx
@@ -0,0 +1,22 @@
+# mode: error
+
+. . .
+. ..
+.. .
+(
+    .
+    ..
+)
+[
+    ..
+    .,
+    .
+    .
+
+    .
+]
+
+_ERRORS = u"""
+3: 0: Expected an identifier or literal
+4: 0: Expected an identifier or literal
+"""
