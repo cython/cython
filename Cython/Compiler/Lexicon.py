e4fd262fe7e1ff1d767cee9ba82be3adbdf41706
0dminnimda <0dminnimda@gmail.com>
2022-06-30 22:03:26 +0300

    Lexicon.py: merge ellipsis with other TEXT

--- Cython/Compiler/Lexicon.py
+++ Cython/Compiler/Lexicon.py
@@ -90,8 +90,7 @@ def make_lexicon():
         (intliteral, Method('strip_underscores', symbol='INT')),
         (fltconst, Method('strip_underscores', symbol='FLOAT')),
         (imagconst, Method('strip_underscores', symbol='IMAG')),
-        (ellipsis, TEXT),
-        (punct | diphthong, TEXT),
+        (ellipsis | punct | diphthong, TEXT),
 
         (bra, Method('open_bracket_action')),
         (ket, Method('close_bracket_action')),
