f910eed01ab2b7d40432eac4c0f837c7fe67e453
da-woods <dw-git@d-woods.co.uk>
2022-07-01 21:39:33 +0100

    Add mangling test and move to a pure py file

--- tests/run/extra_patma_py.py
+++ tests/run/extra_patma_py.py
@@ -18,3 +18,26 @@ def test_array_is_sequence(x):
             return z
         case _:
             return "Not a sequence"
+
+
+def test_duplicate_keys(key1, key2):
+    """
+    Extra to TestValueErrors in test_patma
+    Cython sorts keys into literal and runtime. This tests when two runtime keys clash
+
+    >>> test_duplicate_keys("a", "b")
+    True
+    >>> test_duplicate_keys("a", "a")
+    Traceback (most recent call last):
+    ...
+    ValueError: mapping pattern checks duplicate key ('a')
+    """
+    class Keys:
+        KEY_1 = key1
+        KEY_2 = key2
+
+    match {"a": 1, "b": 2}:
+        case {Keys.KEY_1: _, Keys.KEY_2: _}:
+            return True
+        case _:
+            return False
