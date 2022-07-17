3a919f5cb689ac0e72d4431515ab27171f2e1d72
da-woods <dw-git@d-woods.co.uk>
2022-07-17 12:20:14 +0100

    setup enum34 on pypy2.7

--- Tools/ci-run.sh
+++ Tools/ci-run.sh
@@ -76,6 +76,8 @@ else
 
     if [[ $PYTHON_VERSION != "pypy"* && $PYTHON_VERSION != "3."[1]* ]]; then
       python -m pip install -r test-requirements-cpython.txt || exit 1
+    elif [[ $PYTHON_VERSION == "pypy-2.7" ]]; then
+     python -m pip install -r test-requirements-pypy27.txt || exit 1
     fi
   fi
 fi
