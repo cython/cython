a2b97cb7e32563d4d5a95ee83ab7015c9b30aacf
da-woods <dw-git@d-woods.co.uk>
2022-07-30 21:23:59 +0100

    Add test for the positive case

--- tests/compile/fused_no_numpy.pyx
+++ tests/compile/fused_no_numpy.pyx
@@ -1,3 +1,5 @@
+# mode: compile
+
 # cython: test_fail_if_c_code_has = __Pyx_ImportNumPyArrayTypeIfAvailable
 
 ctypedef fused IntOrFloat:
@@ -5,6 +7,7 @@ ctypedef fused IntOrFloat:
     float
 
 # This function does not use buffers so has no reason to import numpy to
-# look up dtypes
+# look up dtypes. fused_buffers.pyx is the corresponding test for the case
+# where numpy is imported
 def f(IntOrFloat x):
     return x
