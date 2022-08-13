6261d32914b9b24e7c96682307bfdf1ef00567a5
Matus Valo <matusvalo@gmail.com>
2022-08-13 11:07:25 +0200

    Fix memoryview_sum examples

--- docs/examples/userguide/parallelism/memoryview_sum.py
+++ docs/examples/userguide/parallelism/memoryview_sum.py
@@ -1,9 +1,7 @@
 from cython.parallel import prange
 
-@cython.nogil
-@cython.cfunc
-def func(x: cython.double[:], alpha: cython.double) -> cython.void:
+def func(x: cython.double[:], alpha: cython.double):
     i: cython.Py_ssize_t
 
-    for i in prange(x.shape[0]):
+    for i in prange(x.shape[0], nogil=True):
         x[i] = alpha * x[i]
--- docs/examples/userguide/parallelism/memoryview_sum.pyx
+++ docs/examples/userguide/parallelism/memoryview_sum.pyx
@@ -2,8 +2,8 @@ from cython.parallel import prange
 
 
 
-cdef void func(double[:] x, double alpha) nogil:
+def func(double[:] x, double alpha):
     cdef Py_ssize_t i
 
-    for i in prange(x.shape[0]):
+    for i in prange(x.shape[0], nogil=True):
         x[i] = alpha * x[i]
