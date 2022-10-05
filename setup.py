f2e8b2f3dca7436630b1b4a4d42305adcf173ece
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2022-10-05 20:10:51 +0200

    setup.py: Add project_urls for PyPI (#5064)
    Add project_urls dictionary to setup.py with URLs to the Cython documentation, funding, source code and bug tracker. This will add those URLs under the "Project links" section on https://pypi.org/project/cython, making it easy to find these resources directly from PyPI.

--- setup.py
+++ setup.py
@@ -282,6 +282,13 @@ setup(
         "Topic :: Software Development :: Compilers",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
+    project_urls={
+        "Documentation": "https://cython.readthedocs.io/",
+        "Donate": "https://cython.readthedocs.io/en/latest/src/donating.html",
+        "Source Code": "https://github.com/cython/cython",
+        "Bug Tracker": "https://github.com/cython/cython/issues",
+        "User Group": "https://groups.google.com/g/cython-users",
+    },
 
     scripts=scripts,
     packages=packages,
