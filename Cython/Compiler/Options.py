c21784baeb27a8a06139a53c53df082a6076b9f6
Matus Valo <matusvalo@gmail.com>
2022-10-23 07:41:50 +0200

    Set directive scope to noexcept as module

--- Cython/Compiler/Options.py
+++ Cython/Compiler/Options.py
@@ -189,7 +189,6 @@ _directive_defaults = {
     'cdivision_warnings': False,
     'c_api_binop_methods': False,  # was True before 3.0
     'overflowcheck': False,
-    'noexcept': False,
     'overflowcheck.fold': True,
     'always_allow_keywords': True,
     'allow_none_for_extension_args': True,
@@ -219,6 +218,7 @@ _directive_defaults = {
     'np_pythran': False,
     'fast_gil': False,
     'cpp_locals': False,  # uses std::optional for C++ locals, so that they work more like Python locals
+    'noexcept': False,
 
     # set __file__ and/or __path__ to known source/target path at import time (instead of not having them available)
     'set_initial_path' : None,  # SOURCEFILE or "/full/path/to/module"
@@ -386,6 +386,7 @@ directive_scopes = {  # defaults to available everywhere
     'total_ordering': ('cclass', ),
     'dataclasses.dataclass' : ('class', 'cclass',),
     'cpp_locals': ('module', 'function', 'cclass'),  # I don't think they make sense in a with_statement
+    'noexcept': ('module', ),
 }
 
 
