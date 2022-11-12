87e95854665f54f31865d21fa53a8de4b8f5ed43
da-woods <dw-git@d-woods.co.uk>
2022-11-12 08:35:50 +0000

    Be more explicit about inhertited

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -2171,7 +2171,8 @@ class NameNode(AtomicExprNode):
             # In a dataclass, an assignment should not prevent a name from becoming an instance attribute.
             # Hence, "as_target = not is_dataclass".
             self.declare_from_annotation(env, as_target=not is_dataclass)
-        elif self.entry and self.annotation and env.is_c_dataclass_scope:
+        elif (self.entry and self.entry.is_inherited and 
+                self.annotation and env.is_c_dataclass_scope):
             error(self.pos, "Cannot redeclare inherited fields in Cython dataclasses")
         if not self.entry:
             if env.directives['warn.undeclared']:
