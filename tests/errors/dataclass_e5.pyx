4e6e2dff3dbca069d7242a491f22f29a4085e159
Matus Valo <matusvalo@gmail.com>
2022-11-16 00:04:33 +0100

    Fix unittests

--- tests/errors/dataclass_e5.pyx
+++ tests/errors/dataclass_e5.pyx
@@ -15,7 +15,7 @@ cdef class C:
 _WARNINGS = """
 9:7: Found Python 2.x type 'long' in a Python annotation. Did you mean to use 'cython.long'?
 10:7: Found C type 'Py_ssize_t' in a Python annotation. Did you mean to use a Python type?
-10:7: Unknown type declaration in annotation, ignoring
+10:7: Unknown type declaration 'Py_ssize_t' in annotation, ignoring
 12:7: Found C type 'double' in a Python annotation. Did you mean to use a Python type?
-12:7: Unknown type declaration in annotation, ignoring
+12:7: Unknown type declaration 'double' in annotation, ignoring
 """
