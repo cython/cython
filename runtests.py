ab6ca6875206eeeb89587527458c4cf7cfce3645
scoder <stefan_ml@behnel.de>
2022-11-29 07:54:22 +0100

    Undo unrelated change.

--- runtests.py
+++ runtests.py
@@ -2725,7 +2725,7 @@ def runtests(options, cmd_args, coverage=None):
             ('limited_api_bugs.txt', options.limited_api),
             ('windows_bugs.txt', sys.platform == 'win32'),
             ('cygwin_bugs.txt', sys.platform == 'cygwin'),
-            ('windows_bugs_39.txt', sys.platform == 'win32' and sys.version_info[:2] == (3, 9)),
+            ('windows_bugs_39.txt', sys.platform == 'win32' and sys.version_info[:2] == (3, 9))
         ]
 
         exclude_selectors += [
