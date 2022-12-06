7497c89387c951a3cd526d46f12ca09564a23f15
da-woods <dw-git@d-woods.co.uk>
2022-12-06 22:05:29 +0000

    Add test

--- tests/run/cclass_assign_attr_GH3100.pyx
+++ tests/run/cclass_assign_attr_GH3100.pyx
@@ -17,3 +17,24 @@ cdef class Foo:
     meth2 = meth
     classmeth2 = classmeth
     staticmeth2 = staticmeth
+
+cdef class ChangeName:
+    """
+    GH-5079
+    Assigning to the cdef class name shouldn't cause a crash.
+    The important bit of this test is the not crashing - it's
+    possible that typespec/limited-API defined classes will be
+    naturally mutable and that isn't a huge problem
+
+    >>> ChangeName.__name__ = "SomethingElse"  # doctest: +ELLIPSIS
+    Traceback (most recent call last):
+        ...
+    TypeError: ...
+    >>> ChangeName.__name__
+    'ChangeName'
+    """
+
+    # the class seems to need some contents for changing the
+    # name to cause a problem
+    cdef public str attr1
+    cdef public int attr2
