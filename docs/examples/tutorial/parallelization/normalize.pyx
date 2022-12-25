16de1fff469fa35ee6320ee8e55bb9b09880a2eb
da-woods <dw-git@d-woods.co.uk>
2022-12-25 16:47:55 +0000

    Finish off parallel tutorial

---
+++ docs/examples/tutorial/parallelization/normalize.pyx
@@ -0,0 +1,21 @@
+# On MSVC use "/openmp" instead
+# distutils: extra_compile_args = -fopenmp
+# distutils: extra_link_args = -fopenmp
+
+from cython.parallel cimport parallel, prange
+cimport cython
+from libc.math cimport sqrt
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def normalize(double[:] x):
+    cdef Py_ssize_t i
+    cdef double total = 0
+    cdef double norm
+    with nogil, parallel():
+        for i in prange(x.shape[0]):
+            total += x[i]*x[i]
+        norm = sqrt(total)
+        for i in prange(x.shape[0]):
+            x[i] /= norm
+
