81183349a9102775c741e42364ba814fe8a44686
da-woods <dw-git@d-woods.co.uk>
2022-12-31 16:26:59 +0000

    Fix test for typespecs

--- tests/run/cclass_assign_attr_GH3100.pyx
+++ tests/run/cclass_assign_attr_GH3100.pyx
@@ -1,3 +1,13 @@
+cdef extern from *:
+    """
+    #ifdef CYTHON_USE_TYPESPECS
+    #define TYPESPECS 1
+    #else
+    #define TYPESPECS 0
+    #endif
+    """
+    int TYPESPECS
+
 cdef class Foo:
     """
     >>> D = Foo.__dict__
@@ -19,7 +29,24 @@ cdef class Foo:
     staticmeth2 = staticmeth
 
 cdef class ChangeName:
+    # the class seems to need some contents for changing the
+    # name to cause a problem
+    cdef public str attr1
+    cdef public int attr2
+
+if TYPESPECS:
+    __doc__ = """
+    For typespecs, cdef classes are mutable on some Python versions
+    (and it's easiest to leave them that way). Therefore the test
+    is just that reassigning the name doesn't cause a crash
+
+    >>> try:
+    ...     ChangeName.__name__ = "SomethingElse"
+    ... except TypeError:
+    ...     pass  # either type error or changing the name is fine
     """
+else:
+    __doc__ = """
     GH-5079
     Assigning to the cdef class name shouldn't cause a crash.
     The important bit of this test is the not crashing - it's
@@ -34,7 +61,3 @@ cdef class ChangeName:
     'ChangeName'
     """
 
-    # the class seems to need some contents for changing the
-    # name to cause a problem
-    cdef public str attr1
-    cdef public int attr2
