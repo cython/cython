685c3bdf066015016bc25a7016ff7d61b1274580
0dminnimda <0dminnimda@gmail.com>
2023-01-24 12:42:36 +0300

    Fix bound class methods when unpacking builtin method calls (GH-5219)
    The general optimization for unpacking builtin method calls was breaking bound classmethods
    (with crashes on Py2 and wrong answers on Py3).
    
    Closes https://github.com/cython/cython/issues/5051
    Closes https://github.com/cython/cython/pull/5075
    
    Co-authored-by: da-woods <dw-git@d-woods.co.uk>

--- Cython/Utility/ModuleSetupCode.c
+++ Cython/Utility/ModuleSetupCode.c
@@ -541,6 +541,14 @@
   #endif
 #endif
 
+// reinterpret
+
+// TODO: refactor existing code to use those macros
+#define __PYX_REINTERPRET_FUNCION(func_pointer, other_pointer) ((func_pointer)(void(*)(void))(other_pointer))
+// #define __PYX_REINTERPRET_POINTER(pointer_type, pointer) ((pointer_type)(void *)(pointer))
+// #define __PYX_RUNTIME_REINTERPRET(type, var) (*(type *)(&var))
+
+
 /////////////// CInitCode ///////////////
 
 // inline attribute
