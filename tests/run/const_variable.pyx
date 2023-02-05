8f00fad2ace115cb728c8ef7b52081b7b6620d66
Matus Valo <matusvalo@gmail.com>
2023-02-05 21:02:39 +0100

    Improve tests

--- tests/run/const_variable.pyx
+++ tests/run/const_variable.pyx
@@ -2,7 +2,9 @@
 
 cdef const int int_constant = 50
 cdef const int int_constant2 = int_constant
-cdef const int *pint_constant
+cdef const int int_constant3
+int_constant3 = 5
+cdef const int *pint_constant = &int_constant
 
 
 def test_constant_value():
@@ -19,6 +21,9 @@ def test_constant_pointer():
     """
     >>> test_constant_pointer()
     50
+    5
     """
-    pint_constant = &int_constant
+    global pint_constant
+    print(pint_constant[0])
+    pint_constant = &int_constant3
     print(pint_constant[0])
