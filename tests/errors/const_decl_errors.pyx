de8b45cdc579688a68b880b3dfeb3e8f2df29da3
Matus Valo <matusvalo@gmail.com>
2023-02-05 20:48:44 +0100

    Allow initialisation of const variables

--- tests/errors/const_decl_errors.pyx
+++ tests/errors/const_decl_errors.pyx
@@ -2,10 +2,11 @@
 # tag: warnings
 cdef const object o
 
-# TODO: This requires making the assignment at declaration time.
-# (We could fake this case by dropping the const here in the C code,
-# as it's not needed for agreeing with external libraries.
 cdef const int x = 10
+x = 20           # nok
+
+cdef const int y
+cdef const int *z
 
 cdef struct S:
     int member
@@ -24,6 +25,17 @@ cdef func(const int a, const int* b, const (int*) c, const S s, int *const d, in
     f[0] = NULL  # nok
     f = NULL     # ok
     t = &s
+
+cdef func2():
+    global y, z
+    y = 10       # nok
+    z = &y       # ok
+    z[0] = 10    # nok
+
+cdef func3():
+    y = 10       # ok
+    z = &y       # ok
+    z[0] = 10    # ok
 
 cdef volatile object v
 
@@ -44,16 +56,18 @@ cdef const_ok(const int *b, const int *c):
 
 _ERRORS = """
 3:5: Const/volatile base type cannot be a Python object
-8:0: Assignment to const 'x'
-15:4: Assignment to const 'a'
-16:4: Assignment to const 'c'
-17:5: Assignment to const dereference
-18:5: Assignment to const attribute 'member'
-19:4: Assignment to const 'd'
-22:4: Assignment to const 'e'
-24:5: Assignment to const dereference
-26:4: Assignment to const 't'
-28:5: Const/volatile base type cannot be a Python object
+6:4: Assignment to const 'x'
+16:8: Assignment to const 'a'
+17:8: Assignment to const 'c'
+18:5: Assignment to const dereference
+19:5: Assignment to const attribute 'member'
+20:8: Assignment to const 'd'
+23:8: Assignment to const 'e'
+25:5: Assignment to const dereference
+27:8: Assignment to const 't'
+31:8: Assignment to const 'y'
+33:5: Assignment to const dereference
+40:5: Const/volatile base type cannot be a Python object
 """
 
 _WARNINGS = """
