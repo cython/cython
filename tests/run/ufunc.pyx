cc4164032416bfa0f15d8fa03525239a8b0658f6
da-woods <dw-git@d-woods.co.uk>
2023-03-22 21:27:12 +0000

    Fix ufunc GIL handling (#5332)
    It looks like Numpy releases the GIL for us (unless it's PyObject
    arguments), so we should assume that we don't have the GIL, and
    write code to regain it if needed, rather than assuming we need
    to release the GIL.
    
    Fixes #5328

--- tests/run/ufunc.pyx
+++ tests/run/ufunc.pyx
@@ -10,25 +10,34 @@ int_arr_1d = np.arange(20, dtype=int)[::4]
 int_arr_2d = np.arange(500, dtype=int).reshape((50, -1))[5:8, 6:8]
 double_arr_1d = int_arr_1d.astype(np.double)
 double_arr_2d = int_arr_2d.astype(np.double)
+# Numpy has a cutoff at about 500 where it releases the GIL, so test some large arrays
+large_int_arr_1d = np.arange(1500, dtype=int)
+large_int_arr_2d = np.arange(1500*600, dtype=int).reshape((1500, -1))
+large_double_arr_1d = large_int_arr_1d.astype(np.double)
+large_double_arr_2d = large_int_arr_2d.astype(np.double)
 
 # it's fairly hard to test that nogil results in the GIL actually
 # being released unfortunately
 @cython.ufunc
-cdef double tripple_it(long x) nogil:
-    """tripple_it doc"""
+cdef double triple_it(long x) nogil:
+    """triple_it doc"""
     return x*3.
 
-def test_tripple_it():
+def test_triple_it():
     """
     Ufunc also generates a signature so just look at the end
-    >>> tripple_it.__doc__.endswith('tripple_it doc')
+    >>> triple_it.__doc__.endswith('triple_it doc')
     True
-    >>> tripple_it(int_arr_1d)
+    >>> triple_it(int_arr_1d)
     array([ 0., 12., 24., 36., 48.])
-    >>> tripple_it(int_arr_2d)
+    >>> triple_it(int_arr_2d)
     array([[168., 171.],
            [198., 201.],
            [228., 231.]])
+
+    Treat the large arrays just as a "don't crash" test
+    >>> _ = triple_it(large_int_arr_1d)
+    >>> _ = triple_it(large_int_arr_2d)
     """
 
 @cython.ufunc
@@ -41,6 +50,8 @@ def test_to_the_power():
     True
     >>> np.allclose(to_the_power(1., double_arr_2d), np.ones_like(double_arr_2d))
     True
+    >>> _ = to_the_power(large_double_arr_1d, -large_double_arr_1d)
+    >>> _ = to_the_power(large_double_arr_2d, -large_double_arr_2d)
     """
 
 @cython.ufunc
@@ -56,6 +67,7 @@ def test_py_return_value():
     >>> py_return_value(double_arr_1d).dtype
     dtype('O')
     >>> py_return_value(-1.)  # returns None
+    >>> _ = py_return_value(large_double_arr_1d)
     """
 
 @cython.ufunc
@@ -66,6 +78,7 @@ def test_py_arg():
     """
     >>> py_arg(np.array([1, "2.0", 3.0], dtype=object))
     array([1., 2., 3.])
+    >>> _ = py_arg(np.array([1]*1200, dtype=object))
     """
 
 @cython.ufunc
@@ -155,4 +168,32 @@ def test_nested_function():
     True
     >>> nested_function(-1.)
     -2.0
+    """
+
+@cython.ufunc
+cdef double can_throw(double x):
+    if x<0:
+        raise RuntimeError
+    return x
+
+def test_can_throw():
+    """
+    >>> arr = double_arr_1d.copy()
+    >>> arr[1] = -1.
+    >>> can_throw(arr)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    >>> large_arr = large_double_arr_1d.copy()
+    >>> large_arr[-4] = -2.
+    >>> can_throw(large_arr)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    >>> large_arr2d = large_double_arr_2d.copy()
+    >>> large_arr2d[100, 200] = -1.
+    >>> can_throw(large_arr2d)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
     """
