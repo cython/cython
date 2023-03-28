1ff7673c2107a4d619c5a95bcc9751c5b3ccafef
Stefan Behnel <stefan_ml@behnel.de>
2023-03-28 11:39:05 +0200

    Avoid access to PyConfig without holding the GIL when trying to read the old Py_OptimizeFlag. The flag was never meant to be modifiable and thus can be read once at module import time.
    See https://github.com/python/cpython/issues/99872#issuecomment-1333763465

--- Cython/Compiler/Builtin.py
+++ Cython/Compiler/Builtin.py
@@ -477,7 +477,7 @@ def init_builtins():
 
     builtin_scope.declare_var(
         '__debug__', PyrexTypes.c_const_type(PyrexTypes.c_bint_type),
-        pos=None, cname='(!__pyx_get_Py_OptimizeFlag())', is_cdef=True)
+        pos=None, cname='__pyx_assertions_enabled()', is_cdef=True)
 
     global type_type, list_type, tuple_type, dict_type, set_type, frozenset_type
     global slice_type, bytes_type, str_type, unicode_type, basestring_type, bytearray_type
