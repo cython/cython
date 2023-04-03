8d0ba5dc50ab4865ae625c4deef6dd1bb03e7453
Stefan Behnel <stefan_ml@behnel.de>
2023-04-03 17:20:33 +0200

    Remove outdated apt package from CI setup.

--- Tools/ci-run.sh
+++ Tools/ci-run.sh
@@ -12,7 +12,7 @@ elif [[ $OSTYPE == "linux-gnu"* ]]; then
   echo "Installing requirements [apt]"
   sudo apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
   sudo apt update -y -q
-  sudo apt install -y -q gdb python-dbg python3-dbg gcc-$GCC_VERSION || exit 1
+  sudo apt install -y -q gdb python3-dbg gcc-$GCC_VERSION || exit 1
 
   ALTERNATIVE_ARGS=""
   if [[ $BACKEND == *"cpp"* ]]; then
