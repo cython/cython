2ea40bbccf2b0e85b88c0bbaa107edc38d0fd690
scoder <stefan_ml@behnel.de>
2023-04-13 11:32:37 +0200

    Add a "CYTHON_EXTERN_C" macro to allow redefining "__PYX_EXTERN_C" on user side (GH-5371)
    See https://github.com/cython/cython/pull/5366
    
    Add a warning for users who already defined "__PYX_EXTERN_C" before that there is a new macro for this.
    Also try to reverse-engineer expressive names for the different test cases.

--- Cython/Compiler/ModuleNode.py
+++ Cython/Compiler/ModuleNode.py
@@ -891,7 +891,17 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
 
     def generate_extern_c_macro_definition(self, code, is_cpp):
         name = Naming.extern_c_macro
-        code.putln("#ifndef %s" % name)
+        code.putln("#ifdef CYTHON_EXTERN_C")
+        # make sure that user overrides always take precedence
+        code.putln('    #undef %s' % name)
+        code.putln('    #define %s CYTHON_EXTERN_C' % name)
+        code.putln("#elif defined(%s)" % name)
+        code.putln("    #ifdef _MSC_VER")
+        code.putln("    #pragma message (\"Please do not define the '%s' macro externally. Use 'CYTHON_EXTERN_C' instead.\")" % name)
+        code.putln("    #else")
+        code.putln("    #warning Please do not define the '%s' macro externally. Use 'CYTHON_EXTERN_C' instead." % name)
+        code.putln("    #endif")
+        code.putln("#else")
         if is_cpp:
             code.putln('    #define %s extern "C++"' % name)
         else:
