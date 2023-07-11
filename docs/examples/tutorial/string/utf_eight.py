51eb2fbb56e312d6401981d9cf571342609a82d8
0dminnimda <0dminnimda@gmail.com>
2023-07-11 14:56:25 +0300

    utf_eight.py: remove irrelevant comments

--- docs/examples/tutorial/string/utf_eight.py
+++ docs/examples/tutorial/string/utf_eight.py
@@ -1,15 +1,15 @@
 from cython.cimports.libc.stdlib import free
 
-# @cython.cfunc  # https://github.com/cython/cython/issues/2529
+@cython.cfunc
 def tounicode(s: cython.p_char) -> cython.unicode:
     return s.decode('UTF-8', 'strict')
 
-# @cython.cfunc  # https://github.com/cython/cython/issues/2529
+@cython.cfunc
 def tounicode_with_length(
         s: cython.p_char, length: cython.size_t) -> cython.unicode:
     return s[:length].decode('UTF-8', 'strict')
 
-# @cython.cfunc  # https://github.com/cython/cython/issues/2529
+@cython.cfunc
 def tounicode_with_length_and_free(
         s: cython.p_char, length: cython.size_t) -> cython.unicode:
     try:
