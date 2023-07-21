0fe4725950506f58010a4d998c759992ead29017
Matus Valo <matusvalo@gmail.com>
2023-07-21 15:34:31 +0200

    Adjust StringParseContext.find_module()

--- Cython/Compiler/TreeFragment.py
+++ Cython/Compiler/TreeFragment.py
@@ -32,7 +32,7 @@ class StringParseContext(Main.Context):
         Main.Context.__init__(self, include_directories, compiler_directives, cpp=cpp, language_level='3str')
         self.module_name = name
 
-    def find_module(self, module_name, relative_to=None, pos=None, need_pxd=1, absolute_fallback=True):
+    def find_module(self, module_name, relative_to=None, pos=None, need_pxd=1, absolute_fallback=True, relative_import=False):
         if module_name not in (self.module_name, 'cython'):
             raise AssertionError("Not yet supporting any cimports/includes from string code snippets")
         return ModuleScope(module_name, parent_module=None, context=self)
