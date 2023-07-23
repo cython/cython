cca44ea13618c1327bd713414be4d4523477f935
da-woods <dw-git@d-woods.co.uk>
2023-07-23 18:56:18 +0100

    Fix 3.8 vectorcall_function

--- Cython/Utility/ObjectHandling.c
+++ Cython/Utility/ObjectHandling.c
@@ -2317,7 +2317,11 @@ static CYTHON_INLINE PyObject* __Pyx_PyObject_FastCallDict(PyObject *func, PyObj
     #endif
 
     #if CYTHON_VECTORCALL
+    #if Py_VERSION_HEX < 0x03090000
+    vectorcallfunc f = _PyVectorcall_Function(func);
+    #else
     vectorcallfunc f = PyVectorcall_Function(func);
+    #endif
     if (f) {
         return f(func, args, (size_t)nargs, kwargs);
     }
