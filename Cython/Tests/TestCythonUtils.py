573bc6791bae462fb12b811bb413be7564126b87
Stefan Behnel <stefan_ml@behnel.de>
2023-07-29 13:33:49 +0200

    Make "test_print_version" more reliable (by not actually relying on the external state of the stdout/stderr streams) and add another test for the "do not print it" case.

--- Cython/Tests/TestCythonUtils.py
+++ Cython/Tests/TestCythonUtils.py
@@ -150,4 +150,21 @@ class TestCythonUtils(unittest.TestCase):
 
         from .. import __version__ as version
         self.assertIn(version, stdout)
-        self.assertFalse(stderr)
+        if stderr:  # Depends on os.fstat(1/2).
+            self.assertIn(version, stderr)
+
+    def test_print_version_stdouterr(self):
+        orig_stderr = sys.stderr
+        orig_stdout = sys.stdout
+        stdout = sys.stdout = sys.stderr = StringIO()  # same!
+        try:
+            print_version()
+        finally:
+            sys.stdout = orig_stdout
+            sys.stderr = orig_stderr
+
+        stdout = stdout.getvalue()
+
+        from .. import __version__ as version
+        self.assertIn(version, stdout)
+        self.assertEqual(stdout.count(version), 1)
