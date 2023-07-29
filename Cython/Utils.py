5fbfa7ed7bf7fdab8202e1ec1528ca10eef364d1
Stefan Behnel <stefan_ml@behnel.de>
2023-07-29 11:53:48 +0200

    Avoid redundant import.

--- Cython/Utils.py
+++ Cython/Utils.py
@@ -665,9 +665,8 @@ def write_depfile(target, source, dependencies):
 
 
 def print_version():
-    from . import __version__
-    print("Cython version %s" % __version__)
+    print("Cython version %s" % cython_version)
     # For legacy reasons, we also write the version to stderr.
     # New tools should expect it in stdout, but existing ones still pipe from stderr, or from both.
     if not sys.stderr.isatty() and os.fstat(1) != os.fstat(2):
-        sys.stderr.write("Cython version %s\n" % __version__)
+        sys.stderr.write("Cython version %s\n" % cython_version)
