b11f925e404da2fccc4ce1363a47a653c0b53740
Matti Picus <matti.picus@gmail.com>
2023-08-16 12:33:37 +0300

    Remove usages of deprecated numpy.distutils (GH-5620)

--- Demos/setup.py
+++ Demos/setup.py
@@ -12,13 +12,14 @@ ext_modules = cythonize("**/*.pyx", exclude="numpy_*.pyx")
 
 # Only compile the following if numpy is installed.
 try:
-    from numpy.distutils.misc_util import get_numpy_include_dirs
-    numpy_demo = [Extension("*",
-                            ["numpy_*.pyx"],
-                            include_dirs=get_numpy_include_dirs())]
-    ext_modules.extend(cythonize(numpy_demo))
+    from numpy import get_include
 except ImportError:
     pass
+else:
+    numpy_demo = [Extension("*",
+                            ["numpy_*.pyx"],
+                            include_dirs=get_include())]
+    ext_modules.extend(cythonize(numpy_demo))
 
 setup(
   name = 'Demos',
