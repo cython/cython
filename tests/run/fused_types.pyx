1743bf37ad77578e64b4a81cdbeb5868f5f48edb
Stefan Behnel <stefan_ml@behnel.de>
2023-08-26 18:16:20 +0200

    Fix invalid C code with -DCYTHON_CCOMPLEX=0 when assigning a simple (integer) default value to a fused type generated complex argument (which ends up as a struct).
    Closes https://github.com/cython/cython/issues/5644

--- tests/run/fused_types.pyx
+++ tests/run/fused_types.pyx
@@ -626,3 +626,52 @@ def test_null_default():
     print(result, xf_minus_1)
     result = null_default(xf)
     print(result)
+
+
+cdef cython.numeric fused_numeric_default(int a = 1, cython.numeric x = 0):
+    return x + a
+
+def test_fused_numeric_default(int a, x):
+    """
+    >>> test_fused_numeric_default(1, 0)
+    [1, 1.0, (1+0j)]
+
+    >>> test_fused_numeric_default(1, 2)
+    [3, 3.0, (3+0j)]
+
+    >>> test_fused_numeric_default(2, 0)
+    [2, 2.0, (2+0j)]
+
+    >>> test_fused_numeric_default(2, 1)
+    [3, 3.0, (3+0j)]
+    """
+    result = []
+
+    if a == 1 and x == 0:
+        result.append(fused_numeric_default[int]())
+    elif x == 0:
+        result.append(fused_numeric_default[int](a))
+    elif a == 1:
+        result.append(fused_numeric_default[int](1, x))
+    else:
+        result.append(fused_numeric_default[int](a, x))
+
+    if a == 1 and x == 0:
+        result.append(fused_numeric_default[float]())
+    elif x == 0:
+        result.append(fused_numeric_default[float](a))
+    elif a == 1:
+        result.append(fused_numeric_default[float](1, x))
+    else:
+        result.append(fused_numeric_default[float](a, x))
+
+    if a == 1 and x == 0:
+        result.append(fused_numeric_default[cython.doublecomplex]())
+    elif x == 0:
+        result.append(fused_numeric_default[cython.doublecomplex](a))
+    elif a == 1:
+        result.append(fused_numeric_default[cython.doublecomplex](1, x))
+    else:
+        result.append(fused_numeric_default[cython.doublecomplex](a, x))
+
+    return result
