90df8f2c8da9f051153deeae09f34839d571b3b0
Ralf Gommers <ralf.gommers@gmail.com>
2023-08-29 12:19:14 +0200

    Fix a null comparison compiler warning visible with gcc 12
    Closes gh-5663

--- Cython/Utility/ImportExport.c
+++ Cython/Utility/ImportExport.c
@@ -216,8 +216,7 @@ static PyObject *__Pyx_Import(PyObject *name, PyObject *from_list, int level) {
     {
         #if PY_MAJOR_VERSION >= 3
         if (level == -1) {
-            // Avoid C compiler warning if strchr() evaluates to false at compile time.
-            if ((1) && (strchr(__Pyx_MODULE_NAME, '.'))) {
+            if (strchr(__Pyx_MODULE_NAME, '.') != NULL) {
                 /* try package relative import first */
                 module = PyImport_ImportModuleLevelObject(
                     name, $moddict_cname, empty_dict, from_list, 1);
