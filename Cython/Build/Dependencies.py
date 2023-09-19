d6b6df173dc43625c22f23503b22253db35848e2
Harmen Stoppels <me@harmenstoppels.nl>
2023-09-19 21:36:24 +0200

    cythonize 0.29.x: use force=None as default instead of force=False

--- Cython/Build/Dependencies.py
+++ Cython/Build/Dependencies.py
@@ -878,7 +878,7 @@ def create_extension_list(patterns, exclude=None, ctx=None, aliases=None, quiet=
 
 
 # This is the user-exposed entry point.
-def cythonize(module_list, exclude=None, nthreads=0, aliases=None, quiet=False, force=False, language=None,
+def cythonize(module_list, exclude=None, nthreads=0, aliases=None, quiet=False, force=None, language=None,
               exclude_failures=False, **options):
     """
     Compile a set of source modules into C/C++ files and return a list of distutils
