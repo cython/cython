ae7d951bdf1fe74c054a2edd0dea4bbd280af548
Matus Valo <matusvalo@gmail.com>
2023-09-21 09:03:54 +0200

    Add main() function to .pyx examples

--- docs/examples/userguide/memoryviews/memory_layout_2.pyx
+++ docs/examples/userguide/memoryviews/memory_layout_2.pyx
@@ -1,17 +1,17 @@
 # mode: error
 from cython cimport view
 
+def main():
+    # VALID
+    cdef int[::view.indirect, ::1, :] a
+    cdef int[::view.indirect, :, ::1] b
+    cdef int[::view.indirect_contiguous, ::1, :] c
 
-# VALID
-cdef int[::view.indirect, ::1, :] a
-cdef int[::view.indirect, :, ::1] b
-cdef int[::view.indirect_contiguous, ::1, :] c
-
-# INVALID
-cdef int[::view.contiguous, ::view.indirect, :] d
-cdef int[::1, ::view.indirect, :] e
+    # INVALID
+    cdef int[::view.contiguous, ::view.indirect, :] d
+    cdef int[::1, ::view.indirect, :] e
 
 _ERRORS = u"""
-11:8: Only dimension 2 may be contiguous and direct
-12:8: Indirect dimension may not follow Fortran contiguous dimension
+11:12: Only dimension 2 may be contiguous and direct
+12:12: Indirect dimension may not follow Fortran contiguous dimension
 """
