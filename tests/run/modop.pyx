9db7c0df49e79ed5f916bdce700e04f9f2633f4d
Stefan Behnel <stefan_ml@behnel.de>
2023-11-07 17:51:57 +0100

    Remove more outdated Py2/3.x special cases from the tests.

--- tests/run/modop.pyx
+++ tests/run/modop.pyx
@@ -276,13 +276,13 @@ def modint(int int2, int int3):
 
 def modptr():
     """
-    >>> print(modptr() if sys.version_info[0] < 3 else 'spameggs')
+    >>> print(modptr())
     spameggs
     """
     cdef char *str2, *str3
     str2 = "spam%s"
     str3 = "eggs"
-    obj1 = str2 % str3  # '%' operator doesn't work on byte strings in Py3
+    obj1 = str2 % str3
     return obj1
 
 
