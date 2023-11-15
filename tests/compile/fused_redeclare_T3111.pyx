682b4df9d8d55c7b9670a47152a9bcf36c3656f7
Matus Valo <matusvalo@gmail.com>
2023-11-15 21:27:24 +0100

    Fix warning line numbers

--- tests/compile/fused_redeclare_T3111.pyx
+++ tests/compile/fused_redeclare_T3111.pyx
@@ -23,14 +23,14 @@ def foo(dtype_t[:] a, dtype_t_out[:, :] b):
 # unrelated to this test.
 _WARNINGS = """
 # cpdef redeclaration bug, from TestCythonScope.pyx
-25:10: 'cpdef_method' redeclared
-36:10: 'cpdef_cname_method' redeclared
+23:10: 'cpdef_method' redeclared
+34:10: 'cpdef_cname_method' redeclared
 
 # from MemoryView.pyx
-961:29: Ambiguous exception value, same as default return value: 0
-961:29: Ambiguous exception value, same as default return value: 0
-1002:46: Ambiguous exception value, same as default return value: 0
-1002:46: Ambiguous exception value, same as default return value: 0
-1092:29: Ambiguous exception value, same as default return value: 0
-1092:29: Ambiguous exception value, same as default return value: 0
+956:29: Ambiguous exception value, same as default return value: 0
+956:29: Ambiguous exception value, same as default return value: 0
+997:46: Ambiguous exception value, same as default return value: 0
+997:46: Ambiguous exception value, same as default return value: 0
+1087:29: Ambiguous exception value, same as default return value: 0
+1087:29: Ambiguous exception value, same as default return value: 0
 """
