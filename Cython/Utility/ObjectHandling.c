c053250814ff86d38575a10b7a1587420264d2b6
da-woods <dw-git@d-woods.co.uk>
2023-11-16 21:20:17 +0000

    Apply suggestions from code review
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Cython/Utility/ObjectHandling.c
+++ Cython/Utility/ObjectHandling.c
@@ -99,7 +99,7 @@ static int __Pyx_unpack_tuple2_generic(
 static CYTHON_INLINE int __Pyx_unpack_tuple2_exact(
         PyObject* tuple, PyObject** pvalue1, PyObject** pvalue2, int decref_tuple) {
     PyObject *value1 = NULL, *value2 = NULL;
-#if !CYTHON_ASSUME_SAFE_MACROS || CYTHON_AVOID_BORROWED_REFS
+#if CYTHON_AVOID_BORROWED_REFS || !CYTHON_ASSUME_SAFE_MACROS
     value1 = __Pyx_PySequence_ITEM(tuple, 0);  if (unlikely(!value1)) goto bad;
     value2 = __Pyx_PySequence_ITEM(tuple, 1);  if (unlikely(!value2)) goto bad;
 #else
@@ -113,7 +113,7 @@ static CYTHON_INLINE int __Pyx_unpack_tuple2_exact(
     *pvalue1 = value1;
     *pvalue2 = value2;
     return 0;
-#if !CYTHON_ASSUME_SAFE_MACROS || CYTHON_AVOID_BORROWED_REFS
+#if CYTHON_AVOID_BORROWED_REFS || !CYTHON_ASSUME_SAFE_MACROS
 bad:
     Py_XDECREF(value1);
     Py_XDECREF(value2);
@@ -1107,12 +1107,13 @@ __Pyx_PEP560_update_bases(PyObject *bases)
 #if CYTHON_ASSUME_SAFE_MACROS
                 base = PyTuple_GET_ITEM(bases, j);
                 PyList_SET_ITEM(new_bases, j, base);
+                Py_INCREF(base);
 #else
                 base = PyTuple_GetItem(bases, j);
                 if (!base) goto error;
+                Py_INCREF(base);
                 if (PyList_SetItem(new_bases, j, base) < 0) goto error;
 #endif
-                Py_INCREF(base);
             }
         }
 #if CYTHON_ASSUME_SAFE_MACROS
