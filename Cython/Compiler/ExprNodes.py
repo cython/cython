8af78af561d6bbdefcb36d02ba8af864a22e8bc4
da-woods <dw-git@d-woods.co.uk>
2023-11-20 18:56:38 +0000

    Further limited API fixes (#5798)
    1. A bunch of tests enabled (mostly already working)
    2. (some) fstrings work.
    3. cython.array works
    4. assorted other small fixes

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -3634,7 +3634,13 @@ class JoinedStrNode(ExprNode):
             code.putln("%s += %s;" % (ulength_var, ulength))
 
             node.generate_giveref(code)
+            code.putln('#if CYTHON_ASSUME_SAFE_MACROS')
             code.putln('PyTuple_SET_ITEM(%s, %s, %s);' % (list_var, i, node.py_result()))
+            code.putln('#else')
+            code.put_error_if_neg(
+                self.pos,
+                'PyTuple_SetItem(%s, %s, %s)' % (list_var, i, node.py_result()))
+            code.putln('#endif')
             node.generate_post_assignment_code(code)
             node.free_temps(code)
 
@@ -8210,7 +8216,7 @@ class SequenceNode(ExprNode):
         # in non-CPython, use the PySequence protocol (which can fail)
         if not use_loop:
             for i, item in enumerate(self.unpacked_items):
-                code.putln("%s = PySequence_ITEM(sequence, %d); %s" % (
+                code.putln("%s = __Pyx_PySequence_ITEM(sequence, %d); %s" % (
                     item.result(), i,
                     code.error_goto_if_null(item.result(), self.pos)))
                 code.put_gotref(item.result(), item.type)
@@ -8221,7 +8227,7 @@ class SequenceNode(ExprNode):
                 len(self.unpacked_items),
                 ','.join(['&%s' % item.result() for item in self.unpacked_items])))
             code.putln("for (i=0; i < %s; i++) {" % len(self.unpacked_items))
-            code.putln("PyObject* item = PySequence_ITEM(sequence, i); %s" % (
+            code.putln("PyObject* item = __Pyx_PySequence_ITEM(sequence, i); %s" % (
                 code.error_goto_if_null('item', self.pos)))
             code.put_gotref('item', py_object_type)
             code.putln("*(temps[i]) = item;")
@@ -8378,7 +8384,7 @@ class SequenceNode(ExprNode):
                 # resize the list the hard way
                 code.putln("((PyVarObject*)%s)->ob_size--;" % target_list)
                 code.putln('#else')
-                code.putln("%s = PySequence_ITEM(%s, %s-%d); " % (
+                code.putln("%s = __Pyx_PySequence_ITEM(%s, %s-%d); " % (
                     item.py_result(), target_list, length_temp, i+1))
                 code.putln('#endif')
                 item.generate_gotref(code)
