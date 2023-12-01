cb53895ff0af56bcb7056422ec9e824ad7d5211d
da-woods <dw-git@d-woods.co.uk>
2023-12-01 12:50:32 +0000

    Fix conflict enum to_py function with multiple modules
    Fix conflicting names of cpdef enum to_py functions when the an
    enum with the name name exists in multiple modules. Instead use
    the cname to name the to_py function since we have already
    ensured that it is unique and mangled with the module name.
    
    Possibly fixes #5860 (it definitely fixes a real bug, but that
    project has far too many dependencies for me to test, so who knows
    if it fixes *that* bug).

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -4297,7 +4297,7 @@ class EnumMixin:
 
     def create_enum_to_py_utility_code(self, env):
         from .UtilityCode import CythonUtilityCode
-        self.to_py_function = "__Pyx_Enum_%s_to_py" % self.name
+        self.to_py_function = "__Pyx_Enum_%s_to_py" % self.cname
         if self.entry.scope != env.global_scope():
             module_name = self.entry.scope.qualified_name
         else:
