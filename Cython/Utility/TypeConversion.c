30a6534a2279eddfd7a8b75de94897357eeaba2b
Stefan Behnel <stefan_ml@behnel.de>
2024-01-09 10:24:08 +0100

    Fix some C99-isms in 3.0.x branch.

--- Cython/Utility/TypeConversion.c
+++ Cython/Utility/TypeConversion.c
@@ -162,7 +162,7 @@ static CYTHON_INLINE Py_hash_t __Pyx_PyIndex_AsHash_t(PyObject*);
   typedef Py_ssize_t  __Pyx_compact_pylong;
   typedef size_t  __Pyx_compact_upylong;
 
-  #else  // Py < 3.12
+  #else  /* Py < 3.12 */
   #define __Pyx_PyLong_IsNeg(x)  (Py_SIZE(x) < 0)
   #define __Pyx_PyLong_IsNonNeg(x)  (Py_SIZE(x) >= 0)
   #define __Pyx_PyLong_IsZero(x)  (Py_SIZE(x) == 0)
