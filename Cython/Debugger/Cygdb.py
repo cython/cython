0cf4062db84985c8a1f2a24902b8457c6ead32fa
Kent Slaney <kentslaney@users.noreply.github.com>
2024-01-22 14:21:06 -0800

    comment summarizing GDB API calls
    Relevant documentation:
    https://sourceware.org/gdb/current/onlinedocs/gdb.html/Inferiors-Connections-and-Programs.html
    https://sourceware.org/gdb/current/onlinedocs/gdb.html/Progspaces-In-Python.html#index-Progspace_002esymbol_005ffile
    
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Cython/Debugger/Cygdb.py
+++ Cython/Debugger/Cygdb.py
@@ -89,6 +89,7 @@ def make_command_file(path_to_debug_info, prefix_code='',
                 f.write(textwrap.dedent('''\
                     python
                     import sys
+                    # Check if the Python executable provides a symbol table.
                     if not hasattr(gdb.selected_inferior().progspace, "symbol_file"):
                         sys.stderr.write(
                             "''' + interpreter + ''' was not compiled with debug symbols (or it was "
