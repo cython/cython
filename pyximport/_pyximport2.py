84273d60688468fd76df2381decdc4bac11e614d
Stefano Rivera <stefano@rivera.za.net>
2024-02-01 12:16:19 -0800

    pyximport: Use relative paths less, and correctly (#5957)
    * pyximport: Calculate the relative path correctly
    
    It should be relative to the new current directory, not the old one.
    
    * pyximport: Avoid chdir before build on non-Windows
    
    Using a relative path means we lose path information in the built
    module. This can be useful in packages such as stack_data that look up
    source code.

--- pyximport/_pyximport2.py
+++ pyximport/_pyximport2.py
@@ -187,13 +187,13 @@ def build_module(name, pyxfilename, pyxbuild_dir=None, inplace=False, language_l
     from . import pyxbuild
     olddir = os.getcwd()
     common = ''
-    if pyxbuild_dir:
+    if pyxbuild_dir and sys.platform == 'win32':
         # Windows concatenates the pyxbuild_dir to the pyxfilename when
         # compiling, and then complains that the filename is too long
         common = os.path.commonprefix([pyxbuild_dir, pyxfilename])
     if len(common) > 30:
-        pyxfilename = os.path.relpath(pyxfilename)
-        pyxbuild_dir = os.path.relpath(pyxbuild_dir)
+        pyxfilename = os.path.relpath(pyxfilename, common)
+        pyxbuild_dir = os.path.relpath(pyxbuild_dir, common)
         os.chdir(common)
     try:
         so_path = pyxbuild.pyx_to_dll(pyxfilename, extension_mod,
