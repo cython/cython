b08db8c5c281cf7f0b8b464be0ef7dc623cf3ee8
mattip <matti.picus@gmail.com>
2024-02-08 14:19:41 +0200

    fix the test for the extra line

--- tests/errors/nogil_buffer_acquisition.pyx
+++ tests/errors/nogil_buffer_acquisition.pyx
@@ -7,5 +7,5 @@ cdef void func(np.ndarray[np.double_t, ndim=1] myarray) nogil:
     pass
 
 _ERRORS = u"""
-5:15: Buffer may not be acquired without the GIL. Consider using memoryview slices instead.
+6:15: Buffer may not be acquired without the GIL. Consider using memoryview slices instead.
 """
