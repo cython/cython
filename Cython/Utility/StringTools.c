09144fd4a94313a0051b75d1bdaf5b044f5f0ace
da-woods <dw-git@d-woods.co.uk>
2024-02-21 21:57:40 +0000

    Refactor stringtab
    The stringtab can be a global constant rather than a function
    local variable that's largely initialized at runtime.
    Storage of cached strings in the module state is converted to
    a big array of PyObjects (which means some code can be replaced
    with loops).

--- Cython/Utility/StringTools.c
+++ Cython/Utility/StringTools.c
@@ -26,12 +26,12 @@ static CYTHON_INLINE Py_ssize_t __Pyx_Py_UNICODE_ssize_strlen(const Py_UNICODE *
 
 //////////////////// InitStrings.proto ////////////////////
 
-static int __Pyx_InitStrings(__Pyx_StringTabEntry *t); /*proto*/
+static int __Pyx_InitStrings(__Pyx_StringTabEntry const *t, PyObject **target); /*proto*/
 
 //////////////////// InitStrings ////////////////////
 
-static int __Pyx_InitStrings(__Pyx_StringTabEntry *t) {
-    while (t->p) {
+static int __Pyx_InitStrings(__Pyx_StringTabEntry const *t, PyObject **target) {
+    while (t->s) {
         PyObject *str;
         if (t->is_unicode | t->is_str) {
             if (t->intern) {
@@ -46,11 +46,12 @@ static int __Pyx_InitStrings(__Pyx_StringTabEntry *t) {
         }
         if (!str)
             return -1;
-        *t->p = str;
+        *target = str;
         // initialise cached hash value
         if (PyObject_Hash(str) == -1)
             return -1;
         ++t;
+        ++target;
     }
     return 0;
 }
