f8eff85d4167dc5ff0beed20268f7d6cd6020832
da-woods <dw-git@d-woods.co.uk>
2024-02-25 09:03:00 +0000

    Rename

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -8052,7 +8052,7 @@ class SequenceNode(ExprNode):
     unpacked_items = None
     mult_factor = None
     slow = False  # trade speed for code size (e.g. use PyTuple_Pack())
-    references_stolen = True  # set to False in code-generation if we
+    needs_subexpr_cleanup = False  # set to True in code-generation if we
             # didn't steal references to our temps and thus need to dispose
             # of them normally.
 
@@ -8206,7 +8206,7 @@ class SequenceNode(ExprNode):
                 ', '.join(arg.py_result() for arg in self.args),
                 code.error_goto_if_null(target, self.pos)))
             code.put_gotref(target, py_object_type)
-            self.references_stolen = False
+            self.needs_subexpr_cleanup = True
         elif self.type.is_ctuple:
             for i, arg in enumerate(self.args):
                 code.putln("%s.f%s = %s;" % (
@@ -8240,7 +8240,7 @@ class SequenceNode(ExprNode):
                 code.putln('for (%s=0; %s < %s; %s++) {' % (
                     counter, counter, c_mult, counter
                     ))
-                self.references_stolen = False
+                self.needs_subexpr_cleanup = True
             else:
                 offset = ''
 
@@ -8272,7 +8272,7 @@ class SequenceNode(ExprNode):
             code.putln('}')
 
     def generate_subexpr_disposal_code(self, code):
-        if not self.references_stolen:
+        if self.needs_subexpr_cleanup:
             super().generate_subexpr_disposal_code(code)
         else:
             # We call generate_post_assignment_code here instead
