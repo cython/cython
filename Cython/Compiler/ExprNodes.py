802b07c4896d03b250af9c0c8062d0de9669f7d4
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-11 03:14:05 +0000

    Fix handling in exprnodes

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -30,7 +30,7 @@ from . import Naming
 from . import Nodes
 from .Nodes import Node, SingleAssignmentNode
 from . import PyrexTypes
-from .PyrexTypes import py_object_type, typecast, error_type, \
+from .PyrexTypes import c_char_ptr_type, py_object_type, typecast, error_type, \
     unspecified_type
 from . import TypeSlots
 from .Builtin import (
@@ -11473,6 +11473,7 @@ class CythonArrayNode(ExprNode):
 
         shapes_temp = code.funcstate.allocate_temp(py_object_type, True)
         format_temp = code.funcstate.allocate_temp(py_object_type, True)
+        format_ptr_temp = code.funcstate.allocate_temp(c_char_ptr_type, True)
 
         itemsize = "sizeof(%s)" % dtype.empty_declaration_code()
         type_info = Buffer.get_type_information_cname(code, dtype)
@@ -11500,19 +11501,23 @@ class CythonArrayNode(ExprNode):
         ))
         code.put_gotref(shapes_temp, py_object_type)
 
+
         code.putln("#if CYTHON_COMPILING_IN_LIMITED_API")
-        code.putln('%s = __pyx_array_new(%s, %s, PyBytes_AsString(%s), "%s", (char *) %s); %s' % (
-            self.result(),
-            shapes_temp, itemsize, format_temp, self.mode, self.operand.result(),
-            code.error_goto_if_null(self.result(), self.pos),
+        code.putln('%s = PyBytes_AsString(%s); %s' % (
+            format_ptr_temp, format_temp,
+            code.error_goto_if_null(format_ptr_temp, self.pos),
         ))
         code.putln("#else")
-        code.putln('%s = __pyx_array_new(%s, %s, PyBytes_AS_STRING(%s), "%s", (char *) %s); %s' % (
-            self.result(),
-            shapes_temp, itemsize, format_temp, self.mode, self.operand.result(),
-            code.error_goto_if_null(self.result(), self.pos),
+        code.putln('%s = PyBytes_AS_STRING(%s);' % (
+            format_ptr_temp, format_temp,
         ))
         code.putln("#endif")
+
+        code.putln('%s = __pyx_array_new(%s, %s, %s, "%s", (char *) %s); %s' % (
+            self.result(),
+            shapes_temp, itemsize, format_ptr_temp, self.mode, self.operand.result(),
+            code.error_goto_if_null(self.result(), self.pos),
+        ))
         self.generate_gotref(code)
 
         def dispose(temp):
@@ -11521,6 +11526,7 @@ class CythonArrayNode(ExprNode):
 
         dispose(shapes_temp)
         dispose(format_temp)
+        dispose(format_ptr_temp)
 
     @classmethod
     def from_carray(cls, src_node, env):
