cb807b4db634a169a838dfe9827fed7e391b43c3
da-woods <dw-git@d-woods.co.uk>
2024-03-17 09:22:44 +0000

    Code style

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -4658,7 +4658,7 @@ class CTupleType(CType):
 def c_tuple_type(components):
     components = tuple(components)
     if any(c.is_fused for c in components):
-        cname = "<dummy fused ctuple>" # should never end up in code
+        cname = "<dummy fused ctuple>"  # should never end up in code
     else:
         cname = Naming.ctuple_type_prefix + type_list_identifier(components)
     tuple_type = CTupleType(cname, components)
