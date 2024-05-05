bb0080cf9e736aa78845bb3281f53553ee232a6d
da-woods <dw-git@d-woods.co.uk>
2024-05-05 09:15:18 +0100

    Apply suggestions from code review

--- docs/examples/userguide/extension_types/dataclass.pyx
+++ docs/examples/userguide/extension_types/dataclass.pyx
@@ -11,10 +11,10 @@ cdef class MyDataclass:
     a: cython.int = 0
     b: double = cython.dataclasses.field(default_factory = lambda: 10, repr=False)
 
-    # fields can also be declared using `cdef`:  
-    cdef str c   # add `readonly` or `public` to `c` if needs to be accessed by Python for reading or writing.
+    # fields can also be declared using `cdef`:
+    cdef str c   # add `readonly` or `public` to if `c` needs to be accessible from Python
     c = "hello"  # assignment of default value on a separate line
-    # note: `@dataclass(frozen)` is not applicable to `cdef`
+    # note: `@dataclass(frozen)` is not enforced on `cdef` attributes
 
     # typing.InitVar and typing.ClassVar also work
     d: dataclasses.InitVar[double] = 5
