34b485d6ce36606afb581016cc899eff85461652
YoSTEALTH <35307184+YoSTEALTH@users.noreply.github.com>
2024-05-05 02:15:41 -0600

    [dpcs] Improve `@dataclass` example (#4866)

--- docs/examples/userguide/extension_types/dataclass.pyx
+++ docs/examples/userguide/extension_types/dataclass.pyx
@@ -13,8 +13,9 @@ cdef class MyDataclass:
     b: cython.double = dataclasses.field(default_factory = lambda: 10, repr=False)
 
     # fields can also be declared using `cdef`:
-    cdef str c
+    cdef str c   # add `readonly` or `public` to if `c` needs to be accessible from Python
     c = "hello"  # assignment of default value on a separate line
+    # note: `@dataclass(frozen)` is not enforced on `cdef` attributes
 
     # typing.InitVar and typing.ClassVar also work
     d: dataclasses.InitVar[cython.double] = 5
