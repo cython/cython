90afb5dead7014e5d8828844545f805f98dbcda4
da-woods <dw-git@d-woods.co.uk>
2024-06-09 17:02:53 +0100

    Use (and fix) existing test instead

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -1619,7 +1619,7 @@ class CppClassNode(CStructOrUnionDefNode, BlockNode):
     def analyse_declarations(self, env):
         if not env.is_cpp():
             warning(self.pos,
-                    "Using 'cppclass' while Cython is not in C++ mode.",
+                    "Using 'cppclass' while Cython is not in c++ mode",
                     level=1)
         if self.templates is None:
             template_types = template_names = None
