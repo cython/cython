92d73626601408f91a2dadca851d8d50f1f3952c
scoder <stefan_ml@behnel.de>
2024-06-20 11:44:52 +0200

    Fix test code

--- tests/run/pure_py37.py
+++ tests/run/pure_py37.py
@@ -4,18 +4,18 @@
 import cython
 
 
-def test_module_getattr_pointers:
-    cython.p_int
-    cython.pp_int
-    cython.ppp_int   # < py3.7 limit in interpreted mode
+def test_module_getattr_pointers():
+    pint: cython.p_int
+    ppint: cython.pp_int
+    pppint: cython.ppp_int
 
-    cython.pppp_int  # dynamic attr generation
-    cython.pppppppppppppppppppppp_int
+    ppppint: cython.pppp_int  # dynamic attr generation
+    ppppppppppppppppppppppint: cython.pppppppppppppppppppppp_int
 
     # test on another type
-    cython.ppppppppppppppppp_float
+    pppppppppppppppppfloat: cython.ppppppppppppppppp_float
 
 
-def test_module_getattr_import_pointers:
+def test_module_getattr_import_pointers():
     # test direct import
     from cython import pppp_int
