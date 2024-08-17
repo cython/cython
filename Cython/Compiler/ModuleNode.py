8c59bd50965edc3a3ff718f66b6da8a176f10d75
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-08-17 14:27:58 +0100

    Use .as_posix for normalisation of paths on output

--- Cython/Compiler/ModuleNode.py
+++ Cython/Compiler/ModuleNode.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 import json
 import operator
 import os
+import pathlib
 import re
 import sys
 
@@ -967,7 +968,7 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
                     # never include absolute paths
                     file_path = source_desc.get_description()
                 # Always use / as separator
-                file_path = file_path.replace('\\', '/')
+                file_path = str(pathlib.Path(file_path).as_posix())
                 escaped_filename = as_encoded_filename(file_path)
                 code.putln('%s,' % escaped_filename.as_c_string_literal())
         else:
