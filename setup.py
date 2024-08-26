5fa0660e3901bc5a6ec573d0ea320588d59effa2
rathaROG <siv.ratha89@gmail.com>
2024-08-26 21:20:31 +0200

    Mention Python 3.13 support in the `classifiers` in setup.py (GH-6365)
    Closes https://github.com/cython/cython/issues/6364

--- setup.py
+++ setup.py
@@ -292,6 +292,7 @@ def run_build():
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Programming Language :: C",
