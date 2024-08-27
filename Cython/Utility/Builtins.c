b7bdca5d2cc88c149cc8fbc9bcc50f4efd11eab3
Lisandro Dalcin <dalcinl@gmail.com>
2024-08-27 12:03:03 +0300

    Fix CYTHON_LIMITED_API -> __PYX_LIMITED_VERSION_HEX

--- Cython/Utility/Builtins.c
+++ Cython/Utility/Builtins.c
@@ -618,7 +618,7 @@ static CYTHON_INLINE int __Pyx_PySet_Update(PyObject* set, PyObject* it) {
 ///////////////// memoryview_get_from_buffer.proto ////////////////////
 
 // buffer is in limited api from Py3.11
-#if !CYTHON_COMPILING_IN_LIMITED_API || CYTHON_LIMITED_API >= 0x030b0000
+#if !CYTHON_COMPILING_IN_LIMITED_API || __PYX_LIMITED_VERSION_HEX >= 0x030b0000
 #define __Pyx_PyMemoryView_Get_{{name}}(o) PyMemoryView_GET_BUFFER(o)->{{name}}
 #else
 {{py:
@@ -632,7 +632,7 @@ static {{out_type}} __Pyx_PyMemoryView_Get_{{name}}(PyObject *obj); /* proto */
 
 ////////////// memoryview_get_from_buffer /////////////////////////
 
-#if !CYTHON_COMPILING_IN_LIMITED_API || CYTHON_LIMITED_API >= 0x030b0000
+#if !CYTHON_COMPILING_IN_LIMITED_API || __PYX_LIMITED_VERSION_HEX >= 0x030b0000
 #else
 {{py:
 out_types = dict(
