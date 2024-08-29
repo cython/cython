ab35f2f77a33d16c0137d901a3cf3b796909e2f5
scoder <stefan_ml@behnel.de>
2024-08-29 04:25:06 +0200

    Give cdef functions the same position and first line number as def and @cfunc functions (GH-6369)
    Python uses the line of the outermost (first) decorator, not necessarily the function signature line, as first line of the function.
    
    Closes https://github.com/cython/cython/issues/6366

--- tests/errors/cpdef_syntax.pyx
+++ tests/errors/cpdef_syntax.pyx
@@ -3,8 +3,9 @@
 cpdef nogil: pass
 cpdef nogil class test: pass
 
+
 _ERRORS = u"""
-3: 6: cdef blocks cannot be declared cpdef
-4: 6: cdef blocks cannot be declared cpdef
+3: 0: cdef blocks cannot be declared cpdef
+4: 0: cdef blocks cannot be declared cpdef
 4:12: Expected ':', found 'class'
 """
--- tests/run/nogil.pyx
+++ tests/run/nogil.pyx
@@ -219,18 +219,18 @@ _PERFORMANCE_HINTS = """
 5:18: No exception value declared for 'f_in_pxd1' in pxd file.
 6:18: No exception value declared for 'f_in_pxd2' in pxd file.
 20:9: Exception check after calling 'f' will always require the GIL to be acquired.
-24:5: Exception check on 'f' will always require the GIL to be acquired.
-34:5: Exception check on 'release_gil_in_nogil' will always require the GIL to be acquired.
-39:6: Exception check on 'release_gil_in_nogil2' will always require the GIL to be acquired.
+24:0: Exception check on 'f' will always require the GIL to be acquired.
+34:0: Exception check on 'release_gil_in_nogil' will always require the GIL to be acquired.
+39:0: Exception check on 'release_gil_in_nogil2' will always require the GIL to be acquired.
 49:28: Exception check after calling 'release_gil_in_nogil' will always require the GIL to be acquired.
 51:29: Exception check after calling 'release_gil_in_nogil2' will always require the GIL to be acquired.
-55:5: Exception check on 'get_gil_in_nogil' will always require the GIL to be acquired.
-59:6: Exception check on 'get_gil_in_nogil2' will always require the GIL to be acquired.
+55:0: Exception check on 'get_gil_in_nogil' will always require the GIL to be acquired.
+59:0: Exception check on 'get_gil_in_nogil2' will always require the GIL to be acquired.
 68:24: Exception check after calling 'get_gil_in_nogil' will always require the GIL to be acquired.
 70:25: Exception check after calling 'get_gil_in_nogil2' will always require the GIL to be acquired.
-133:5: Exception check on 'copy_array_exception' will always require the GIL to be acquired.
+133:0: Exception check on 'copy_array_exception' will always require the GIL to be acquired.
 184:28: Exception check after calling 'copy_array_exception' will always require the GIL to be acquired.
-187:5: Exception check on 'voidexceptnogil_in_pxd' will always require the GIL to be acquired.
+187:0: Exception check on 'voidexceptnogil_in_pxd' will always require the GIL to be acquired.
 195:30: Exception check after calling 'voidexceptnogil_in_pxd' will always require the GIL to be acquired.
 198:36: Exception check after calling 'voidexceptnogil_in_other_pxd' will always require the GIL to be acquired.
 """
