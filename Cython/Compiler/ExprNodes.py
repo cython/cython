004a5e054a37cc8ea7702f7945090f26aea738e4
da-woods <dw-git@d-woods.co.uk>
2024-10-30 22:06:09 +0000

    Pick a few small fixes out of #6226
    So that they don't get lost based on being part of a larger
    change that may not be our proposed behaviour.
    
    A couple of `is_target` set on the lhs on generated assignments
    that are generated too late to pick it up in analysis.
    
    Skip `self.arg.generate_result_code` because it already gets
    called indirectly via `ExprNode.generate_evaluation_code` ->
    `ExprNode.generate_subexpr_evaluation_code`.

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -14114,7 +14114,7 @@ class CastNode(CoercionNode):
         return self.arg.result_as(self.type)
 
     def generate_result_code(self, code):
-        self.arg.generate_result_code(code)
+        pass
 
 
 class PyTypeTestNode(CoercionNode):
