277908edb81b728f5fab14e8048950772f0dac76
Ovsk <bbb23exposed@gmail.com>
2024-11-05 12:12:30 -0500

    fix: use specialization_name in tempita function name

--- Cython/Utility/Builtins.c
+++ Cython/Utility/Builtins.c
@@ -734,12 +734,12 @@ static inline int __Pyx_bit_count_base(unsigned PY_LONG_LONG x, int size) {
 
 //////////////////// bit_count.proto ////////////////////
 
-static CYTHON_INLINE {{type_.sign_and_name()}} __Pyx_{{type_}}_bit_count({{type_.sign_and_name()}} x);
+static CYTHON_INLINE {{type_.sign_and_name()}} __Pyx_{{type_.specialization_name()}}_bit_count({{type_.sign_and_name()}} x);
 
 //////////////////// bit_count.proto ////////////////////
 //@requires: bit_count_base
 
-static CYTHON_INLINE {{ type_.sign_and_name()}} __Pyx_{{type_}}_bit_count({{type_.sign_and_name()}} {{'x' if type_.signed else 'value'}}) {
+static CYTHON_INLINE {{ type_.sign_and_name()}} __Pyx_{{type_.specialization_name()}}_bit_count({{type_.sign_and_name()}} {{'x' if type_.signed else 'value'}}) {
 {{if type_.signed}}
     if (x == CHAR_MIN) return 1;
     unsigned {{type_.rank_name()}} value = (x < 0) ? -x : x;
