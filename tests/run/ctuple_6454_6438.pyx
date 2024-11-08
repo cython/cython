aa7459ca6679138b6e23115bde3c974ba1c4f48a
Ovsk <bbb23exposed@gmail.com>
2024-11-08 17:06:49 -0500

    revert accidental changes

--- tests/run/ctuple_6454_6438.pyx
+++ tests/run/ctuple_6454_6438.pyx
@@ -12,17 +12,10 @@ ctypedef (long, long, s) z
 
 ctypedef (void*, i, void*, v, z, z*) c
 
-ctypedef fused a:
-    v
-    z
+cdef bint test(c p):
+    return (1, 2, s(5, 4, 3)) not in p
 
-cdef v test(a p):
-    return p
-
-"""assert test((NULL, 5, NULL, (1, 2, s(5, 4, 3)), (11, 22, s(55, 44, 33)), NULL)) is False
+assert test((NULL, 5, NULL, (1, 2, s(5, 4, 3)), (11, 22, s(55, 44, 33)), NULL)) is False
 assert test((NULL, 5, NULL, (1, 2, s(1, 4, 3)), (11, 22, s(55, 44, 33)), NULL)) is True
 assert test((NULL, 5, NULL, (0, 2, s(5, 4, 3)), (11, 22, s(55, 44, 33)), NULL)) is True
 assert test((NULL, 5, NULL, (11, 22, s(55, 44, 33)), (1, 2, s(5, 4, 3)), NULL)) is False
-"""
-
-print(test((1, 2, s(5, 4, 3))))
