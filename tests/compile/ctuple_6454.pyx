17702b9476bce5e1fdc9e100480060b8e0f8fbc5
Ovsk <bbb23exposed@gmail.com>
2024-11-08 17:48:29 -0500

    add assignment test

--- tests/compile/ctuple_6454.pyx
+++ tests/compile/ctuple_6454.pyx
@@ -4,7 +4,13 @@ ctypedef fused ab:
     (int, int, int)
     (int, int, int, int)
 
-cdef int test(ab a):
+cdef int test(ab x):
     return 1
+
+ctypedef (float, float) c
+ctypedef (int, int) d
+
+cdef d test2(c x):
+    return x
 
 print(test((1, 2, 3)))
