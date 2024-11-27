e8e1d567c3bbbbc0fe5f6d37db0de3f71a660866
da-woods <dw-git@d-woods.co.uk>
2024-11-27 08:25:59 +0000

    Change double_critical_section test to be more robust

--- tests/run/critical_sections.pyx
+++ tests/run/critical_sections.pyx
@@ -35,57 +35,53 @@ def test_single_critical_section(n_threads, n_loops):
     expected = ((n_loops * (n_loops - 1))/2)*n_threads
     assert count == expected, (count, expected)
 
+
 def test_double_critical_section(n_loops):
     """
     >>> test_double_critical_section(100)
     """
-    cdef int a = 2
-    cdef int b = 3
+    cdef int a = 0
+    cdef int b = 0
     barrier = threading.Barrier(3)
-    finished_event = threading.Event()
-    failed = threading.Event()
     lock_a = object()
     lock_b = object()
 
-    def reader1():
+    def a_thread():
+        nonlocal a
         barrier.wait()
-        while not finished_event.is_set():
-            for _ in range(10):
-                with cython.critical_section(lock_a):
-                    if not (a==2 or a==3):
-                        failed.set()
+        for i in range(n_loops):
+            with cython.critical_section(lock_a):
+                a += i
 
-    def reader2():
+    def b_thread():
+        nonlocal b
         barrier.wait()
-        while not finished_event.is_set():
-            for _ in range(10):
-                with cython.critical_section(lock_b):
-                    if not (b==2 or b==3):
-                        failed.set()
+        for i in range(n_loops):
+            with cython.critical_section(lock_b):
+                b += i
 
-    def swapper():
+    def ab_thread():
         nonlocal a, b
         barrier.wait()
-        for _ in range(n_loops):
+        for i in range(n_loops):
             with cython.critical_section(lock_a, lock_b):
-                old_a = a
-                old_b = b
-                a = 1000
-                b = 2000
-                a = old_b
-                b = old_a
-        finished_event.set()
-    t1 = threading.Thread(target=reader1)
-    t2 = threading.Thread(target=reader2)
-    t3 = threading.Thread(target=swapper)
-    t1.start()
-    t2.start()
-    t3.start()
-    t1.join()
-    t2.join()
-    t3.join()
+                a += i
+                b += i
 
-    assert not failed.is_set()
+    threads = [
+        threading.Thread(target=target)
+        for target in [a_thread, b_thread, ab_thread]
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    # Each variable is modified from 2 threads
+    expected = ((n_loops * (n_loops - 1))/2)*2
+    assert a == expected, (a, expected)
+    assert b == expected, (b, expected)
+
 
 def test_critical_section_in_generators(n_threads, n_loops):
     """
