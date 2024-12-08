7723f251b7b5ef1fea025c99d4e4abef4320dda2
da-woods <dw-git@d-woods.co.uk>
2024-12-08 16:48:17 +0000

    Fix error gotos

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -4068,20 +4068,20 @@ class DefNodeWrapper(FuncDefNode):
             code.put_var_gotref(self.star_arg.entry)
             code.put_incref(Naming.self_cname, py_object_type)
             code.put_giveref(Naming.self_cname, py_object_type)
-            code.error_goto_if_neg("__Pyx_PyTuple_SET_ITEM(%s, 0, %s)" % (
-                self.star_arg.entry.cname, Naming.self_cname), self.pos)
+            code.putln(code.error_goto_if_neg("__Pyx_PyTuple_SET_ITEM(%s, 0, %s)" % (
+                self.star_arg.entry.cname, Naming.self_cname), self.pos))
             temp = code.funcstate.allocate_temp(PyrexTypes.c_py_ssize_t_type, manage_ref=False)
             code.putln("for (%s=0; %s < %s; %s++) {" % (
                 temp, temp, Naming.nargs_cname, temp))
             code.putln("PyObject* item = __Pyx_PyTuple_GET_ITEM(%s, %s);" % (
                 Naming.args_cname, temp))
             code.putln("#if !CYTHON_ASSUME_SAFE_MACROS")
-            code.error_goto_if_null("item", self.pos)
+            code.putln(code.error_goto_if_null("item", self.pos))
             code.putln("#endif")
             code.put_incref("item", py_object_type)
             code.put_giveref("item", py_object_type)
-            code.error_goto_if_neg("__Pyx_PyTuple_SET_ITEM(%s, %s+1, item)" % (
-                self.star_arg.entry.cname, temp), self.pos)
+            code.putln(code.error_goto_if_neg("__Pyx_PyTuple_SET_ITEM(%s, %s+1, item)" % (
+                self.star_arg.entry.cname, temp), self.pos))
             code.putln("}")
             code.funcstate.release_temp(temp)
             self.star_arg.entry.xdecref_cleanup = 0
