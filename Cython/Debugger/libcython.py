b6665c7ec15c9346088c5827ba60324238235efd
Zachary Spector <public@zacharyspector.com>
2024-12-21 23:27:29 +1300

    Update docstring

--- Cython/Debugger/libcython.py
+++ Cython/Debugger/libcython.py
@@ -313,7 +313,7 @@ class CythonBase:
     @default_selected_gdb_frame()
     def get_cython_lineno(self, frame):
         """
-        Get the current Cython line number. Returns 0 if there is no
+        Get the current Cython line number. Returns ("<no filename>", 0) if there is no
         correspondence between the C and Cython code.
         """
         cyfunc = self.get_cython_function(frame)
