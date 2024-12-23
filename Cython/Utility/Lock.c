484124a97eeb84fb53f2252edc53cd28baa7bf31
da-woods <dw-git@d-woods.co.uk>
2024-12-23 07:53:56 +0000

    Probably don't use this

--- Cython/Utility/Lock.c
+++ Cython/Utility/Lock.c
@@ -1,63 +1,86 @@
 ////////////////////// CythonLockType.proto ////////////////////
 //@proto_block: utility_code_proto_before_types
 
-// TODO - this imposes an ABI so we need to do something clever on public functions/classes
+// We support two implementations - a Py3.13+ version using PyMutex and
+// an older version using PyThread_type_lock.
+// In principle it'd be possible to also use things like c++ std::mutex
+// (in the absence of PyMutex). I've decided against this for ABI reasons.
+
+// There is an ABI problem - if the lock type is ever shared between
+// modules (e.g. in a cdef class in a pxd file) then it's important that
+// the modules agree on the definition.
+
+// Where possible we try not to use the shared ABI to remove a layer of
+// indirection, and to ensure we aren't forced into a slower implementation
+// by a Limited API module being imported first. 
+
+#ifndef CYTHON_USE_CYTHON_LOCK_ABI
+#define CYTHON_USE_CYTHON_LOCK_ABI 0
+#endif
+
+static void __Pyx__InitCythonLock(__Pyx_CythonLockType *lock); /* proto */
+static void __Pyx__DeallocCythonLock(__Pyx_CythonLockType *lock); /* proto */
+static int __Pyx__LockCythonLock(__Pyx_CythonLockType *lock); /* proto */
+static int __Pyx__UnlockCythonLock(__Pyx_CythonLockType *lock); /* proto */
+static int __Pyx__LockCythonLock_Gil(__Pyx_CythonLockType *lock); /* proto */
+static int __Pyx__LockCythonLock_NoGil(__Pyx_CythonLockType *lock); /* proto */
+
+#if CYTHON_USE_CYTHON_LOCK_ABI
+typedef union {
+    // As of 3.13.0, Py_Mutex is smaller than PyThread_type_lock,
+    // so the size is the same whether or not we can see "as_mutex".
+    #if PY_VERSION_HEX > 0x030d0000 && !CYTHON_COMPILING_IN_LIMITED_API
+    Py_Mutex as_mutex;
+    #endif
+    PyThread_type_lock as_pythread_lock;
+} __Pyx_CythonLockType;
+
+typedef struct {
+    void (*init)(__Pyx_CythonLockType*);
+    void (*dealloc)(__Pyx_CythonLockType*);
+    int (*lock)(__Pyx_CythonLockType*);
+    int (*unlock)(__Pyx_CythonLockType*);
+    // Optimized versions for when we know the GIL state
+    int (*lock_nogil)(__Pyx_CythonLockType*);
+    int (*lock_gil)(__Pyx_CythonLockType*)
+}  __Pyx_CythonLockAbi;
+
+static __Pyx_CythonLockAbi __Pyx_CythonLockDefaultAbi = {
+    &__Pyx__InitCythonLock,
+    &__Pyx__DeallocCythonLock,
+    &__Pyx__LockCythonLock,
+    &__Pyx__UnlockCythonLock,
+    &__Pyx__LockCythonLock_NoGil,
+    &__Pyx__LockCythonLock_Gil
+};
+
+static __Pyx_CythonLockAbi* __Pyx_CythonLockSharedAbi = NULL;
+
+#define __Pyx_InitCythonLock(l) __Pyx_CythonLockSharedAbi->init(&(l))
+#define __Pyx_DeallocCythonLock(l) __Pyx_CythonLockSharedAbi->dealloc(&(l))
+#define __Pyx_LockCythonLock(l) __Pyx_CythonLockSharedAbi->lock(&(l))
+#define __Pyx_UnlockCythonLock __Pyx_CythonLockSharedAbi->unlock(&(l))
+#define __Pyx_LockCythonLock_NoGil __Pyx_CythonLockSharedAbi->lock_nogil(&(l))
+#define __Pyx_LockCythonLock_Gil __Pyx_CythonLockSharedAbi->lock_gil(&(l))
+#else
+
+#endif
 
 #if PY_VERSION_HEX > 0x030d0000 && !CYTHON_COMPILING_IN_LIMITED_API
 #define __Pyx_CythonLockType Py_Mutex
 #define __Pyx_InitCythonLock(l) (void)(l)
 #define __Pyx_DeleteCythonLock(l) (void)(l)
 // Py_Mutex takes care of all GIL handling itself
-#define __Pyx_LockCythonLock(l) (PyMutex_Lock(l), 0)
-#define __Pyx_UnlockCythonLock(l) (PyMutex_Unlock(l), 0)
+#define __Pyx_LockCythonLock(l) (PyMutex_Lock(&l), 0)
+#define __Pyx_UnlockCythonLock(l) (PyMutex_Unlock(&l), 0)
 
 #else
 
 static void __Pyx_RaiseCythonLockError(const char* what); /* proto */
 
-#if defined(__cplusplus) && __cplusplus >= 201103L
-#include <mutex>
-#include <new>
-#include <system_error>
-union __Pyx_CythonLockType { std::mutex m; };
-#define __Pyx_InitCythonLock(l) new (&(l).m) std::mutex()
-#define __Pyx_DeleteCythonLock(l) (l).m.~mutex()
-
-static CYTHON_INLINE int __Pyx__LockCythonTryLock(__Pyx_CythonLockType *l_ptr) {
-    try {
-        return l_ptr->m.try_lock();
-    } catch (const std::system_error& e) {
-        __Pyx_RaiseCythonLockError(e.what());
-        return -1;
-    }
-}
-
-static CYTHON_INLINE int __Pyx__LockCythonLock_NoGil(__Pyx_CythonLockType *l_ptr) {
-    try {
-        l_ptr->m.lock();
-        return 1;
-    } catch (const std::system_error& e) {
-        __Pyx_RaiseCythonLockError(e.what());
-        return -1;
-    }
-}
-
-static CYTHON_INLINE int __Pyx__UnlockCythonLock(__Pyx_CythonLockType *l_ptr) {
-    try {
-        l_ptr->m.unlock();
-        return 0;
-    } catch (const std::system_error& e) {
-        __Pyx_RaiseCythonLockError(e.what());
-        return -1;
-    }
-}
-
-#elif __STDC_VERSION__ >= 201112L && !defined(__STDC_NO_THREADS__)
-#include  <threads.h>
-#define __Pyx_CythonLockType mtx_t
-#define __Pyx_InitCythonLock(l) mtx_init(&(l), mtx_plain)
-#define __Pyx_DeleteCythonLock(l) mtx_destroy(&(l))
-
+#define __Pyx_CythonLockType PyThread_type_lock
+#define __Pyx_InitCythonLock(l) l = PyThread_allocate_lock()
+#define __Pyx_DeleteCythonLock(l) = PyThread_free_lock()
 static CYTHON_INLINE int __Pyx__LockCythonTryLock(__Pyx_CythonLockType *l_ptr) {
     int result = mtx_trylock(l_ptr);
     if (likely(result == thrd_success)) {
@@ -79,23 +102,9 @@ static CYTHON_INLINE int __Pyx__LockCythonLock_NoGil(__Pyx_CythonLockType *l_ptr
 }
 
 static CYTHON_INLINE int __Pyx__UnlockCythonLock(__Pyx_CythonLockType *l_ptr) {
-    int result = mtx_unlock(l_ptr);
-    if (unlikely (result == thrd_error)) {
-        __Pyx_RaiseCythonLockError("Error when acquiring cython.lock");
-        return -1;
-    }
+    PyThread_release_lock(*l_ptr);
     return 0;
 }
-
-// TODO other implementations like pthreads, MSVC.
-
-#else
-
-#define __Pyx_CythonLockType PyThread_type_lock
-#define __Pyx_InitCythonLock(l) l = PyThread_allocate_lock()
-#define __Pyx_DeleteCythonLock(l) = PyThread_free_lock()
-
-#endif
 
 #define __Pyx_LockCythonLock(l) __Pyx__LockCythonLock(&(l))
 static int __Pyx__LockCythonLock(__Pyx_CythonLockType *lock); /* proto*/
