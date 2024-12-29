63e7cd1e2e2b5c3f0a5b4a1ac1ab5a3597d32338
da-woods <dw-git@d-woods.co.uk>
2024-12-29 10:33:51 +0000

    Not limited API

--- Cython/Utility/Optimize.c
+++ Cython/Utility/Optimize.c
@@ -444,11 +444,11 @@ static CYTHON_INLINE int __Pyx_dict_iter_next(
 #if !CYTHON_COMPILING_IN_PYPY
     if (source_is_dict) {
         int result;
-#if PY_VERSION_HEX >= 0x030d0000
+#if PY_VERSION_HEX >= 0x030d0000 && !CYTHON_COMPILING_IN_LIMITED_API
         Py_BEGIN_CRITICAL_SECTION(iter_obj);
 #endif
         result = __Pyx_dict_iter_next_source_is_dict(iter_obj, orig_length, ppos, pkey, pvalue, pitem);
-#if PY_VERSION_HEX >= 0x030d0000
+#if PY_VERSION_HEX >= 0x030d0000 && !CYTHON_COMPILING_IN_LIMITED_API
         Py_END_CRITICAL_SECTION();
 #endif
         return result;
