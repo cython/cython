a26589d323dc1edd602993ecbf359ca190051b7e
da-woods <dw-git@d-woods.co.uk>
2025-01-23 21:48:37 +0000

    Add critical_section decorator (#6577)
    Allows critical_section to be applied as a function decorator, to put
    the entire function body in a with critical_section block (using the
    first argument as the lock).
    
    This is just a useful bit of syntax (I hope) - it doesn't do anything
    that you can't already do with the with statement.
    
    It's inspired by a somewhat similar feature in Python's argument clinic
    (see e.g.
    https://github.com/python/cpython/blob/3f6a618e49b1c8c12a7bc0c26e846735e108dc97/Modules/_queuemodule.c#L284)
    which seems like a useful tool there so I thought I'd copy it.

--- docs/src/userguide/freethreading.rst
+++ docs/src/userguide/freethreading.rst
@@ -87,6 +87,24 @@ We suggest reading the Python documentation to understand how critical sections 
 On non-freethreading builds ``cython.critical_section`` does nothing - you get the
 same guarantees simply from the fact you hold the GIL.
 
+As an alternative syntax, ``cython.critical_section`` can be used as a decorator
+or a function taking at least one argument.  In this case the critical section
+lasts the duration of the function and locks on the first argument::
+
+    @cython.cclass
+    class C:
+        @cython.critical_section
+        def func(self, *args):
+            ...
+
+        # equivalent to:
+        def func(self, *args):
+            with cython.critical_section(self):
+                ...
+
+Our expectation is that this will be most useful for locking on the ``self`` argument
+of methods in C classes.
+
 Pitfalls
 ========
 
--- tests/errors/critical_sections.pyx
+++ tests/errors/critical_sections.pyx
@@ -18,6 +18,30 @@ def f():
         with cython.critical_section(o):  # critical sections require the GIL
             pass
 
+@cython.critical_section
+def crit_sec_func():
+    pass
+
+@cython.critical_section
+def crit_sec_func2(*args):
+    pass
+
+@cython.critical_section
+def crit_sec_func3(**kwds):
+    pass
+
+@cython.critical_section(True)  # should not have arguments
+def crit_sec_func4(arg):
+    pass
+
+@cython.critical_section
+def has_int_arg(int arg):
+    pass
+
+@cython.critical_section
+cdef void some_nogil_function(a) noexcept nogil:
+    pass
+
 
 _ERRORS = """
 7:9: critical_section directive accepts one or two positional arguments
@@ -26,6 +50,14 @@ _ERRORS = """
 13:33: Arguments to cython.critical_section must be Python objects.
 15:33: Arguments to cython.critical_section must be Python objects.
 18:13: Critical sections require the GIL
+21:0: critical_section directive can only be applied to a function with one or more positional arguments
+25:0: critical_section directive can only be applied to a function with one or more positional arguments
+29:0: critical_section directive can only be applied to a function with one or more positional arguments
+33:0: critical_section decorator does not take arguments
+37:0: Arguments to cython.critical_section must be Python objects.
+41:0: Critical sections require the GIL
+
 # slightly extraneous, but no real harm
 18:37: Creating temporary Python reference not allowed without gil
+41:0: Creating temporary Python reference not allowed without gil
 """
