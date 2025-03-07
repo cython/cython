34287672c19c06c6c0df946cb5b68080dd965f39
Aditya Pillai <dev@pillai.io>
2025-03-07 11:34:32 -0500

    add runnable code

--- tests/run/static_method_inheritance.pyx
+++ tests/run/static_method_inheritance.pyx
@@ -1,19 +1,36 @@
 # mode: run
 
 cdef class A:
-    pass
+    @staticmethod
+    def moo():
+        return "moo"
 
 cdef class B(A):
-    pass
+    @staticmethod
+    def moo():
+        return "boo"
 
 cdef class Foo:
     @staticmethod
     cdef A meth():
         return A()
 
-
 cdef class Bar(Foo):
     @staticmethod
     cdef B meth():
         return B()
+
+def call_foo():
+    """
+    >>> call_foo()
+    'moo'
+    """
+    return Foo.meth().moo()
+
+def call_bar():
+    """
+    >>> call_bar()
+    'boo'
+    """
+    return Bar.meth().moo()
 
