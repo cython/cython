d99f2fb6ca6f173b6513000d14a7d72abc74d538
da-woods <dw-git@d-woods.co.uk>
2025-04-01 10:39:26 +0100

    Fix logic for c_safe_identifier to extension types (GH-6679)
    "visibility" in this case means "visibility to Python". It's where the
    class comes from that matters to whether we mangle the name.
    
    It's slightly debatable what we should be doing for api or public
    classes, but it's probably reasonable to treat them as the user's
    responsibility to get them right.
    
    Fixes https://github.com/cython/cython/issues/6678

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -2496,7 +2496,8 @@ class CClassScope(ClassScope):
                         % name)
             if not cname:
                 cname = name
-                if visibility == 'private':
+                if not (self.parent_type.is_external or self.parent_type.entry.api or
+                        self.parent_type.entry.visibility == "public"):
                     cname = c_safe_identifier(cname)
                 cname = punycodify_name(cname, Naming.unicode_structmember_prefix)
             entry = self.declare(name, cname, type, pos, visibility)
