c59b19a0296a46a2703276b351d62d2d629c029d
da-woods <dw-git@d-woods.co.uk>
2025-05-26 16:18:58 +0100

    Remove use of C abort function
    It isn't available in the Limited API without an extra include (just because the Limited API includes slightly different headers than the full API).
    
    Using `Py_FatalError` seems the most descriptive way of fixing it.

--- Cython/Utility/Overflow.c
+++ Cython/Utility/Overflow.c
@@ -361,7 +361,7 @@ static CYTHON_INLINE {{TYPE}} __Pyx_{{BINOP}}_{{NAME}}_checking_overflow({{TYPE}
             return ({{TYPE}}) __Pyx_{{BINOP}}_unsigned_long_long_checking_overflow(a, b, overflow);
 #endif
         } else {
-            abort(); return 0; /* handled elsewhere */
+            Py_FatalError("Please report this as a bug in Cython - this code path should not be executed"); return 0; /* handled elsewhere */
         }
     } else {
         if ((sizeof({{TYPE}}) == sizeof(int))) {
@@ -373,7 +373,7 @@ static CYTHON_INLINE {{TYPE}} __Pyx_{{BINOP}}_{{NAME}}_checking_overflow({{TYPE}
             return ({{TYPE}}) __Pyx_{{BINOP}}_long_long_checking_overflow(a, b, overflow);
 #endif
         } else {
-            abort(); return 0; /* handled elsewhere */
+            Py_FatalError("Please report this as a bug in Cython - this code path should not be executed"); return 0; /* handled elsewhere */
         }
     }
 }
