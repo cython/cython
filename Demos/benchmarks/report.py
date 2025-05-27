c729ab5c6dc0834495629ed2029c8b5cff36e389
Stefan Behnel <stefan_ml@behnel.de>
2025-05-27 12:38:11 +0200

    CI: Include benchmark results in github report.

---
+++ Demos/benchmarks/report.py
@@ -0,0 +1,79 @@
+"""
+Report benchmark results from CSV files in Markdown format.
+"""
+
+import csv
+import itertools
+import operator
+
+
+def concat_files(csv_files):
+    for csv_file in csv_files:
+        with open(csv_file) as f:
+            yield from f
+
+
+def read_rows(csv_rows):
+    # CSV Format: benchmark, revision_name, pyversion, tmin, tmed, tmax
+    reader = csv.reader(csv_rows)
+
+    # Sort by "benchmark, revision_name, pyversion"
+    rows = sorted(reader, key=operator.itemgetter(0, 1, 2))
+    return rows
+
+
+def build_table(rows):
+    # Collect all revision names and Python versions, keeping their original order.
+    # (The set may not be the same for all benchmarks.)
+    revisions = list({row[1]: 1 for row in rows})
+    python_versions = list({row[2]: 1 for row in rows})
+
+    # Prepare table column mapping and header.
+    pos = itertools.count(1)
+    column_map = {
+        (pyversion, revision):  next(pos)
+        for pyversion in python_versions
+        for revision in revisions
+    }
+    header = ["Benchmark timings"] + [f"Py{pyversion}: {revision[:25]}" for (pyversion, revision) in column_map]
+    row_template = [''] * len(header)
+
+    # For each benchmark, report all timings in separate columns.
+    table = []
+    for benchmark, bm_rows in itertools.groupby(rows, key=operator.itemgetter(0)):
+        row = row_template[:]
+        table.append(row)
+
+        row[0] = benchmark
+        for _, revision_name, pyversion, tmin, tmed, tmax, diff in bm_rows:
+            row[column_map[(pyversion, revision_name)]] = f"{tmed} ({diff.strip(' ()')})" if diff else tmed
+
+    return header, table
+
+
+def generate_markdown(header, table):
+    # Size the table columns.
+    column_lengths = [
+        max(map(len, map(operator.itemgetter(i), itertools.chain([header], table))))
+        for i in range(len(header))
+    ]
+
+    # Generate Markdown formatted table lines.
+    row_format = ("| {:<%ds}" + " | {:>%ds}" * (len(column_lengths) - 1) + " |\n") % tuple(column_lengths)
+    format_row = row_format.format
+
+    yield format_row(*header)
+    yield format_row(*['-' * length for length in column_lengths])
+    yield from itertools.starmap(format_row, table)
+
+
+def main(csv_files):
+    rows = read_rows(concat_files(csv_files))
+    header, table = build_table(rows)
+    for line in generate_markdown(header, table):
+        print(line, end='')
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
