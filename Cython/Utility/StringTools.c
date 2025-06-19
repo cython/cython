23e3b3809b21f691726cbf6dce0fb8446b489e47
Stefan Behnel <stefan_ml@behnel.de>
2025-06-19 23:47:20 +0200

    Add even more context to the error message when failing to import a compression module.

--- Cython/Utility/StringTools.c
+++ Cython/Utility/StringTools.c
@@ -115,8 +115,9 @@ static PyObject *__Pyx_DecompressString(const char *s, Py_ssize_t length, int al
 
 import_failed:
     PyErr_Format(PyExc_ImportError,
-        "Failed to import '%.20s.decompress' - cannot initialise module strings.", module_name);
-    goto bad;
+        "Failed to import '%.20s.decompress' - cannot initialise module strings. "
+        "String compression was configured with the C macro 'CYTHON_COMPRESS_STRINGS=%d'.",
+        module_name, algo);
 bad:
     Py_XDECREF(module);
     Py_DECREF(methodname);
