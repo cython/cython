328d52d82f12930c21b6e7476e4630a92229af32
Stefan Behnel <stefan_ml@behnel.de>
2025-06-22 06:40:01 +0200

    Always use "__int64" in MSVC since it seems to be generally available according to https://github.com/mattn/gntp-send/blob/090b1276fb4a30601113551bed88c58329646818/include/msinttypes/stdint.h#L71-L90

--- Cython/Utility/ModuleSetupCode.c
+++ Cython/Utility/ModuleSetupCode.c
@@ -518,8 +518,6 @@
             typedef unsigned short    uint16_t;
             typedef signed   int      int32_t;
             typedef unsigned int      uint32_t;
-            typedef signed   long long  int64_t;
-            typedef unsigned long long  uint64_t;
         #else
             typedef signed   __int8   int8_t;
             typedef unsigned __int8   uint8_t;
@@ -527,9 +525,9 @@
             typedef unsigned __int16  uint16_t;
             typedef signed   __int32  int32_t;
             typedef unsigned __int32  uint32_t;
-            typedef signed   __int64  int64_t;
-            typedef unsigned __int64  uint64_t;
         #endif
+        typedef signed   __int64  int64_t;
+        typedef unsigned __int64  uint64_t;
     #endif
     #if _MSC_VER < 1300
         #ifdef _WIN64
