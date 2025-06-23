6c0b5e58fe3161bcea1d0ad4397c7e78b51d79e5
Stefan Behnel <stefan_ml@behnel.de>
2025-06-23 10:35:47 +0200

    Always include "stdint.h", also on Windows.

--- Cython/Utility/ModuleSetupCode.c
+++ Cython/Utility/ModuleSetupCode.c
@@ -509,45 +509,8 @@
 
 #define __Pyx_void_to_None(void_result) ((void)(void_result), Py_INCREF(Py_None), Py_None)
 
-#if defined(_MSC_VER) && _MSC_VER < 1600
-    // Visual Studio 2010 (_MSC_VER == 1600) has "stdint.h".
-    #ifndef _MSC_STDINT_H_
-        #if _MSC_VER < 1300
-            typedef signed   char     int8_t;
-            typedef unsigned char     uint8_t;
-            typedef signed   short    int16_t;
-            typedef unsigned short    uint16_t;
-            typedef signed   int      int32_t;
-            typedef unsigned int      uint32_t;
-        #else
-            typedef signed   __int8   int8_t;
-            typedef unsigned __int8   uint8_t;
-            typedef signed   __int16  int16_t;
-            typedef unsigned __int16  uint16_t;
-            typedef signed   __int32  int32_t;
-            typedef unsigned __int32  uint32_t;
-        #endif
-        typedef signed   __int64  int64_t;
-        typedef unsigned __int64  uint64_t;
-    #endif
-    #if _MSC_VER < 1300
-        #ifdef _WIN64
-            typedef unsigned long long  __pyx_uintptr_t;
-        #else
-            typedef unsigned int        __pyx_uintptr_t;
-        #endif
-    #else
-        #ifdef _WIN64
-            typedef unsigned __int64    __pyx_uintptr_t;
-        #else
-            typedef unsigned __int32    __pyx_uintptr_t;
-        #endif
-    #endif
-#else
-    #include <stdint.h>
-    typedef uintptr_t  __pyx_uintptr_t;
-#endif
-
+#include <stdint.h>
+typedef uintptr_t  __pyx_uintptr_t;
 
 #ifndef CYTHON_FALLTHROUGH
   #if defined(__cplusplus)
