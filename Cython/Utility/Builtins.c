1d72e6ca1b6c3ab915d80d408b65c90bfea2341f
Stefan Behnel <stefan_ml@behnel.de>
2025-06-25 05:09:41 +0200

    Optimise "dict.setdefault()" in the Limited API 3.12+.

--- Cython/Utility/Builtins.c
+++ Cython/Utility/Builtins.c
@@ -633,7 +633,10 @@ static CYTHON_INLINE PyObject *__Pyx_PyDict_SetDefault(PyObject *d, PyObject *ke
 
 static CYTHON_INLINE PyObject *__Pyx_PyDict_SetDefault(PyObject *d, PyObject *key, PyObject *default_value) {
     PyObject* value;
-#if CYTHON_COMPILING_IN_LIMITED_API
+#if CYTHON_COMPILING_IN_LIMITED_API && __PYX_LIMITED_VERSION_HEX > 0x030C0000
+    PyObject *args[] = {d, key, default_value};
+    value = PyObject_VectorcallMethod(PYIDENT("setdefault"), args, 3 | PY_VECTORCALL_ARGUMENTS_OFFSET, NULL);
+#elif CYTHON_COMPILING_IN_LIMITED_API
     value = PyObject_CallMethod(d, "setdefault", "OO", key, default_value);
 #elif PY_VERSION_HEX >= 0x030d0000
     PyDict_SetDefaultRef(d, key, default_value, &value);
