c270419fe5ac87fe20b8bffd5f330786628d835f
Stefan Behnel <stefan_ml@behnel.de>
2025-07-22 15:57:28 +0200

    Reduce call overhead in code writer.

--- Cython/Compiler/Code.py
+++ Cython/Compiler/Code.py
@@ -761,6 +761,7 @@ class UtilityCode(UtilityCodeBase):
             self.specialize_list.append(s)
             return s
 
+    @cython.final
     def _put_code_section(self, writer: "CCodeWriter", output: "GlobalState", code_type: str):
         code_string = getattr(self, code_type)
         if not code_string:
@@ -2484,6 +2485,7 @@ class CCodeWriter:
         else:
             self._write_to_buffer(s)
 
+    @cython.final
     def _write_lines(self, s):
         # Cygdb needs to know which Cython source line corresponds to which C line.
         # Therefore, we write this information into "self.buffer.markers" and then write it from there
@@ -2708,6 +2710,7 @@ class CCodeWriter:
             return
         self.last_pos = (pos, trace)
 
+    @cython.final
     def emit_marker(self):
         pos, trace = self.last_pos
         self.last_marked_pos = pos
@@ -2719,12 +2722,14 @@ class CCodeWriter:
         if trace:
             self.write_trace_line(pos)
 
+    @cython.final
     def write_trace_line(self, pos):
         if self.funcstate and self.funcstate.can_trace and self.globalstate.directives['linetrace']:
             self.indent()
             self._write_lines(
                 f'__Pyx_TraceLine({pos[1]:d},{self.pos_to_offset(pos):d},{not self.funcstate.gil_owned:d},{self.error_goto(pos)})\n')
 
+    @cython.final
     def _build_marker(self, pos):
         source_desc, line, col = pos
         assert isinstance(source_desc, SourceDescriptor)
@@ -2740,6 +2745,7 @@ class CCodeWriter:
         self.write(code)
         self.bol = 0
 
+    @cython.final
     def put_or_include(self, code, name):
         include_dir = self.globalstate.common_utility_include_dir
         if include_dir and len(code) > 1024:
@@ -2778,10 +2784,7 @@ class CCodeWriter:
 
     def put(self, code):
         fix_indent = False
-        if "{" in code:
-            dl = code.count("{")
-        else:
-            dl = 0
+        dl: cython.Py_ssize_t = code.count("{") if "{" in code else 0
         if "}" in code:
             dl -= code.count("}")
             if dl < 0:
@@ -2810,20 +2813,25 @@ class CCodeWriter:
             utility._put_code_section(
                 self.globalstate['cleanup_globals'], self.globalstate, "cleanup")
 
+    @cython.final
     def increase_indent(self):
         self.level += 1
 
+    @cython.final
     def decrease_indent(self):
         self.level -= 1
 
+    @cython.final
     def begin_block(self):
         self.putln("{")
         self.increase_indent()
 
+    @cython.final
     def end_block(self):
         self.decrease_indent()
         self.putln("}")
 
+    @cython.final
     def indent(self):
         self._write_to_buffer("  " * self.level)
 
@@ -3296,16 +3304,15 @@ class CCodeWriter:
 
         qualified_name should be the qualified name of the function.
         """
-        format_tuple = (
+        self.funcstate.uses_error_indicator = True
+        self.putln('__Pyx_WriteUnraisable("%s", %s, %s, %s, %d, %d);' % (
             qualified_name,
             Naming.clineno_cname,
             Naming.lineno_cname,
             Naming.filename_cname,
             self.globalstate.directives['unraisable_tracebacks'],
             nogil,
-        )
-        self.funcstate.uses_error_indicator = True
-        self.putln('__Pyx_WriteUnraisable("%s", %s, %s, %s, %d, %d);' % format_tuple)
+        ))
         self.globalstate.use_utility_code(
             UtilityCode.load_cached("WriteUnraisableException", "Exceptions.c"))
 
