1849925178155c04672a81f7cc668c5c5d2980ce
da-woods <dw-git@d-woods.co.uk>
2025-07-27 07:19:58 +0100

    Remove Py2 InstanceType from Plex
    It no longer exists in Python 3 so it fails. You only see the
    failure of course if you manage to misuse plex, so it isn't a
    "real" bug

--- Cython/Plex/Regexps.py
+++ Cython/Plex/Regexps.py
@@ -164,11 +164,7 @@ class RE:
                                             num, self.__class__.__name__, repr(value)))
 
     def wrong_type(self, num, value, expected):
-        if type(value) == types.InstanceType:
-            got = "%s.%s instance" % (
-                value.__class__.__module__, value.__class__.__name__)
-        else:
-            got = type(value).__name__
+        got = type(value).__name__
         raise Errors.PlexTypeError("Invalid type for argument %d of Plex.%s "
                                    "(expected %s, got %s" % (
                                        num, self.__class__.__name__, expected, got))
