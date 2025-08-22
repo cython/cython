b454a53a522771dabdc267ffed386f0fad3fd7af
da-woods <dw-git@d-woods.co.uk>
2025-08-22 07:50:28 +0100

    Avoid and test some undefined behaviour in integer shifts (#7091)
    1. It's undefined to shift more than the width of the integer (except
    possibly on C++20 onwards?). This cases problems on MSVC when shifting
    exactly the width of the integer (#7089).
    
    2. It's undefined to shift negative values. Practically I think this is
    OK in most of the real platforms we handle, so it might be worthwhile
    adding a few other known good special cases.

--- Cython/Utility/Optimize.c
+++ Cython/Utility/Optimize.c
@@ -1210,6 +1210,20 @@ static {{c_ret_type}} __Pyx_Unpacked_{{cfunc_name}}(PyObject *op1, PyObject *op2
     const PY_LONG_LONG ll{{'a' if order == 'CObj' else 'b'}} = intval;
     PY_LONG_LONG ll{{ival}}, llx;
     {{endif}}
+    {{if op == 'Rshift' or op == 'Lshift'}}
+// shifting negative numbers is technically implementation defined on C, and
+// C++ before C++20. Most implementation do the right thing though so
+// special case ones we know are good.
+#if (defined(__cplusplus) && __cplusplus >= 202002L) \
+        || (defined(__GNUC__) || (defined(__clang__))) && \
+            (defined(__arm__) || defined(__x86_64__) || defined(__i386__)) \
+        || (defined(_MSC_VER) && \
+            (defined(_M_ARM) || defined(_M_AMD64) || defined(_M_IX86)))
+    const int negative_shift_works = 1;
+#else
+    const int negative_shift_works = 0;
+#endif
+    {{endif}}
 
     // special cases for 0: + - * % / // | ^ & >> <<
     if (unlikely(__Pyx_PyLong_IsZero({{pyval}}))) {
@@ -1315,6 +1329,15 @@ static {{c_ret_type}} __Pyx_Unpacked_{{cfunc_name}}(PyObject *op1, PyObject *op2
                 x = q;
             }
         {{else}}
+             
+            {{if op == 'Rshift' or op == 'Lshift'}}
+            if ((!negative_shift_works) && unlikely(a < 0)) goto fallback;
+            {{endif}}
+            {{if op == 'Rshift'}}
+            if (unlikely(b >= (long) (sizeof(long)*8))) {
+                x = (a < 0) ? -1 : 0;
+            } else
+            {{endif}}
             x = a {{c_op}} b;
             {{if op == 'Lshift'}}
             if (unlikely(!(b < (long) (sizeof(long)*8) && a == x >> b)) && a) {
@@ -1341,12 +1364,23 @@ static {{c_ret_type}} __Pyx_Unpacked_{{cfunc_name}}(PyObject *op1, PyObject *op2
                 llx = q;
             }
         {{else}}
+            {{if op == 'LShift' or op == 'Rshift'}}
+            if ((!negative_shift_works) && unlikely(a < 0)) goto fallback;
+            {{endif}}
+            {{if op == 'Rshift'}}
+            if (unlikely(llb >= (long long) (sizeof(long long)*8))) {
+                llx = (lla < 0) ? -1 : 0;
+            } else
+            {{endif}}
             llx = lla {{c_op}} llb;
             {{if op == 'Lshift'}}
             if (likely(lla == llx >> llb)) /* then execute 'return' below */
             {{endif}}
         {{endif}}
         return PyLong_FromLongLong(llx);
+{{if op == 'Lshift' or op == 'Rshift'}}
+  fallback:
+{{endif}}
 
     return __Pyx_Fallback_{{cfunc_name}}(op1, op2, inplace);
 
