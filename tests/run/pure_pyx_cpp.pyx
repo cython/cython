8a0a478985bb2edb65cd6cfe6d6d1452dc96e4d9
Daniel Grunwald <daniel.grunwald@qt.io>
2025-09-05 14:52:07 +0200

    Add comparison test.

--- tests/run/pure_pyx_cpp.pyx
+++ tests/run/pure_pyx_cpp.pyx
@@ -34,36 +34,57 @@ def test_fn_pointer_type():
     double (*)(int) except *
     double (*)(int) except *
     double (*)(int) except * nogil
+    double (*)(int) except * nogil
+    double (*)(int) except *
     double (*)(int) except *
     double (*)(int, ...) except *
+    double (*)(int, ...) except *
     """
-    cdef double (*f1)(int)
-    f2: cython.pointer[cython.function_type([cython.int], cython.double)]
-    f3: cython.pointer[cython.function_type([cython.int], cython.double, nogil=True)]
-    f4: cython.pointer[cython.function_type([cython.int], cython.double, nogil=False)]
-    f5: cython.pointer[cython.function_type([cython.int], cython.double, has_varargs=True)]
-    print(cython.typeof(f1))
-    print(cython.typeof(f2))
-    print(cython.typeof(f3))
-    print(cython.typeof(f4))
-    print(cython.typeof(f5))
+    cdef double (*f1a)(int)
+    f1b: cython.pointer[cython.function_type([cython.int], cython.double)]
+    cdef double (*f2a)(int) nogil
+    f2b: cython.pointer[cython.function_type([cython.int], cython.double, nogil=True)]
+    cdef double (*f3a)(int)
+    f3b: cython.pointer[cython.function_type([cython.int], cython.double, nogil=False)]
+    cdef double (*f4a)(int, ...)
+    f4b: cython.pointer[cython.function_type([cython.int], cython.double, has_varargs=True)]
+    print(cython.typeof(f1a))
+    print(cython.typeof(f1b))
+    print(cython.typeof(f2a))
+    print(cython.typeof(f2b))
+    print(cython.typeof(f3a))
+    print(cython.typeof(f3b))
+    print(cython.typeof(f4a))
+    print(cython.typeof(f4b))
 
 def test_fn_pointer_type_exceptions():
     """
     >>> test_fn_pointer_type_exceptions()
     double (*)(int) noexcept
     double (*)(int) noexcept
+    double (*)(int) noexcept
     double (*)(int) except +
+    double (*)(int) except +
+    double (*)(int) except -1.0
     double (*)(int) except -1
+    double (*)(int) except? -1.0
     double (*)(int) except? -1
     """
-    f1: cython.pointer[cython.function_type([cython.int], cython.double, noexcept=True)]
-    f2: cython.pointer[cython.function_type([cython.int], cython.double, check_exception=False)]
-    f3: cython.pointer[cython.function_type([cython.int], cython.double, except_cpp=True)]
-    f4: cython.pointer[cython.function_type([cython.int], cython.double, exceptval=-1)]
-    f5: cython.pointer[cython.function_type([cython.int], cython.double, exceptval=-1, check_exception=True)]
-    print(cython.typeof(f1))
-    print(cython.typeof(f2))
-    print(cython.typeof(f3))
-    print(cython.typeof(f4))
-    print(cython.typeof(f5))
+    cdef double (*f1a)(int) noexcept
+    f1b: cython.pointer[cython.function_type([cython.int], cython.double, noexcept=True)]
+    f1c: cython.pointer[cython.function_type([cython.int], cython.double, check_exception=False)]
+    cdef double (*f2a)(int) except +
+    f2b: cython.pointer[cython.function_type([cython.int], cython.double, except_cpp=True)]
+    cdef double (*f3a)(int) except -1
+    f3b: cython.pointer[cython.function_type([cython.int], cython.double, exceptval=-1)]
+    cdef double (*f4a)(int) except? -1
+    f4b: cython.pointer[cython.function_type([cython.int], cython.double, exceptval=-1, check_exception=True)]
+    print(cython.typeof(f1a))
+    print(cython.typeof(f1b))
+    print(cython.typeof(f1c))
+    print(cython.typeof(f2a))
+    print(cython.typeof(f2b))
+    print(cython.typeof(f3a))
+    print(cython.typeof(f3b))
+    print(cython.typeof(f4a))
+    print(cython.typeof(f4b))
