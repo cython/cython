d830dd4adc0ce820fc830b4fe452fb8dc59b0cee
da-woods <dw-git@d-woods.co.uk>
2025-09-10 07:33:36 +0100

    Fix gc tests

--- Cython/Compiler/Symtab.py
+++ Cython/Compiler/Symtab.py
@@ -2454,7 +2454,7 @@ class CClassScope(ClassScope):
         # C attributes, then it needs to participate in GC.
         if self.has_cyclic_pyobject_attrs and not self.directives.get('no_gc', False):
             return True
-        if self.parent_type.is_external:
+        if self.parent_type.is_external and not self.parent_type.is_builtin_type:
             # It's impossible to really know - external types are often incomplete.
             return True
         base_type = self.parent_type.base_type
