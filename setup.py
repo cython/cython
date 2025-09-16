af3f36d1c3d46d78d037bb6eeddb734984ff343e
Stefan Behnel <stefan_ml@behnel.de>
2025-09-16 09:30:06 +0200

    setup.py: Mark Py3.14 as supported.

--- setup.py
+++ setup.py
@@ -443,6 +443,7 @@ def run_build():
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
             "Programming Language :: Python :: 3.13",
+            "Programming Language :: Python :: 3.14",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
             "Programming Language :: Python :: Implementation :: Stackless",
