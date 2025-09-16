5fb8c7d19c55a23daa42b637c25868d7236e5553
Stefan Behnel <stefan_ml@behnel.de>
2025-09-16 14:59:26 +0200

    Revert "Remove Py2-only code."
    This reverts commit 208f14b92dbb5edf0030ad846f7b8be402e8362c.

--- Cython/Compiler/TypeSlots.py
+++ Cython/Compiler/TypeSlots.py
@@ -552,9 +552,12 @@ class TypeFlagsSlot(SlotDescriptor):
 
     def slot_code(self, scope):
         value = "Py_TPFLAGS_DEFAULT"
-        if not scope.directives['type_version_tag']:
-            # Remove flag from 'Py_TPFLAGS_DEFAULT'.
-            value = f"({value}&~Py_TPFLAGS_HAVE_VERSION_TAG)"
+        if scope.directives['type_version_tag']:
+            # it's not in 'Py_TPFLAGS_DEFAULT' in Py2
+            value += "|Py_TPFLAGS_HAVE_VERSION_TAG"
+        else:
+            # it's enabled in 'Py_TPFLAGS_DEFAULT' in Py3
+            value = "(%s&~Py_TPFLAGS_HAVE_VERSION_TAG)" % value
         value += "|Py_TPFLAGS_CHECKTYPES|Py_TPFLAGS_HAVE_NEWBUFFER"
         if not scope.parent_type.is_final_type:
             value += "|Py_TPFLAGS_BASETYPE"
