cython/cython

commit c3594439237fe694021b684e525775fcfb14fc44

Avoid copying whole with block (#7124)
The whole with block was being copied as part of copying the associated
finally block. Override __deepcopy__ to avoid copying it.

Fixes #7122

Cython/Compiler/Nodes.py
@@ -140,6 +140,24 @@ class CheckAnalysers(type):
         return super().__new__(cls, name, bases, attrs)
 
 
+class CopyWithUpTreeRefsMixin:
+    def __deepcopy__(self, memo):
+        # Any references to objects further up the tree should not be deep-copied.
+        # However, if they're in memo (because they've already been deep-copied because
+        # we're copying from far enough up the tree) then they should be replaced
+        # with the memorised value.
+
+        cls = self.__class__
+        result = cls.__new__(cls)
+        for k, v in self.__dict__.items():
+            if k in self.uptree_ref_attrs:
+                # Note that memo being keyed by "id" is a bit of an implementation detail;
+                # the documentation says to treat it as opaque.
+                v = memo.get(id(v), v)
+            setattr(result, k, v)
+        return result
+
+
 def _with_metaclass(cls):
     if DebugFlags.debug_trace_code_generation:
         return add_metaclass(VerboseCodeWriter)(cls)
@@ -9066,18 +9084,12 @@ class CriticalSectionStatNode(TryFinallyStatNode):
         error(self.pos, "Critical sections require the GIL")
 
 
-class CriticalSectionExitNode(StatNode):
+class CriticalSectionExitNode(StatNode, CopyWithUpTreeRefsMixin):
     """
     critical_section - the CriticalSectionStatNode that owns this
     """
     child_attrs = []
-
-    def __deepcopy__(self, memo):
-        # This gets deepcopied when generating finally_clause and
-        # finally_except_clause. In this case the node has essentially
-        # no state, except for a reference to its parent.
-        # We definitely don't want to let that reference be copied.
-        return self
+    uptree_ref_attrs = ["critical_section"]
 
     def analyse_expressions(self, env):
         return self
@@ -9185,11 +9197,12 @@ class CythonLockStatNode(TryFinallyStatNode):
         code.end_block()
 
 
-class CythonLockExitNode(StatNode):
+class CythonLockExitNode(StatNode, CopyWithUpTreeRefsMixin):
     """
     lock_stat_node   CythonLockStatNode   the associated with block
     """
     child_attrs = []
+    uptree_ref_attrs = ["lock_stat_node"]
 
     def analyse_expressions(self, env):
         return self

tests/run/withstat.pyx
@@ -220,3 +220,352 @@ class NestedWith(unittest.TestCase):
             self.assertEqual(2, a2)
             self.assertEqual(10, b1)
             self.assertEqual(20, b2)
+
+
+def long_with_block(a):
+    """
+    https://github.com/cython/cython/issues/7122 - copying the with block
+    used to generate recursion error
+    >>> long_with_block(0)
+    enter
+    exit <type 'NoneType'> <type 'NoneType'> <type 'NoneType'>
+    336
+    """
+    with ContextManager(None):
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+        a += 1
+    print(a)

Cython/Compiler/ExprNodes.py
@@ -3574,7 +3574,7 @@ class AsyncNextNode(AtomicExprNode):
         self.generate_gotref(code)
 
 
-class WithExitCallNode(ExprNode):
+class WithExitCallNode(ExprNode, Nodes.CopyWithUpTreeRefsMixin):
     # The __exit__() call of a 'with' statement.  Used in both the
     # except and finally clauses.
 
@@ -3583,6 +3583,7 @@ class WithExitCallNode(ExprNode):
     # await_expr  AwaitExprNode               the await expression of an 'async with' statement
 
     subexprs = ['args', 'await_expr']
+    uptree_ref_attrs = ['with_stat']
     test_if_run = True
     await_expr = None
 

commit 32f435150928f9c6bcd5fb2bd3933bd488bcda1b

Fix issue with with statements in fused functions
introduced by #7124

Cython/Compiler/Nodes.py
@@ -154,6 +154,8 @@ class CopyWithUpTreeRefsMixin:
                 # Note that memo being keyed by "id" is a bit of an implementation detail;
                 # the documentation says to treat it as opaque.
                 v = memo.get(id(v), v)
+            else:
+                v = copy.deepcopy(v, memo)
             setattr(result, k, v)
         return result
 

tests/run/withstat.pyx
@@ -569,3 +569,35 @@ def long_with_block(a):
         a += 1
         a += 1
     print(a)
+
+ctypedef fused numeric:
+    int
+    float
+
+def with_fused(numeric x, bint raise_something):
+    """
+    Both fused types and with block involve awkward deepcopies and
+    so combining them should be checked.
+
+    >>> with_fused(1, True)
+    Traceback (most recent call last):
+        ...
+    RuntimeError
+    >>> with_fused(1, False)
+    enter
+    1
+    exit <type 'NoneType'> <type 'NoneType'> <type 'NoneType'>
+    >>> with_fused(2.5, True)
+    Traceback (most recent call last):
+        ...
+    RuntimeError
+    >>> with_fused(2.5, False)
+    enter
+    2.5
+    exit <type 'NoneType'> <type 'NoneType'> <type 'NoneType'>
+    """
+    with ContextManager(None):
+        if raise_something:
+            raise RuntimeError()
+        else:
+            print(x)