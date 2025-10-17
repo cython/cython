c26a298c86d8a7480e2ef9f319af3dd9e3b9bb11
Matus Valo <matusvalo@users.noreply.github.com>
2025-10-17 18:33:26 +0000

    Export additional C functons part 2

--- Cython/Utility/Optimize.c
+++ Cython/Utility/Optimize.c
@@ -198,7 +198,7 @@ static PyObject* __Pyx__PyList_PopIndex(PyObject* L, PyObject* py_ix, Py_ssize_t
 #endif
 
 
-/////////////// dict_getitem_default.proto ///////////////
+/////////////// dict_getitem_default.export ///////////////
 
 static PyObject* __Pyx_PyDict_GetItemDefault(PyObject* d, PyObject* key, PyObject* default_value); /*proto*/
 
