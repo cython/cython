bbe48384586be9c4becda826d8ae8de781fa5d53
Stefan Behnel <stefan_ml@behnel.de>
2025-10-19 14:14:49 +0200

    Prepare release of 3.1.5.

--- Cython/Shadow.py
+++ Cython/Shadow.py
@@ -1,7 +1,7 @@
 # cython.* namespace for pure mode.
 
 # Possible version formats: "3.1.0", "3.1.0a1", "3.1.0a1.dev0"
-__version__ = "3.1.4"
+__version__ = "3.1.5"
 
 
 # BEGIN shameless copy from Cython/minivect/minitypes.py
