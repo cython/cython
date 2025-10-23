693a77c1ab5c00f18516fd6be94608e016f34e36
Matus Valo <matusvalo@users.noreply.github.com>
2025-10-23 21:43:33 +0000

    Error out when variable is redeclared

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -2316,6 +2316,8 @@ class NameNode(AtomicExprNode):
         elif (self.entry and self.entry.is_inherited and
                 self.annotation and env.is_c_dataclass_scope):
             error(self.pos, "Cannot redeclare inherited fields in Cython dataclasses")
+        elif self.entry and self.annotation:
+            error(self.pos, f"'{self.name}' redeclared")
         if not self.entry:
             if env.directives['warn.undeclared']:
                 warning(self.pos, "implicit declaration of '%s'" % self.name, 1)
