d82cd2a736e9e1587b2f1a6596a21087c0a53e64
da-woods <dw-git@d-woods.co.uk>
2025-10-27 21:11:14 +0000

    Fix renaming

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -9031,11 +9031,12 @@ class CriticalSectionStatNode(TryFinallyStatNode):
         return super().analyse_declarations(env)
 
     def analyse_expressions(self, env):
+        cy_pymutex_type = PyrexTypes.get_cy_pymutex_type()
         mutex_count = 0
         for i, arg in enumerate(self.args):
             arg = arg.analyse_expressions(env)
-            if (arg.type is PyrexTypes.cy_pymutex_type or (
-                    arg.type.is_ptr and arg.type.base_type is PyrexTypes.cy_pymutex_type)):
+            if (arg.type is cy_pymutex_type or (
+                    arg.type.is_ptr and arg.type.base_type is cy_pymutex_type)):
                 mutex_count += 1
                 self.is_pymutex_critical_section = True
             elif arg.type.is_pyobject:
