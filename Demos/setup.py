e625cc46300e0dbef116e7102a889c986303654d
MironAtHome <mironatpro@hotmail.com>
2025-11-01 22:41:08 -0700

    Further improve setup.py for Demos and under libraries Use variable for library name Ensure plaform specific handling of include directories Fix header line of setup.py under ./libraries Apply python formatter to align code

--- Demos/setup.py
+++ Demos/setup.py
@@ -4,21 +4,19 @@
 import os
 import sys
 import sysconfig
+
 sys.path.insert(0, "..")
 
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-ext_modules = cythonize("**/*.pyx",
-                            exclude=["numpy_*.pyx",
-                                     "callback/*.pyx",
-                                     "libraries/*.pyx"])
+ext_modules = cythonize(
+    "**/*.pyx", exclude=["numpy_*.pyx", "callback/*.pyx", "libraries/*.pyx"]
+)
 
 # Compile a mix of custom c code and cython .pyx
-ext_def = [Extension("*",
-                sources=["callback/*.pyx",
-                         "callback/cheesefinder.c"])]
+ext_def = [Extension("*", sources=["callback/*.pyx", "callback/cheesefinder.c"])]
 ext_modules.extend(cythonize(ext_def))
 
 # Only compile the following if numpy is installed.
@@ -27,37 +25,55 @@ try:
 except ImportError:
     pass
 else:
-    ext_def = [Extension("*",
-                    sources=["numpy_*.pyx"],
-                    include_dirs=[get_include()])]
+    ext_def = [Extension("*", sources=["numpy_*.pyx"], include_dirs=get_include().split(os.path.pathsep))]
     ext_modules.extend(cythonize(ext_def))
 
-# Compile and link custom library
+# For demo purposes, we build our own tiny library.
+libname_no_ext = "mymath"
+libname = f"lib{libname_no_ext}.a"  # gcc is default
+# referebce Python language native platform Python.h header include directory
+py_include = sysconfig.get_path("include")
+source_dir = "libraries"
+c_sources = f"{source_dir}/mymath.c"  # space separated list of source files
 try:
     if not sys.platform.startswith("win"):
-        print("building libmymath.a")
-        assert os.system(f"gcc -I{sysconfig.get_path("include")} -shared -fPIC -c libraries/mymath.c -o mymath.o") == 0
-        assert os.system("ar rcs libmymath.a mymath.o") == 0
+        print(f"building {libname}")
+        assert (
+            os.system(
+                f"gcc -I\"{"\" -I\"".join(py_include.split(os.path.pathsep))}\" -shared -fPIC -c {c_sources} -o mymath.o"
+            )
+            == 0
+        )
+        assert os.system(f"ar rcs {libname} mymath.o") == 0
     elif sys.platform.startswith("win"):
-        print(f"building mymath.lib")
+        libname = f"{libname_no_ext}.lib"
+        print(f"building {libname}")
         from distutils import ccompiler
+
         cc = ccompiler.new_compiler(os.name)
         cc.initialize()
-        obj_list = cc.compile(sources = ["libraries/mymath.c"], include_dirs = [sysconfig.get_path("include")])
-        cc.create_static_lib(objects = obj_list, output_libname = "mymath", output_dir = ".")
+        obj_list = cc.compile(sources=c_sources.split(), include_dirs=py_include.split(os.path.pathsep))
+        cc.create_static_lib(
+            objects=obj_list, output_libname=libname_no_ext, output_dir="."
+        )
 except Exception as ex:
     print(ex)
-    print("Error building external library, please create libmymath.lib manually.")
+    print(f"Error building external library, please create {libname} manually")
     exit(1)
 
-ext_def = [Extension("*",
-                sources=["libraries/*.pyx"],
-                include_dirs=[os.path.join(os.getcwd(), "libraries")],  # path to .h file(s)
-                library_dirs=[os.getcwd()],  # path to .a or .so file(s)
-                libraries=['mymath'])]
+# define extension and use libraries parameter to pass a list of custom built / 3rd party libraries
+ext_def = [
+    Extension(
+        "*",
+        sources=[f"{source_dir}/*.pyx"],
+        include_dirs=[os.path.join(os.getcwd(), source_dir)],  # path to .h file(s)
+        library_dirs=[os.getcwd()],  # path to .a or .so file(s)
+        libraries=[libname_no_ext],
+    )
+]
 ext_modules.extend(cythonize(ext_def))
 
 setup(
-    name = 'Demos',
-    ext_modules = ext_modules,
+    name="Demos",
+    ext_modules=ext_modules,
 )
