2eb9a4260c493f49fd26aa9035a02ebbb927443e
scoder <stefan_ml@behnel.de>
2025-11-05 09:54:36 +0100

    Simplify condition.

--- Cython/Debugger/Cygdb.py
+++ Cython/Debugger/Cygdb.py
@@ -53,10 +53,8 @@ def make_command_file(path_to_debug_info, prefix_code='',
                 import sys
                 virtualenv = os.getenv('VIRTUAL_ENV')
                 if virtualenv:
-                    if sys.platform == "win32":
-                        path_to_activate_this_py = os.path.join(virtualenv, 'Scripts', 'activate_this.py')
-                    else:
-                        path_to_activate_this_py = os.path.join(virtualenv, 'bin', 'activate_this.py')
+                    scripts_dir = 'Scripts' if sys.platform == "win32" else 'bin'
+                    path_to_activate_this_py = os.path.join(virtualenv, scripts_dir, 'activate_this.py')
                     print("gdb command file: Activating virtualenv: %s; path_to_activate_this_py: %s" % (
                         virtualenv, path_to_activate_this_py))
                     with open(path_to_activate_this_py) as f:
