2669df8f10570dcce62041d94539d0314e42f0bf
Mars-aliens <1367126980z@gmail>
2025-11-09 18:49:29 +0800

    fix assertionerror

--- docs/examples/tutorial/pure/pep_526.py
+++ docs/examples/tutorial/pure/pep_526.py
@@ -1,20 +1,20 @@
-#def func():
-#    # Cython types are evaluated as for cdef declarations
-#    x: cython.int               
-#    y: cython.double = 0.57721  
-#    z: cython.float = 0.57721   
+def func():
+    # Cython types are evaluated as for cdef declarations
+    x: cython.int               
+    y: cython.double = 0.57721  
+    z: cython.float = 0.57721   
 
     # Python types shadow Cython types for compatibility reasons
-#    a: float = 0.54321          
-#    b: int = 5                  
-#    c: int = 6                 
-#    pass
+    a: float = 0.54321          
+    b: int = 5                  
+    c: int = 6                 
+    pass
 
-#@cython.cclass
-#class A:
-#    a: cython.int
-#    b: cython.int
+@cython.cclass
+class A:
+    a: cython.int
+    b: cython.int
 
- #   def __init__(self, b=0):
- #       self.a = 3
- #       self.b = b
+    def __init__(self, b=0):
+        self.a = 3
+        self.b = b
