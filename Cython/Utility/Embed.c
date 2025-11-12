cython/cython

commit 0045f57a95563a409a8ccf0d62815229d80b7e7f

Simplify and clean up the embedded main import code to make it work how it's officially supposed to work: use the inittab.

Cython/Utility/Embed.c
@@ -26,32 +26,23 @@ static int __Pyx_main(int argc, wchar_t **argv) {
 #endif
     if (argc && argv)
         Py_SetProgramName(argv[0]);
+
+    #if PY_MAJOR_VERSION < 3
+    if (PyImport_AppendInittab("%(module_name)s", init%(module_name)s) < 0) return 1;
+    #else
+    if (PyImport_AppendInittab("%(module_name)s", PyInit_%(module_name)s) < 0) return 1;
+    #endif
+
     Py_Initialize();
     if (argc && argv)
         PySys_SetArgv(argc, argv);
+
     { /* init module '%(module_name)s' as '__main__' */
       PyObject* m = NULL;
       %(module_is_main)s = 1;
-      #if PY_MAJOR_VERSION < 3
-          init%(module_name)s();
-      #elif CYTHON_PEP489_MULTI_PHASE_INIT
-          m = PyInit_%(module_name)s();
-          if (!PyModule_Check(m)) {
-              PyModuleDef *mdef = (PyModuleDef *) m;
-              PyObject *modname = PyUnicode_FromString("__main__");
-              m = NULL;
-              if (modname) {
-                  // FIXME: not currently calling PyModule_FromDefAndSpec() here because we do not have a module spec!
-                  // FIXME: not currently setting __file__, __path__, __spec__, ...
-                  m = PyModule_NewObject(modname);
-                  Py_DECREF(modname);
-                  if (m) PyModule_ExecDef(m, mdef);
-              }
-          }
-      #else
-          m = PyInit_%(module_name)s();
-      #endif
-      if (PyErr_Occurred()) {
+      m = PyImport_ImportModule("%(module_name)s");
+
+      if (!m && PyErr_Occurred()) {
           PyErr_Print(); /* This exits with the right code if SystemExit. */
           #if PY_MAJOR_VERSION < 3
           if (Py_FlushLine()) PyErr_Clear();

commit d38ebf9a6cf96817d90108fec944e9ba43b71e75

Fix multiprocessing on embedded executable
Fixes #7146

originally broken in https://github.com/cython/cython/commit/0045f57a95563a409a8ccf0d62815229d80b7e7f.

My general enthusiasm for embedded mode isn't great so doesn't
currently extend to converting the example in the issue into a
test. Although given that I can't find any other embedding tests
I don't think I'm the only one that feels that way.

Cython/Utility/Embed.c
@@ -27,7 +27,8 @@ static int __Pyx_main(int argc, wchar_t **argv)
     fpsetmask(m & ~FP_X_OFL);
 #endif
 
-    {{for mname in (module_name,) + embed_modules}}
+    if (PyImport_AppendInittab("__main__", PyInit_{{module_name}}) < 0) return 1;
+    {{for mname in embed_modules}}
     if (PyImport_AppendInittab("{{mname}}", PyInit_{{mname}}) < 0) return 1;
     {{endfor}}
 
@@ -64,7 +65,7 @@ static int __Pyx_main(int argc, wchar_t **argv)
     { /* init module '{{module_name}}' as '__main__' */
       PyObject* m = NULL;
       {{module_is_main}} = 1;
-      m = PyImport_ImportModule("{{module_name}}");
+      m = PyImport_ImportModule("__main__");
 
       if (!m && PyErr_Occurred()) {
           PyErr_Print(); /* This exits with the right code if SystemExit. */