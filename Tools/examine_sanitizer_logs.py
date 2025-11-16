f2e3270e4e53cdcb5724d773e73c1df6f129c4ed
da-woods <dw-git@d-woods.co.uk>
2025-11-16 17:39:45 +0000

    Don't fail if no logs are generated

--- Tools/examine_sanitizer_logs.py
+++ Tools/examine_sanitizer_logs.py
@@ -22,6 +22,10 @@ def check_file(filename) -> int:
 
 failed_count = 0
 
+if len(sys.argv) == 2 and sys.argv[1].endswith('.*'):
+    # No issues so the pattern has not expanded
+    print(f"No logs found with pattern '{sys.argv[1]}'")
+    exit(0)
 for arg in sys.argv[1:]:
     print(f"Looking at file '{arg}':")
     failed_count += check_file(arg)
