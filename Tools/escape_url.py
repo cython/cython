136417b45ac07487bee2d465f40b6282d9e38d6e
Matus Valo <matusvalo@users.noreply.github.com>
2025-12-19 20:23:27 +0000

    Avoid using separate python script
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Tools/escape_url.py
+++
@@ -1,9 +0,0 @@
-import fileinput
-import urllib.parse
-
-if __name__ == '__main__':
-    import fileinput
-
-    for line in fileinput.input():
-        line = line.strip()
-        print(urllib.parse.quote(line, safe=''))
