71104e96d8139db4bbf1c663df40c86b97f1d6fa
da-woods <dw-git@d-woods.co.uk>
2026-02-28 09:18:45 +0000

    Try to fix gcc tests
    For those tests, bump the ubuntu image 24.04 (so we have access
    to more recent GCC versions). Also bump the GCC version to GCC15
    (since I think the idea was to use something up-to-date rather
    than gcc13 specifically)

--- Tools/ci-run.sh
+++ Tools/ci-run.sh
@@ -10,7 +10,6 @@ if [[ $TEST_CODE_STYLE == "1" ]]; then
 elif [[ $OSTYPE == "linux-gnu"* && ! "$EXTERNAL_OVERRIDE_CC" ]]; then
   echo "Setting up linux compiler"
   echo "Installing requirements [apt]"
-  sudo apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
   sudo apt-get update -y -q
   sudo apt-get install -y -q gdb python3-dbg gcc-$GCC_VERSION || exit 1
 
