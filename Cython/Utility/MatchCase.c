01833b0ac44ee6e5c79a8d84ef2978919b8f1d53
da-woods <dw-git@d-woods.co.uk>
2026-03-07 08:54:21 +0000

    Comment change
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Cython/Utility/MatchCase.c
+++ Cython/Utility/MatchCase.c
@@ -307,8 +307,8 @@ static PyObject *__Pyx_MatchCase_OtherSequenceSliceToList(PyObject *x, Py_ssize_
 ////////////////////// OtherSequenceSliceToList //////////////////////////
 
 // This is substantially based off ceval unpack_iterable.
-// It's also pretty similar to itertools.islice
-// Indices must be positive - there's no wraparound or boundschecking
+// It's also pretty similar to itertools.islice.
+// Indices must be positive - there's no wraparound or boundschecking.
 
 static PyObject *__Pyx_MatchCase_OtherSequenceSliceToList(PyObject *x, Py_ssize_t start, Py_ssize_t end) {
     int total = end-start;
