e900ab795c0906dd6b3ab7c1110fad0dff1cb690
loqs <loqs@users.noreply.github.com>
2026-03-13 23:07:23 +0000

    Remove unused code in Tempita.

--- Cython/Tempita/_tempita.py
+++ Cython/Tempita/_tempita.py
@@ -385,11 +385,6 @@ def sub(content, delimiters=None, **kw):
     return tmpl.substitute(kw)
 
 
-def paste_script_template_renderer(content, vars, filename=None):
-    tmpl = Template(content, name=filename)
-    return tmpl.substitute(vars)
-
-
 class bunch(dict):
 
     def __init__(self, **kw):
@@ -1024,67 +1019,3 @@ def isolate_expression(string: str, start_pos, end_pos) -> str:
         # It'll sometimes give (end_row_past_finish, 0)
         parts.append(lines[erow][:ecol])
     return ''.join(parts)
-
-_fill_command_usage = """\
-%prog [OPTIONS] TEMPLATE arg=value
-
-Use py:arg=value to set a Python value; otherwise all values are
-strings.
-"""
-
-
-def fill_command(args=None):
-    import sys
-    import optparse
-    import pkg_resources
-    import os
-    if args is None:
-        args = sys.argv[1:]
-    dist = pkg_resources.get_distribution('Paste')
-    parser = optparse.OptionParser(
-        version=coerce_text(dist),
-        usage=_fill_command_usage)
-    parser.add_option(
-        '-o', '--output',
-        dest='output',
-        metavar="FILENAME",
-        help="File to write output to (default stdout)")
-    parser.add_option(
-        '--env',
-        dest='use_env',
-        action='store_true',
-        help="Put the environment in as top-level variables")
-    options, args = parser.parse_args(args)
-    if len(args) < 1:
-        print('You must give a template filename')
-        sys.exit(2)
-    template_name = args[0]
-    args = args[1:]
-    vars = {}
-    if options.use_env:
-        vars.update(os.environ)
-    for value in args:
-        if '=' not in value:
-            print('Bad argument: %r' % value)
-            sys.exit(2)
-        name, value = value.split('=', 1)
-        if name.startswith('py:'):
-            name = name[:3]
-            value = eval(value)
-        vars[name] = value
-    if template_name == '-':
-        template_content = sys.stdin.read()
-        template_name = '<stdin>'
-    else:
-        with open(template_name, 'rb') as f:
-            template_content = f.read()
-    template = Template(template_content, name=template_name)
-    result = template.substitute(vars)
-    if options.output:
-        with open(options.output, 'wb') as f:
-            f.write(result)
-    else:
-        sys.stdout.write(result)
-
-if __name__ == '__main__':
-    fill_command()
