154b681b60746cedefe542eceb196b7d0c34a512
Stefan Behnel <stefan_ml@behnel.de>
2026-03-22 11:28:39 +0100

    Fix test.

--- tests/compile/fused_redeclare_T3111.pyx
+++ tests/compile/fused_redeclare_T3111.pyx
@@ -27,10 +27,10 @@ _WARNINGS = """
 36:4: 'cpdef_cname_method' redeclared
 
 # from MemoryView.pyx
-962:29: Ambiguous exception value, same as default return value: 0
-962:29: Ambiguous exception value, same as default return value: 0
-1003:46: Ambiguous exception value, same as default return value: 0
-1003:46: Ambiguous exception value, same as default return value: 0
-1093:29: Ambiguous exception value, same as default return value: 0
-1093:29: Ambiguous exception value, same as default return value: 0
+866:29: Ambiguous exception value, same as default return value: 0
+866:29: Ambiguous exception value, same as default return value: 0
+907:46: Ambiguous exception value, same as default return value: 0
+907:46: Ambiguous exception value, same as default return value: 0
+997:29: Ambiguous exception value, same as default return value: 0
+997:29: Ambiguous exception value, same as default return value: 0
 """
