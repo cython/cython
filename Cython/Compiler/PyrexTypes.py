335fe54b0dcd1e6b5d0a0e36e7921c2dc677689f
mv-python <matusvalo@users.noreply.github.com>
2026-05-01 14:24:44 +0200

    Update Cython/Compiler/PyrexTypes.py
    Co-authored-by: scoder <stefan_ml@behnel.de>

--- Cython/Compiler/PyrexTypes.py
+++ Cython/Compiler/PyrexTypes.py
@@ -4995,7 +4995,7 @@ class BuiltinTypeConstructorObjectType(BuiltinObjectType, PythonTypeConstructorM
         )
 
     def __hash__(self):
-        return hash(self._full_type_name(self.name, self.subscripted_types))
+        return hash(self.name) ^ hash(self.subscripted_types)
 
     def __str__(self):
         if self.subscripted_types:
