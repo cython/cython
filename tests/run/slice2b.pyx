6c1d0cd1e13206c07e4fa034a69039b53a821f0c
Matus Valo <matusvalo@users.noreply.github.com>
2026-05-19 21:05:29 +0200

    slice object is hashable from python 3.12

--- tests/run/slice2b.pyx
+++ tests/run/slice2b.pyx
@@ -1,3 +1,4 @@
+import sys
 import cython
 
 cdef extern from *:
@@ -15,12 +16,15 @@ def slice_of_typed_value():
     a[:] = L
     return a
 
-def slice_as_dict_key():
-    """
-    >>> slice_as_dict_key()
-    Python object
-    some_value
-    """
-    d = { slice(1, 5): "some_value" }
-    print(cython.typeof(d[1:5]))
-    print(d[1:5])
+if sys.version_info >= (3, 12):
+    # Starting with python 3.12 slice objects are hashable
+
+    def slice_as_dict_key():
+        """
+        >>> slice_as_dict_key()
+        Python object
+        some_value
+        """
+        d = { slice(1, 5): "some_value" }
+        print(cython.typeof(d[1:5]))
+        print(d[1:5])
