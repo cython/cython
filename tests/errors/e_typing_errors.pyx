c814b1823aa1b99e38084500dffc0d4cd30f06aa
Matus Valo <matusvalo@users.noreply.github.com>
2026-05-28 21:45:17 +0200

    Add negative tests

--- tests/errors/e_typing_errors.pyx
+++ tests/errors/e_typing_errors.pyx
@@ -34,6 +34,8 @@ def bitwise_or_ctypes(i: cython.int | None, f: None | cython.float , c: cython.c
     pass
 
 def subscripted_types_assignments():
+    ta: tuple[str, cython.float] = ('a', 5.0)
+    tb: tuple[str, cython.int] = ta
     la: list[cython.float] = [5.0]
     lb: list[cython.int] = la
     sa: set[cython.float] = {5.0}
@@ -59,6 +61,15 @@ def subscripted_types_assignments():
 
 
 def subscripted_types_assignments_to_variable():
+    ta: tuple[str, cython.float] = ('foo', 5.0)
+    tb: tuple[str, cython.int] = ('bar', 1)
+    z: cython.int = ta[0]
+    zz: cython.float = 1.0
+    tb[0] = zz
+    h: cython.int
+    for h in ta:
+        pass
+
     la: list[cython.float] = [5.0]
     lb: list[cython.int] = [1]
     a: cython.int = la[0]
@@ -83,8 +94,8 @@ def subscripted_types_assignments_to_variable():
         pass
 
     fa: frozenset[cython.int] = {1}
-    k: cython.p_int
-    for k in sa:
+    l: cython.p_int
+    for l in sa:
         pass
 
 # OK
@@ -115,6 +126,8 @@ def bitwise_or_pytypes(i: int | None, f: None | float , c: complex | None):
     py_lc: list[complex | None] = []
 
 def allowed_subscripted_types_assignments():
+    ta: tuple[cython.float, str] = (1.0, 'bar')
+    tb: tuple[cython.float, str] = ta
     la: list[cython.float] = [5.0]
     lb: list[cython.float] = la
     sa: set[cython.int] = {5}
@@ -124,49 +137,69 @@ def allowed_subscripted_types_assignments():
     da: dict[cython.int, cython.int] = {1: 2}
     db: dict[cython.int, cython.int] = da
 
+    t1: tuple = ta
     l1: list = la
     s1: set = sa
     f1: frozenset = fa
     d1: dict = da
 
+    ta = t1
     la = l1
     sa = s1
     fa = f1
     da = d1
 
 def forbidden_subscripted_types_assignments():
+    ta: tuple[cython.float, str] = (1, 'bar')
     la: list[cython.int] = [5]
     sa: set[cython.int] = {5}
     fa: frozenset[cython.int] = frozenset({1})
     da: dict[cython.int, cython.int] = {1: 2}
 
+    ta = la
+    ta = sa
+    ta = fa
+    ta = da
+    la = ta
     la = sa
     la = fa
     la = da
+    sa = ta
     sa = la
     sa = fa
     sa = da
+    fa = ta
     fa = la
     fa = sa
     fa = da
+    da = ta
     da = la
     da = sa
     da = fa
 
+    t: tuple = (1, 'foo')
     l: list = [1]
     s: set = {1}
     f: frozenset = frozenset({1})
     d: dict = {1:3}
 
+    ta = l
+    ta = d
+    ta = f
+    ta = s
+    la = t
     la = d
     la = f
     la = s
+    sa = t
     sa = l
     sa = d
     sa = f
+    fa = t
     fa = l
     fa = s
     fa = d
+    sa = t
     sa = l
     sa = f
     sa = d
@@ -186,6 +219,12 @@ def forbidden_subscripted_types_args():
     dc: dict[str, str] = {}
     dd: dict[bytes, bytes] = {}
     dict_bytes(da, db, dc, dd)
+
+
+def forbidden_tuple_assignments():
+    a: tuple[str, int] = ('bar', 1)
+    b: tuple[int, str] = a
+    c: tuple[None, int] = a
 
 _ERRORS = """
 13:42: typing.Optional[...] cannot be applied to type int
@@ -208,62 +247,81 @@ _ERRORS = """
 33:60: '[...] | None' cannot be applied to type float
 33:78: '[...] | None' cannot be applied to type double complex
 33:104: '[...] | None' cannot be applied to type long
-38:27: Cannot assign type 'list[float] object' to 'list[int] object'
-40:26: Cannot assign type 'set[float] object' to 'set[int] object'
-42:32: Cannot assign type 'frozenset[float] object' to 'frozenset[int] object'
-44:39: Cannot assign type 'dict[float,float] object' to 'dict[int,int] object'
-45:41: Cannot assign type 'dict[float,float] object' to 'dict[int,float] object'
-46:41: Cannot assign type 'dict[float,float] object' to 'dict[float,int] object'
-47:9: Cannot assign type 'list[float] object' to 'set[float] object'
-48:9: Cannot assign type 'frozenset[float] object' to 'set[float] object'
-49:9: Cannot assign type 'dict[float,float] object' to 'set[float] object'
-50:9: Cannot assign type 'list[float] object' to 'frozenset[float] object'
-51:9: Cannot assign type 'set[float] object' to 'frozenset[float] object'
-52:9: Cannot assign type 'dict[float,float] object' to 'frozenset[float] object'
-53:9: Cannot assign type 'set[float] object' to 'list[float] object'
-54:9: Cannot assign type 'frozenset[float] object' to 'list[float] object'
-55:9: Cannot assign type 'dict[float,float] object' to 'list[float] object'
-56:9: Cannot assign type 'set[float] object' to 'dict[float,float] object'
-57:9: Cannot assign type 'frozenset[float] object' to 'dict[float,float] object'
-58:9: Cannot assign type 'list[float] object' to 'dict[float,float] object'
-64:22: Cannot assign type 'float' to 'int'
-66:12: Cannot assign type 'float' to 'int'
-68:13: Cannot assign type 'float' to 'int'
-73:22: Cannot assign type 'float' to 'int'
-75:12: Cannot assign type 'float' to 'int'
-77:13: Cannot assign type 'float' to 'int'
-82:8: Cannot convert Python object to 'int *'
-82:13: Cannot assign type 'int' to 'int *'
-85:33: Cannot assign type 'set object' to 'frozenset[int] object'
-87:8: Cannot convert Python object to 'int *'
-87:13: Cannot assign type 'int' to 'int *'
-143:9: Cannot assign type 'set[int] object' to 'list[int] object'
-144:9: Cannot assign type 'frozenset[int] object' to 'list[int] object'
-145:9: Cannot assign type 'dict[int,int] object' to 'list[int] object'
-146:9: Cannot assign type 'list[int] object' to 'set[int] object'
-147:9: Cannot assign type 'frozenset[int] object' to 'set[int] object'
-148:9: Cannot assign type 'dict[int,int] object' to 'set[int] object'
-149:9: Cannot assign type 'list[int] object' to 'frozenset[int] object'
-150:9: Cannot assign type 'set[int] object' to 'frozenset[int] object'
-151:9: Cannot assign type 'dict[int,int] object' to 'frozenset[int] object'
-152:9: Cannot assign type 'list[int] object' to 'dict[int,int] object'
-153:9: Cannot assign type 'set[int] object' to 'dict[int,int] object'
-154:9: Cannot assign type 'frozenset[int] object' to 'dict[int,int] object'
-161:9: Cannot assign type 'dict object' to 'list[int] object'
-162:9: Cannot assign type 'frozenset object' to 'list[int] object'
-163:9: Cannot assign type 'set object' to 'list[int] object'
-164:9: Cannot assign type 'list object' to 'set[int] object'
-165:9: Cannot assign type 'dict object' to 'set[int] object'
-166:9: Cannot assign type 'frozenset object' to 'set[int] object'
-167:9: Cannot assign type 'list object' to 'frozenset[int] object'
-168:9: Cannot assign type 'set object' to 'frozenset[int] object'
-169:9: Cannot assign type 'dict object' to 'frozenset[int] object'
-170:9: Cannot assign type 'list object' to 'set[int] object'
-171:9: Cannot assign type 'frozenset object' to 'set[int] object'
-172:9: Cannot assign type 'dict object' to 'set[int] object'
-183:15: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
-188:15: Cannot convert 'bytes' object to str implicitly, decoding required
-188:19: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
-188:23: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
-188:27: Cannot convert 'bytes' object to str implicitly, decoding required
+38:33: Cannot assign type 'tuple[str object,float] object' to 'tuple[str object,int] object'
+40:27: Cannot assign type 'list[float] object' to 'list[int] object'
+42:26: Cannot assign type 'set[float] object' to 'set[int] object'
+44:32: Cannot assign type 'frozenset[float] object' to 'frozenset[int] object'
+46:39: Cannot assign type 'dict[float,float] object' to 'dict[int,int] object'
+47:41: Cannot assign type 'dict[float,float] object' to 'dict[int,float] object'
+48:41: Cannot assign type 'dict[float,float] object' to 'dict[float,int] object'
+49:9: Cannot assign type 'list[float] object' to 'set[float] object'
+50:9: Cannot assign type 'frozenset[float] object' to 'set[float] object'
+51:9: Cannot assign type 'dict[float,float] object' to 'set[float] object'
+52:9: Cannot assign type 'list[float] object' to 'frozenset[float] object'
+53:9: Cannot assign type 'set[float] object' to 'frozenset[float] object'
+54:9: Cannot assign type 'dict[float,float] object' to 'frozenset[float] object'
+55:9: Cannot assign type 'set[float] object' to 'list[float] object'
+56:9: Cannot assign type 'frozenset[float] object' to 'list[float] object'
+57:9: Cannot assign type 'dict[float,float] object' to 'list[float] object'
+58:9: Cannot assign type 'set[float] object' to 'dict[float,float] object'
+59:9: Cannot assign type 'frozenset[float] object' to 'dict[float,float] object'
+60:9: Cannot assign type 'list[float] object' to 'dict[float,float] object'
+75:22: Cannot assign type 'float' to 'int'
+77:12: Cannot assign type 'float' to 'int'
+79:13: Cannot assign type 'float' to 'int'
+84:22: Cannot assign type 'float' to 'int'
+86:12: Cannot assign type 'float' to 'int'
+88:13: Cannot assign type 'float' to 'int'
+93:8: Cannot convert Python object to 'int *'
+93:13: Cannot assign type 'int' to 'int *'
+96:33: Cannot assign type 'set object' to 'frozenset[int] object'
+98:8: Cannot convert Python object to 'int *'
+98:13: Cannot assign type 'int' to 'int *'
+159:9: Cannot assign type 'list[int] object' to 'tuple[float,str object] object'
+160:9: Cannot assign type 'set[int] object' to 'tuple[float,str object] object'
+161:9: Cannot assign type 'frozenset[int] object' to 'tuple[float,str object] object'
+162:9: Cannot assign type 'dict[int,int] object' to 'tuple[float,str object] object'
+163:9: Cannot assign type 'tuple[float,str object] object' to 'list[int] object'
+164:9: Cannot assign type 'set[int] object' to 'list[int] object'
+165:9: Cannot assign type 'frozenset[int] object' to 'list[int] object'
+166:9: Cannot assign type 'dict[int,int] object' to 'list[int] object'
+167:9: Cannot assign type 'tuple[float,str object] object' to 'set[int] object'
+168:9: Cannot assign type 'list[int] object' to 'set[int] object'
+169:9: Cannot assign type 'frozenset[int] object' to 'set[int] object'
+170:9: Cannot assign type 'dict[int,int] object' to 'set[int] object'
+171:9: Cannot assign type 'tuple[float,str object] object' to 'frozenset[int] object'
+172:9: Cannot assign type 'list[int] object' to 'frozenset[int] object'
+173:9: Cannot assign type 'set[int] object' to 'frozenset[int] object'
+174:9: Cannot assign type 'dict[int,int] object' to 'frozenset[int] object'
+175:9: Cannot assign type 'tuple[float,str object] object' to 'dict[int,int] object'
+176:9: Cannot assign type 'list[int] object' to 'dict[int,int] object'
+177:9: Cannot assign type 'set[int] object' to 'dict[int,int] object'
+178:9: Cannot assign type 'frozenset[int] object' to 'dict[int,int] object'
+186:9: Cannot assign type 'list object' to 'tuple[float,str object] object'
+187:9: Cannot assign type 'dict object' to 'tuple[float,str object] object'
+188:9: Cannot assign type 'frozenset object' to 'tuple[float,str object] object'
+189:9: Cannot assign type 'set object' to 'tuple[float,str object] object'
+190:9: Cannot assign type 'tuple object' to 'list[int] object'
+191:9: Cannot assign type 'dict object' to 'list[int] object'
+192:9: Cannot assign type 'frozenset object' to 'list[int] object'
+193:9: Cannot assign type 'set object' to 'list[int] object'
+194:9: Cannot assign type 'tuple object' to 'set[int] object'
+195:9: Cannot assign type 'list object' to 'set[int] object'
+196:9: Cannot assign type 'dict object' to 'set[int] object'
+197:9: Cannot assign type 'frozenset object' to 'set[int] object'
+198:9: Cannot assign type 'tuple object' to 'frozenset[int] object'
+199:9: Cannot assign type 'list object' to 'frozenset[int] object'
+200:9: Cannot assign type 'set object' to 'frozenset[int] object'
+201:9: Cannot assign type 'dict object' to 'frozenset[int] object'
+202:9: Cannot assign type 'tuple object' to 'set[int] object'
+203:9: Cannot assign type 'list object' to 'set[int] object'
+204:9: Cannot assign type 'frozenset object' to 'set[int] object'
+205:9: Cannot assign type 'dict object' to 'set[int] object'
+216:15: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
+221:15: Cannot convert 'bytes' object to str implicitly, decoding required
+221:19: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
+221:23: Cannot convert Unicode string to 'bytes' implicitly, encoding required.
+221:27: Cannot convert 'bytes' object to str implicitly, decoding required
+226:25: Cannot assign type 'tuple[str object,int object] object' to 'tuple[int object,str object] object'
+227:26: Cannot assign type 'tuple[str object,int object] object' to 'tuple[int object] object'
 """
