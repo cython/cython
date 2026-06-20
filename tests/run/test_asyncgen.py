8d3f21a6f5cd68f956a9099ce09c0ca025875d9c
da-woods <dw-git@d-woods.co.uk>
2026-06-20 08:47:38 +0100

    Remove `from StringIO import StringIO`
    and a few other related Py2 imports.

--- tests/run/test_asyncgen.py
+++ tests/run/test_asyncgen.py
@@ -19,24 +19,9 @@ import contextlib
 
 #from test.support import import_module
 
-#asyncio = import_module("asyncio")
+import asyncio
 
 ZERO = 0
-
-try:
-    import asyncio
-except ImportError:
-    try:
-        from unittest import skip
-    except ImportError:
-        def requires_asyncio(c):
-            return None
-    else:
-        requires_asyncio = skip("tests require asyncio")
-    asyncio = None
-else:
-    def requires_asyncio(c):
-        return c
 
 
 def not_pypy(f):
@@ -126,10 +111,7 @@ except ImportError:
 def exec(code_string, l, g):
     from Cython.Compiler.Errors import CompileError
     from Cython.Shadow import inline
-    try:
-        from StringIO import StringIO
-    except ImportError:
-        from io import StringIO
+    from io import StringIO
 
     old_stderr = sys.stderr
     try:
@@ -508,7 +490,6 @@ class AsyncGenTest(TimedTest):
         self.assertTrue(inspect_isawaitable(g.aclose()))
 
 
-@requires_asyncio
 class AsyncGenAsyncioTest(TimedTest):
 
     def setUp(self):
