de6be175d2cb571e4a3e2bcf86ffc354885bb206
Matus Valo <matusvalo@users.noreply.github.com>
2026-07-12 00:09:32 +0200

    Improve tests

--- tests/compile/branch_hints.pyx
+++ tests/compile/branch_hints.pyx
@@ -89,3 +89,64 @@ cpdef int nogil_if_raise(int x) except -1 nogil:
         raise ValueError()
     else:
         x = 2
+
+@cython.test_assert_path_exists(
+    "//IfClauseNode",
+    "//IfClauseNode[@branch_hint = 'likely']",
+)
+def branch_hint_likely(int a, int b):
+    """
+    >>> branch_hint_likely(5, 3)
+    'a > b'
+    >>> branch_hint_likely(3, 5)
+    'a < b'
+    """
+    if cython.likely(a > b):
+        return "a > b"
+    return "a < b"
+
+@cython.test_assert_path_exists(
+    "//IfClauseNode",
+    "//IfClauseNode[@branch_hint = 'unlikely']",
+)
+def branch_hint_unlikely(int a, int b):
+    """
+    >>> branch_hint_unlikely(5, 5)
+    'a == b'
+    >>> branch_hint_unlikely(3, 5)
+    'a != b'
+    """
+    if cython.unlikely(a == b):
+        return "a == b"
+    return "a != b"
+
+@cython.test_assert_path_exists(
+    "//IfClauseNode",
+    "//IfClauseNode[@branch_hint = 'likely']",
+)
+def branch_hint_likely_exception(int a, int b):
+    # Cython automatically injects `unlikely()` in
+    # if conditions having branch with single raise line.
+    # This tests verifies that Cython correctly injects likely()
+    """
+    >>> branch_hint_likely_exception(5, 5)   # doctest:+IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    ...
+    ValueError
+    >>> branch_hint_likely_exception(3, 5)
+    'not raised'
+    """
+    if cython.likely(a == b):
+        raise ValueError()
+    return 'not raised'
+
+@cython.test_assert_path_exists(
+    "//IfClauseNode",
+    "//IfClauseNode[@branch_hint = 'unlikely']",
+)
+def branch_hint_comprehension_cmp(int a, int b):
+    """
+    >>> branch_hint_comprehension_cmp(5, 3)
+    [3]
+    """
+    return [i for i in range(a) if cython.unlikely(b == i)]
