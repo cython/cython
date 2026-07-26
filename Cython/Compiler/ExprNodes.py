3772fd0d5e2942641862cffae6f87859a736b8e6
Matus Valo <matusvalo@users.noreply.github.com>
2026-07-26 19:28:39 +0200

    Coerce call methods

--- Cython/Compiler/ExprNodes.py
+++ Cython/Compiler/ExprNodes.py
@@ -6311,10 +6311,15 @@ class CallNode(ExprNode):
             method_obj_type = function.obj.type
             result_type = Builtin.find_return_type_of_builtin_method(self.pos, env, method_obj_type, function.attribute)
             self.may_return_none = result_type is py_object_type
+            ret_type = py_object_type
             if result_type.is_pyobject:
-                self.type = result_type
+                ret_type = result_type
             elif result_type.equivalent_type:
-                self.type = result_type.equivalent_type
+                ret_type = result_type.equivalent_type
+            else:
+                ret_type = result_type
+            return self.coerce_to(ret_type, env)
+        return self
 
     def analyse_as_type_constructor(self, env):
         """
@@ -6472,6 +6477,7 @@ class SimpleCallNode(CallNode):
         if function.is_attribute and function.entry and function.entry.is_cmethod:
             # Take ownership of the object from which the attribute
             # was obtained, because we need to pass it as 'self'.
+
             self.self = function.obj
             function.obj = CloneNode(self.self)
 
@@ -6497,8 +6503,8 @@ class SimpleCallNode(CallNode):
             self.arg_tuple = TupleNode(self.pos, args = self.args)
             self.arg_tuple = self.arg_tuple.analyse_types(env).coerce_to_pyobject(env)
             self.args = None
-            self.set_py_result_type(env, function, func_type)
             self.is_temp = 1
+            return self.set_py_result_type(env, function, func_type)
         else:
             self.args = [ arg.analyse_types(env) for arg in self.args ]
             self.analyse_c_function_call(env)
@@ -7490,9 +7496,8 @@ class GeneralCallNode(CallNode):
         self.positional_args = self.positional_args.analyse_types(env)
         self.positional_args = \
             self.positional_args.coerce_to_pyobject(env)
-        self.set_py_result_type(env, self.function)
         self.is_temp = 1
-        return self
+        return self.set_py_result_type(env, self.function)
 
     def map_to_simple_call_node(self):
         """
