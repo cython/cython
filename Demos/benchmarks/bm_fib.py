a01f5df7320e063c06d297db1cd736c4e7c75ff3
Stefan Behnel <stefan_ml@behnel.de>
2026-08-09 21:38:27 +0200

    Benchmarks: Add Python call variants to recursive Fibonacci benchmark.

--- Demos/benchmarks/bm_fib.py
+++ Demos/benchmarks/bm_fib.py
@@ -2,17 +2,29 @@ import cython
 import time
 
 
+def fib_untyped(x):
+    return 1 if x < 2 else fib_untyped(x-2) + fib_untyped(x-1)
+
 @cython.ccall
 def fib_float(x: float) -> float:
     return 1 if x < 2 else fib_float(x-2) + fib_float(x-1)
+
+def fib_float_pycall(x: float) -> float:
+    return 1 if x < 2 else fib_float_pycall(x-2) + fib_float_pycall(x-1)
 
 @cython.ccall
 def fib_int(x: int) -> int:
     return 1 if x < 2 else fib_int(x-2) + fib_int(x-1)
 
+def fib_int_pycall(x: int) -> int:
+    return 1 if x < 2 else fib_int_pycall(x-2) + fib_int_pycall(x-1)
+
 @cython.ccall
 def fib_cull(x: cython.ulonglong) -> cython.ulonglong:
     return 1 if x < 2 else fib_cull(x-2) + fib_cull(x-1)
+
+def fib_cull_pycall(x: cython.ulonglong) -> cython.ulonglong:
+    return 1 if x < 2 else fib_cull_pycall(x-2) + fib_cull_pycall(x-1)
 
 
 def run_fib(fib, N, scale: cython.int, timer=time.perf_counter):
@@ -34,7 +46,7 @@ def run_benchmark(repeat=True, scale=1):
 
     collected_timings = {
         f'bm_{bm_func.__name__}': repeat_to_accuracy(run_fib, bm_func, 28, scale=scale, repeat=repeat)[0]
-        for bm_func in [fib_float, fib_int, fib_cull]
+        for bm_func in [fib_untyped, fib_float, fib_float_pycall, fib_int, fib_int_pycall, fib_cull, fib_cull_pycall]
     }
 
     for name, timings in collected_timings.items():
