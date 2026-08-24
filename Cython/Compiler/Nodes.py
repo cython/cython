aca14d3c95a77d980c275a54c689eebde8581c35
Stefan Behnel <stefan_ml@behnel.de>
2026-08-24 17:55:04 +0200

    Unpack coercion nodes in ExprStatNode. Their result is never actually used for anything.
    Closes https://github.com/cython/cython/issues/7922

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -6204,9 +6204,14 @@ class ExprStatNode(StatNode):
 
     def analyse_expressions(self, env):
         self.expr.result_is_used = False  # hint that .result() may safely be left empty
-        self.expr = self.expr.analyse_expressions(env)
+        expr = self.expr.analyse_expressions(env)
+        from .ExprNodes import CoerceFromPyTypeNode, CoerceToPyTypeNode, PyTypeTestNode
+        while isinstance(expr, (CoerceFromPyTypeNode, CoerceToPyTypeNode, PyTypeTestNode)):
+            # Useless coercion, ignore.
+            expr = expr.arg
         # Repeat in case of node replacement.
-        self.expr.result_is_used = False  # hint that .result() may safely be left empty
+        expr.result_is_used = False  # hint that .result() may safely be left empty
+        self.expr = expr
         return self
 
     def nogil_check(self, env):
