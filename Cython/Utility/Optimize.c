e58a259a6a3094dc0388bb342cf81ca2e3c7cca6
scoder <stefan_ml@behnel.de>
2026-08-25 20:01:58 +0200

    Use std::isfinite() is C++ and isfinite() in C. (GH-7930)
    Closes https://github.com/cython/cython/issues/7915

--- Cython/Utility/Optimize.c
+++ Cython/Utility/Optimize.c
@@ -1721,7 +1721,7 @@ static {{c_ret_type}} __Pyx_PyObject_CompareFloatInt{{func_suffix}}(PyObject *op
         Py_ssize_t iop2 = __Pyx_PyLong_CompactValue(op2);
         if (float_op1 {{c_op}} ((double)iop2)) {{return_true}}; else {{return_false}};
     }
-    if (unlikely(!isfinite(float_op1))) {
+    if (unlikely(!__Pyx_isfinite(float_op1))) {
         // CPython just compares inf/nan to 0.0
         if (float_op1 {{c_op}} 0.0) {{return_true}}; else {{return_false}};
     } else {
@@ -1738,7 +1738,7 @@ static {{c_ret_type}} __Pyx_PyObject_CompareFloatInt{{func_suffix}}(PyObject *op
         }
     }
     #else
-    if (unlikely(!isfinite(float_op1))) {
+    if (unlikely(!__Pyx_isfinite(float_op1))) {
         // CPython just compares inf/nan to 0.0
         if (float_op1 {{c_op}} 0.0) {{return_true}}; else {{return_false}};
     } else {
@@ -1787,7 +1787,7 @@ static {{c_ret_type}} __Pyx_PyObject_CompareIntFloat{{func_suffix}}(PyObject *op
         Py_ssize_t iop1 = __Pyx_PyLong_CompactValue(op1);
         if (((double)iop1) {{c_op}} float_op2) {{return_true}}; else {{return_false}};
     }
-    if (unlikely(!isfinite(float_op2))) {
+    if (unlikely(!__Pyx_isfinite(float_op2))) {
         // CPython just compares inf/nan to 0.0
         if (0.0 {{c_op}} float_op2) {{return_true}}; else {{return_false}};
     } else {
@@ -1804,7 +1804,7 @@ static {{c_ret_type}} __Pyx_PyObject_CompareIntFloat{{func_suffix}}(PyObject *op
         }
     }
     #else
-    if (unlikely(!isfinite(float_op2))) {
+    if (unlikely(!__Pyx_isfinite(float_op2))) {
         // CPython just compares inf/nan to 0.0
         if (0.0 {{c_op}} float_op2) {{return_true}}; else {{return_false}};
     } else {
