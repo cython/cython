14f0f72a0626e3c9a35b6170b94c5daf512c9f4a
Stefan Behnel <stefan_ml@behnel.de>
2026-08-25 12:14:34 +0200

    Also add linker flag to test.

--- tests/run/pycmp_lto.pyx
+++ tests/run/pycmp_lto.pyx
@@ -1,6 +1,7 @@
 # mode: run
 # tag: comparison,int,pylong,float,total_ordering,gh7908
 # distutils: extra_compile_args = -O3 -flto=auto
+# distutils: extra_link_args = -flto=auto
 
 # -flto=auto made this test fail in GCC.
 # See https://github.com/cython/cython/issues/7908
