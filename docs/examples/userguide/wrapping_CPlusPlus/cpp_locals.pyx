0e66fb28b1cbcb8f508053d56bbbbbcf6ece6c1c
scoder <stefan_ml@behnel.de>
2026-09-01 06:37:02 +0200

    Document how "cpp_locals=True" behaves for extension type attributes (GH-7950)
    There was a test for it but it did not show up in the documentation, and it's not obvious from the name that the directive has an impact on attributes at all.

---
+++ docs/examples/userguide/wrapping_CPlusPlus/cpp_locals.pyx
@@ -0,0 +1,30 @@
+# tag: cpp, cpp17
+# cython: cpp_locals = True
+
+from Rectangle cimport Rectangle
+
+
+def function(dont_write):
+    cdef Rectangle c  # not initialized
+
+    if dont_write:
+        return c.getArea()  # UnboundLocalError
+    else:
+        c = Rectangle(1, 2, 3, 4)  # initialized here
+        return c.getArea()  # OK
+
+
+cdef class RectOwner:
+    cdef Rectangle c_rect  # not initialized
+
+    def __cinit__(self):
+        # Here is often a good place to initialise "self.c_rect".
+        pass
+
+    def setRect(self, x, y, w, h):
+        self.c_rect = Rectangle(x, y, w, h)  # initialized here
+
+    def getArea(self):
+        # Attribute read access gives UnboundLocalError
+        # if setRect() was not called yet.
+        return self.c_rect.getArea()
