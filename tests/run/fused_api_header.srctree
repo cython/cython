cython/cython

commit f66b5c529769c275a49e0961657922998b8cc592

Improve names in fused `__pyx_api__` (GH-7656)
So that they're based on the types of the argument rather than
the slightly arbitrary order of the types within a fused type.
This should mean they keep being right if the fused type is expanded.

For an example fused function:

```
# pxd file
cdef fused IntOrFloat:
    int
    float

cdef fused FloatOrObject:
    float
    object

cdef IntOrFloat fused_func(IntOrFloat x, FloatOrObject y)
```

the old names in the `__pyx_capi__` dictionary (which are retained as aliases for now) are:

```
__pyx_fuse_1_1fused_func
__pyx_fuse_1_0fused_func
__pyx_fuse_0_1fused_func
__pyx_fuse_0_0fused_func
```

while the new names are:

```
fused_func[float,object]
fused_func[float,float]
fused_func[int,object]
fused_func[int,float]
```

The old values are kept for compatibility, and will be used if the new values
aren't found (ensuring compatibility with old modules). I'd intend to remove this
in the end but likely not any time soon.

Related to https://github.com/cython/cython/issues/7603

Cython/Compiler/ModuleNode.py
@@ -4114,6 +4114,10 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
             # (signature, name, cname)
             (entry.type.signature_string(), entry.name, entry.cname)
             for entry in entries
+        ] + [
+            # Repeat exports under the old name for fused functions
+            (entry.type.signature_string(), entry.legacy_capi_name, entry.cname)
+            for entry in entries if entry.is_fused_specialized
         ]
         code.globalstate.use_utility_code(
             UtilityCode.load_cached("FunctionExport", "ImportExport.c"))
@@ -4175,9 +4179,23 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
         if not entries:
             return
 
-        imports = [
+        fused_imports = [
             # (signature, name, cname)
             (entry.type.signature_string(), entry.name, entry.cname)
+            for entry in entries if entry.is_fused_specialized
+        ]
+        if fused_imports:
+            code.globalstate.use_utility_code(
+                UtilityCode.load_cached("FunctionImportFused", "ImportExport.c"))
+            _generate_import_code(
+                code, self.pos, fused_imports, module.qualified_name,
+                f"__Pyx_ImportFusedFunction_{Naming.cyversion}", "void (**{name})(void)")
+
+        imports = [
+            # (signature, name, cname)
+            (entry.type.signature_string(),
+             entry.legacy_capi_name if entry.is_fused_specialized else entry.name,
+             entry.cname)
             for entry in entries
         ]
         code.globalstate.use_utility_code(

Cython/Compiler/FusedNode.py
@@ -111,12 +111,7 @@ class FusedCFuncDefNode(StatListNode):
             copied_node.entry.used = True
             env.entries[copied_node.entry.name] = copied_node.entry
 
-            specialised_type_names = [
-                sarg.type.declaration_code('', for_display=True)
-                for (farg, sarg) in zip(self.node.args, copied_node.args)
-                if farg.type.is_fused
-            ]
-            copied_node.name = StringEncoding.EncodedString(f"{copied_node.name}[{','.join(specialised_type_names)}]")
+            copied_node.name = PyrexTypes.get_fused_specialized_name(copied_node.name, self.node.args, copied_node.args)
 
             if not self.replace_fused_typechecks(copied_node):
                 break
@@ -156,8 +151,14 @@ class FusedCFuncDefNode(StatListNode):
                       "be determined from the function arguments")
                 self.py_func = None  # this is just to let the compiler exit gracefully
                 return
+
+            # Make the argument types in the CFuncDeclarator specific
+            self._specialize_function_args(copied_node.cfunc_declarator.args,
+                                           fused_to_specific)
             entry = copied_node.entry
-            type.specialize_entry(entry, cname)
+            type.specialize_entry(
+                entry, cname,
+                name=PyrexTypes.get_fused_specialized_name(copied_node.declared_name(), self.node.args, copied_node.args))
 
             # Reuse existing Entries (e.g. from .pxd files).
             for orig_entry in env.cfunc_entries:
@@ -185,10 +186,6 @@ class FusedCFuncDefNode(StatListNode):
 
             copied_node.return_type = type.return_type
             self.create_new_local_scope(copied_node, env, fused_to_specific)
-
-            # Make the argument types in the CFuncDeclarator specific
-            self._specialize_function_args(copied_node.cfunc_declarator.args,
-                                           fused_to_specific)
 
             # If a cpdef, declare all specialized cpdefs (this
             # also calls analyse_declarations)

Cython/Compiler/Nodes.py
@@ -2880,7 +2880,7 @@ class CFuncDefNode(FuncDefNode):
 
         name = self.entry.name
         self.py_func = DefNode(pos=self.pos,
-                               name=self.entry.name,
+                               name=self.entry.cname if self.entry.is_fused_specialized else self.entry.name,
                                args=self.args,
                                star_arg=None,
                                starstar_arg=None,
@@ -3373,6 +3373,7 @@ class DefNode(FuncDefNode):
             self.pos,
             target=self,
             name=self.entry.name,
+            cname=self.entry.cname,
             args=self.args,
             star_arg=self.star_arg,
             starstar_arg=self.starstar_arg,
@@ -3797,10 +3798,10 @@ class DefNodeWrapper(FuncDefNode):
 
     def analyse_declarations(self, env):
         target_entry = self.target.entry
-        name = self.name
+        cname = self.cname
         prefix = env.next_id(env.scope_prefix)
-        target_entry.func_cname = punycodify_name(Naming.pywrap_prefix + prefix + name)
-        target_entry.pymethdef_cname = punycodify_name(Naming.pymethdef_prefix + prefix + name)
+        target_entry.func_cname = punycodify_name(Naming.pywrap_prefix + prefix + cname)
+        target_entry.pymethdef_cname = punycodify_name(Naming.pymethdef_prefix + prefix + cname)
 
         self.signature = target_entry.signature
 

Cython/Compiler/PyrexTypes.py
@@ -1281,6 +1281,13 @@ class BufferType(BaseType):
     def can_coerce_from_pyobject(self,env):
         return True
 
+    def declaration_code(self, entity_code,
+            for_display = 0, dll_linkage = None, pyrex = 0):
+        if for_display or pyrex:
+            return str(self)
+        return self.base.declaration_code(entity_code,
+            for_display=for_display, dll_linkage=dll_linkage, pyrex=pyrex)
+
     def as_argument_type(self):
         return self
 
@@ -3962,7 +3969,14 @@ class CFuncType(CType):
         permutations = self.get_all_specialized_permutations()
 
         new_cfunc_entries = []
+        original_args = [arg for arg in self.args]
         for cname, fused_to_specific in permutations:
+            specialized_args = [
+                fused_to_specific[arg.type] if arg.is_fused else arg
+                for arg in original_args
+            ]
+            func_name = _get_fused_specialized_name_from_arg_types(
+                self.entry.name, original_args, specialized_args)
             new_func_type = self.entry.type.specialize(fused_to_specific)
 
             if self.optional_arg_count:
@@ -3970,7 +3984,7 @@ class CFuncType(CType):
                 self.declare_opt_arg_struct(new_func_type, cname)
 
             new_entry = copy.deepcopy(self.entry)
-            new_func_type.specialize_entry(new_entry, cname)
+            new_func_type.specialize_entry(new_entry, cname, name=func_name)
 
             new_entry.type = new_func_type
             new_func_type.entry = new_entry
@@ -3998,9 +4012,9 @@ class CFuncType(CType):
             # types we don't (because it must be derivable from the arguments)
             subtypes=self.subtypes if include_function_return_type else ['args'])
 
-    def specialize_entry(self, entry, cname):
+    def specialize_entry(self, entry, cname, name=None):
         assert not self.is_fused
-        specialize_entry(entry, cname)
+        specialize_entry(entry, cname, name=name)
 
     def can_coerce_to_pyobject(self, env):
         # duplicating the decisions from create_to_py_utility_code() here avoids writing out unused code
@@ -4094,15 +4108,20 @@ class CFuncType(CType):
         return True
 
 
-def specialize_entry(entry, cname):
+def specialize_entry(entry, cname, name=None):
     """
     Specialize an entry of a copied fused function or method
     """
     entry.is_fused_specialized = True
-    entry.name = get_fused_cname(cname, entry.name)
+    entry.legacy_capi_name = get_fused_cname(cname, entry.name)
+    method_cname = get_fused_cname(cname, entry.name)
+    if name is not None:
+        entry.name = name
+    else:
+        entry.name = method_cname
 
     if entry.is_cmethod:
-        entry.cname = entry.name
+        entry.cname = method_cname
         if entry.is_inherited:
             entry.cname = StringEncoding.EncodedString(
                     "%s.%s" % (Naming.obj_base_cname, entry.cname))
@@ -4121,6 +4140,22 @@ def get_fused_cname(fused_cname, orig_cname):
     assert fused_cname and orig_cname
     return StringEncoding.EncodedString('%s%s%s' % (Naming.fused_func_prefix,
                                                     fused_cname, orig_cname))
+
+def _get_fused_specialized_name_from_arg_types(name, original_arg_types, specialized_arg_types):
+    specialised_type_names = [
+        sarg_type.declaration_code('', for_display=True)
+        for (farg_type, sarg_type) in zip(original_arg_types, specialized_arg_types)
+        if farg_type.is_fused
+    ]
+    return StringEncoding.EncodedString(f"{name}[{','.join(specialised_type_names)}]")
+
+
+def get_fused_specialized_name(name, original_args, specialized_args):
+    return _get_fused_specialized_name_from_arg_types(
+        name,
+        [arg.type for arg in original_args],
+        [arg.type for arg in specialized_args]
+    )
 
 def unique(somelist):
     seen = set()

Cython/Compiler/UFuncs.py
@@ -145,7 +145,13 @@ class UFuncConversion:
         context_types = dict(arg_types + out_types)
         self.node.entry.used = True
 
-        ufunc_cname = self.global_scope.next_id(self.node.entry.name + "_ufunc_def")
+        ufunc_cname = self.node.entry.name[0]
+        if not ufunc_cname.isidentifier():
+            ufunc_cname = ''.join(
+                [ ufunc_cname[0] if (ufunc_cname[0].isalpha() or ufunc_cname == '_') else '_' ] +
+                [ (u if u.isalnum() or u == '_' else '_') for u in ufunc_cname[1] ])
+        ufunc_cname = Symtab.punycodify_name(ufunc_cname)
+        ufunc_cname = self.global_scope.next_id(ufunc_cname + "_ufunc_def")
 
         will_be_called_without_gil = not (any(t.is_pyobject for _, t in arg_types) or
             any(t.is_pyobject for _, t in out_types))

Cython/Utility/ImportExport.c
@@ -509,11 +509,14 @@ static int __Pyx_ImportFunction_$cyversion(PyObject *module, const char *funcnam
 #ifndef __PYX_HAVE_RT_ImportFunction_$cyversion
 #define __PYX_HAVE_RT_ImportFunction_$cyversion
 static int __Pyx_ImportFunction_$cyversion(PyObject *module, const char *funcname, void (**f)(void), const char *sig) {
+    if (*f) {
+        return 0; // already imported under a different name
+    }
     union {
         void (*fp)(void);
         void *p;
     } tmp;
-    int result = __Pyx_ImportFromPxd_$cyversion(module, funcname, &tmp.p, sig, "function");
+    int result = __Pyx_ImportFromPxd_$cyversion(module, funcname, &tmp.p, sig, "function", 0);
     if (result == 0) {
         *f = tmp.fp;
     }
@@ -521,6 +524,34 @@ static int __Pyx_ImportFunction_$cyversion(PyObject *module, const char *funcnam
 }
 #endif
 
+/////////////// FunctionImportFused.proto ///////////////
+//@substitute: naming
+
+static int __Pyx_ImportFusedFunction_$cyversion(PyObject *module, const char *funcname, void (**f)(void), const char *sig); /*proto*/
+
+/////////////// FunctionImportFused ///////////////
+//@substitute: naming
+//@requires: PxdImportShared
+
+// Account for updated naming of fused functions in the API dictionary.
+// This looks them up under the new name, but if they aren't found isn't considered a failure.
+// For now they are still looked up under the old name afterwards and it becomes an error if that fails.
+// Eventually this can go away when enough time has passed that both importers and exporters will be
+// using the new scheme.
+#ifndef __PYX_HAVE_RT_ImportFusedFunction_$cyversion
+#define __PYX_HAVE_RT_ImportFusedFunction_$cyversion
+static int __Pyx_ImportFusedFunction_$cyversion(PyObject *module, const char *funcname, void (**f)(void), const char *sig) {
+    union {
+        void (*fp)(void);
+        void *p;
+    } tmp;
+    int result = __Pyx_ImportFromPxd_$cyversion(module, funcname, &tmp.p, sig, "function", 1);
+    if (result == 0) {
+        *f = tmp.fp;
+    }
+    return result;
+}
+#endif
 
 /////////////// GetApiDict.proto ///////////////
 
@@ -579,7 +610,7 @@ bad:
 
 #ifndef __PYX_HAVE_RT_ImportFromPxd_$cyversion
 #define __PYX_HAVE_RT_ImportFromPxd_$cyversion
-static int __Pyx_ImportFromPxd_$cyversion(PyObject *module, const char *name, void **p, const char *sig, const char *what) {
+static int __Pyx_ImportFromPxd_$cyversion(PyObject *module, const char *name, void **p, const char *sig, const char *what, int optional) {
     PyObject *d = 0;
     PyObject *cobj = 0;
 
@@ -594,10 +625,16 @@ static int __Pyx_ImportFromPxd_$cyversion(PyObject *module, const char *name, vo
     Py_XINCREF(cobj);
 #endif
     if (!cobj) {
-        PyErr_Format(PyExc_ImportError,
-            "%.200s does not export expected C %.8s %.200s",
-                PyModule_GetName(module), what, name);
-        goto bad;
+        if (optional && !PyErr_Occurred()) {
+            PyErr_Clear();
+            *p = NULL;
+            goto allowed_failure;
+        } else {
+            PyErr_Format(PyExc_ImportError,
+                "%.200s does not export expected C %.8s %.200s",
+                    PyModule_GetName(module), what, name);
+            goto bad;
+        }
     }
     if (!PyCapsule_IsValid(cobj, sig)) {
         PyErr_Format(PyExc_TypeError,
@@ -608,8 +645,9 @@ static int __Pyx_ImportFromPxd_$cyversion(PyObject *module, const char *name, vo
     *p = PyCapsule_GetPointer(cobj, sig);
     if (!(*p))
         goto bad;
-    Py_DECREF(d);
     Py_DECREF(cobj);
+allowed_failure:
+    Py_DECREF(d);
     return 0;
 bad:
     Py_XDECREF(d);
@@ -630,7 +668,7 @@ static int __Pyx_ImportVoidPtr_$cyversion(PyObject *module, const char *name, vo
 #ifndef __PYX_HAVE_RT_ImportVoidPtr_$cyversion
 #define __PYX_HAVE_RT_ImportVoidPtr_$cyversion
 static int __Pyx_ImportVoidPtr_$cyversion(PyObject *module, const char *name, void **p, const char *sig) {
-    return __Pyx_ImportFromPxd_$cyversion(module, name, p, sig, "variable");
+    return __Pyx_ImportFromPxd_$cyversion(module, name, p, sig, "variable", 0);
 }
 #endif
 

tests/run/fused_capi.srctree
@@ -1,0 +1,89 @@
+PYTHON setup.py build_ext --inplace
+PYTHON test_import_from_old_works.py
+PYTHON test_import_from_new_works.py
+
+################## setup.py #################
+
+from Cython.Build.Dependencies import cythonize
+
+from distutils.core import setup
+
+setup(
+  ext_modules = cythonize("*.pyx"),
+)
+
+################## has_fused_funcs.pxd ###############
+
+ctypedef fused IntOrFloat:
+    int
+    float
+
+ctypedef fused IntOrObject:
+    int
+    object
+
+cdef IntOrFloat add_one(IntOrFloat x)
+
+cdef bint greater_than(IntOrObject x, IntOrFloat y)
+
+cdef int actual_add_two(int x)
+
+################# has_fused_funcs.pyx ################
+
+cdef IntOrFloat add_one(IntOrFloat x):
+    return x + 1
+
+cdef bint greater_than(IntOrObject x, IntOrFloat y):
+    return x > y
+
+cdef int actual_add_two(int x):
+    return x + 2
+
+################# use_fused_funcs.pyx #####################
+
+cimport has_fused_funcs
+
+from decimal import Decimal
+
+assert has_fused_funcs.add_one(1) == 2, f"add_one result {has_fused_funcs.add_one(1)}"
+assert has_fused_funcs.add_one(2.0) == 3.0
+assert not has_fused_funcs.greater_than(Decimal('-1.0'), 1.0)
+assert not has_fused_funcs.greater_than(-2, 1.0)
+assert has_fused_funcs.greater_than(Decimal('1.5'), 1)
+assert has_fused_funcs.greater_than(2, 1)
+
+################# test_import_from_old_works.py #####################
+
+import has_fused_funcs
+
+# Get rid of all of the "new style" C API entries 
+for k in list(has_fused_funcs.__pyx_capi__.keys()):
+    if (('add_one' in k or 'greater_than' in k)
+            and not '__pyx_fuse' in k):
+        del has_fused_funcs.__pyx_capi__[k]
+
+import use_fused_funcs
+
+################# test_import_from_new_works.py #####################
+
+import has_fused_funcs
+
+# Get rid of all of the "old style" C API entries 
+for k in list(has_fused_funcs.__pyx_capi__.keys()):
+    if '__pyx_fuse' in k:
+        del has_fused_funcs.__pyx_capi__[k]
+
+import use_fused_funcs
+
+################ test_new_before_old.py #######################
+
+import has_fused_funcs
+
+# swap out the signature for add_one with an equivalent but difference function
+# to show that this is the version that gets used as a preference.
+has_fused_funcs.__pyx_capi__['add_one[int]'] = has_fused_funcs.__pyx_capi__['actual_add_two']
+
+try:
+    import use_fused_funcs
+catch AssertionError as e:
+    assert e.args[0] == "add_one result {3}"

commit 1128dbb7bbe60f785610c6c06af2db355a9447e7

Fix invalid C names in fused-function API headers (GH-7972)
Exporting a fused function with `cdef api` in Cython 3.3.0 produces an invalid C API header.
Cython translation succeeds, but C/C++ code that includes the generated header fails to compile.

This regressed in https://github.com/cython/cython/pull/7656 - now type-based dictionary keys
such as `identity[int]` are also emitted as C identifiers and macro names.
Before that change, the header used valid names such as `__pyx_fuse_0identity`.

Keep the existing C identifiers in the header, but look up functions
using the new type-based keys first, falling back to the older keys.
This matches normal `cimport` behavior and preserves both the lookup
improvements from #7656 and compatibility with older modules.

Includes regression tests for C and C++ header consumers, both
export-name formats, preference for the new keys, reordered fused types,
and missing or incompatible API entries.

Cython/Compiler/ModuleNode.py
@@ -501,9 +501,10 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
             h_code.putln("")
             for entry in api_funcs:
                 type = CPtrType(entry.type)
-                cname = env.mangle(Naming.func_prefix_api, entry.name)
+                api_name = entry.legacy_capi_name if entry.is_fused_specialized else entry.name
+                cname = env.mangle(Naming.func_prefix_api, api_name)
                 h_code.putln("static %s = 0;" % type.declaration_code(cname))
-                h_code.putln("#define %s %s" % (entry.name, cname))
+                h_code.putln("#define %s %s" % (api_name, cname))
                 h_code.globalstate.use_entry_utility_code(entry)
         if api_vars:
             h_code.putln("")
@@ -517,6 +518,8 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
             put_utility_code("VoidPtrImport", "ImportExport.c")
         if api_funcs:
             put_utility_code("FunctionImport", "ImportExport.c")
+            if any(entry.is_fused_specialized for entry in api_funcs):
+                put_utility_code("FunctionImportFused", "ImportExport.c")
         if api_extension_types:
             put_utility_code("TypeImport", "ImportExport.c")
         h_code.putln("")
@@ -525,11 +528,16 @@ class ModuleNode(Nodes.Node, Nodes.BlockNode):
         h_code.putln('module = PyImport_ImportModule(%s);' % env.qualified_name.as_c_string_literal())
         h_code.putln("if (!module) goto bad;")
         for entry in api_funcs:
-            cname = env.mangle(Naming.func_prefix_api, entry.name)
+            api_name = entry.legacy_capi_name if entry.is_fused_specialized else entry.name
+            cname = env.mangle(Naming.func_prefix_api, api_name)
             sig = entry.type.signature_string()
+            if entry.is_fused_specialized:
+                h_code.putln(
+                    'if (__Pyx_ImportFusedFunction_%s(module, %s, (void (**)(void))&%s, "%s") < 0) goto bad;'
+                    % (Naming.cyversion, entry.name.as_c_string_literal(), cname, sig))
             h_code.putln(
                 'if (__Pyx_ImportFunction_%s(module, %s, (void (**)(void))&%s, "%s") < 0) goto bad;'
-                % (Naming.cyversion, entry.name.as_c_string_literal(), cname, sig))
+                % (Naming.cyversion, api_name.as_c_string_literal(), cname, sig))
         for entry in api_vars:
             cname = env.mangle(Naming.varptr_prefix_api, entry.name)
             sig = entry.type.empty_declaration_code()

tests/run/fused_api_header.srctree
@@ -1,0 +1,172 @@
+# tag: cpp
+
+PYTHON setup.py build_ext --inplace
+PYTHON test.py
+PYTHON test_legacy.py
+PYTHON test_new_names.py
+PYTHON test_prefer_new.py
+PYTHON test_import_errors.py missing
+PYTHON test_import_errors.py signature
+PYTHON setup_reordered.py build_ext --build-lib=reordered --build-temp=build/reordered
+PYTHON test_reordered.py
+
+################### setup.py ###################
+
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize([
+        Extension("api_exporter", ["api_exporter.pyx"]),
+        Extension("api_consumer_c", ["api_consumer_c.pyx"]),
+        Extension("api_consumer_cpp", ["api_consumer_cpp.pyx"], language="c++"),
+    ]),
+)
+
+################### api_exporter.pxd ###################
+
+ctypedef fused number:
+    int
+    double
+
+cdef api number identity(number value) noexcept
+cdef api number dereference(const number *value) noexcept
+cdef api int increment(int value) noexcept
+
+################### api_exporter.pyx ###################
+
+cdef number identity(number value) noexcept:
+    return value
+
+cdef number dereference(const number *value) noexcept:
+    return value[0]
+
+cdef int increment(int value) noexcept:
+    return value + 1
+
+################### api_consumer.pxi ###################
+
+cdef extern from "api_exporter_api.h":
+    int import_api_exporter() except -1
+    int __pyx_fuse_0identity(int value)
+    double __pyx_fuse_1identity(double value)
+    int __pyx_fuse_0dereference(const int *value)
+    double __pyx_fuse_1dereference(const double *value)
+    int increment(int value)
+
+def check(int expected_identity=7):
+    import_api_exporter()
+    cdef int int_value = 7
+    cdef double double_value = 2.5
+    assert __pyx_fuse_0identity(int_value) == expected_identity
+    assert __pyx_fuse_1identity(double_value) == double_value
+    assert __pyx_fuse_0dereference(&int_value) == int_value
+    assert __pyx_fuse_1dereference(&double_value) == double_value
+    assert increment(int_value) == 8
+
+################### api_consumer_c.pyx ###################
+
+include "api_consumer.pxi"
+
+################### api_consumer_cpp.pyx ###################
+
+include "api_consumer.pxi"
+
+################### test.py ###################
+
+import api_consumer_c
+import api_consumer_cpp
+
+api_consumer_c.check()
+api_consumer_cpp.check()
+
+################### test_legacy.py ###################
+
+import api_exporter
+
+for name in list(api_exporter.__pyx_capi__):
+    if "[" in name:
+        del api_exporter.__pyx_capi__[name]
+
+import test
+
+################### test_new_names.py ###################
+
+import api_exporter
+
+for name in list(api_exporter.__pyx_capi__):
+    if name.startswith("__pyx_fuse_"):
+        del api_exporter.__pyx_capi__[name]
+
+assert "identity[int]" in api_exporter.__pyx_capi__
+import test
+
+################### test_prefer_new.py ###################
+
+import api_exporter
+import api_consumer_c
+import api_consumer_cpp
+
+api_exporter.__pyx_capi__["identity[int]"] = api_exporter.__pyx_capi__["increment"]
+api_consumer_c.check(8)
+api_consumer_cpp.check(8)
+
+################### test_import_errors.py ###################
+
+import sys
+
+import api_exporter
+import api_consumer_c
+import api_consumer_cpp
+
+if sys.argv[1] == "missing":
+    del api_exporter.__pyx_capi__["identity[int]"]
+    del api_exporter.__pyx_capi__["__pyx_fuse_0identity"]
+    expected_error = ImportError
+    expected_message = "does not export expected C function"
+else:
+    assert sys.argv[1] == "signature"
+    api_exporter.__pyx_capi__["identity[int]"] = api_exporter.__pyx_capi__["identity[double]"]
+    expected_error = TypeError
+    expected_message = "wrong signature"
+
+for consumer in (api_consumer_c, api_consumer_cpp):
+    try:
+        consumer.check()
+    except expected_error as error:
+        assert expected_message in str(error), str(error)
+    else:
+        raise AssertionError("An invalid API import was accepted")
+
+################### setup_reordered.py ###################
+
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+setup(ext_modules=cythonize([
+    Extension("api_exporter", ["reordered/api_exporter.pyx"]),
+]))
+
+################### reordered/api_exporter.pxd ###################
+
+ctypedef fused number:
+    double
+    int
+
+cdef api number identity(number value) noexcept
+cdef api number dereference(const number *value) noexcept
+cdef api int increment(int value) noexcept
+
+################### reordered/api_exporter.pyx ###################
+
+include "../api_exporter.pyx"
+
+################### test_reordered.py ###################
+
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path("reordered").resolve()))
+import api_exporter
+assert Path(api_exporter.__file__).parent.name == "reordered", api_exporter.__file__
+import test