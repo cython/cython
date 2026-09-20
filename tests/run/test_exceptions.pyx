f4e3381e33d1b732a35697f1cf1034aab7fdda5a
da-woods <dw-git@d-woods.co.uk>
2026-09-20 20:47:02 +0100

    Fix change of message in test_exceptions
    In the current main branch of Python it's changed from "may not be
    deleted" to "cannot be deleted". This is causing a test failure
    on the sanitizer runs (although it isn't the significant test
    failure there).

--- tests/run/test_exceptions.pyx
+++ tests/run/test_exceptions.pyx
@@ -702,7 +702,7 @@ class ExceptionTests(TimedTest):
         except Exception as e:
             exc = e
 
-        msg = "may not be deleted"
+        msg = "not be deleted"
         self.assertRaisesRegex(TE, msg, delattr, exc, 'args')
         self.assertRaisesRegex(TE, msg, delattr, exc, '__traceback__')
         self.assertRaisesRegex(TE, msg, delattr, exc, '__cause__')
