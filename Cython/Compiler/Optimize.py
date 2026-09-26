6d777daeec850e35a9b61bd0a7508ab41fc4c441
da-woods <dw-git@d-woods.co.uk>
2026-09-26 16:05:51 +0100

    Remove lookup that shouldn't be there

--- Cython/Compiler/Optimize.py
+++ Cython/Compiler/Optimize.py
@@ -494,7 +494,7 @@ class IterationTransform(Visitor.EnvTransform):
         # analyse with boundscheck and wraparound
         # off (because we're confident we know the size)
         env = self.current_env()
-        new_directives = Options.options_in_thread.copy_inherited_directives(env.directives, boundscheck=False, wraparound=False)
+        new_directives = Options.copy_inherited_directives(env.directives, boundscheck=False, wraparound=False)
         target_assign = Nodes.CompilerDirectivesNode(
             target_assign.pos,
             directives=new_directives,
